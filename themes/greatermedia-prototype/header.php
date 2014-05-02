4d14cbf13961b87a36bc741af14e828e3716ae13
Taylor Dewey <td@tddewey.com>
2014-05-01 23:30:45 -0700

    Foundation for when Gigya RaaS is turned on
    add placeholder for a listen live player.
    Add login/register buttons
    Add JS to eventually interface with gigya (service needs to be turned on)

--- themes/greatermedia-prototype/header.php
+++ themes/greatermedia-prototype/header.php
@@ -3,20 +3,30 @@
  * The template for displaying the header.
  *
  * @package Greater Media Prototype
- * @since 0.1.0
+ * @since   0.1.0
  */
- ?><!DOCTYPE html>
-		<head>
+?><!DOCTYPE html>
+<head>
 	<title>Greater Media Prototype</title>
 	<?php wp_head(); ?>
-		</head>
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+</head>
 <body <?php body_class(); ?>>
 <main>
+	<header class="page-header">
+		<div class="account">
+			<div class="player">
+				<a href="#"><i class="fa fa-play-circle"> Listen Live</i></a>
+			</div>
+			<a href="" class="register">register</a>
+			<a href="" class="login">Login</a>
+		</div>
+		<p class="site-ids">
+			Network: <?php echo get_current_site()->site_name; ?> (ID: <?php echo get_current_site()->id; ?>)<br>
+			Blog ID: <?php echo get_current_blog_id(); ?>
+		</p>
 
-<header class="page-header">
-	<p class="site-ids">
-		Network: <?php echo get_current_site()->site_name; ?> (ID: <?php echo get_current_site()->id; ?>)<br>
-		Blog ID: <?php echo get_current_blog_id(); ?>
-	</p>
-	<h1><a href="<?php echo home_url(); ?>"><?php echo get_bloginfo('name'); ?></a></h1>
-</header>
+		<h1>
+			<a href="<?php echo home_url(); ?>"><?php echo get_bloginfo( 'name' ); ?></a>
+		</h1>
+	</header>
