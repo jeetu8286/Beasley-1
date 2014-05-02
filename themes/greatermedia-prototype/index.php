820a149d794d000c263c38555c9c919f1b91ce56
Taylor Dewey <td@tddewey.com>
2014-05-02 10:01:25 -0700

    add theme index.php and properly ignore root index.php file

---
+++ themes/greatermedia-prototype/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The main template file
+ *
+ * @package Greater Media Prototype
+ * @since 0.1.0
+ */
+ 
+ get_header();
+
+	while( have_posts() ):
+		the_post();
+		?>
+			<article <?php post_class(); ?>>
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<?php the_excerpt( 'read more >' ); ?>
+			</article>
+		<?php
+	endwhile;
+get_footer();
+?>
