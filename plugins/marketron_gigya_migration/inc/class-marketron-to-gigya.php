ad269b636173a29821a554a9b476ca423b42721b
Dave Ross <dave.ross@10up.com>
2014-08-20 13:17:09 -0400

    Converted Marketron->Gigya process to a WP_CLI command

--- plugins/marketron_gigya_migration/inc/class-marketron-to-gigya.php
+++ plugins/marketron_gigya_migration/inc/class-marketron-to-gigya.php
@@ -1,7 +1,54 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dave-work
- * Date: 8/20/14
- * Time: 12:36 PM
- */ 
+
+class GMMarketronToGigya extends WP_CLI_Command {
+
+	/**
+	 * Convert a Marketron export to a Gigya JSON file. Echoes to stdout if no output file is specified
+	 *
+	 * ## OPTIONS
+	 *
+	 * <marketron>
+	 * : The Marketron XML file to import
+	 *
+	 * <output>
+	 * : The filename for output
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp marketron_to_gigya convert --marketron="marketron_export.xml" [--output=<filename>]
+	 *
+	 * @synopsis <convert> --marketron=<filename> --xsl=<filename> [--output=<filename>]
+	 */
+	public function convert( $args, $assoc_args ) {
+
+		if ( ! isset( $assoc_args['marketron'] ) || empty( $assoc_args['marketron'] ) ) {
+			WP_CLI::error( 'Please provide the filename of the Marketron export file using the --marketron parameter' );
+		}
+
+		// Load the Marketron export
+		$xml = new DOMDocument;
+		$xml->load( $assoc_args['marketron'] );
+
+		// Load the XML source
+		$xsl = new DOMDocument;
+		$xsl->load( '../marketron_to_gigya.xsl' );
+
+		// Configure the transformer
+		$proc = new XSLTProcessor;
+		$proc->importStyleSheet( $xsl ); // attach the xsl rules
+
+		$transformed_document = $proc->transformToXML( $xml );
+
+		if ( isset( $assoc_args['output'] ) && ! empty( $assoc_args['output'] ) ) {
+			file_put_contents( $assoc_args['output'], TidyJSON::tidy( $transformed_document ) );
+		} else {
+			echo TidyJSON::tidy( $transformed_document );
+		}
+
+	}
+
+}
+
+WP_CLI::add_command( 'marketron_to_gigya', 'GMMarketronToGigya' );
+
+
--- plugins/marketron_gigya_migration/marketron_to_gigya.php
+++ plugins/marketron_gigya_migration/marketron_to_gigya.php
@@ -1,22 +1,6 @@
 <?php
 
-include 'tidyjson.php';
-
-define( 'INPUT_FILE', 'marketron_export_20140723.xml' );
-define( 'XSLT_DOC', 'marketron_to_gigya.xsl' );
-
-// Load the XML source
-$xml = new DOMDocument;
-$xml->load( INPUT_FILE );
-
-$xsl = new DOMDocument;
-$xsl->load( XSLT_DOC );
-
-// Configure the transformer
-$proc = new XSLTProcessor;
-$proc->importStyleSheet( $xsl ); // attach the xsl rules
-
-$transformed_document = $proc->transformToXML( $xml );
-//echo $transformed_document ;
-echo TidyJSON::tidy( $transformed_document );
-
+if ( defined('WP_CLI') && WP_CLI ) {
+	include 'tidyjson.php';
+	include trailingslashit(__DIR__) . 'inc/class-marketron-to-gigya.php';
+}
