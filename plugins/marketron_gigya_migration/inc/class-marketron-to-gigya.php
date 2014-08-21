82ad9fc54930d33075cf4ea5c78981c096c87e40
Dave Ross <dave.ross@10up.com>
2014-08-21 10:07:21 -0400

    JSON linting, WP_CLI documentation, code comments

--- plugins/marketron_gigya_migration/inc/class-marketron-to-gigya.php
+++ plugins/marketron_gigya_migration/inc/class-marketron-to-gigya.php
@@ -1,5 +1,7 @@
 <?php
 
+use Seld\JsonLint\JsonParser;
+
 class GMMarketronToGigya extends WP_CLI_Command {
 
 	/**
@@ -17,9 +19,11 @@ class GMMarketronToGigya extends WP_CLI_Command {
 	 *
 	 *     wp marketron_to_gigya convert --marketron="marketron_export.xml" [--output=<filename>]
 	 *
-	 * @synopsis <convert> --marketron=<filename> --xsl=<filename> [--output=<filename>]
+	 * @synopsis --marketron=<filename> [--output=<filename>]
 	 */
 	public function convert( $args, $assoc_args ) {
+
+		self::check_dependencies();
 
 		if ( ! isset( $assoc_args['marketron'] ) || empty( $assoc_args['marketron'] ) ) {
 			WP_CLI::error( 'Please provide the filename of the Marketron export file using the --marketron parameter' );
@@ -31,18 +35,52 @@ class GMMarketronToGigya extends WP_CLI_Command {
 
 		// Load the XML source
 		$xsl = new DOMDocument;
-		$xsl->load( '../marketron_to_gigya.xsl' );
+		$xsl->load( trailingslashit( MARKETRON_TO_GIGYA_PATH ) . 'marketron_to_gigya.xsl' );
 
 		// Configure the transformer
 		$proc = new XSLTProcessor;
 		$proc->importStyleSheet( $xsl ); // attach the xsl rules
 
+		// Perform the XSL Transformation
 		$transformed_document = $proc->transformToXML( $xml );
 
+		// XSLTProcessor assumes it's translating XML->XML so it adds an XML declaration
+		$transformed_document = str_replace( '<?xml version="1.0"?>', '', $transformed_document );
+
+		$tidied_document = TidyJSON::tidy( $transformed_document );
+
+		// Fill in a placeholder password
+		$tidied_document = str_replace( '%password%', md5( '12345' ), $tidied_document );
+
+//		self::jsonlint( $tidied_document );
+//die();
 		if ( isset( $assoc_args['output'] ) && ! empty( $assoc_args['output'] ) ) {
-			file_put_contents( $assoc_args['output'], TidyJSON::tidy( $transformed_document ) );
+			// Output a file
+			file_put_contents( $assoc_args['output'], $tidied_document );
 		} else {
-			echo TidyJSON::tidy( $transformed_document );
+			// Output to stdout
+			echo $tidied_document;
+		}
+
+	}
+
+	public static function check_dependencies() {
+
+		if ( ! class_exists( 'XSLTProcessor' ) ) {
+			WP_CLI::error( "PHP's XSLT support is required to run the convertor. Please install it ('apt-get install php5-xsl' on Linux)." );
+			die();
+		}
+
+	}
+
+	public static function jsonlint( $json ) {
+
+		$parser = new JsonParser();
+
+		// returns null if it's valid json, or a ParsingException object.
+		$result = $parser->lint( $json );
+		if ( null !== $result ) {
+			print_r( $result );
 		}
 
 	}
