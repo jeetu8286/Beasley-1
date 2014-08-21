f3dfe48b0f334155a269fb5095c35b558ee3c5cd
Dave Ross <dave.ross@10up.com>
2014-08-21 10:06:13 -0400

    Load the Composer autoloader

--- plugins/marketron_gigya_migration/marketron_to_gigya.php
+++ plugins/marketron_gigya_migration/marketron_to_gigya.php
@@ -1,6 +1,17 @@
 <?php
+/*
+Plugin Name: Marketron to Gigya
+Description:
+Version: 1.0
+Author: 10up
+Author URI: http://10up.com
+*/
+
+if ( ! defined( 'ABSPATH' ) ) die( "Please don't try to access this file directly." );
+define( 'MARKETRON_TO_GIGYA_PATH', dirname( __FILE__ ) );
 
 if ( defined('WP_CLI') && WP_CLI ) {
+	include "vendor/autoload.php";
 	include 'tidyjson.php';
 	include trailingslashit(__DIR__) . 'inc/class-marketron-to-gigya.php';
 }
