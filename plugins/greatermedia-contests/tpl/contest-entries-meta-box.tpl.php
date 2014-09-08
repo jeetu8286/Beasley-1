22f667f02ce9f795ec84a82c58c9eaeaf1498ba5
Dave Ross <dave.ross@10up.com>
2014-09-08 17:21:10 -0400

    Contest entry UGC preview in a metabox

--- plugins/greatermedia-contests/tpl/contest-entries-meta-box.tpl.php
+++ plugins/greatermedia-contests/tpl/contest-entries-meta-box.tpl.php
@@ -1,6 +1,6 @@
 <ul>
 	<?php foreach ( $entries as $entry ): ?>
 		<?php $entry_ugc = GreaterMediaUserGeneratedContent::for_post_id( $entry->ID ); ?>
-		<li><?php echo $entry_ugc->render_moderation_row(); ?></li>
+		<li class="contest-entry-ugc-preview"><?php echo $entry_ugc->render_preview(); ?></li>
 	<?php endforeach; ?>
 </ul>
