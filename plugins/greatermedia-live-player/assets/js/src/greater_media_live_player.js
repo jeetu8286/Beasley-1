693d063e4ea84fac74365e7003efaff8f66dc24c
Allen Moore <allen@10up.com>
2014-09-19 16:47:48 -0400

    add pjax time to master js file

--- plugins/greatermedia-live-player/assets/js/src/greater_media_live_player.js
+++ plugins/greatermedia-live-player/assets/js/src/greater_media_live_player.js
@@ -12,6 +12,27 @@
 	var menuLinkSelector = 'li a',
 		article = 'article';
 
+	function pjaxTime() {
+		$(':checkbox').attr('checked', $.cookie('pjax'))
+
+		if ( !$(':checkbox').attr('checked') )
+			$.fn.pjax = $.noop
+
+		$(':checkbox').change(function() {
+			if ( $.pjax == $.noop ) {
+				$(this).removeAttr('checked')
+				return alert( "Sorry, your browser doesn't support pjax :(" )
+			}
+
+			if ( $(this).attr('checked') )
+				$.cookie('pjax', true)
+			else
+				$.cookie('pjax', null)
+
+			window.location = location.href
+		})
+	}
+
 	function togglePlayer(){
 		var toggleButton = $('.gmlp-nav-toggle'),
 			body = $('body');
@@ -24,6 +45,7 @@
 	$(document).ready(function($){
 		togglePlayer();
 		$(document).pjax(menuLinkSelector, article);
+		pjaxTime();
 	});
 
 } )(jQuery);
