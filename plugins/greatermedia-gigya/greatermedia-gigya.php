33a0bca94f01d8959284adb556b76d49df210f9c
Darshan Sawardekar <darshan@sawardekar.org>
2014-09-19 21:16:00 +0530

    Adds ability to gracefully handle lack of composer vendor.

--- plugins/greatermedia-gigya/greatermedia-gigya.php
+++ plugins/greatermedia-gigya/greatermedia-gigya.php
@@ -5,11 +5,14 @@ Description: Greater Media Gigya
 Author: 10up
 */
 
-require_once __DIR__ . '/vendor/autoload.php';
-
 function gmr_gigya_main() {
 	$plugin = new \GreaterMedia\Gigya\Plugin( __FILE__ );
 	$plugin->enable();
 }
 
-gmr_gigya_main();
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+	gmr_gigya_main();
+} else {
+	error_log( 'Error: Composer packages not found, Please run $ composer install.' );
+}
