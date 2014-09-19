50db1632f50f1997dab5ec207e9d87d5955ba375
Allen Moore <allen@10up.com>
2014-09-19 17:46:24 -0400

    trying this again

--- plugins/greatermedia-live-player/assets/js/src/greater_media_live_player.js
+++ plugins/greatermedia-live-player/assets/js/src/greater_media_live_player.js
@@ -12,6 +12,27 @@
 	var menuLinkSelector = 'li a',
 		article = 'article';
 
+	function pjaxTime(){
+		$(':checkbox').attr('checked', $.cookie('pjax'))
+
+		if ( !$(':checkbox').attr('checked') )
+			$.fn.pjax = $.noop
+
+		$(':checkbox').change(function() {
+			if ( $.pjax == $.noop ) {
+				$(this).removeAttr('checked')
+				return alert( "Sorry, your browser doesn't support pjax :(" )
+			}
+
+			if ( $(this).attr('checked') )
+				$.cookie('pjax', true)
+			else
+				$.cookie('pjax', null)
+
+			window.location = location.href
+		})
+	}
+
 	function togglePlayer(){
 		var toggleButton = $('.gmlp-nav-toggle'),
 			body = $('body');
@@ -23,6 +44,7 @@
 
 	$(document).ready(function($){
 		togglePlayer();
+		pjaxTime();
 		$(document).pjax(menuLinkSelector, article);
 	});
 
