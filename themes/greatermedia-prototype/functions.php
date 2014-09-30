9f933b407b921ff1cc9a655af43141809418b330
Allen Moore <allen@10up.com>
2014-09-30 15:10:51 -0400

    add respond.js and html5shiv to prototype theme

--- themes/greatermedia-prototype/functions.php
+++ themes/greatermedia-prototype/functions.php
@@ -9,33 +9,33 @@
  * theme's file, so the child theme functions would be used.
  *
  * @package Greater Media Prototype
- * @since 0.1.0
+ * @since   0.1.0
  */
 
- // Useful global constants
- define( 'GMIPROTO_VERSION', '0.1.0' );
+// Useful global constants
+define( 'GMIPROTO_VERSION', '0.1.0' );
 
- /*
+/*
   * Theme support
   */
- add_theme_support( 'gmr-customizer' );
+add_theme_support( 'gmr-customizer' );
 
- /*
+/*
   * Required files
   */
- require_once( __DIR__ . '/includes/customizer/loader.php' );
- include __DIR__ . '/includes/class-cpt-personality.php';
+require_once( __DIR__ . '/includes/customizer/loader.php' );
+include __DIR__ . '/includes/class-cpt-personality.php';
 
- /**
-  * Set up theme defaults and register supported WordPress features.
-  *
-  * @uses load_theme_textdomain() For translation/localization support.
-  *
-  * @since 0.1.0
-  */
- function gmiproto_setup() {
-	 add_theme_support( 'post-thumbnails' );
-	 GMI_Personality::hooks();
+/**
+ * Set up theme defaults and register supported WordPress features.
+ *
+ * @uses  load_theme_textdomain() For translation/localization support.
+ *
+ * @since 0.1.0
+ */
+function gmiproto_setup() {
+	add_theme_support( 'post-thumbnails' );
+	GMI_Personality::hooks();
 	/**
 	 * Makes Greater Media Prototype available for translation.
 	 *
@@ -44,46 +44,50 @@
 	 * to change 'gmiproto' to the name of your theme in all template files.
 	 */
 	load_theme_textdomain( 'gmiproto', get_template_directory() . '/languages' );
- }
- add_action( 'after_setup_theme', 'gmiproto_setup' );
+}
 
- /**
-  * Enqueue scripts and styles for front-end.
-  *
-  * @since 0.1.0
-  */
- function gmiproto_scripts_styles() {
+add_action( 'after_setup_theme', 'gmiproto_setup' );
+
+/**
+ * Enqueue scripts and styles for front-end.
+ *
+ * @since 0.1.0
+ */
+function gmiproto_scripts_styles() {
 	$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
 	wp_enqueue_script( 'gmiproto', get_template_directory_uri() . "/assets/js/greater_media_prototype{$postfix}.js", array(), GMIPROTO_VERSION, true );
-
+	wp_enqueue_script( 'respond-js', get_template_directory_uri() . '/assets/js/vendor/respond.min.js', array(), '1.4.2', false );
+	wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/assets/js/vendor/html5shiv.min.js', array(), '3.7.2', false );
 	wp_enqueue_style( 'gmiproto', get_template_directory_uri() . "/assets/css/greater_media_prototype{$postfix}.css", array(), GMIPROTO_VERSION );
- }
- add_action( 'wp_enqueue_scripts', 'gmiproto_scripts_styles' );
+}
 
- /**
-  * Add humans.txt to the <head> element.
-  */
- function gmiproto_header_meta() {
+add_action( 'wp_enqueue_scripts', 'gmiproto_scripts_styles' );
+
+/**
+ * Add humans.txt to the <head> element.
+ */
+function gmiproto_header_meta() {
 	$humans = '<link type="text/plain" rel="author" href="' . get_template_directory_uri() . '/humans.txt" />';
 
 	echo apply_filters( 'gmiproto_humans', $humans );
- }
- add_action( 'wp_head', 'gmiproto_header_meta' );
+}
 
- /**
+add_action( 'wp_head', 'gmiproto_header_meta' );
+
+/**
  * Used by hook: 'customize_preview_init'
  *
  * @see add_action('customize_preview_init',$func)
  */
- function gmi_customizer_live_preview()
- {
+function gmi_customizer_live_preview() {
 	wp_enqueue_script(
 		'gmi-theme-customizer',
 		get_template_directory_uri() . "/assets/js/theme-customize.min.js",
-		array( 'jquery','customize-preview' ),
+		array( 'jquery', 'customize-preview' ),
 		GMIPROTO_VERSION,
 		true
 	);
 }
+
 add_action( 'customize_preview_init', 'gmi_customizer_live_preview' );
