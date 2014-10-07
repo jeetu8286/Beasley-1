3ad00955f07fd25491bda39e85dce29573d18c85
Allen Moore <allen@10up.com>
2014-10-07 17:36:55 -0400

    initial commit with greatermedia theme

---
+++ themes/greatermedia/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package Greater Media
+ * @since 0.1.0
+ */
---
+++ themes/greatermedia/header.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The template for displaying the header.
+ *
+ * @package Greater Media
+ * @since 0.1.0
+ */
+ ?><!DOCTYPE html>
