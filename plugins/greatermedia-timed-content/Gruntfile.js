757e416c3b5223392f910e3360c4f4d21d84eca2
Dave Ross <dave.ross@10up.com>
2014-10-09 10:40:18 -0400

    datetimepicker library

--- plugins/greatermedia-timed-content/Gruntfile.js
+++ plugins/greatermedia-timed-content/Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
 			},
 			build: {
 				// Don't include 'js/gm-timed-content-tinymce.js' as WP's tinymce integration handles loading it for us
-				src: ['js/greatermedia-timed-content.js', 'js/vendor/date.format/date.format.js', 'js/vendor/date.format/date-toisostring.js'],
+				src: ['js/greatermedia-timed-content.js', 'js/vendor/date.format/date.format.js', 'js/vendor/date.format/date-toisostring.js', 'js/vendor/datetimepicker/jquery.datetimepicker.js'],
 				dest: 'js/dist/<%= pkg.name %>.min.js'
 			}
 		}
