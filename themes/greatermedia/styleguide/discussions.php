8c0f24c2bf5518f208c2145205886e659ac03df9
Allen Moore <allen@10up.com>
2014-10-09 14:57:58 -0400

    add some additional scss to color section

---
+++ themes/greatermedia/styleguide/discussions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Partial for the Discussions section on the Style Guide
+ *
+ * @package Greater Media
+ * @since 0.1.0
+ */
+ ?>
+<section id="discussions" class="styleguide-discussions styleguide-sections">
+	<div class="styleguide-content">
+		<h2 class="styleguide-section-title"><?php _e( 'Discussions', 'greatermedia' ); ?></h2>
+	</div>
+</section>
