243825fe5c30796baaffdf4859e4c0d8f0d54aca
Allen Moore <allen@10up.com>
2014-10-09 16:46:08 -0400

    added a better approach to color palette to reduce code

--- themes/greatermedia/styleguide/colors.php
+++ themes/greatermedia/styleguide/colors.php
@@ -9,91 +9,42 @@
 <section id="colors" class="sg-colors sg-sections">
 	<div class="sg-content">
 		<h2 class="sg-section-title"><?php _e( 'Colors', 'greatermedia' ); ?></h2>
-		<ul class="sg-color-list">
-			<li class="sg-color-swatch white">
-				<span class="sg-color-swatch-variable"><?php _e( '$color: white', '' ); ?></span>
-				<span class="sg-color-swatch-hex"><?php _e( '#ffffff', '' ); ?></span>
-			</li>
-			<?php /* <li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li> */ ?>
-			<li class="sg-color-swatch light-gray">
-				<span class="sg-color-swatch-variable"><?php _e( '$color: light-gray', '' ); ?></span>
-				<span class="sg-color-swatch-hex"><?php _e( '#f5f5f5', '' ); ?></span>
-			</li>
-			<?php /* <li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li> */ ?>
-			<li class="sg-color-swatch soft-gray">
-				<span class="sg-color-swatch-variable"><?php _e( '$color: soft-gray', '' ); ?></span>
-				<span class="sg-color-swatch-hex"><?php _e( '#eeeeee', '' ); ?></span>
-			</li>
-			<?php /* <li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li> */ ?>
-			<li class="sg-color-swatch meta-gray">
-				<span class="sg-color-swatch-variable"><?php _e( '$color: meta-gray', '' ); ?></span>
-				<span class="sg-color-swatch-hex"><?php _e( '#dddddd', '' ); ?></span>
-			</li>
-			<?php /* <li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li> */ ?>
-			<li class="sg-color-swatch gray">
-				<span class="sg-color-swatch-variable"><?php _e( '$color: gray', '' ); ?></span>
-				<span class="sg-color-swatch-hex"><?php _e( '#888888', '' ); ?></span>
-			</li>
-			<?php /* <li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li> */ ?>
-			<li class="sg-color-swatch dark-gray">
-				<span class="sg-color-swatch-variable"><?php _e( '$color: dark-gray', '' ); ?></span>
-				<span class="sg-color-swatch-hex"><?php _e( '#333333', '' ); ?></span>
-			</li>
-			<?php /* <li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li> */ ?>
-			<li class="sg-color-swatch black">
-				<span class="sg-color-swatch-variable"><?php _e( '$color: black', '' ); ?></span>
-				<span class="sg-color-swatch-hex"><?php _e( '#000000', '' ); ?></span>
-			</li>
-			<?php /* <li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li>
-			<li class="sg-color-swatch">
-			</li> */ ?>
-		</ul>
+		<div class="sg-colors-table">
+			<div class="sg-color color-black">
+				<div class="sg-color-swatch">
+					<span class="sg-color-variable"><?php _e( '$color: black', 'greatermedia' ); ?></span>
+				</div>
+			</div>
+			<div class="sg-color color-dark-gray">
+				<div class="sg-color-swatch">
+					<span class="sg-color-variable"><?php _e( '$color: dark-gray', 'greatermedia' ); ?></span>
+				</div>
+			</div>
+			<div class="sg-color color-gray">
+				<div class="sg-color-swatch">
+					<span class="sg-color-variable"><?php _e( '$color: gray', 'greatermedia' ); ?></span>
+				</div>
+			</div>
+			<div class="sg-color color-meta-gray">
+				<div class="sg-color-swatch">
+					<span class="sg-color-variable"><?php _e( '$color: meta-gray', 'greatermedia' ); ?></span>
+				</div>
+			</div>
+			<div class="sg-color color-soft-gray">
+				<div class="sg-color-swatch">
+					<span class="sg-color-variable"><?php _e( '$color: soft-gray', 'greatermedia' ); ?></span>
+				</div>
+			</div>
+			<div class="sg-color color-light-gray">
+				<div class="sg-color-swatch">
+					<span class="sg-color-variable"><?php _e( '$color: light-gray', 'greatermedia' ); ?></span>
+				</div>
+			</div>
+			<div class="sg-color color-white">
+				<div class="sg-color-swatch">
+					<span class="sg-color-variable"><?php _e( '$color: white', 'greatermedia' ); ?></span>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
