ab57d0a46b4451d11028e6286379f4a65005dd98
Darshan Sawardekar <darshan@10up.com>
2014-10-14 20:22:18 +0530

    Adds ability to only show forms associated with contests.

--- plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/ListEntryTypesAjaxHandler.php
+++ plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/ListEntryTypesAjaxHandler.php
@@ -9,15 +9,23 @@ class ListEntryTypesAjaxHandler extends AjaxHandler {
 	}
 
 	public function run( $params ) {
-		// TODO: filter by params entryType
-		$forms      = \RGFormsModel::get_forms( true );
-		$entryTypes = array();
+		// TODO: This is a job for wpdb, currently in-memory ...
+		// The Tables are wp_posts, wp_postmeta, wp_rg_form
+		// +pagination
+		$contests_args = array( 'post_type' => 'contest', 'post_status' => 'publish' );
+		$query = new \WP_Query( $contests_args );
+		$contests = $query->get_posts();
+		$entryTypes = [];
 
-		foreach ( $forms as $form ) {
-			$entryTypes[] = array(
-				'label' => $form->title,
-				'value' => $form->id,
-			);
+		foreach ( $contests as $contest ) {
+			$contest_form_id = get_post_meta( $contest->ID, 'contest_form_id', true );
+			if ( $contest_form_id ) {
+				$form = \GFAPI::get_form( $contest_form_id );
+				$entryTypes[] = array(
+					'label' => $form['title'],
+					'value' => $form['id'],
+				);
+			}
 		}
 
 		return $entryTypes;
