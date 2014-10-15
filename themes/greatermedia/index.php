5a7c59c3d9e9444164ab8fe3ec282af09b802f23
Allen Moore <allen@10up.com>
2014-10-15 15:58:25 -0400

    add option to read more on blog roll

--- themes/greatermedia/index.php
+++ themes/greatermedia/index.php
@@ -22,15 +22,15 @@ get_header(); ?>
 
 							<div class="article-types">
 
-								<?php greatermedia_post_formats(); ?>
+								<div class="article-type--<?php greatermedia_post_formats(); ?>"><?php greatermedia_post_formats(); ?></div>
 
 							</div>
 
 							<div class="byline">
 								by
 								<span class="vcard author"><span class="fn url"><?php the_author_posts_link(); ?></span></span>
-								<time datetime="<?php the_time( 'c' ); ?>" class="post-date updated">
-									on <?php the_time( 'l, F jS' ); ?> at <?php the_time( 'g:ia' ); ?></time>
+								<time datetime="<?php the_time( 'c' ); ?>" class="post-date updated"> on <?php the_time( 'l, F jS' ); ?></time>
+								<a href="<?php the_permalink(); ?>/#comments" class="article-comments--count"><?php comments_number( 'No Comments', '1 Comment', '% Comments' ); ?></a>
 							</div>
 
 							<h2 class="entry-title" itemprop="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
