440eff890f9296cfa794a46503ad4884921502ce
Allen Moore <allen@10up.com>
2014-10-17 14:52:14 -0400

    re-factor some classes in sass

--- themes/greatermedia/header-styleguide.php
+++ themes/greatermedia/header-styleguide.php
@@ -44,39 +44,39 @@
 		</head>
 
 		<body <?php body_class(); ?> onload="prettyPrint()">
-			<div id="sg-nav-toggle" class="sg-nav-toggle">
-				<div class="sg-nav-toggle-span"></div>
+			<div id="sg-nav--toggle" class="sg-nav--toggle">
+				<div class="sg-nav--toggle--span"></div>
 			</div>
 			<nav id="nav" class="sg-nav" role="navigation">
-				<div class="sg-nav-content">
-					<ul class="sg-nav-list">
-						<li class="sg-nav-list-item"><a href="#header"><?php _e( 'Home', 'greatermedia' ); ?></a></li>
-						<li class="sg-nav-list-item"><a href="#colors"><?php _e( 'Colors', 'greatermedia' ); ?></a></li>
-						<li class="sg-nav-list-item"><?php _e( 'Base Styles', 'greatermedia' ); ?>
+				<div class="sg-nav--content">
+					<ul class="sg-nav--list">
+						<li class="sg-nav--item"><a href="#header"><?php _e( 'Home', 'greatermedia' ); ?></a></li>
+						<li class="sg-nav--item"><a href="#colors"><?php _e( 'Colors', 'greatermedia' ); ?></a></li>
+						<li class="sg-nav--item"><?php _e( 'Base Styles', 'greatermedia' ); ?>
 							<ul class="sg-subnav-list">
-								<li class="sg-nav-list-item"><a href="#headings"><?php _e( 'Headings', 'greatermedia' ); ?></a></li>
-								<li class="sg-nav-list-item"><a href="#lists"><?php _e( 'Lists', 'greatermedia' ); ?></a></li>
-								<li class="sg-nav-list-item"><a href="#misc-typography"><?php _e( 'Miscellaneous Typography', 'greatermedia' ); ?></a></li>
-								<li class="sg-nav-list-item"><a href="#paragraph"><?php _e( 'Paragraph', 'greatermedia' ); ?></a></li>
+								<li class="sg-nav--item"><a href="#headings"><?php _e( 'Headings', 'greatermedia' ); ?></a></li>
+								<li class="sg-nav--item"><a href="#lists"><?php _e( 'Lists', 'greatermedia' ); ?></a></li>
+								<li class="sg-nav--item"><a href="#misc-typography"><?php _e( 'Miscellaneous Typography', 'greatermedia' ); ?></a></li>
+								<li class="sg-nav--item"><a href="#paragraph"><?php _e( 'Paragraph', 'greatermedia' ); ?></a></li>
 							</ul>
 						</li>
-						<li class="sg-nav-list-item"><?php _e( 'Typography', 'greatermedia' ); ?>
-							<ul class="sg-subnav-list">
-								<li class="sg-nav-list-item"><a href="#sans-serif"><?php _e( 'Sans Serif', 'greatermedia' ); ?></a></li>
-								<li class="sg-nav-list-item"><a href="#serif"><?php _e( 'Serif', 'greatermedia' ); ?></a></li>
+						<li class="sg-nav--list--item"><?php _e( 'Typography', 'greatermedia' ); ?>
+							<ul class="sg-nav--sublist">
+								<li class="sg-nav--item"><a href="#sans-serif"><?php _e( 'Sans Serif', 'greatermedia' ); ?></a></li>
+								<li class="sg-nav--item"><a href="#serif"><?php _e( 'Serif', 'greatermedia' ); ?></a></li>
 							</ul>
 						</li>
-						<li class="sg-nav-list-item"><a href="#icons"><?php _e( 'Icons', 'greatermedia' ); ?></a></li>
-						<li class="sg-nav-list-item"><a href="#buttons"><?php _e( 'Buttons', 'greatermedia' ); ?></a></li>
-						<li class="sg-nav-list-item"><a href="#forms"><?php _e( 'Forms', 'greatermedia' ); ?></a></li>
-						<li class="sg-nav-list-item"><a href="#navigations"><?php _e( 'Navigations', 'greatermedia' ); ?></a></li>
-						<li class="sg-nav-list-item"><a href="#discussions"><?php _e( 'Discussions', 'greatermedia' ); ?></a></li>
-						<li class="sg-nav-list-item"><a href="#layout"><?php _e( 'Layout', 'greatermedia' ); ?></a></li>
+						<li class="sg-nav--item"><a href="#icons"><?php _e( 'Icons', 'greatermedia' ); ?></a></li>
+						<li class="sg-nav--item"><a href="#buttons"><?php _e( 'Buttons', 'greatermedia' ); ?></a></li>
+						<li class="sg-nav--item"><a href="#forms"><?php _e( 'Forms', 'greatermedia' ); ?></a></li>
+						<li class="sg-nav--item"><a href="#navigations"><?php _e( 'Navigations', 'greatermedia' ); ?></a></li>
+						<li class="sg-nav--item"><a href="#discussions"><?php _e( 'Discussions', 'greatermedia' ); ?></a></li>
+						<li class="sg-nav--item"><a href="#layout"><?php _e( 'Layout', 'greatermedia' ); ?></a></li>
 					</ul>
 				</div>
 			</nav>
 			<header id="header" class="sg-header sg-sections" role="banner">
 				<div class="sg-content">
-					<h1 class="sg-header-title"><?php _e( 'Greater Media Style Guide', 'greatermedia' ); ?></h1>
+					<h1 class="sg-header--title"><?php _e( 'Greater Media Style Guide', 'greatermedia' ); ?></h1>
 				</div>
 			</header>
--- themes/greatermedia/styleguide/base-styles.php
+++ themes/greatermedia/styleguide/base-styles.php
@@ -8,10 +8,10 @@
  ?>
 <div id="base-styles" class="sg-sections">
 	<div id="headings" class="sg-content">
-		<h2 class="sg-section-title"><?php _e( 'Base Styles', 'greatermedia' ); ?></h2>
+		<h2 class="sg-section--title"><?php _e( 'Base Styles', 'greatermedia' ); ?></h2>
 	</div>
-	<div id="headings" class="sg-section-content">
-		<h3 class="sg-subsection-title"><?php _e( 'Headings', 'greatermedia' ); ?></h3>
+	<div id="headings" class="sg-section--content">
+		<h3 class="sg-subsection--title"><?php _e( 'Headings', 'greatermedia' ); ?></h3>
 		<section>
 			<h1>H1. The Quick Brown</h1>
 			<h2>H2. Fox Jumped Over the Lazy</h2>
@@ -20,7 +20,7 @@
 			<h5>h5. yzal eht revo depmuj xof</h5>
 			<h6>H6. Nworb Kciuq eht</h6>
 		</section>
-		<div class="sg-markup-controls">
+		<div class="sg-markup--controls">
 			<a href="#" class="sg-btn sg-btn--source">HTML</a>
 		</div>
 		<div class="sg-source">
@@ -36,8 +36,8 @@
 			</pre>
 		</div>
 	</div>
-	<div id="lists" class="sg-section-content">
-		<h3 class="sg-subsection-title"><?php _e( 'Lists', 'greatermedia' ); ?></h3>
+	<div id="lists" class="sg-section--content">
+		<h3 class="sg-subsection--title"><?php _e( 'Lists', 'greatermedia' ); ?></h3>
 		<section>
 			<dl>
 				<dt>Description list, definition term</dt>
@@ -80,7 +80,7 @@
 				<li>List Item 3</li>
 			</ul>
 		</section>
-		<div class="sg-markup-controls">
+		<div class="sg-markup--controls">
 			<a href="#" class="sg-btn sg-btn--source">HTML</a>
 		</div>
 		<div class="sg-source">
@@ -130,8 +130,8 @@
 			</pre>
 		</div>
 	</div>
-	<div id="misc-typography" class="sg-section-content">
-		<h3 class="sg-subsection-title"><?php _e( 'Miscellaneous Typography', 'greatermedia' ); ?></h3>
+	<div id="misc-typography" class="sg-section--content">
+		<h3 class="sg-subsection--title"><?php _e( 'Miscellaneous Typography', 'greatermedia' ); ?></h3>
 		<section>
 			<p>Lorem <sup>superscript</sup> dolor
 				<sub>subscript</sub> amet, consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam.
@@ -142,7 +142,7 @@
 				"This stylesheet is going to help so freaking much."
 			</blockquote>
 		</section>
-		<div class="sg-markup-controls">
+		<div class="sg-markup--controls">
 			<a href="#" class="sg-btn sg-btn--source">HTML</a>
 		</div>
 		<div class="sg-source">
@@ -160,8 +160,8 @@
 			</pre>
 		</div>
 	</div>
-	<div id="paragraph" class="sg-section-content">
-		<h3 class="sg-subsection-title"><?php _e( 'Paragraph', 'greatermedia' ); ?></h3>
+	<div id="paragraph" class="sg-section--content">
+		<h3 class="sg-subsection--title"><?php _e( 'Paragraph', 'greatermedia' ); ?></h3>
 		<section>
 			<img style="width:250px;height:150px;" src="http://placekitten.com/250/150" alt="Cute Kitten" class="alignright" />
 			<p>Lorem ipsum dolor sit amet,
@@ -171,7 +171,7 @@
 				<em>emphasis</em> consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. <strong>Strong</strong> mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.
 			</p>
 		</section>
-		<div class="sg-markup-controls">
+		<div class="sg-markup--controls">
 			<button class="sg-btn sg-btn--source">HTML</button>
 		</div>
 		<div class="sg-source">
