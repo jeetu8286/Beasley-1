0f22c337c331e9da3ad0b1429d2cacb1eaed3cd7
Allen Moore <allen@10up.com>
2014-10-21 11:07:55 -0400

    move sidebars into separate files

--- themes/greatermedia/index.php
+++ themes/greatermedia/index.php
@@ -79,44 +79,13 @@ get_header(); ?>
 
 			</section>
 
-			<aside class="sidebar" role="complementary">
+			<?php
 
-				Sidebar area
+			get_sidebar();
 
-			</aside>
+			get_sidebar( 'live-player');
 
-			<aside class="live-player">
-
-				<div class="now-playing--logo">
-
-				</div>
-
-				<?php do_action( 'gm_live_player' ); ?>
-
-				<div class="now-playing--title">
-
-				</div>
-
-				<div class="now-playing--artist">
-
-				</div>
-
-				<div class="live-player--social">
-
-					<ul>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-					</ul>
-
-				</div>
-
-				<div class="live-player--next">
-					Up Next: <span class="live-player--next--artist">Pierre Robert</span>
-				</div>
-
-			</aside>
+			?>
 
 		</div>
 
---
+++ themes/greatermedia/sidebar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The default sidebar
+ *
+ * @package Greater Media
+ * @since   0.1.0
+ */
+?>
+<aside class="sidebar" role="complementary">
+
+	Sidebar area
+
+</aside>
