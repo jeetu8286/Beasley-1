18fc9788003879fb98ce8a0d89035594e78ab923
Dave Ross <dave.ross@10up.com>
2014-10-21 16:46:29 -0400

    GMR-43 Timed Content

---
+++ plugins/greatermedia-timed-content/Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+			},
+			build: {
+				// Don't include 'js/gm-timed-content-tinymce.js' as WP's tinymce integration handles loading it for us
+				src: ['js/greatermedia-timed-content-admin.js', 'js/vendor/date.format/date.format.js', 'js/vendor/date.format/date-toisostring.js', 'js/vendor/datetimepicker/jquery.datetimepicker.js'],
+				dest: 'js/dist/<%= pkg.name %>.min.js'
+			}
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default task(s).
+	grunt.registerTask('default', ['uglify']);
+
+};
---
+++ plugins/greatermedia-timed-content/greatermedia-timed-content.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Plugin Name: Greater Media Timed Content
+Description: Timed Content Plugin
+Version: 1.0
+Author: 10up
+Author URI: http://10up.com
+Text Domain: greatermedia-timed-content
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( "Please don't try to access this file directly." );
+}
+
+// For testing/debugging
+add_post_type_support('post', 'timed-content');
+
+define( 'GREATER_MEDIA_TIMED_CONTENT_PATH', dirname( __FILE__ ) );
+define( 'GREATER_MEDIA_TIMED_CONTENT_URL', plugins_url( trailingslashit( basename( dirname( __FILE__ ) ) ) ) );
+
+// This is a requirement
+if ( ! class_exists( 'VisualShortcode' ) ) {
+	include realpath( trailingslashit( __DIR__ ) . '../visual-shortcode/visual-shortcode.php' );
+}
+
+include trailingslashit( GREATER_MEDIA_TIMED_CONTENT_PATH ) . 'inc/class-greatermedia-timed-content.php';
