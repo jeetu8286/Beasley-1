053a9fe93bf562ac0c7f38492a7882863ba5ee82
Dave Ross <dave.ross@10up.com>
2014-10-24 10:17:27 -0400

    Added missing closing brackets on jQuery selectors

--- plugins/greatermedia-login-restricted-content/js/greatermedia-login-restricted-content.js
+++ plugins/greatermedia-login-restricted-content/js/greatermedia-login-restricted-content.js
@@ -2,10 +2,10 @@ jQuery(function () {
 
 	// Hide content if the criteria (logged in or not logged in) is met
 	if ('undefined' !== typeof GreaterMediaGigyaAuth && GreaterMediaGigyaAuth.is_gigya_user_logged_in()) {
-		jQuery('.login-restricted-content').filter('[data-status="logged-out"').hide();
+		jQuery('.login-restricted-content').filter('[data-status="logged-out"]').hide();
 	}
 	else {
-		jQuery('.login-restricted-content').filter('[data-status="logged-in"').hide();
+		jQuery('.login-restricted-content').filter('[data-status="logged-in"]').hide();
 	}
 
 });
