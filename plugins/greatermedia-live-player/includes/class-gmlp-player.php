2ae4416b6b5810194cdd04dba90eb1ac57ac0bdf
EduardMaghakyan <eduard.maghakyan@10up.com>
2014-10-26 00:30:49 +0400

    added seletors accodring to theme

--- plugins/greatermedia-live-player/includes/class-gmlp-player.php
+++ plugins/greatermedia-live-player/includes/class-gmlp-player.php
@@ -79,7 +79,7 @@ class GMLP_Player {
 		wp_enqueue_script( 'jquery-ui-button');
 		wp_enqueue_script( 'gmlp-js', GMLIVEPLAYER_URL . "assets/js/greater_media_live_player{$postfix}.js", array( 'jquery' ), GMLIVEPLAYER_VERSION, true );
 		//wp_enqueue_style( 'gmlp-styles', GMLIVEPLAYER_URL . "assets/css/greater_media_live_player{$postfix}.css", array(), GMLIVEPLAYER_VERSION );
-
+		wp_localize_script( 'gmlp-js', 'gmlp', array( 'logged_in' => is_user_logged_in() ) );
 	}
 
 	/**
