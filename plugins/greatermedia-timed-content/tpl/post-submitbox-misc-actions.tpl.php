0d64b7a24c3fbf6dfd115b45e2afaee0553f00d2
Dave Ross <dave.ross@10up.com>
2014-10-27 15:15:45 -0400

    Run screen-reader text through translation function

--- plugins/greatermedia-timed-content/tpl/post-submitbox-misc-actions.tpl.php
+++ plugins/greatermedia-timed-content/tpl/post-submitbox-misc-actions.tpl.php
@@ -1,5 +1,5 @@
 <div class="misc-pub-section exptime misc-pub-exptime">
 <span id="exptimestamp">	<?php _e( 'Expires on:', 'greatermedia-timed-content' ); ?> <b><?php echo esc_html( $rendered_expiration_timestamp ); ?></b></span>
-<a href="#edit_exptimestamp" class="edit-exptimestamp hide-if-no-js"><span aria-hidden="true">Edit</span> <span class="screen-reader-text">Edit expiration date and time</span></a>
+<a href="#edit_exptimestamp" class="edit-exptimestamp hide-if-no-js"><span aria-hidden="true">Edit</span> <span class="screen-reader-text"><?php _e( 'Edit expiration date and time', 'greatermedia-timed-content' ); ?></span></a>
 <div id='exptimestampdiv' class='hide-if-js'></div>
 </div>
