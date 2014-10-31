c6c0541fc9c5d264b96462a4db8ff65c5039b4f0
Allen Moore <allen@10up.com>
2014-10-31 14:03:36 -0400

    featured content blocks on front page

--- themes/greatermedia/partials/frontpage-featured.php
+++ themes/greatermedia/partials/frontpage-featured.php
@@ -14,8 +14,45 @@
 			</div>
 			<div class="featured-artist--content">
 				<div class="featured-artist--heading">
-					Arist of the Month
-					<h2 class="featured-artist--heading_title">Minshara</h2>
+					<h2 class="featured-artist--title">Arist of the Month</h2>
+					<h3 class="featured-artist--subtitle">Minshara</h3>
+				</div>
+				<div class="featured-artist--bio">
+					Min•sha•ra (min SHä rə) 1. adj. the classification for a planet capable of supporting humanoid life; 2. n. electronic pop rock band from Harrisburg / Philadelphia, Pennsylvania From The Viper Room in LA to Webster Hall in NYC, Minshara has been spreading infectious pop melodies, dance grooves, and rock
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="featured-content">
+			<div class="featured-content--block">
+				<div class="featured-content--image">
+					<img src="http://placehold.it/400x400&text=featured+image">
+				</div>
+				<div class="featured-content--meta">
+					<h2 class="featured-content--title">MMR Rocks the Flyers</h2>
+					MMR Rocks the Flyers<br>
+					Flyers All Access
+				</div>
+			</div>
+			<div class="featured-content--block">
+				<div class="featured-content--image">
+					<img src="http://placehold.it/400x400&text=featured+image">
+				</div>
+				<div class="featured-content--meta">
+					<h2 class="featured-content--title">MMR Rocks the Flyers</h2>
+					MMR Rocks the Flyers<br>
+					Flyers All Access
+				</div>
+			</div>
+			<div class="featured-content--block">
+				<div class="featured-content--image">
+					<img src="http://placehold.it/400x400&text=featured+image">
+				</div>
+				<div class="featured-content--meta">
+					<h2 class="featured-content--title">MMR Rocks the Flyers</h2>
+					MMR Rocks the Flyers<br>
+					Flyers All Access
 				</div>
 			</div>
 		</div>
