233d2779370c2cbe87d7eace4ff4976d258d0af5
Dave Ross <dave.ross@10up.com>
2014-11-05 17:35:27 -0500

    Started implementing a contest entry preview for embedded_form entries

--- plugins/greatermedia-contests/inc/class-greatermedia-contest-entry-embedded-form.php
+++ plugins/greatermedia-contests/inc/class-greatermedia-contest-entry-embedded-form.php
@@ -7,7 +7,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 class GreaterMediaContestEntryEmbeddedForm extends GreaterMediaContestEntry {
 
 	public function render_preview() {
-		return "This is an embedded form submission";
+
+		$html = '<div class="contest_entry">';
+
+		$html .= '<p>' . date( 'm/d/Y h:ia', strtotime( $this->post->post_date ) ) . ' ' . wp_kses_data( $this->entrant_name ) . '</p>';
+
+		$entry_data = json_decode( $this->entry_reference );
+		if ( ! empty( $entry_data ) ) {
+
+			$html .= '<table>';
+
+			foreach ( $entry_data as $key => $value ) {
+
+				$html .= '<tr>';
+				$html .= '<th>' . wp_kses_data( $key ) . '</th>';
+				$html .= '<td>' . wp_kses_data( $value ) . '</td>';
+				$html .= '</tr>';
+
+			}
+
+			$html .= '</table>';
+
+		}
+
+		$html .= '</div>';
+
+		echo $html;
+
 	}
 
 }
