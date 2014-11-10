4c6b73745a5e1c3bcb64ffef79388f56f2fd899e
Allen Moore <allen@10up.com>
2014-11-10 09:59:11 -0500

    clean up js a little to meet 10up standards

--- themes/greatermedia/assets/js/greater_media.js
+++ themes/greatermedia/assets/js/greater_media.js
@@ -6,17 +6,17 @@
 
 	var headroom, livePlayerFix, livePlayerInit, livePlayerLocation,
 
-		body = document.querySelector('body'),
-		mobileNavButton = document.querySelector('.mobile-nav--toggle'),
-		header = document.getElementById('header'),
+		body = document.querySelector( 'body' ),
+		mobileNavButton = document.querySelector( '.mobile-nav--toggle' ),
+		header = document.getElementById( 'header' ),
 		headerHeight = header.scrollHeight,
-		livePlayer = document.getElementById('live-player--sidebar'),
-		livePlayerStreamSelect = document.querySelector('.live-player__stream--current'),
-		livePlayerStreams = document.querySelector('.live-player__stream--available'),
+		livePlayer = document.getElementById( 'live-player--sidebar' ),
+		livePlayerStreamSelect = document.querySelector( '.live-player__stream--current' ),
+		livePlayerStreams = document.querySelector( '.live-player__stream--available' ),
 		wpAdminHeight = 32,
-		onAir = document.getElementById('on-air'),
-		nowPlaying = document.getElementById('now-playing'),
-		liveLinks = document.getElementById('live-links');
+		onAir = document.getElementById( 'on-air' ),
+		nowPlaying = document.getElementById( 'now-playing' ),
+		liveLinks = document.getElementById( 'live-links' );
 
 	/**
 	 * adds a class to the live player that causes it to become fixed to the top of the window while also removing the
@@ -25,8 +25,8 @@
 	livePlayerFix = function() {
 		// Using an if statement to check the class
 		livePlayer.style.top = '0px';
-		livePlayer.classList.remove('live-player--init');
-		livePlayer.classList.add('live-player--fixed');
+		livePlayer.classList.remove( 'live-player--init' );
+		livePlayer.classList.add( 'live-player--fixed' );
 	};
 
 	/**
@@ -34,13 +34,13 @@
 	 * that causes the live player to become fixed to the top of the window
 	 */
 	livePlayerInit = function() {
-		if (body.classList.contains('logged-in')) {
+		if (body.classList.contains( 'logged-in' )) {
 			livePlayer.style.top = headerHeight + wpAdminHeight + 'px';
 		} else {
 			livePlayer.style.top = headerHeight + 'px';
 		}
-		livePlayer.classList.remove('live-player--fixed');
-		livePlayer.classList.add('live-player--init');
+		livePlayer.classList.remove( 'live-player--fixed' );
+		livePlayer.classList.add( 'live-player--init' );
 	};
 
 	/**
@@ -48,7 +48,7 @@
 	 *
 	 * @type {Window.Headroom}
 	 */
-	headroom = new Headroom(header, {
+	headroom = new Headroom( header, {
 		"offset": headerHeight,
 		"tolerance": {
 			"up": 0,
@@ -67,50 +67,55 @@
 	});
 
 	/**
-	 * Initiates `headroom`
+	 * Initiates `headroom` if the window size is above 768px
 	 */
-	if(window.innerWidth >= 768) {
+	if ( window.innerWidth >= 768 ) {
 		headroom.init();
 	}
-
-	window.addEventListener('resize', function() {
-		if(window.innerWidth >= 768) {
-			headroom.init();
-		}
-	});
 
 	/**
 	 * Toggles a class to the body when the mobile nav button is clicked
 	 */
-	mobileNavButton.onclick = function(){
-		body.classList.toggle('mobile-nav--open');
-	};
+	function toggleNavButton() {
+		body.classList.toggle( 'mobile-nav--open' );
+	}
+	mobileNavButton.addEventListener( 'click', toggleNavButton, false );
 
-	livePlayerStreamSelect.onclick = function(){
-		this.classList.toggle('open');
-	};
+	/**
+	 * Toggles a class to the Live Play Stream Select box when the box is clicked
+	 */
+	function toggleStreamSelect() {
+		livePlayerStreamSelect.classList.toggle( 'open' );
+	}
+	livePlayerStreamSelect.addEventListener( 'click', toggleStreamSelect, false );
 
 	/**
 	 * Toggles a class to the live links when the live player is clicked clicked on smaller screens
 	 */
-	if(window.innerWidth <= 767) {
-		onAir.onclick = function () {
-			body.classList.toggle('live-player--open');
-		};
-		nowPlaying.onclick = function () {
-			body.classList.toggle('live-player--open');
-		};
+	function onAirClick() {
+		body.classList.toggle( 'live-player--open' );
 	}
 
-	window.addEventListener('resize', function() {
-		if(window.innerWidth <= 767) {
-			onAir.onclick = function () {
-				body.classList.toggle('live-player--open');
-			};
-			nowPlaying.onclick = function () {
-				body.classList.toggle('live-player--open');
-			};
+	function nowPlayingClick() {
+		body.classList.toggle( 'live-player--open' );
+	}
+
+	if( window.innerWidth <= 767 ) {
+		onAir.addEventListener( 'click', onAirClick, false );
+		nowPlaying.addEventListener( 'click', nowPlayingClick, false );
+	}
+
+	/**
+	 * A fail-safe for when the browser window is resized
+	 */
+	window.addEventListener( 'resize', function() {
+		if( window.innerWidth <= 767 ) {
+			onAir.addEventListener( 'click', onAirClick, false );
+			nowPlaying.addEventListener( 'click', nowPlayingClick, false );
 		}
-	}, false);
+		if( window.innerWidth >= 768 ) {
+			headroom.init();
+		}
+	});
 
 })();
