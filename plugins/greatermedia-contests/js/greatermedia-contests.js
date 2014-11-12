486dd321144c9d791871bdc53826f766bf582b90
Dave Ross <dave.ross@10up.com>
2014-11-12 15:55:12 -0500

    Parsley.js validation on form submit

--- plugins/greatermedia-contests/js/greatermedia-contests.js
+++ plugins/greatermedia-contests/js/greatermedia-contests.js
@@ -1,25 +1,29 @@
 jQuery(function () {
 
 	// Attach a submit handler to contest forms
-	jQuery('.' + GreaterMediaContests.form_class).submit(function () {
+	jQuery('.' + GreaterMediaContests.form_class).submit(function (e) {
 
 		var form = this;
+		e.preventDefault();
+		if ( jQuery(this).parsley().isValid() ) {
 
-		// Submit the form via AJAX
-		jQuery.post(
-			GreaterMediaContests.ajax_url,
-			jQuery(this).serialize(),
-			function (data, textStatus, jqXHR) {
+			// Submit the form via AJAX
+			jQuery.post(
+				GreaterMediaContests.ajax_url,
+				jQuery(this).serialize(),
+				function (data, textStatus, jqXHR) {
 
-				if ('success' === textStatus && data.data.message) {
-					var wrapper = document.createElement('p');
-					wrapper.class = 'contest_thank_you';
-					wrapper.innerText = data.data.message;
-					jQuery(form).replaceWith(wrapper);
+					if ('success' === textStatus && data.data.message) {
+						var wrapper = document.createElement('p');
+						wrapper.class = 'contest_thank_you';
+						wrapper.innerText = data.data.message;
+						jQuery(form).replaceWith(wrapper);
+					}
+
 				}
+			);
 
-			}
-		);
+		}
 
 		return false;
 
