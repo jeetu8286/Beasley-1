a381223a294e54caff0070c304b3a701cd040a7e
Darshan Sawardekar <darshan@10up.com>
2014-11-13 13:46:28 +0530

    Fixes circular logic for full text types.

--- plugins/greatermedia-gigya/js/src/views/constraint_view.js
+++ plugins/greatermedia-gigya/js/src/views/constraint_view.js
@@ -106,7 +106,7 @@ var ConstraintView = Backbone.View.extend({
 		'not contains'
 	],
 
-	fullTextOperators: [
+	nonFullTextOperators: [
 		'equals',
 		'not equals',
 	],
@@ -116,7 +116,7 @@ var ConstraintView = Backbone.View.extend({
 		'not equals',
 	],
 
-	fullTextTypes: [
+	nonFullTextTypes: [
 		'profile:zip',
 		'profile:state',
 		'profile:country',
@@ -127,8 +127,8 @@ var ConstraintView = Backbone.View.extend({
 		if (valueType === 'integer' || valueType === 'float') {
 			return this.numericOperators;
 		} else if (valueType === 'string') {
-			if (this.isFullTextType(type)) {
-				return this.fullTextOperators;
+			if (this.isNonFullTextType(type)) {
+				return this.nonFullTextOperators;
 			} else {
 				return this.stringOperators;
 			}
@@ -149,8 +149,12 @@ var ConstraintView = Backbone.View.extend({
 		return this.model.hasMeta('choices');
 	},
 
-	isFullTextType: function(type) {
-		return _.indexOf(this.fullTextTypes, type) !== -1;
+	isNonFullTextType: function(type) {
+		if (type === undefined) {
+			return false;
+		} else {
+			return _.indexOf(this.nonFullTextTypes, type) !== -1;
+		}
 	}
 
 });
