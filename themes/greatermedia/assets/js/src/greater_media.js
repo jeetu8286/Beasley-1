5e545f8642162cee5da0a34352c7a7e76d0f05be
Allen Moore <allen@10up.com>
2014-11-15 13:21:13 -0500

    fixes for gmr-225

--- themes/greatermedia/assets/js/src/greater_media.js
+++ themes/greatermedia/assets/js/src/greater_media.js
@@ -5,7 +5,6 @@
  * Licensed under the GPLv2+ license.
  */
 (function() {
-	'use strict';
 
 	var headroom, livePlayerFix, livePlayerInit, livePlayerLocation, livePlayerScroll,
 
@@ -20,7 +19,30 @@
 		onAir = document.getElementById( 'on-air' ),
 		nowPlaying = document.getElementById( 'now-playing' ),
 		liveLinks = document.getElementById( 'live-links'),
-		h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+		windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+
+	function getScrollXY() {
+		var x = 0, y = 0;
+		if( typeof( window.pageYOffset ) == 'number' ) {
+			// Netscape
+			x = window.pageXOffset;
+			y = window.pageYOffset;
+		} else if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
+			// DOM
+			x = document.body.scrollLeft;
+			y = document.body.scrollTop;
+		} else if( document.documentElement && ( document.documentElement.scrollLeft || document.documentElement.scrollTop ) ) {
+			// IE6 standards compliant mode
+			x = document.documentElement.scrollLeft;
+			y = document.documentElement.scrollTop;
+		}
+		return [x, y];
+	}
+
+	var xy = getScrollXY();
+	var x = xy[0];
+	var y = xy[1];
 
 	/**
 	 * adds a class to the live player that causes it to become fixed to the top of the window while also removing the
@@ -35,6 +57,7 @@
 		}
 		livePlayer.classList.remove( 'live-player--init' );
 		livePlayer.classList.add( 'live-player--fixed' );
+		livePlayer.style.height = windowHeight - headerHeight - wpAdminHeight + 'px';
 	};
 
 	/**
@@ -49,19 +72,19 @@
 		}
 		livePlayer.classList.remove( 'live-player--fixed' );
 		livePlayer.classList.add( 'live-player--init' );
-		livePlayer.style.height = h - headerHeight - wpAdminHeight + 'px';
+		livePlayer.style.height = windowHeight - headerHeight - wpAdminHeight + 'px';
 	};
 
-	livePlayerScroll = function() {
-		function lpScroll() {
-			if ( header.classList.contains('header--unpinned') || header.classList.contains('headroom--top')) {
-				livePlayer.style.height = h - wpAdminHeight + 'px';
-			} else {
-				livePlayer.style.height = '100%';
-			}
+	function lpScroll() {
+		if ( header.classList.contains( 'header--pinned' )) {
+			livePlayer.style.height = windowHeight  - headerHeight - wpAdminHeight + 'px';
+		} else if ( header.classList.contains( 'header--unpinned' ) ) {
+			livePlayer.style.height = windowHeight  - wpAdminHeight + 'px';
+		} else {
+			livePlayer.style.height = '100%';
 		}
-		window.addEventListener( 'scroll', lpScroll, false );
-	};
+	}
+	window.addEventListener( 'scroll', lpScroll, false );
 
 	/**
 	 * adds headroom.js functionality to the header
@@ -70,21 +93,16 @@
 	 */
 	headroom = new Headroom( header, {
 		offset: headerHeight,
-		tolerance : {
-			down : 0,
-			up : headerHeight
-		},
+		tolerance : 0,
 		classes: {
 			"pinned": "header--pinned",
 			"unpinned": "header--unpinned"
 		},
 		onTop : function() {
 			livePlayerInit();
-			livePlayerScroll();
 		},
 		onNotTop : function() {
 			livePlayerFix();
-			livePlayerScroll();
 		}
 	});
 
