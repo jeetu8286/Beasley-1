fd037c57a592c6fe64e534ef880cb4961b506305
Allen Moore <allen@10up.com>
2014-11-18 11:23:43 -0500

    change element locations to make mobile more in line with mockups

--- themes/greatermedia/partials/post-video.php
+++ themes/greatermedia/partials/post-video.php
@@ -6,6 +6,14 @@
  * @since   0.1.0
  */
 ?>
+<section class="entry--video__meta" itemprop="articleBody">
+
+	<time datetime="<?php the_time( 'c' ); ?>" class="entry__date"><?php the_time( 'M. j, Y' ); ?></time>
+
+	<h2 class="entry__title"><?php the_title(); ?></h2>
+
+</section>
+
 <section class="entry--video__thumbnail">
 
 	<?php if ( has_post_thumbnail() ) {
@@ -17,13 +25,5 @@
 		<img src="http://placehold.it/600x400&text=image">
 
 	<?php } ?>
-
-</section>
-
-<section class="entry--video__meta" itemprop="articleBody">
-
-	<time datetime="<?php the_time( 'c' ); ?>" class="entry__date"><?php the_time( 'M. j, Y' ); ?></time>
-
-	<h2 class="entry__title"><?php the_title(); ?></h2>
 
 </section>
