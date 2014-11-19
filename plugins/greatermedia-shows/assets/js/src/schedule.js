9f6c85fbb71fa732ac55582c4a12f2c43426b743
Eugene Manuilov <eugene.manuilov@10up.com>
2014-11-19 11:28:44 +0200

    Restricted max date for the show scheduler.

--- plugins/greatermedia-shows/assets/js/src/schedule.js
+++ plugins/greatermedia-shows/assets/js/src/schedule.js
@@ -6,6 +6,7 @@
 		$('#start-from-date').datepicker({
 			dateFormat : 'M d, yy',
 			minDate: 'now',
+			maxDate: '+1w',
 			altField: '#start-from-date-value',
 			altFormat: 'yy-mm-dd'
 		});
@@ -24,7 +25,7 @@
 		$('.remove-show').click(function () {
 			var link = $(this).attr('href');
 			
-			$('body').append(popup_tmpl.replace(/{url}/g, function(match, key) {
+			$('body').append(popup_tmpl.replace(/{url}/g, function() {
 				return link;
 			}));
 			
