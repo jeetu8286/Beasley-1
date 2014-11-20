a48ede62373f17596edd250c98a730084ef66dc2
Allen Moore <allen@10up.com>
2014-11-20 15:59:17 -0500

    add sub title to highlights area

--- themes/greatermedia/index.php
+++ themes/greatermedia/index.php
@@ -21,7 +21,7 @@ get_header();
 
 			<div class="highlights__community">
 
-				<h2 class="highlights__heading"><?php bloginfo( 'name' ); ?><?php _e( ' Community Highlights', 'greatermedia' ); ?></h2>
+				<h2 class="highlights__heading"><?php bloginfo( 'name' ); ?><?php _e( ' Community Highlights', 'greatermedia' ); ?><span class="highlights__heading--span"><?php _e( 'YouRock', 'greatermedia' ); ?></span></h2>
 
 				<div class="highlights__community--item">
 
@@ -57,7 +57,7 @@ get_header();
 
 			<div class="highlights__events">
 
-				<h2 class="highlights__heading"><?php _e( 'Upcoming', 'greatermedia' ); ?></h2>
+				<h2 class="highlights__heading"><?php _e( 'Upcoming', 'greatermedia' ); ?><span class="highlights__heading--span"><?php _e( 'Events', 'greatermedia' ); ?></span></h2>
 
 				<div class="highlights__event--item">
 					
