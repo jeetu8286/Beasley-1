141ee4f0957c9bc790af443fa29f3080874de90b
Allen Moore <allen@10up.com>
2014-11-20 16:18:50 -0500

    add additional template files

---
+++ themes/greatermedia/search.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Search Results template file
+ *
+ * @package Greater Media
+ * @since   0.1.0
+ *
+ * @todo this template file still needs to be layed out according to the design
+ */
+
+get_header(); ?>
+
+	<main class="main" role="main">
+
+		<div class="container">
+
+			<section class="content">
+
+				<h2 class="page__title"><?php printf( __( 'Search Results for: %s', 'greatermedia' ), '<span class="search__term">' . get_search_query() . '</span>' ); ?></h2>
+
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+					<h2 class="entry__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+				</article>
+
+				<?php endwhile; ?>
+
+					<div class="posts-pagination">
+
+						<div class="posts-pagination--previous"><?php next_posts_link( '<i class="fa fa-angle-double-left"></i>Previous' ); ?></div>
+						<div class="posts-pagination--next"><?php previous_posts_link( 'Next<i class="fa fa-angle-double-right"></i>' ); ?></div>
+
+					</div>
+
+				<?php else : ?>
+
+					<article id="post-not-found" class="hentry cf">
+
+						<header class="article-header">
+
+							<h1><?php _e( 'Oops, Post Not Found!', 'greatermedia' ); ?></h1>
+
+						</header>
+
+						<section class="entry-content">
+
+							<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'greatermedia' ); ?></p>
+
+						</section>
+
+					</article>
+
+				<?php endif; ?>
+
+			</section>
+
+		</div>
+
+	</main>
+
+<?php get_footer();
