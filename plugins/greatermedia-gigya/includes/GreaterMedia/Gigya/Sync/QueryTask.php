9ee7a64220d4d5a99665ab090aaa207926786485
Darshan Sawardekar <darshan@10up.com>
2014-11-27 18:41:06 +0530

    Adds QueryTask and tests.

---
+++ plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Sync/QueryTask.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace GreaterMedia\Gigya\Sync;
+
+class QueryTask extends SyncTask {
+
+	public $page_size = 100;
+	public $collector = null;
+
+	function get_task_name() {
+		return 'query_task';
+	}
+
+	function run() {
+		$query      = $this->get_query();
+		$cursor     = $this->get_cursor();
+		$store_type = $this->get_store_type();
+
+		$paginator = new QueryPaginator( $store_type, $this->page_size );
+		$matches   = $paginator->fetch( $query, $cursor );
+
+		$users = $this->find_users( $matches['results'] );
+		$this->save_users( $users );
+
+		return $matches;
+	}
+
+	function find_users( $results ) {
+		if ( function_exists( 'array_column' ) ) {
+			return array_column( $results, 'UID' );
+		} else {
+			$users = array();
+
+			foreach ( $results as $result ) {
+				$users[] = $result['UID'];
+			}
+
+			return $users;
+		}
+	}
+
+	function save_users( $users ) {
+		$collector = $this->get_collector();
+		$collector->collect( $users );
+	}
+
+	function after( $matches ) {
+		if ( array_key_exists( 'progress', $matches ) ) {
+			$this->get_sentinel()->set_task_progress(
+				$this->get_store_type(),
+				$matches['progress']
+			);
+		}
+
+		if ( $matches['has_next'] ) {
+			$params           = $this->params;
+			$params['cursor'] = $matches['cursor'];
+
+			$this->enqueue( $params );
+		}
+	}
+
+	function get_query() {
+		return $this->get_param( 'query' );
+	}
+
+	function get_cursor() {
+		if ( $this->has_param( 'cursor' ) ) {
+			return $this->get_param( 'cursor' );
+		} else {
+			return 0;
+		}
+	}
+
+	function get_store_type() {
+		return $this->get_param( 'store_type' );
+	}
+
+	function get_collector() {
+		if ( is_null( $this->collector ) ) {
+			$this->collector = new UserCollector(
+				$this->get_site_id(),
+				$this->get_member_query_id()
+			);
+		}
+
+		return $this->collector;
+	}
+
+}
