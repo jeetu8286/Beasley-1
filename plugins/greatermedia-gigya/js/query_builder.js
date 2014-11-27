4110ac3ae53bb4fd54a9e8bfe8d4f271e6d7e3e6
Darshan Sawardekar <darshan@10up.com>
2014-11-27 11:00:35 +0530

    Updates Grunt to minify all javascript files.

--- plugins/greatermedia-gigya/js/query_builder.js
+++ plugins/greatermedia-gigya/js/query_builder.js
@@ -51,44 +51,6 @@ if (typeof Object.create != 'function') {
 	})();
 }
 
-var WpAjaxApi = function(config) {
-	this.config = config;
-};
-
-WpAjaxApi.prototype = {
-
-	nonceFor: function(action) {
-		return this.config[action + '_nonce'];
-	},
-
-	urlFor: function(action) {
-		var queryParams = {};
-		queryParams[action + '_nonce'] = this.nonceFor(action);
-
-		var url = this.config.ajax_url;
-		url += url.indexOf('?') === -1 ? '?' : '&';
-		url += $.param(queryParams);
-
-		return url;
-	},
-
-	request: function(action, data) {
-		if (!data) {
-			data = {};
-		}
-
-		var url         = this.urlFor(action);
-		var requestData = {
-			'action': action,
-			'action_data': JSON.stringify(data)
-		};
-
-		return $.post(url, requestData);
-	},
-
-};
-
-
 this["JST"] = this["JST"] || {};
 
 this["JST"]["src/templates/constraint.jst"] = function(obj) {
