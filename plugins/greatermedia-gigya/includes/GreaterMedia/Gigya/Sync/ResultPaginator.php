7f394afe928e307e17ee57a7961c4e3314491bef
Darshan Sawardekar <darshan@10up.com>
2014-12-01 13:05:56 +0530

    Adds ResultPaginator and tests.
    * Paginates over the `member_query_results` user_ids.

---
+++ plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Sync/ResultPaginator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace GreaterMedia\Gigya\Sync;
+
+class ResultPaginator {
+
+	public $site_id;
+	public $member_query_id;
+	public $page_size;
+
+	function __construct( $site_id, $member_query_id, $page_size ) {
+		$this->site_id         = $site_id;
+		$this->member_query_id = $member_query_id;
+		$this->page_size       = $page_size;
+	}
+
+	function fetch( $cursor = 0 ) {
+		$db = $this->get_job_db();
+		$query = $this->query_for( $cursor );
+
+		return $db->get_col( $query );
+	}
+
+	function query_for( $cursor ) {
+		$query = <<<SQL
+SELECT user_id
+FROM member_query_results
+WHERE
+	site_id = %d and
+	member_query_id = %d
+LIMIT %d, %d;
+SQL;
+
+		$db     = $this->get_job_db();
+		$params = array(
+			$this->site_id,
+			$this->member_query_id,
+			$cursor,
+			$this->page_size,
+		);
+
+		return $db->prepare( $query, $params );
+	}
+
+	function get_job_db() {
+		return TempDatabase::get_instance()->get_db();
+	}
+
+}
+
+
