4d1dd647a6e08199296835581fece5f465968520
Darshan Sawardekar <darshan@10up.com>
2014-12-01 19:31:22 +0530

    Adds PreviewResultsTask and tests.
    * Takes compiled results and fetches first 5 Gigya users.

--- plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Sync/PreviewResultsTask.php
+++ plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Sync/PreviewResultsTask.php
@@ -4,16 +4,55 @@ namespace GreaterMedia\Gigya\Sync;
 
 class PreviewResultsTask extends SyncTask {
 
+	public $page_size    = 5;
+	public $force_delete = true;
+
 	function get_task_name() {
 		return 'preview_results';
 	}
 
 	function run() {
+		$paginator = new ResultPaginator(
+			$this->get_site_id(),
+			$this->get_member_query_id(),
+			$this->page_size
+		);
 
+		$count = $paginator->count();
+
+		if ( $count > 0 ) {
+			$user_ids = $paginator->fetch();
+			$finder   = new GigyaUserFinder();
+			$users    = $finder->find( $user_ids );
+		} else {
+			$user_ids = array();
+			$users    = array();
+		}
+
+		$this->save_users( $users, $count );
+	}
+
+	function save_users( $users, $count ) {
+		$results = array(
+			'total' => $count,
+			'users' => $users,
+		);
+
+		$json = json_encode( $results );
+
+		update_post_meta(
+			$this->get_member_query_id(),
+			'member_query_preview_results',
+			$json
+		);
 	}
 
 	function after( $result ) {
+		$this->get_sentinel()->set_task_progress( 'preview_results', 100 );
 
+		if ( $this->force_delete ) {
+			wp_delete_post( $this->get_member_query_id(), true );
+		}
 	}
 
 }
