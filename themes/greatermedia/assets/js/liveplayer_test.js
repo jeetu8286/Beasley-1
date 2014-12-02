2e9a21fcf07801d58eaf0a42391541a37f1ae16d
Allen Moore <allen@10up.com>
2014-12-02 15:14:01 -0500

    remove banner from theme gruntfile and re-run grunt

--- themes/greatermedia/assets/js/liveplayer_test.js
+++ themes/greatermedia/assets/js/liveplayer_test.js
@@ -1,6 +1,3 @@
-/*! Greater Media - v0.1.0 - 2014-12-01
- * http://greatermedia.com
- * Copyright (c) 2014; * Licensed GPLv2+ */
 var gigya = gigya || {};
 gigya.accounts = gigya.accounts || {};
 gigya.accounts.eventHandlers = gigya.accounts.eventHandlers || {};
--- themes/greatermedia/assets/js/liveplayer_test_auth.js
+++ themes/greatermedia/assets/js/liveplayer_test_auth.js
@@ -1,6 +1,3 @@
-/*! Greater Media - v0.1.0 - 2014-12-01
- * http://greatermedia.com
- * Copyright (c) 2014; * Licensed GPLv2+ */
 /**
  * Public API
  */
