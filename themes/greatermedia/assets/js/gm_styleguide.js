d4e39a095e88c0c076f0ad40ecd37fde0d5a26aa
AndrewMowe <andrew@team-eight.com>
2014-12-02 21:36:11 -0800

    grunt

--- themes/greatermedia/assets/js/gm_styleguide.js
+++ themes/greatermedia/assets/js/gm_styleguide.js
@@ -1,4 +1,4 @@
-/*! Greater Media - v0.1.0 - 2014-11-24
+/*! Greater Media - v0.1.0 - 2014-12-02
  * http://greatermedia.com
  * Copyright (c) 2014; * Licensed GPLv2+ */
 (function ($,window,undefined) {
