9fca60755333198749c048338bc6fa398d1ecdb8
Chris Marslender <chrismarslender@gmail.com>
2014-12-10 16:50:39 -0700

    Throw 404 on the various show endpoints (and the main show page) if support for the specific sections is not enabled for the show post

--- plugins/greatermedia-shows/includes/gmi-show-endpoints.php
+++ plugins/greatermedia-shows/includes/gmi-show-endpoints.php
@@ -4,6 +4,7 @@ namespace GreaterMedia\Shows;
 
 add_action( 'init', __NAMESPACE__ . '\add_show_section_tag' );
 add_action( 'init', __NAMESPACE__ . '\add_rewrites' );
+add_action( 'template_redirect', __NAMESPACE__ . '\template_redirect' );
 add_action( 'template_include', __NAMESPACE__ . '\filter_template' );
 
 /**
@@ -37,6 +38,63 @@ function add_rewrites() {
 
 		\add_rewrite_rule( $rule, sprintf( 'index.php?%1$s=$matches[1]&show_section=$matches[2]&page=$matches[3]', $post_type ) , 'top' );
 	}
+}
+
+/**
+ * Checks if we should be allowing access to this show's homepage or not, based on the settings on the show page in wp-admin.
+ *
+ * Checks for Homepage support (applies to about also), in addition to Albums, Podcasts, and Videos.
+ */
+function template_redirect() {
+	if ( get_post_type() !== \ShowsCPT::SHOW_CPT ) {
+		return;
+	}
+
+	if ( ! supports_homepage( get_the_ID() ) ) {
+		block_show_access();
+		return;
+	}
+
+	$section = get_query_var( 'show_section' );
+	if ( $section && in_array( $section, get_sections() ) ) {
+		switch( $section ) {
+			case 'albums':
+				$allowed = supports_albums( get_the_ID() );
+				break;
+			case 'podcasts':
+				$allowed = supports_podcasts( get_the_ID() );
+				break;
+			case 'videos':
+				$allowed = supports_videos( get_the_ID() );
+				break;
+			case 'about':
+			default:
+				// These are just allowed through, as long as homepage is enabled, which it is if we're this far
+				$allowed = true;
+				break;
+		}
+
+		if ( ! $allowed ) {
+			block_show_access();
+			return;
+		}
+	}
+}
+
+/**
+ * Blocks access to the current page by setting a 404 and clearing the show_section.
+ *
+ * Clearing the section is required, otherwise we will end up overridding the 404 template in the filter_tempalte function below
+ */
+function block_show_access() {
+	global $wp_query;
+
+	if ( get_post_type() !== \ShowsCPT::SHOW_CPT ) {
+		return;
+	}
+
+	$wp_query->set_404();
+	$wp_query->set( 'show_section', null );
 }
 
 /**
