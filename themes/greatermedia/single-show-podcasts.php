a28dedd6accb233aa31d4f874750b42022e660ae
Chris Marslender <chrismarslender@gmail.com>
2014-12-10 18:45:43 -0700

    Add helpers to get podcast query for a show page and pagination helper. Add podcasts output to the podcasts template

--- themes/greatermedia/single-show-podcasts.php
+++ themes/greatermedia/single-show-podcasts.php
@@ -22,6 +22,32 @@
 
 					</article>
 
+					<div class="podcasts">
+
+						<?php
+						$podcast_query = \GreaterMedia\Shows\get_show_podcast_query();
+
+						while( $podcast_query->have_posts() ) : $podcast_query->the_post(); ?>
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/OnDemandEvent">
+
+								<header class="entry-header">
+									<h2 class="entry-title" itemprop="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+								</header>
+
+								<div class="entry-content">
+									<?php the_excerpt(); ?>
+								</div>
+
+							</article>
+						<?php
+						endwhile;
+						wp_reset_query();
+						?>
+
+						<div class="podcast-paging"><?php echo \GreaterMedia\Shows\get_show_endpoint_pagination_links( $podcast_query ); ?></div>
+
+					</div>
+
 			</section>
 
 		</div>
