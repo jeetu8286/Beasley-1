01114946fb18c8e5dcffdd137b206b91dc059608
Chris Marslender <chrismarslender@gmail.com>
2014-12-11 12:44:15 -0700

    Add "archive" type view no the albums endpoint for shows

--- themes/greatermedia/single-show-albums.php
+++ themes/greatermedia/single-show-albums.php
@@ -10,19 +10,31 @@
 
 			<section class="content">
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+				<div class="albums">
 
-					<header class="entry-header">
+					<?php
+					$album_query = \GreaterMedia\Shows\get_show_album_query();
 
-						<h2 class="entry-title" itemprop="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?> Albums</a></h2>
+					while( $album_query->have_posts() ) : $album_query->the_post(); ?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
 
-					</header>
+							<header class="entry-header">
+								<h2 class="entry-title" itemprop="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+							</header>
 
-					<div class="entry-content">
-						single-show-albums.php
-					</div>
+							<div class="entry-content">
+								<?php the_excerpt(); ?>
+							</div>
 
-				</article>
+						</article>
+					<?php
+					endwhile;
+					wp_reset_query();
+					?>
+
+					<div class="video-paging"><?php echo \GreaterMedia\Shows\get_show_endpoint_pagination_links( $album_query ); ?></div>
+
+				</div>
 				
 			</section>
 
