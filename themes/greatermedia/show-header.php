85dbe92be6b1057cd935bce85c9d8977c84714bd
AndrewMowe <andrew@team-eight.com>
2014-12-11 14:50:58 -0800

    add video link

---
+++ themes/greatermedia/show-header.php
@@ -0,0 +1,21 @@
+<div class="show__header">
+	<div class="show__cast">
+		<img src="http://placehold.it/135x135&text=cast">
+	</div>
+	<nav class="show__nav">
+		<a href="<?php the_permalink(); ?>"><h1 class="show__title"><?php the_title(); ?></h1></a>
+		<ul>
+			<?php \GreaterMedia\Shows\about_link_html( get_the_ID() ); ?>
+			<?php \GreaterMedia\Shows\podcasts_link_html( get_the_ID() ); ?>
+			<?php \GreaterMedia\Shows\galleries_link_html( get_the_ID() ); ?>
+			<?php \GreaterMedia\Shows\video_link_html( get_the_ID() ); ?>
+		</ul>
+	</nav>
+	<div class="show__meta">
+		<em>Weekdays</em>
+		<em>5:30am - 10:30am</em>
+		<a href="http://www.facebook.com/sharer/sharer.php?u=[URL]&title=[TITLE]" class="icon-facebook social-share-link"></a>
+		<a href="http://twitter.com/home?status=[TITLE]+[URL]" class="icon-twitter social-share-link"></a>
+		<a href="https://plus.google.com/share?url=[URL]" class="icon-google-plus social-share-link"></a>
+	</div>
+</div>
