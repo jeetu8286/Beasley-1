7d42c7b4bc1a1ef06e28b749d075b84211c54952
Darshan Sawardekar <darshan@10up.com>
2014-12-12 20:37:11 +0530

    Adds API helpers to save actions to Gigya from the frontend.

---
+++ plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Ajax/SaveGigyaActionAjaxHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace GreaterMedia\Gigya\Ajax;
+
+class SaveGigyaActionAjaxHandler extends AjaxHandler {
+
+	function get_action() {
+		return 'save_gigya_action';
+	}
+
+	function is_public() {
+		return true;
+	}
+
+	function run( $params ) {
+		$action  = $params['action'];
+		$user_id = $params['user_id'];
+
+		// don't allow direct user_ids - ie:- A user can only save
+		// actions as themselves or as a guest, and not under someone
+		// else's user_id.
+		if ( $user_id !== 'guest' && $user_id != 'logged_in_user' ) {
+			$user_id = 'guest';
+		}
+
+		// if want to use against logged in user and not logged in - abort
+		if ( $user_id === 'logged_in_user' && ! is_gigya_user_logged_in() ) {
+			return false;
+		} else {
+			save_gigya_action( $action, $user_id );
+			return true;
+		}
+	}
+
+}
