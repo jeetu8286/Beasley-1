24854a5a4fa11e08742645e379d27fa25c00ffda
Chris Marslender <chrismarslender@gmail.com>
2014-12-13 22:38:28 -0700

    Make events dynamic on front page. Falls back to non-curated if no posts are set, otherwise is curated

--- themes/greatermedia/partials/frontpage-highlights.php
+++ themes/greatermedia/partials/frontpage-highlights.php
@@ -37,31 +37,38 @@
 
 				<h2 class="highlights__heading"><?php _e( 'Upcoming', 'greatermedia' ); ?><span class="highlights__heading--span"><?php _e( 'Events', 'greatermedia' ); ?></span></h2>
 
-				<div class="highlights__event--item">
+				<?php
+				$events_query = \GreaterMedia\HomepageCuration\get_events_query();
+				while( $events_query->have_posts() ) : $events_query->the_post(); ?>
+					<div class="highlights__event--item">
 
-					<div class="highlights__event--thumb">
-						<img src="http://placehold.it/156x156&text=image">
+						<?php if ( has_post_thumbnail() ) : ?>
+							<div class="highlights__event--thumb">
+								<?php the_post_thumbnail( array( 156, 156 ) ); // todo Image Size 156x156 ?>
+							</div>
+						<?php endif; ?>
+
+						<div class="highlights__event--meta">
+							<h3 class="highlights__event--title"><?php the_title(); ?></h3>
+							<?php
+							/*
+							 * Moved the class from the span to the time so I could add both the start and end times to the datetime attributes
+							 */
+							$start = tribe_get_start_date( get_the_ID(), false, 'M d' );
+							$start_c = tribe_get_start_date( get_the_ID(), false, 'c' );
+							$end = tribe_get_end_date( get_the_ID(), false, 'M d' );
+							$end_c = tribe_get_end_date( get_the_ID(), false, 'c' );
+							if ( $start != $end ) {
+								printf( '<span class="highlights__event--date"><time datetime="%1$s">%2$s</time> - <time datetime="%3$s">%4$s</time></span>', $start_c, $start, $end_c, $end );
+							} else {
+								printf( '<span class="highlights__event--date"><time datetime="%1$s">%2$s</time></span>', $start_c, $start );
+							}
+							?>
+						</div>
+
 					</div>
-
-					<div class="highlights__event--meta">
-						<h3 class="highlights__event--title">Preston & Steve’s Camp Out For Hunger 2014</h3>
-						<time datetime="<?php the_time( 'c' ); ?>" class="highlights__event--date">Dec 1 - Dec 5</time>
-					</div>
-
-				</div>
-
-				<div class="highlights__event--item">
-
-					<div class="highlights__event--thumb">
-						<img src="http://placehold.it/156x156&text=image">
-					</div>
-
-					<div class="highlights__event--meta">
-						<h3 class="highlights__event--title">A.B.A.T.E. Motorcycle Toy Run For The Kids!</h3>
-						<time datetime="<?php the_time( 'c' ); ?>" class="highlights__event--date">Nov 2</time>
-					</div>
-
-				</div>
+				<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
 
 			</div>
 
