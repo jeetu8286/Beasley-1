fcafad4ed45ba22fd61c31faa4af2a4de8add59f
Allen Moore <allen@10up.com>
2014-12-15 09:17:59 -0500

    pull image data in to the gallery; add progress of images

--- themes/greatermedia/assets/js/src/greater_media_gallery.js
+++ themes/greatermedia/assets/js/src/greater_media_gallery.js
@@ -4,14 +4,14 @@
 	var document = window.document,
 		$document = $( document ),
 		$window = $( window ),
-		slideshow = $( '.gallery__slides.cycle-slideshow' ),
+		slideshow = $( '.main.cycle-slideshow' ),
 		gallery = document.querySelectorAll( '.gallery' ),
-		main = document.querySelector( '.gallery__slides' ),
+		main = document.querySelector( '.gallery__slide--images' ),
 		$gallery = $( gallery ),
 		$main = $( main ),
-		$main_wrapper = $( '.gallery .container' ),
+		$main_wrapper = $( '.gallery__slides' ),
 		$caption = $( '.caption' ),
-		$sidebar = $( '.sidebar' ),
+		$sidebar = $( '.gallery__thumbnails' ),
 		$slide_paging = $( '.slide-paging' ),
 		$slide_paging_previews = $( '.slide-paging-previews' ),
 		$toolbar_thumbnails = $( '.toolbar-thumbnails' ),
@@ -22,43 +22,6 @@
 	 */
 	function bind_events() {
 		var hashChange = false;
-
-		/**
-		 * Bind the fullscreen toggle.
-		 */
-		$gallery.on( 'click', '.fullscreen', function ( e ) {
-			e.preventDefault();
-			if ( isFullscreen() ) {
-				collapse_full();
-			} else if ( isWidescreen() ) {
-				collapse_wide();
-				expand_full();
-			} else {
-				expand_full();
-			}
-		} );
-
-		$gallery.on( 'click', '.widescreen', function( e ) {
-			e.preventDefault();
-			if ( isWidescreen() ) {
-				collapse_wide();
-			} else if ( isFullscreen() ) {
-				collapse_full();
-				expand_wide();
-			} else {
-				expand_wide();
-			}
-		} );
-
-		$gallery.on( 'click', '.fake-radio', function ( e ) {
-			$( '.fake-radio' ).removeClass( 'fa fa-check' );
-			$( '.sharing-option input' ).removeAttr( 'checked' );
-			$( e.target )
-				.addClass( 'fa fa-check' )
-				.prev( 'input' ).attr( 'checked', 'checked' );
-
-			update_share_urls();
-		} );
 
 		/**
 		 * Make sure thumbnails are updated before the slideshow cycles.
@@ -129,47 +92,7 @@
 			} );
 		} );
 
-		$( '.toolbar-item.thumbnails' ).on( 'click', function( e ) {
-			var $button_text = $( this ).children( '.toolbar-text' );
-			if ( GMR_Button_Text.hide_thumbnails == $button_text.text() ) {
-				$toolbar_thumbnails.hide();
-				$caption.addClass( 'thumbnails-hidden' );
-				$button_text.text( GMR_Button_Text.show_thumbnails );
-			} else {
-				$toolbar_thumbnails.show();
-				$caption.removeClass( 'thumbnails-hidden' );
-				$button_text.text( GMR_Button_Text.hide_thumbnails );
-			}
-		} );
-
-		$( '.toolbar-item.info' ).on( 'click', function( e ) {
-			var $button_text = $( this ).children( '.toolbar-text' );
-			if ( GMR_Button_Text.hide_info == $button_text.text() ) {
-				$caption.hide();
-				$button_text.text( GMR_Button_Text.show_info );
-			} else {
-				$caption.show();
-				$button_text.text( GMR_Button_Text.hide_info );
-			}
-		} );
-
 		$window.resize( responsive_thumbnails );
-	}
-
-	/**
-	 * Is the slideshow in full screen mode?
-	 * @returns bool
-	 */
-	function isFullscreen() {
-		return $gallery.hasClass( 'isfullscreen' );
-	}
-
-	/**
-	 * Is the slideshow in widescreen mode?
-	 * @returns bool
-	 */
-	function isWidescreen() {
-		return $gallery.hasClass( 'iswidescreen' );
 	}
 
 	/**
@@ -178,69 +101,6 @@
 	 */
 	function isMobile() {
 		return $gallery.hasClass( 'ismobile' );
-	}
-
-	/**
-	 * Expand the gallery
-	 */
-	function expand_full() {
-		$gallery.addClass( 'isfullscreen' );
-		$( '.toolbar-item.fullscreen .toolbar-text' ).text( GMR_Button_Text.exit_fullscreen );
-		$( '.gallery-toolbar' ).after( $caption );
-		$caption.addClass( 'slide-overlay-control' );
-		if ( 'Show Info' == $( '.toolbar-item.info .toolbar-text' ).text() ) {
-			$caption.hide();
-		}
-		$toolbar_thumbnails
-			.append( $slide_paging_previews )
-			.append( $slide_paging );
-		if ( is16x9() ) {
-			regroup_thumbnails( 15 );
-			update_thumbnails( $main.data("cycle.opts").currSlide );
-		}
-	}
-
-	/**
-	 * Collapse the gallery
-	 */
-	function collapse_full() {
-		$gallery.removeClass( 'isfullscreen' );
-		$( '.toolbar-item.fullscreen .toolbar-text' ).text( GMR_Button_Text.fullscreen );
-		$caption.removeClass( 'slide-overlay-control' ).show();
-		$sidebar
-			.prepend( $caption )
-			.prepend( $slide_paging )
-			.prepend( $slide_paging_previews );
-		if ( is16x9() ) {
-			regroup_thumbnails( 10 );
-			update_thumbnails( $main.data("cycle.opts").currSlide );
-		}
-	}
-
-	/**
-	 * Expand the gallery (widescreen)
-	 */
-	function expand_wide() {
-		$gallery.addClass( 'iswidescreen' );
-		$( '.toolbar-item.widescreen .toolbar-text' ).text( GMR_Button_Text.exit_widescreen );
-		$sidebar.before( $slide_paging_previews );
-		regroup_thumbnails( 8 );
-		update_thumbnails( $main.data("cycle.opts").currSlide );
-	}
-
-	/**
-	 * Collapse the gallery (widescreen)
-	 */
-	function collapse_wide() {
-		$gallery.removeClass( 'iswidescreen' );
-		$( '.toolbar-item.widescreen .toolbar-text' ).text( GMR_Button_Text.widescreen );
-		$sidebar.prepend( $slide_paging_previews );
-		if ( is16x9() ) {
-			regroup_thumbnails( 10 );
-		} else {
-			regroup_thumbnails( 15 );
-		}
-		update_thumbnails( $main.data("cycle.opts").currSlide );
 	}
 
 	/**
@@ -262,11 +122,6 @@
 
 		// If we're on a small screen but ismobile is not set, shift things around
 		if ( $window.width() < 768 && ! isMobile() ) {
-			if ( isFullscreen() ) {
-				collapse_full();
-			} else if ( isWidescreen() ) {
-				collapse_wide();
-			}
 			$gallery.addClass( 'ismobile' );
 			$sidebar.prepend( $caption );
 			regroup_thumbnails( get_thumbs_per_page() );
@@ -276,11 +131,6 @@
 		// If the window is being resized, adjust the thumbnail and main image height
 		if ( isMobile() ) {
 			var main_height, thumb_height;
-			if ( is16x9() ) {
-				main_height = Math.round( $main_wrapper.width() * 0.559 );
-			} else {
-				main_height = Math.round( $main_wrapper.width() * 0.729 );
-			}
 			$main.css( 'height', ( main_height - 20 ) + 'px' );
 			$main_wrapper.css( 'height', main_height );
 
@@ -335,12 +185,6 @@
 
 	function get_thumbs_per_page() {
 		var slides_per_page = 15;
-		if ( is16x9() && ! isFullscreen() ) {
-			slides_per_page = 10;
-		}
-		if ( isWidescreen() ) {
-			slides_per_page = 8;
-		}
 		if ( isMobile() ) {
 			slides_per_page = 4;
 		}
--- themes/greatermedia/includes/gallery/class-gmr-gallery.php
+++ themes/greatermedia/includes/gallery/class-gmr-gallery.php
@@ -95,16 +95,6 @@ class GreaterMediaGallery {
 			wp_enqueue_script( 'cycle-swipe', get_template_directory_uri() . '/assets/js/vendor/cycle2/jquery.cycle2.swipe.min.js', array( 'cycle' ), '20141007', true );
 			wp_enqueue_script( 'cycle-carousel', get_template_directory_uri() . '/assets/js/vendor/cycle2/jquery.cycle2.carousel.min.js', array( 'cycle' ), '20141007', true );
 			wp_register_script( 'gmr-gallery', get_template_directory_uri() . '/assets/js/src/greater_media_gallery.js', array( 'jquery' ), GREATERMEDIA_VERSION, true );
-			wp_localize_script( 'gmr-gallery', 'GMR_Button_Text', array(
-				'fullscreen'      => __( 'Fullscreen', 'greatermedia' ),
-				'exit_fullscreen' => __( 'Exit Fullscreen', 'greatermedia' ),
-				'widescreen'      => __( 'Widescreen', 'greatermedia' ),
-				'exit_widescreen' => __( 'Exit Widescreen', 'greatermedia' ),
-				'show_thumbnails' => __( 'Show Thumbnails', 'greatermedia' ),
-				'hide_thumbnails' => __( 'Hide Thumbnails', 'greatermedia' ),
-				'show_info'       => __( 'Show Info', 'greatermedia' ),
-				'hide_info'       => __( 'Hide Info', 'greatermedia' ),
-			) );
 			wp_enqueue_script( 'gmr-gallery' );
 
 			$main_post_id         = get_queried_object_id();
@@ -112,191 +102,135 @@ class GreaterMediaGallery {
 			$main_post_short_link = wp_get_shortlink( $main_post_id );
 
 			$thumbnails_per_page = 15;
-
 			$image_count_text = sprintf( __( 'Image %s of %s', 'greatermedia' ), '{{slideNum}}</span>', '{{slideCount}}' );
 			?>
 			<div class="gallery">
 				<div class="container">
-					<div class="gallery__slides cycle-slideshow"
-					     data-cycle-log="false"
-					     data-slides="> .slide"
-					     data-cycle-prev=".prev-img"
-					     data-cycle-next=".next-img"
-					     data-cycle-timeout="0"
-					     data-cycle-caption=".slide-paging-text"
-					     data-cycle-caption-template="<span class='highlight'><?php echo esc_attr( $image_count_text ); ?>"
-					     data-cycle-center-horz="true"
-					     data-cycle-center-vert="true"
-					     data-cycle-manual-speed="200"
-					     data-cycle-swipe="true"
-					     data-cycle-fx="scrollHorz"
-					     data-cycle-swipe-fx="scrollHorz"
-					     data-cycle-manual-fx="scrollHoriz">
-						<span class="prev-img slide-overlay-control-nohide"><span class="fa fa-angle-left"></span></span>
-						<span class="next-img slide-overlay-control-nohide"><span class="fa fa-angle-right"></span></span>
-						<?php
-						while ( $gallery->have_posts() ) {
-							$gallery->the_post();
-							$slide_hash = get_post_field( 'post_name', get_the_ID() );
-							$slide_link = get_permalink( $main_post_id ) . '#' . $slide_hash;
-
-							$attr = array(
-								'data-cycle-hash'           => get_post_field( 'post_name', get_the_ID() ),
-								'data-cycle-slide_shorturl' => $slide_link,
-								'data-cycle-slide_title'    => urlencode( get_the_title() ),
-							);
-							$image = wp_get_attachment_image_src( get_the_ID(), 'gmr-gallery', false );
-							$image_url = $image[0];
-							?>
-							<div class="gallery__slide--image"
-								<?php
-								foreach ( $attr as $attr_name => $attr_value ) {
-									echo $attr_name . '="' . esc_attr( $attr_value ) . '" ';
-								}
-								?>
-								 style="background-image: url(<?php echo esc_url( $image_url ); ?>);">
-							</div>
-						<?php
-
-						}
-						$gallery->rewind_posts();
-						?>
-					</div>
-					<div class="gallery-toolbar slide-overlay-control">
-						<?php /* removing for now, may be added later <div class="toolbar-item embed">
-					<a id="load-iframe" href="#iframe-source">
-						<span class="toolbar-icon fa fa-code"></span>
-						<span class="toolbar-text"><?php _e( 'Embed', 'greatermedia' ); ?></span>
-					</a>
-				</div> */ ?>
-						<div class="toolbar-item fullscreen">
-							<span class="toolbar-icon fa fa-arrows-alt"></span>
-							<span class="toolbar-text"><?php _e( 'Fullscreen', 'greatermedia' ); ?></span>
-						</div>
-						<div class="toolbar-item widescreen">
-							<span class="toolbar-icon fa fa-arrows-h"></span>
-							<span class="toolbar-text"><?php _e( 'Widescreen', 'greatermedia' ); ?></span>
-						</div>
-						<div class="toolbar-item share">
-							<span class="toolbar-icon fa fa-share-square-o"></span>
-							<span class="toolbar-text"><?php _ex( 'Share', 'verb', 'greatermedia' ); ?></span>
-							<div class="sharing-overlay">
-							<span class="sharing-option">
-								<input type="radio" name="what-to-share" id="share-image" checked="checked" />
-								<span class="fake-radio fa fa-check"></span>
-								<label for="share-image"><?php _e( 'Share image only', 'greatermedia' ); ?></label>
-							</span>
-							<span class="sharing-option">
-								<input type="radio" name="what-to-share" id="share-gallery" />
-								<span class="fake-radio"></span>
-								<label for="share-gallery"><?php _e( 'Share gallery', 'greatermedia' ); ?></label>
-							</span>
-								<div id="gallery-share-icons">
-									<a href="#" target="_blank" class="fa fa-twitter"></a>
-									<a href="#" target="_blank" class="fa fa-facebook"></a>
-									<a href="#" target="_blank" class="fa fa-linkedin"></a>
-								</div>
-								<input type="hidden" class="slide-url" value="" />
-								<input type="hidden" class="slide-title" value="" />
-								<input type="hidden" class="gallery-url" value="<?php echo esc_attr( $main_post_short_link ); ?>" />
-								<input type="hidden" class="gallery-title" value="<?php echo esc_attr( urlencode( $main_post_title ) ); ?>" />
-								<div class="short-url"></div>
-							</div>
-						</div>
-						<div class="toolbar-item thumbnails">
-							<span class="toolbar-icon fa fa-th-large"></span>
-							<span class="toolbar-text"><?php _e( 'Hide Thumbnails', 'greatermedia' ); ?></span>
-						</div>
-						<div class="toolbar-item info">
-							<span class="toolbar-icon fa fa-info-circle"></span>
-							<span class="toolbar-text"><?php _e( 'Hide Info', 'greatermedia' ); ?></span>
-						</div>
-						<div class="toolbar-thumbnails"></div>
-					</div>
-				</div>
-				<div class="sidebar">
-					<?php
-					$pager_text = sprintf( _x( 'Group %s', 'noun: group number', 'greatermedia' ), '{{slideNum}}' );
-					?>
-					<div class="slide-paging-previews cycle-slideshow"
-					     data-cycle-log="false"
-					     data-slides=".slide-previews-group"
-					     data-cycle-prev=".prev-group"
-					     data-cycle-next=".next-group"
-					     data-cycle-pager=".slide-group-pager"
-					     data-cycle-pager-template="<button class='btn btn-link indicator'><i class='dot'><?php echo esc_attr( $pager_text ); ?></i></button>"
-					     data-cycle-pager-active-class="current"
-					     data-cycle-timeout="0"
-					     data-cycle-manual-speed="200"
-					     data-cycle-swipe="true"
-					     data-cycle-fx="scrollHorz"
-					     data-cycle-swipe-fx="scrollHorz">
-
-						<div class="slide-previews-group">
+					<div class="gallery__slides">
+						<div class="gallery__slide--images cycle-slideshow"
+						     data-cycle-log="false"
+						     data-slides="> .gallery__slide--image"
+						     data-cycle-prev=".gallery__prev--btn"
+						     data-cycle-next=".gallery__next--btn"
+						     data-cycle-timeout="0"
+						     data-cycle-caption=".slide-paging-text"
+						     data-cycle-caption-template="<span class='highlight'><?php echo esc_attr( $image_count_text ); ?>"
+						     data-cycle-center-horz="true"
+						     data-cycle-center-vert="true"
+						     data-cycle-manual-speed="200"
+						     data-cycle-swipe="true"
+						     data-cycle-fx="scrollHorz"
+						     data-cycle-swipe-fx="scrollHorz"
+						     data-cycle-manual-fx="scrollHoriz">
 							<?php
-							$image_count = 0;
 							while ( $gallery->have_posts() ) {
 								$gallery->the_post();
-								$thumb_url = wp_get_attachment_image_src( get_the_ID(), 'gmr-gallery-thumbnail' );
+								$slide_hash = get_post_field( 'post_name', get_the_ID() );
+								$slide_link = get_permalink( $main_post_id ) . '#' . $slide_hash;
 
-								if ( $image_count > 0 && $image_count % $thumbnails_per_page == 0 ) {
-									echo '</div><div class="slide-previews-group">';
-								}
+								$attr = array(
+									'data-cycle-hash'           => get_post_field( 'post_name', get_the_ID() ),
+									'data-cycle-slide_shorturl' => $slide_link,
+									'data-cycle-slide_title'    => urlencode( get_the_title() ),
+								);
+								$image = wp_get_attachment_image_src( get_the_ID(), 'gmr-gallery', false );
+								$image_url = $image[0];
+								?>
+								<div class="gallery__slide--image"
+									<?php
+									foreach ( $attr as $attr_name => $attr_value ) {
+										echo $attr_name . '="' . esc_attr( $attr_value ) . '" ';
+									}
+									?>
+									 style="background-image: url(<?php echo esc_url( $image_url ); ?>);">
+								</div>
+							<?php
 
-								echo '<div id="preview-' . $image_count . '" style="background-image: url(' . $thumb_url[0] . ');" data-cycle-hash="' . get_post_field( 'post_name', get_the_ID() ) . '" data-cycle-index="' . $image_count . '"></div>';
-								$image_count++;
 							}
 							$gallery->rewind_posts();
 							?>
 						</div>
 					</div>
-					<div class="slide-paging">
-						<span class="slide-paging-text"></span>
-						<div class="slide-paging-arrows carousel-controls">
-							<button type="button" class="btn btn-link btn-left prev-group"><i class="arrow-left"><?php _ex( 'Left', 'direction', 'greatermedia' ); ?></i></button>
-							<button type="button" class="btn btn-link btn-right next-group"><i class="arrow-right"><?php _ex( 'Right', 'direction', 'greatermedia' ); ?></i></button>
-							<div class="slide-group-pager"></div>
+					<div class="gallery__meta">
+						<div class="gallery__prev">
+							<button type="button" class="gallery__prev--btn slide-overlay-control-nohide"><span class="fa fa-angle-left"><?php _e( 'Prev', 'greatermedia'); ?></span></button>
 						</div>
-					</div>
-					<div class="caption cycle-slideshow"
-					     data-cycle-log="false"
-					     data-cycle-slides="> div"
-					     data-cycle-prev=".prev"
-					     data-cycle-next=".next"
-					     data-cycle-timeout="0"
-					     data-cycle-auto-height="false"
-					     data-cycle-manual-speed="1">
-						<?php
-						while ( $gallery->have_posts() ){
-							$gallery->the_post();
-							$slide_hash = get_post_field( 'post_name', get_the_ID() );
-							echo '<div data-cycle-hash="' . $slide_hash . '">';
-							?>
+						<div class="gallery__content cycle-slideshow"
+						     data-cycle-log="false"
+						     data-cycle-slides="> div"
+						     data-cycle-prev=".gallery__prev--btn"
+						     data-cycle-next=".gallery__next--btn"
+						     data-cycle-timeout="0"
+						     data-cycle-auto-height="false"
+						     data-cycle-manual-speed="1">
 							<?php
-							the_excerpt();
-							echo '</div>';
-						}
-						$gallery->rewind_posts();
-						?>
-					</div>
-				</div> <!-- / gallery sidebar -->
-				<?php wp_reset_postdata(); ?>
-				<div class="slideshow-info">
-					<h1><?php the_title(); ?></h1>
-					<div class="meta-social">
-						<time datetime="<?php the_time( 'c' ); ?>"><?php the_date(); ?></time>
-						<span class="byline"><?php the_author(); ?></span>
-						<a href="<?php the_permalink(); ?>#disqus_thread" class="comments comments-number">Comments</a>
-
-						<div class="article-share">
-							<?php
-							if ( function_exists( 'sharing_display' ) ) {
-								echo sharing_display();
+							while ( $gallery->have_posts() ){
+								global $post;
+								$gallery->the_post();
+								$slide_hash = get_post_field( 'post_name', get_the_ID() );
+								$image_title = get_the_title( $post ); // title of the gallery image
+								echo '<div data-cycle-hash="' . $slide_hash . '">';
+								echo '<div class="gallery__title">';
+								echo $image_title;
+								echo '</div>';
+								echo '</div>';
 							}
+							$gallery->rewind_posts();
 							?>
 						</div>
-
+						<div class="slide-paging">
+							<span class="slide-paging-text"></span>
+						</div>
+						<div class="gallery__next">
+							<button type="button" class="gallery__next--btn slide-overlay-control-nohide"><span class="fa fa-angle-right"><?php _e( 'Next', 'greatermedia'); ?></span></button>
+						</div>
 					</div>
+					<div class="gallery__thumbnails">
+						<?php
+						$pager_text = sprintf( _x( 'Group %s', 'noun: group number', 'greatermedia' ), '{{slideNum}}' );
+						?>
+						<div class="slide-paging-previews cycle-slideshow"
+						     data-cycle-log="false"
+						     data-slides=".slide-previews-group"
+						     data-cycle-prev=".prev-group"
+						     data-cycle-next=".next-group"
+						     data-cycle-pager=".slide-group-pager"
+						     data-cycle-pager-template="<button class='btn btn-link indicator'><i class='dot'><?php echo esc_attr( $pager_text ); ?></i></button>"
+						     data-cycle-pager-active-class="current"
+						     data-cycle-timeout="0"
+						     data-cycle-manual-speed="200"
+						     data-cycle-swipe="true"
+						     data-cycle-fx="scrollHorz"
+						     data-cycle-swipe-fx="scrollHorz">
+
+							<div class="slide-previews-group">
+								<?php
+								$image_count = 0;
+								while ( $gallery->have_posts() ) {
+									$gallery->the_post();
+									$thumb_url = wp_get_attachment_image_src( get_the_ID(), 'gmr-gallery-thumbnail' );
+
+									if ( $image_count > 0 && $image_count % $thumbnails_per_page == 0 ) {
+										echo '</div><div class="slide-previews-group">';
+									}
+
+									echo '<div id="preview-' . $image_count . '" style="background-image: url(' . $thumb_url[0] . ');" data-cycle-hash="' . get_post_field( 'post_name', get_the_ID() ) . '" data-cycle-index="' . $image_count . '"></div>';
+									$image_count++;
+								}
+								$gallery->rewind_posts();
+								?>
+							</div>
+						</div>
+						<div class="slide-paging">
+							<div class="slide-paging-arrows carousel-controls">
+								<button type="button" class="btn btn-link btn-left prev-group"><i class="arrow-left"><?php _ex( 'Left', 'direction', 'greatermedia' ); ?></i></button>
+								<button type="button" class="btn btn-link btn-right next-group"><i class="arrow-right"><?php _ex( 'Right', 'direction', 'greatermedia' ); ?></i></button>
+								<div class="slide-group-pager"></div>
+							</div>
+						</div>
+					</div> <!-- / gallery sidebar -->
+
+					<?php wp_reset_postdata(); ?>
 				</div>
 			</div><!-- / gallery -->
 		<?php
