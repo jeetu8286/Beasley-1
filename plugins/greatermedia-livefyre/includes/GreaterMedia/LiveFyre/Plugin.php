0a07dc9b0c96f4499b05c6497e847e0599c8a8bc
Darshan Sawardekar <darshan@10up.com>
2014-12-16 15:19:12 +0530

    Adds LiveFyre settings page for Network and Site keys.
    * WIP: May become a network settings page.

--- plugins/greatermedia-livefyre/includes/GreaterMedia/LiveFyre/Plugin.php
+++ plugins/greatermedia-livefyre/includes/GreaterMedia/LiveFyre/Plugin.php
@@ -5,7 +5,29 @@ namespace GreaterMedia\LiveFyre;
 class Plugin {
 
 	function enable() {
-		error_log( 'GreaterMedia\LiveFyre\Plugin enabled' );
+		add_action( 'admin_menu', array( $this, 'initialize_admin_menu' ) );
+
+		if ( $this->is_ajax_request() ) {
+			$this->register_ajax_handlers();
+		}
+	}
+
+	function initialize_admin_menu() {
+		$settings_page = new Settings\Page();
+		$settings_page->register();
+	}
+
+	function register_ajax_handlers() {
+		$handlers   = array();
+		$handlers[] = new Ajax\ChangeLiveFyreSettings();
+
+		foreach ( $handlers as $handler ) {
+			$handler->register();
+		}
+	}
+
+	function is_ajax_request() {
+		return defined( 'DOING_AJAX' ) && DOING_AJAX;
 	}
 
 }
