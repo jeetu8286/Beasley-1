5b3c3fd3923cc569fa5270a9c4f4e42c6b44e583
Chris Marslender <chrismarslender@gmail.com>
2014-12-16 12:59:33 -0700

    Allow ordering and adding/deleting galleries from the album page

--- plugins/greatermedia-galleries/includes/album-metaboxes.php
+++ plugins/greatermedia-galleries/includes/album-metaboxes.php
@@ -44,6 +44,20 @@ class GreaterMediaGalleryAlbumMetaboxes {
 	 */
 	public static function render_gallery_metabox( \WP_Post $post ) {
 		wp_nonce_field( 'save_album_galleries', 'album_gallery_nonce' );
+
+		$limit = 1000; // Arbitrary high limit, since we need something here
+
+		$options = array(
+			'args' => array(
+				'post_type' => GreaterMediaGalleryCPT::GALLERY_POST_TYPE,
+				'post_parent__in' => array( 0, $post->ID ), // Galleries with no parent, or with THIS parent
+			),
+			'limit' => $limit,
+		);
+
+		$children_ids = self::get_child_gallery_ids( $post->ID );
+
+		pf_render( 'gmr_album_galleries', implode( ',', $children_ids ), $options );
 	}
 
 	/**
@@ -56,6 +70,44 @@ class GreaterMediaGalleryAlbumMetaboxes {
 			return;
 		}
 
+		if ( ! isset( $_POST['gmr_album_galleries'] ) ) {
+			return;
+		}
+
+		$old_gallery_ids = self::get_child_gallery_ids( $post_id );
+		$gallery_ids = array_map( 'intval', explode( ',', $_POST['gmr_album_galleries'] ) );
+
+		// Get IDs of galleries that were deleted, by figuring out what ids are in the old array and not in the new array
+		$deleted_galleries = array_diff( $old_gallery_ids, $gallery_ids );
+		foreach( $deleted_galleries as $deleted_gallery_id ) {
+			$deleted_gallery_post = get_post( $deleted_gallery_id );
+			$deleted_gallery_post->post_parent = 0;
+			wp_update_post( $deleted_gallery_post );
+		}
+
+		// Loop through the new posts, and update the menu order and post parent on all of them
+		$count = 0;
+		foreach( $gallery_ids as $gallery_id ) {
+			$count++;
+			$gallery_post = get_post( $gallery_id );
+			$gallery_post->post_parent = $post_id;
+			$gallery_post->menu_order = $count;
+			wp_update_post( $gallery_post );
+		}
+	}
+
+	public static function get_child_gallery_ids( $post_id ) {
+		$children_args = array(
+			'post_type' => GreaterMediaGalleryCPT::GALLERY_POST_TYPE,
+			'post_parent' => $post_id,
+			'fields' => 'ids',
+			'limit' => 1000,
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+		);
+		$children_ids = get_posts( $children_args );
+
+		return $children_ids;
 	}
 
 }
