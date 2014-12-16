1e50f997136abd4e03895d010bdc4ce505a45ffe
Chris Marslender <chrismarslender@gmail.com>
2014-12-15 20:28:40 -0700

    Start moving galleries to plugin - Move CPTs

--- plugins/greatermedia-galleries/includes/post-types.php
+++ plugins/greatermedia-galleries/includes/post-types.php
@@ -1,23 +1,19 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die( "Please don't try to access this file directly." );
-}
-
 /**
  * Class GreaterMediaGalleryCPT
  */
 class GreaterMediaGalleryCPT {
 
+	const ALBUM_POST_TYPE = 'gmr_album';
 	const GALLERY_POST_TYPE = 'gmr_gallery';
 
 	/**
 	 * Hook into the appropriate actions when the class is initiated.
 	 */
 	public static function init() {
-
 		add_action( 'init', array( __CLASS__, 'gallery_cpt' ), 0 );
-
+		add_action( 'init', array( __CLASS__, 'album_cpt' ), 0 );
 	}
 
 	/**
@@ -68,6 +64,53 @@ class GreaterMediaGalleryCPT {
 			'capability_type'     => 'post',
 		);
 		register_post_type( self::GALLERY_POST_TYPE, $args );
+
+	}
+
+	public static function album_cpt() {
+
+		$labels = array(
+			'name'                => _x( 'Albums', 'Post Type General Name', 'greatermedia' ),
+			'singular_name'       => _x( 'Album', 'Post Type Singular Name', 'greatermedia' ),
+			'menu_name'           => __( 'Albums', 'greatermedia' ),
+			'parent_item_colon'   => __( 'Parent Album:', 'greatermedia' ),
+			'all_items'           => __( 'All Albums', 'greatermedia' ),
+			'view_item'           => __( 'View Album', 'greatermedia' ),
+			'add_new_item'        => __( 'Add New Album', 'greatermedia' ),
+			'add_new'             => __( 'Add New', 'greatermedia' ),
+			'edit_item'           => __( 'Edit Album', 'greatermedia' ),
+			'update_item'         => __( 'Update Album', 'greatermedia' ),
+			'search_items'        => __( 'Search Albums', 'greatermedia' ),
+			'not_found'           => __( 'Not found', 'greatermedia' ),
+			'not_found_in_trash'  => __( 'Not found in Trash', 'greatermedia' ),
+		);
+		$rewrite = array(
+			'slug'                => 'album',
+			'with_front'          => true,
+			'pages'               => true,
+			'feeds'               => true,
+		);
+		$args = array(
+			'label'               => __( 'album', 'greatermedia' ),
+			'description'         => __( 'A post type for Albums', 'greatermedia' ),
+			'labels'              => $labels,
+			'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', ),
+			'taxonomies'          => array( 'post_tag' ),
+			'hierarchical'        => true,
+			'public'              => true,
+			'show_ui'             => true,
+			'show_in_menu'        => 'edit.php?post_type=' . SELF::GALLERY_POST_TYPE,
+			'show_in_nav_menus'   => true,
+			'show_in_admin_bar'   => true,
+			'menu_icon'           => 'dashicons-format-gallery',
+			'can_export'          => true,
+			'has_archive'         => true,
+			'exclude_from_search' => false,
+			'publicly_queryable'  => true,
+			'rewrite'             => $rewrite,
+			'capability_type'     => 'post',
+		);
+		register_post_type( self::ALBUM_POST_TYPE, $args );
 
 	}
 
--- themes/greatermedia/includes/gallery/loader.php
+++ themes/greatermedia/includes/gallery/loader.php
@@ -1,4 +1,3 @@
 <?php
 
-include __DIR__ . '/class-gmr-gallery-cpt.php';
 include __DIR__ . '/class-gmr-gallery.php';
