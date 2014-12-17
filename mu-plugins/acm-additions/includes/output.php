6ea343acd0e953e12b312f21fe298a98ab74eb50
Chris Marslender <chrismarslender@gmail.com>
2014-12-16 19:44:53 -0700

    Add render code for the ad slots

--- mu-plugins/acm-additions/includes/output.php
+++ mu-plugins/acm-additions/includes/output.php
@@ -2,18 +2,41 @@
 
 namespace GreaterMedia\AdCodeManager;
 
-add_action( 'wp_head', __NAMESPACE__ . '\wp_head' );
+add_action( 'wp_footer', __NAMESPACE__ . '\load_js' );
 add_filter( 'acm_output_html', __NAMESPACE__ . '\render_tag', 15, 2 );
 
-function wp_head() {
-	// todo output any JS needed for rendering ads
+function load_js() {
+	?>
+	<script type="text/javascript" src="//ox-d.greatermedia.com/w/1.0/jstag"></script>
+	<?php
 }
 
 function render_tag( $output_html, $tag_id ) {
+	static $random_number;
+
+	if ( is_null( $random_number ) ) {
+		$random_number =  str_pad( rand( 0, 999999999999999 ), 15, rand( 0, 9 ), STR_PAD_LEFT );
+	}
 	ob_start();
 
 	?>
-	<pre><img src="http://placehold.it/300x250"/></pre>
+	<div id="%openx_id%_%tag%">
+		<noscript>
+			<iframe id="9ee0446165" name="9ee0446165" src="//ox-d.greatermedia.com/w/1.0/afr?auid=%openx_id%&cb=<?php echo intval( $random_number ) ?>" frameborder="0" scrolling="no">
+				<a href="http://ox-d.greatermedia.com/w/1.0/rc?cs=9ee0446165&cb=<?php echo intval( $random_number ); ?>" >
+					<img src="//ox-d.greatermedia.com/w/1.0/ai?auid=%openx_id%&cs=9ee0446165&cb=<?php echo intval( $random_number ); ?>" border="0" alt="">
+				</a>
+			</iframe>
+		</noscript>
+	</div>
+	<script type="text/javascript">
+		var OX_ads = OX_ads || [];
+		OX_ads.push({
+			slot_id: "%openx_id%_%tag%",
+			auid: "%openx_id%"
+		});
+	</script>
+
 	<?php
 
 	$output = ob_get_clean();
