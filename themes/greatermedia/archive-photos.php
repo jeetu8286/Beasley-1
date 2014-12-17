fb38a5913e2149b932c5ed48c0984e180ea659ab
Chris Marslender <chrismarslender@gmail.com>
2014-12-16 18:43:33 -0700

    Add some markup to the archive-photos template

--- themes/greatermedia/archive-photos.php
+++ themes/greatermedia/archive-photos.php
@@ -1,3 +1,61 @@
 <?php
+/**
+ * Show post type archive template
+ *
+ */
 
-die( 'loaded photos archive template' );
+get_header(); ?>
+
+	<main class="main" role="main">
+
+		<div class="container">
+
+			<section class="content">
+
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+					<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+						<header class="entry-header">
+
+							<h2 class="entry-title" itemprop="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+						</header>
+
+						<div class="entry-content">
+
+							<?php the_content(); ?>
+
+						</div>
+
+					</article>
+
+				<?php endwhile; ?>
+
+				<?php else : ?>
+
+					<article id="post-not-found" class="hentry cf">
+
+						<header class="article-header">
+
+							<h1><?php _e( 'Oops, Post Not Found!', 'greatermedia' ); ?></h1>
+
+						</header>
+
+						<section class="entry-content">
+
+							<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'greatermedia' ); ?></p>
+
+						</section>
+
+					</article>
+
+				<?php endif; ?>
+
+			</section>
+
+		</div>
+
+	</main>
+
+<?php get_footer(); ?>
