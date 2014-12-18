72165652f844f1bace945cebfeba2d0dbb509b49
Allen Moore <allen@10up.com>
2014-12-18 15:35:44 -0500

    fix height issue when the ad stops

--- plugins/greatermedia-live-player/includes/class-gmlp-player.php
+++ plugins/greatermedia-live-player/includes/class-gmlp-player.php
@@ -9,7 +9,6 @@ class GMLP_Player {
 	public static function init() {
 
 		add_action( 'wp_footer', array( __CLASS__, 'load_js' ), 50 );
-		add_action( 'wp_footer', array( __CLASS__, 'render_player_container' ), 50 );
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ), 50 );
 		add_action( 'gm_live_player', array( __CLASS__, 'render_player' ) );
 		add_action( 'radio_callsign', array( __CLASS__, 'get_radio_callsign' ) );
@@ -38,21 +37,13 @@ class GMLP_Player {
 				<div id="pauseButton" class="live-stream__btn--pause"></div>
 				<div id="resumeButton" class="live-stream__btn--resume"></div>
 			</div>
+			<div id="live-stream__container" class="live-stream__container">
+				<div id="td_container" class="live-stream__container--player"></div>
+			</div>
+
 		</div>
 
 	<?php
-
-	}
-
-	/**
-	 * The player container is where pre-roll videos and such show. Moving this outside of the player itself and calling
-	 * this in wp_footer will allow the pre-roll to be a overlay while playing.
-	 */
-	public static function render_player_container() {
-
-		echo '<div id="live-stream__container" class="live-stream__container">';
-		echo '<div id="td_container" class="live-stream__container--player"></div>';
-		echo '</div>';
 
 	}
 
