d62d08554e454e5e207485c456adbcd883d46cbc
Chris Marslender <chrismarslender@gmail.com>
2014-12-19 00:20:41 -0700

    Add extra markup before/after default core shortcode, to allow for our custom styling. Check for audio support. If we have it, set up audio element, and listen for clicks on our buttons. If not, fall back to default mediaelement

--- plugins/greatermedia-live-player/assets/js/greater_media_live_player.js
+++ plugins/greatermedia-live-player/assets/js/greater_media_live_player.js
@@ -13,12 +13,16 @@
 		pauseButton = $('#pauseButton');
 
 
+	var enablePjax = function() {
+		$(document).pjax('a:not(.ab-item)', 'section.content', {'fragment': 'section.content', 'maxCacheLength': 500, 'timeout' : 5000});
+	};
+
 	playButton.on('click', function(event) {
 		event.preventDefault();
 		// add gif file for testing
 		// call pjax to update container
 		if( !gmlp.logged_in ) {
-			$(document).pjax('a:not(.ab-item)', 'section.content', {'fragment': 'section.content', 'maxCacheLength': 500, 'timeout' : 5000});
+			enablePjax();
 		}
 
 	});
@@ -26,5 +30,47 @@
 	pauseButton.on('click', function(event) {
 		event.preventDefault();
 	});
+
+
+	// For "inline" audio support
+	if ( "undefined" !== typeof Modernizr && Modernizr.audio ) {
+		var inlineAudio = new Audio(),// todo Make sure to check compatibility first, or else fall back to media element!
+			content = document.querySelectorAll( '.content'),
+			$content = $(content); // Because getElementsByClassName is not supported in IE8 ಠ_ಠ
+
+		var resetStates = function() {
+			$('.podcast__btn--play.playing').removeClass('playing');
+			$('.podcast__btn--pause.playing').removeClass('playing');
+		};
+
+		inlineAudio.addEventListener( 'ended', resetStates );
+
+		$content.on('click', '.podcast__btn--play', function(e) {
+			var $play = $(e.currentTarget),
+				$pause = $play.parent().find('.podcast__btn--pause');
+
+			inlineAudio.src = $play.attr('data-mp3-src');
+			inlineAudio.play();
+
+			resetStates();
+
+			$play.addClass('playing');
+			$pause.addClass('playing');
+
+			enablePjax();
+		});
+
+		$content.on('click', '.podcast__btn--pause', function(e) {
+			inlineAudio.pause();
+
+			resetStates();
+		});
+	} else {
+		var $meFallbacks = $('.gmr-mediaelement-fallback audio'),
+			$customInterfaces = $('.podcast__play');
+
+		$meFallbacks.mediaelementplayer();
+		$customInterfaces.hide();
+	}
 
 } )(jQuery,window);
---
+++ plugins/greatermedia-live-player/includes/audio-shortcodes.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Class GMLP_Player
+ */
+class GMR_Audio_Shortcodes {
+
+	public static function init() {
+		add_filter( 'wp_audio_shortcode', array( __CLASS__, 'custom_audio_styling' ), 10, 5 );
+	}
+
+	public static function custom_audio_styling( $html, $atts, $audio, $post_id, $library ) {
+		if ( is_admin() ) {
+			return $html;
+		}
+
+		/*
+		 * Spec supports mp3 only, as do the browsers we're trying to use audio element with.
+		 * Anything else will just use media element, rather than the live player
+		 * support could be expanded later if necessary, checking types with wp_get_audio_extensions() to know supported
+		 * audio types in core, but we'd likely need better browser support, or some fixes to mediaelement so that
+		 * it works better (at all) when not attached to a real element in the visible DOM
+		 */
+		if ( ! isset( $atts['mp3'] ) || empty( $atts['mp3'] ) ) {
+			$new_html = '<div class="gmr-mediaelement">';
+			$new_html .= $html;
+			$new_html .= '</div>';
+
+			return $new_html;
+		}
+
+		$mp3_src = $atts['mp3'];
+		if ( ! function_exists( 'wp_read_audio_metadata' ) ) {
+			include_once trailingslashit( ABSPATH ) . 'wp-admin/includes/media.php';
+		}
+
+		$metadata_defaults = array(
+			'title' => '',
+			'length_formatted' => '',
+		);
+
+		if ( function_exists( 'wp_read_audio_metadata' ) ) {
+			$fileinfo = parse_url( $mp3_src );
+			$file_path = ABSPATH . $fileinfo['path'];
+			$metadata = wp_read_audio_metadata( $file_path );
+			$metadata = wp_parse_args( $metadata, $metadata_defaults );
+		} else {
+			$metadata = $metadata_defaults;
+		}
+
+		ob_start();
+		?>
+		<div class="podcast__play">
+			<button class="podcast__btn--play" data-mp3-src="<?php echo esc_attr( $mp3_src );?>" data-mp3-title="<?php echo esc_attr( $metadata['title'] ); ?>"></button>
+			<button class="podcast__btn--pause"></button>
+			<span class="podcast__runtime"><?php echo esc_html( $metadata['length_formatted'] ); ?></span>
+		</div>
+		<div class="gmr-mediaelement-fallback"><?php echo $html; ?></div>
+
+		<?php
+		$new_html = ob_get_clean();
+		return $new_html;
+	}
+
+}
+
+GMR_Audio_Shortcodes::init();
