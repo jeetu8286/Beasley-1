f174db102a2f57bc684887abae0814a22c06d911
Chris Marslender <chrismarslender@gmail.com>
2014-12-19 13:53:03 -0700

    Block API user from admin as well

--- mu-plugins/restrict-api-user.php
+++ mu-plugins/restrict-api-user.php
@@ -5,7 +5,7 @@
  * @todo This needs to be full-on roles/caps before launch
  */
 
-add_filter( 'restricted_site_access_is_restricted', function( $is_restricted, $wp ) {
+function gmr_restrict_api_user_access() {
 	$user = wp_get_current_user();
 
 	$api_user_name = 'nowplayingwmgk';
@@ -13,6 +13,12 @@ add_filter( 'restricted_site_access_is_restricted', function( $is_restricted, $w
 	if ( $api_user_name == $user->data->user_login ) {
 		wp_die("The current user is only authorized for API access");
 	}
+}
+
+add_filter( 'restricted_site_access_is_restricted', function( $is_restricted, $wp ) {
+	gmr_restrict_api_user_access();
 
 	return $is_restricted;
 }, 10, 2 );
+
+add_filter( 'admin_init', 'gmr_restrict_api_user_access' );
