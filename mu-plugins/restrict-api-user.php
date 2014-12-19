d19bf5de0ce400c1be3c462c817b32655c7b6508
Chris Marslender <chrismarslender@gmail.com>
2014-12-19 14:15:32 -0700

    Ok - not working, clearly

--- mu-plugins/restrict-api-user.php
+++ mu-plugins/restrict-api-user.php
@@ -14,11 +14,3 @@ function gmr_restrict_api_user_access() {
 		wp_die("The current user is only authorized for API access");
 	}
 }
-
-add_filter( 'restricted_site_access_is_restricted', function( $is_restricted, $wp ) {
-	gmr_restrict_api_user_access();
-
-	return $is_restricted;
-}, 10, 2 );
-
-add_filter( 'admin_init', 'gmr_restrict_api_user_access' );
