b0cf522aa797374b25987e80d268a6c49ef9469b
Eugene Manuilov <eugene.manuilov@10up.com>
2014-12-22 17:09:17 +0200

    Fixed social share buttons available at contest page.

--- themes/greatermedia/single-contest.php
+++ themes/greatermedia/single-contest.php
@@ -29,12 +29,14 @@ get_header(); ?>
 							<section class="col__inner--left">
 
 								<header class="entry__header">
+									<?php $encoded_permalink = urlencode( get_permalink() ); ?>
+									<?php $encoded_title = urlencode( get_the_title() ); ?>
 
 									<time class="entry__date" datetime="<?php echo get_the_time(); ?>"><?php the_date('F j'); ?></time>
 									<h2 class="entry__title" itemprop="headline"><?php the_title(); ?></h2>
-									<a class="icon-facebook social-share-link" href="http://www.facebook.com/sharer/sharer.php?u=[URL]&title=[TITLE]"></a>
-									<a class="icon-twitter social-share-link" href="http://twitter.com/home?status=[TITLE]+[URL]"></a>
-									<a class="icon-google-plus social-share-link" href="https://plus.google.com/share?url=[URL]"></a>
+									<a class="icon-facebook social-share-link" href="http://www.facebook.com/sharer/sharer.php?u=<?php echo $encoded_permalink; ?>&title=<?php echo $encoded_title; ?>"></a>
+									<a class="icon-twitter social-share-link" href="http://twitter.com/home?status=<?php echo $encoded_title; ?>+<?php echo $encoded_permalink; ?>"></a>
+									<a class="icon-google-plus social-share-link" href="https://plus.google.com/share?url=<?php echo $encoded_permalink; ?>"></a>
 
 								</header>
 
