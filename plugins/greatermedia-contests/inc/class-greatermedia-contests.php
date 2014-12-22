24a8359a0d9325af34964dd1fbc9a08728b0418f
Eugene Manuilov <eugene.manuilov@10up.com>
2014-12-22 20:27:32 +0200

    Added contest menu icon.

--- plugins/greatermedia-contests/inc/class-greatermedia-contests.php
+++ plugins/greatermedia-contests/inc/class-greatermedia-contests.php
@@ -93,6 +93,7 @@ class GreaterMediaContests {
 			'show_in_nav_menus'   => true,
 			'show_in_admin_bar'   => true,
 			'menu_position'       => 5,
+			'menu_icon'           => 'dashicons-forms',
 			'can_export'          => true,
 			'has_archive'         => true,
 			'exclude_from_search' => false,
