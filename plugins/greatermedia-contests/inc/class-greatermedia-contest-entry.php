0ce534fb50f593ba8352c3e9caf07b43d52391a9
Eugene Manuilov <eugene.manuilov@10up.com>
2014-12-23 13:18:24 +0200

    Fixed contest entry creation issue.

--- plugins/greatermedia-contests/inc/class-greatermedia-contest-entry.php
+++ plugins/greatermedia-contests/inc/class-greatermedia-contest-entry.php
@@ -55,7 +55,11 @@ class GreaterMediaContestEntry {
 	 */
 	public function save() {
 
-		$post_id = wp_update_post( $this->post, true );
+		if ( empty( $this->post->ID ) ) {
+			$post_id = wp_insert_post( $this->post, true );
+		} else {
+			$post_id = wp_update_post( $this->post, true );
+		}
 
 		// Refresh the data
 		$this->post = get_post( $post_id );
