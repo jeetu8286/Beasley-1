1b9c3307c94636328049139a27221a4b02209c5e
Eugene Manuilov <eugene.manuilov@10up.com>
2014-12-25 11:46:32 +0200

    Fixed contest form submission issue which appeared when a form contained a dropdown element.

--- plugins/greatermedia-contests/js/greatermedia-contests.js
+++ plugins/greatermedia-contests/js/greatermedia-contests.js
@@ -1,65 +1,56 @@
-jQuery(function () {
+(function ($) {
+	var __ready = function() {
+		$('.' + GreaterMediaContests.form_class).submit(function() {
+			var form = $(this);
 
-	// Attach a submit handler to contest forms
-	jQuery('.' + GreaterMediaContests.form_class).submit(function (e) {
-
-		var form = this;
-		e.preventDefault();
-		if (!jQuery(this).parsley || jQuery(this).parsley().isValid()) {
-
-			// Marshall the data
-			var form_data = new FormData();
-			jQuery(this).find('input').each(function () {
-				var input = this;
-				if ('file' === input.type) {
-					jQuery(this.files).each(function (key, value) {
-						form_data.append(input.name, value);
-					});
-				}
-				else {
-					form_data.append(input.name, input.value);
-				}
-			});
-
-			jQuery(this).find('textarea').each(function () {
-				form_data.append(this.name, this.value);
-			});
-
-			// Submit the form via AJAX
-			jQuery.ajax({
-				url        : GreaterMediaContests.ajax_url,
-				type       : 'post',
-				data       : form_data,
-				processData: false, // Don't process the files
-				contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-				dataType   : 'json',
-				success    : function (data, textStatus, jqXHR) {
-
-					if ('success' === textStatus && data.data.message) {
-						var wrapper = document.createElement('p');
-						wrapper.class = 'contest_thank_you';
-						wrapper.innerText = data.data.message;
-						jQuery(form).replaceWith(wrapper);
+			if (!form.parsley || form.parsley().isValid()) {
+				var form_data = new FormData();
+				
+				form.find('input').each(function() {
+					var input = this;
+					if ('file' === input.type) {
+						$(this.files).each(function(key, value) {
+							form_data.append(input.name, value);
+						});
 					}
+					else {
+						form_data.append(input.name, input.value);
+					}
+				});
 
-				}
-			});
+				form.find('textarea, select').each(function() {
+					form_data.append(this.name, this.value);
+				});
 
+				$.ajax({
+					url: GreaterMediaContests.ajax_url,
+					type: 'post',
+					data: form_data,
+					processData: false, // Don't process the files
+					contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+					dataType: 'json',
+					success: function(data, textStatus, jqXHR) {
+						if ('success' === textStatus && data.data.message) {
+							var wrapper = document.createElement('p');
+							wrapper.class = 'contest_thank_you';
+							wrapper.innerText = data.data.message;
+							form.replaceWith(wrapper);
+						}
+					}
+				});
+			}
 
-		}
+			return false;
+		});
+	};
 
-		return false;
-
-	});
-
-});
+	$(document).bind('pjax:end', __ready).ready(__ready);
+})(jQuery);
 
 /**
  * Set up date pickers for browsers without a native control
  */
-document.addEventListener(
-	"DOMContentLoaded",
-	function () {
+document.addEventListener("DOMContentLoaded", function () {
 		var supported_input_types = supported_input_types();
 
 		/**
