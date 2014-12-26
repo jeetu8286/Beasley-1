e7742144e5c51cb065d09edba80855708164e27d
Dave Ross <dave.ross@10up.com>
2014-12-26 17:52:27 -0500

    Pass a relative URL as a "dest" to gigya_profile_path('login') in the logged-in content plugin

--- plugins/greatermedia-login-restricted-content/inc/class-greatermedia-login-restricted-content.php
+++ plugins/greatermedia-login-restricted-content/inc/class-greatermedia-login-restricted-content.php
@@ -267,15 +267,16 @@ class GreaterMediaLoginRestrictedContent extends VisualShortcode {
 		global $post, $wp;
 
 		$login_restriction = self::sanitize_login_restriction( get_post_meta( $post->ID, '_post_login_restriction', true ) );
-		$current_url       = home_url( add_query_arg( array(), $wp->request ) );
+		$current_url = '/' . trim( $wp->request, '/' );
+
 
 		if ( ( 'logged-in' === $login_restriction ) && ! is_gigya_user_logged_in() ) {
-			$login_url = gigya_profile_path( 'login', array( 'dest' => $current_url ) );
+			$login_url   = gigya_profile_path( 'login', array( 'dest' => $current_url ) );
 			include GREATER_MEDIA_LOGIN_RESTRICTED_CONTENT_PATH . '/tpl/login-restricted-post-render.tpl.php';
 
 			return;
 		} elseif ( ( 'logged-out' === $login_restriction ) && is_gigya_user_logged_in() ) {
-			$logout_url = gigya_profile_path( 'logout', array( 'dest' => $current_url ) );
+			$logout_url   = gigya_profile_path( 'logout', array( 'dest' => $current_url ) );
 			include GREATER_MEDIA_LOGIN_RESTRICTED_CONTENT_PATH . '/tpl/logout-restricted-post-render.tpl.php';
 
 			return;
