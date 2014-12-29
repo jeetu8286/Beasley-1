aea37d7a263b014945a844f2434590c308e95fad
Dave Ross <dave.ross@10up.com>
2014-12-29 15:02:21 -0500

    More verbose message on age-restricted content "shield"

--- plugins/greatermedia-age-restricted-content/tpl/age-restricted-post-render.tpl.php
+++ plugins/greatermedia-age-restricted-content/tpl/age-restricted-post-render.tpl.php
@@ -1,3 +1,13 @@
-<span class="login-restricted-shield" id="login-restricted-shield-<?php echo esc_attr($post->ID); ?>">
-	<div>Please <a href="<?php echo esc_url($login_url); ?>"><?php _e('sign in'); ?></a> to view this content</div>
+<span class="login-restricted-shield" id="login-restricted-shield-<?php echo esc_attr( $post->ID ); ?>">
+	<p>You must be at least
+		<?php if ( '18plus' === $age_restriction ) : ?>
+			18
+		<?php elseif ( '21plus' === $age_restriction ) : ?>
+			21
+		<?php endif; ?>
+		years old to view this content.
+		<?php if ( ! is_gigya_user_logged_in()) : ?>
+			Please <a href="<?php echo esc_url( $login_url ); ?>"><?php _e( 'sign in' ); ?></a> to continue.
+		<?php endif; ?>
+	</p>
 </span>
