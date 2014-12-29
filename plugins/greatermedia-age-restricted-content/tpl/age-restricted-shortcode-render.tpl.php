3b7bbe5041e239c2cf7dfabab643d85fca1a3cfe
Dave Ross <dave.ross@10up.com>
2014-12-29 16:41:01 -0500

    Changed age restricted shortcake messaging to match full-page restriction

--- plugins/greatermedia-age-restricted-content/tpl/age-restricted-shortcode-render.tpl.php
+++ plugins/greatermedia-age-restricted-content/tpl/age-restricted-shortcode-render.tpl.php
@@ -1,1 +1,11 @@
-You must be signed in and <?php if ( '18plus' === $age_restriction ) : ?>18<?php elseif ( '21plus' === $age_restriction ) : ?>21<?php endif; ?> years of age or older to view this content. Please <a href="<?php echo esc_url($login_url); ?>"><?php _e('sign in'); ?></a>.
+<p>You must be at least
+	<?php if ( '18plus' === $age_restriction ) : ?>
+		18
+	<?php elseif ( '21plus' === $age_restriction ) : ?>
+		21
+	<?php endif; ?>
+	years old to view this content.
+	<?php if ( ! is_gigya_user_logged_in()) : ?>
+		Please <a href="<?php echo esc_url( $login_url ); ?>"><?php _e( 'sign in' ); ?></a> to continue.
+	<?php endif; ?>
+</p>
