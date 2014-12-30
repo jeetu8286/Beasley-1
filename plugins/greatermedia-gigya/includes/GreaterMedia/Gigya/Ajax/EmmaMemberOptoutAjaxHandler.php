1e8cbfbe10de2735016debe78504cb27538616e1
Darshan Sawardekar <darshan@10up.com>
2014-12-30 19:41:57 +0530

    Logs input json for optout requests.

--- plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Ajax/EmmaMemberOptoutAjaxHandler.php
+++ plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Ajax/EmmaMemberOptoutAjaxHandler.php
@@ -17,6 +17,7 @@ class EmmaMemberOptoutAjaxHandler extends AjaxHandler {
 	function handle_ajax() {
 		error_log( 'Handle optout - $_GET = ' . print_r( $_GET, true ) );
 		error_log( 'Handle optout - $_POST = ' . print_r( $_POST, true ) );
+		error_log( 'Handle optout - json = ' . print_r( file_get_contents( 'php://input' ), true ) );
 
 		wp_send_json_success( true );
 	}
