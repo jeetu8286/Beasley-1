adf2668eb07007d641b19548a772a614961a130e
Taylor Dewey <td@tddewey.com>
2014-12-30 16:20:13 -0800

    Introduces a custom mobile menu nav walker that provides the markup needed to display panels properly.
    Introduces a custom mobile menu walker class. This class provides some additional markup for top level nav items that have sub-menus and adds a context header for the top of sub-menu panels.
    
    This also fixes some styles that I messed up in the last commit.

--- themes/greatermedia/header.php
+++ themes/greatermedia/header.php
@@ -63,7 +63,7 @@
 						'link_after'      => '',
 						'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
 						'depth'           => 0,
-						'walker'          => ''
+						'walker'          => new GreaterMediaMobileNavWalker()
 					);
 
 					wp_nav_menu( $mobile_nav );
