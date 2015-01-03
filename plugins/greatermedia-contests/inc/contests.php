b4813604e21e7bce89d776f62a8ff2d8a50e6633
Eugene Manuilov <eugene.manuilov@10up.com>
2015-01-03 12:18:32 +0200

    Increased post_per_page parameter up to 20 for contest submissions query.

--- plugins/greatermedia-contests/inc/contests.php
+++ plugins/greatermedia-contests/inc/contests.php
@@ -480,7 +480,7 @@ function gmr_contests_submissions_query( $contest_id = null ) {
 	return new WP_Query( array(
 		'post_type'      => GMR_SUBMISSIONS_CPT,
 		'post_parent'    => $contest_id,
-		'posts_per_page' => 5,
+		'posts_per_page' => 20,
 		'paged'          => $submission_paged,
 	) );
 }
