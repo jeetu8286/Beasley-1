b7b8ab908509e1d4053cbd1475acd25d6d072354
Eugene Manuilov <eugene.manuilov@10up.com>
2015-01-04 11:38:52 +0200

    Fixed issue which appears when an array of submitted values should be displayed at submission preview screen.

--- themes/greatermedia/partials/submission-preview.php
+++ themes/greatermedia/partials/submission-preview.php
@@ -1,21 +1,25 @@
 <section class="col__inner--left">
 	<?php the_content(); ?>
 </section>
-<section class="col__inner--right"><?php 
+
+<section class="col__inner--right">
+	<?php
 	$contest_entry_id = get_post_meta( get_the_ID(), 'contest_entry_id', true );
 	if ( $contest_entry_id && class_exists( 'GreaterMediaFormbuilderRender', false ) ) :
 		$fields = GreaterMediaFormbuilderRender::parse_entry( get_post()->post_parent, $contest_entry_id );
-		if ( ! empty( $fields ) ) :
-			?><dl class="contest-submission--entries"><?php
-				foreach ( $fields as $field ) :
-					?><dt><?php echo esc_html( $field['label'] ); ?></dt>
-					<dd><?php echo esc_html( $field['value'] ); ?></dd><?php
-				endforeach;
-			?></dl>
+		if ( ! empty( $fields ) ) : ?>
+			<dl class="contest-submission--entries">
+				<?php foreach ( $fields as $field ) : ?>
+					<dt><?php echo esc_html( $field['label'] ); ?></dt>
+					<dd>
+						<?php echo esc_html( is_array( $field['value'] ) ? implode( ', ', $field['value'] ) : $field['value'] ); ?>
+					</dd>
+				<?php endforeach; ?>
+			</dl>
 
 			<p>
 				Submitted by <?php echo esc_html( gmr_contest_submission_get_author() ); ?> On <?php echo get_the_date( '', $contest_entry_id ); ?>
-			</p><?php
-		endif;
-	endif;
-?></section>
+			</p>
+		<?php endif; ?>
+	<?php endif; ?>
+</section>
