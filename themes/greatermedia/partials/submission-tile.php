234bf68bd6fc0b42f44007afec1f7c6576bc383b
Eugene Manuilov <eugene.manuilov@10up.com>
2015-01-05 00:12:22 +0200

    Fixed submission tile thumbnail image size issue.

--- themes/greatermedia/partials/submission-tile.php
+++ themes/greatermedia/partials/submission-tile.php
@@ -4,7 +4,7 @@
 			<i class="fa fa-thumbs-o-up"></i> <?php echo number_format( get_post_field( 'menu_order', null ), 0 ); ?>
 		</span>
 
-		<?php the_post_thumbnail(); ?>
+		<?php the_post_thumbnail( 'gmr-featured-secondary' ); ?>
 
 		<span class="contest__submission--author"><?php echo esc_html( gmr_contest_submission_get_author() ); ?></span>
 		<span class="contest__submission--date"><?php echo esc_html( get_the_date() ); ?></span>
