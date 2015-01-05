24e2c63c3a9634bae6360373b4aabf04c9063b2d
Chancey Mathews <chancey.mathews@10up.com>
2015-01-04 22:09:06 -0500

    Home page posts loop: Improve layout consistency across breakpoints, add all post format icons overlays, add event date overlay to event thumbnail, line everything up with the grid, enforce specific aspect ratios, and put thumbnails and categories on the correct side.

--- themes/greatermedia/functions.php
+++ themes/greatermedia/functions.php
@@ -54,6 +54,9 @@ function greatermedia_setup() {
 	 */
 	add_theme_support( 'post-thumbnails' );
 	add_image_size( 'gm-article-thumbnail',     		1580,   9999,   false   ); // thumbnails used for articles
+	add_image_size( 'gm-entry-thumbnail-1-1' ,          500,    500,    true    );
+	add_image_size( 'gm-entry-thumbnail-4-3' ,          400,    300,    true    );
+	// add_image_size( 'gm-entry-thumbnail-3-2' ,          500,    500,    true    );
 	add_image_size( 'gmr-gallery',              		800,    534,    true    ); // large images for the gallery
 	add_image_size( 'gmr-gallery-thumbnail',    		100,    100             ); // thumbnails for the gallery
 	add_image_size( 'gmr-featured-primary',     		2800,   1000,   true    ); // image for primary featured post on front page
