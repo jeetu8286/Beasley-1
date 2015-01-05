033c7cea430b0748812cf99382ac89b585b28df0
Chancey Mathews <chancey.mathews@10up.com>
2015-01-05 00:41:38 -0500

    Highlights: Make the whole block respond to hover.

--- themes/greatermedia/partials/frontpage-highlights.php
+++ themes/greatermedia/partials/frontpage-highlights.php
@@ -43,31 +43,31 @@
 				$events_query = \GreaterMedia\HomepageCuration\get_events_query();
 				while( $events_query->have_posts() ) : $events_query->the_post(); ?>
 					<div class="highlights__event--item">
-
-						<?php if ( has_post_thumbnail() ) : ?>
-							<div class="highlights__event--thumb">
-								<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'gmr-featured-secondary' ); ?></a>
+						<a href="<?php the_permalink(); ?>">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<div class="highlights__event--thumb">
+									<?php the_post_thumbnail( 'gmr-featured-secondary' ); ?>
+								</div>
+							<?php endif; ?>
+	
+							<div class="highlights__event--meta">
+								<h3 class="highlights__event--title"><?php the_title(); ?></h3>
+								<?php
+								/*
+								 * Moved the class from the span to the time so I could add both the start and end times to the datetime attributes
+								 */
+								$start = tribe_get_start_date( get_the_ID(), false, 'M d' );
+								$start_c = tribe_get_start_date( get_the_ID(), false, 'c' );
+								$end = tribe_get_end_date( get_the_ID(), false, 'M d' );
+								$end_c = tribe_get_end_date( get_the_ID(), false, 'c' );
+								if ( $start != $end ) {
+									printf( '<span class="highlights__event--date"><time datetime="%1$s">%2$s</time> - <time datetime="%3$s">%4$s</time></span>', $start_c, $start, $end_c, $end );
+								} else {
+									printf( '<span class="highlights__event--date"><time datetime="%1$s">%2$s</time></span>', $start_c, $start );
+								}
+								?>
 							</div>
-						<?php endif; ?>
-
-						<div class="highlights__event--meta">
-							<h3 class="highlights__event--title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-							<?php
-							/*
-							 * Moved the class from the span to the time so I could add both the start and end times to the datetime attributes
-							 */
-							$start = tribe_get_start_date( get_the_ID(), false, 'M d' );
-							$start_c = tribe_get_start_date( get_the_ID(), false, 'c' );
-							$end = tribe_get_end_date( get_the_ID(), false, 'M d' );
-							$end_c = tribe_get_end_date( get_the_ID(), false, 'c' );
-							if ( $start != $end ) {
-								printf( '<span class="highlights__event--date"><time datetime="%1$s">%2$s</time> - <time datetime="%3$s">%4$s</time></span>', $start_c, $start, $end_c, $end );
-							} else {
-								printf( '<span class="highlights__event--date"><time datetime="%1$s">%2$s</time></span>', $start_c, $start );
-							}
-							?>
-						</div>
-
+						</a>
 					</div>
 				<?php endwhile; ?>
 				<?php wp_reset_query(); ?>
