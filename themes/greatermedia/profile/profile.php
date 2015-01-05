9aea52812cef5dc638e014b4b7165b5ad8bd6feb
Taylor Dewey <td@tddewey.com>
2015-01-04 16:04:01 -0800

    Add defaults for cookies-required, forgot-password, and join profile pages.

--- themes/greatermedia/profile/profile.php
+++ themes/greatermedia/profile/profile.php
@@ -19,6 +19,14 @@
 		);
 	}
 
+	if ( 'join' === $profile_page ) {
+		$option_key = 'join';
+		$defaults = array(
+			'heading' => 'Join',
+			'message' => 'Membership gives you access to all areas of the site, including full membership-only contests and the ability to submit content to share with the site and other members.'
+		);
+	}
+
 	if ( 'account' === $profile_page ) {
 		$option_key = 'profile';
 		$defaults = array (
@@ -26,6 +34,24 @@
 			'message' => 'Help us get to know you better, manage your communication preferences, or change your password.'
 		);
 	}
+
+	if ( 'forgot-password' === $profile_page ) {
+		$option_key = 'password';
+		$defaults = array (
+			'heading' => 'Password Reset',
+			'message' => 'Forgot your password? No worries, it happens. We\'ll send you a password reset email.'
+		);
+	}
+
+if ( 'cookies-required' === $profile_page ) {
+	$option_key = 'cookies';
+	$defaults = array (
+		'heading' => 'Cookies required',
+		'message' => 'It doesn\'t look like your browser is letting us set a cookie. These small bits of information are stored in your browser and allow us to ensure you stay logged in. They are required to use the site and can generally be authorized in your browser\'s preferences or settings screen.'
+	);
+}
+
+	if ( '' )
 
 $page_heading = get_option( "gmr_{$option_key}_page_heading" );
 $page_message = get_option( "gmr_{$option_key}_page_message" );
