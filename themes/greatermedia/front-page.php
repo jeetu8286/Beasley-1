f8bc135f1c37caeaf3ee3bf1e59c43b154fe14b3
Chancey Mathews <chancey.mathews@10up.com>
2015-01-05 17:13:20 -0500

    Break entry out into its own partial.

--- themes/greatermedia/front-page.php
+++ themes/greatermedia/front-page.php
@@ -36,68 +36,7 @@ get_header();
 						<div class='entry2-ad-wrap'>
 					<?php endif; ?>
 				
-					<?php 
-					$post_classes = array( 'entry2' );
-					if ( ! empty( trim( $post->post_excerpt ) ) ) {
-						$post_classes[] = 'has-excerpt'; 
-					} 
-					if ( has_post_thumbnail() || 'tribe_events' == $post->post_type ) {
-						$post_classes[] = 'has-thumbnail';
-					}
-					?>
-
-					<article id="post-<?php the_ID(); ?>" <?php post_class( $post_classes ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-						<?php 
-						if ( has_post_thumbnail() || 'tribe_events' == $post->post_type ) : 
-							if ( has_post_format( 'audio' ) ) {
-								$thumbnail_size = 'gm-entry-thumbnail-1-1';
-							} else {
-								$thumbnail_size = 'gm-entry-thumbnail-4-3';
-							}
-						?>
-							<section class="entry2__thumbnail" style='background-image: url(<?php gm_post_thumbnail_url( $thumbnail_size ); ?>)'>
-								<a href="<?php the_permalink(); ?>">
-									<?php if ( 'tribe_events' == $post->post_type): ?>
-										<div class='entry2__thumbnail--event-date'>
-											<div class='entry2__thumbnail--day-of-week'><?php echo tribe_get_start_date( get_the_ID(), false, 'l' ); ?></div>
-											<div class='entry2__thumbnail--month-and-day'><?php echo tribe_get_start_date( get_the_ID(), false, 'M j' ); ?></div>										
-										</div>
-									<?php endif; ?>
-								</a>								
-							</section>
-						<?php endif; ?>
-
-						<section class="entry2__meta">
-							<?php if ( 'tribe_events' != $post->post_type): ?>
-								<time datetime="<?php the_time( 'c' ); ?>" class="entry2__date"><?php the_time( 'F j' ); ?></time>
-							<?php endif; ?>
-							<h2 class="entry2__title" itemprop="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-							
-							<?php if ( ! empty( trim( $post->post_excerpt ) ) ): ?>
-								<div class="entry2__excerpt">
-									<?php the_excerpt(); ?>
-								</div>
-							<?php endif; ?>
-							
-							<?php if ( 'tribe_events' == $post->post_type ): ?>
-								<ul class="entry2__event--details">
-									<li class="entry2__event--item"><?php echo tribe_get_start_time(); ?></li>
-									<li class="entry2__event--item"><?php echo tribe_get_venue(); ?></li>
-									<li class="entry2__event--item"><?php echo tribe_get_cost(); ?></li>
-								</ul>
-							<?php endif; ?>
-						</section>
-
-						<footer class="entry2__footer">
-							<?php
-							$category = get_the_category();
-
-							if( isset( $category[0] ) ){
-								echo '<a href="' . esc_url( get_category_link($category[0]->term_id ) ) . '" class="entry2__footer--category">' . esc_html( $category[0]->cat_name ) . '</a>';
-							}
-							?>
-						</footer>
-					</article>
+					<?php get_template_part( 'partials/entry' ); ?>
 					
 					<?php if ( 0 == $post_count % 5 ):	?>
 							<div class='entry2-ad-wrap__ad mobile'>
