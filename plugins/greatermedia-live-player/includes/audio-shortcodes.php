904cee17c3bf00dacc58f96457b475b6cad49179
EduardMaghakyan <eduard.maghakyan@10up.com>
2015-01-06 19:10:26 +0400

    adjustments to download button position

--- plugins/greatermedia-live-player/includes/audio-shortcodes.php
+++ plugins/greatermedia-live-player/includes/audio-shortcodes.php
@@ -69,11 +69,11 @@ class GMR_Audio_Shortcodes {
 		$new_html .= '<span class="podcast__runtime">' . esc_html( $metadata['length_formatted'] ) . '</span>';
 		$new_html .= '</div>';
 		$new_html .= '<div class="podcast__meta">';
-		$new_html .= '<time datetime="' . get_the_time( 'c' ) . '">' . get_the_time( 'd F' ) . '</time>';
+		$new_html .= '<time datetime="' . get_the_time( 'c' ) . '">' . get_the_time( 'd F' ) . '</time><br/>';
+		$new_html .= '<h3>' . get_the_title() . '</h3>';
 		if( $downloadable == 'on' || $downloadable == '' ) {
 			$new_html .= '<a href="' . esc_attr( $mp3_src ) . '" download="' . esc_attr( $mp3_src ) . '" class="podcast__download" download>Download</a>';
 		}
-		$new_html .= '<h3>' . get_the_title() . '</h3>';
 		$new_html .= '<p>' . get_the_excerpt() . '</p>' ;
 		if( $parent_podcast_id ) {
 			$new_html .= '<a href="' . get_permalink( $parent_podcast_id ) . '" target="_blank">'. $parent_podcast->post_title .'</a>';
