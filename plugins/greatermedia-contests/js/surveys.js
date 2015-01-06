e961479154de6fa7c22a4539c0e6c939a848ac30
Eugene Manuilov <eugene.manuilov@10up.com>
2015-01-06 13:44:35 +0200

    Fixed survey submitted data bug.

--- plugins/greatermedia-contests/js/surveys.js
+++ plugins/greatermedia-contests/js/surveys.js
@@ -1,58 +1,61 @@
 /*! Greater Media Contests - v1.0.3
  * http://10up.com/
  * Copyright (c) 2015; * Licensed GPLv2+ */
-jQuery(function () {
+(function ($, gmr) {
+	var __ready = function () {
+		$('.' + gmr.form_class).submit(function() {
+			var form = $(this);
 
-	// Attach a submit handler to survey forms
-	jQuery( '.' + GreaterMediaSurveys.form_class ).submit(function (e) {
-		var form = this;
-		e.preventDefault();
-		if (!jQuery(this).parsley || jQuery(this).parsley().isValid()) {
+			if (!form.parsley || form.parsley().isValid()) {
+				var form_data = new FormData();
 
-			// Marshall the data
-			var form_data = new FormData();
-			jQuery(this).find('input').each(function () {
-				var input = this;
-				if ('file' === input.type) {
-					jQuery(this.files).each(function (key, value) {
-						form_data.append(input.name, value);
-					});
-				}
-				else {
-					form_data.append(input.name, input.value);
-				}
-			});
+				form.find('input').each(function() {
+					var input = this;
 
-			jQuery(this).find('textarea').each(function () {
-				form_data.append(this.name, this.value);
-			});
-
-			// Submit the form via AJAX
-			jQuery.ajax({
-				url        : GreaterMediaSurveys.ajax_url,
-				type       : 'post',
-				data       : form_data,
-				processData: false, // Don't process the files
-				contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-				dataType   : 'json',
-				success    : function (data, textStatus, jqXHR) {
-					if ('success' === textStatus && data.data.message) {
-						var wrapper = document.createElement('p');
-						wrapper.class = 'survey_thank_you';
-						wrapper.innerText = data.data.message;
-						jQuery(form).replaceWith(wrapper);
+					if ('file' === input.type) {
+						$(this.files).each(function(key, value) {
+							form_data.append(input.name, value);
+						});
+					} else if ('radio' === input.type || 'checkbox' === input.type) {
+						if (input.checked) {
+							form_data.append(input.name, input.value);
+						}
+					} else {
+						form_data.append(input.name, input.value);
 					}
+				});
 
-				}
-			});
+				form.find('textarea, select').each(function() {
+					form_data.append(this.name, this.value);
+				});
 
-		}
+				form.find('input, textarea, select, button').attr('disabled', 'disabled');
+				form.find('i.fa').show();
 
-		return false;
+				$.ajax({
+					url: gmr.ajax_url,
+					type: 'post',
+					data: form_data,
+					processData: false, // Don't process the files
+					contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+					dataType: 'json',
+					success: function (data, textStatus, jqXHR) {
+						if ('success' === textStatus && data.data.message) {
+							var wrapper = document.createElement('p');
+							wrapper.class = 'survey_thank_you';
+							wrapper.innerText = data.data.message;
+							form.replaceWith(wrapper);
+						}
+					}
+				});
+			}
 
-	});
+			return false;
+		});
+	};
 
-});
+	$(document).bind('pjax:end', __ready).ready(__ready);
+})(jQuery, GreaterMediaSurveys);
 document.addEventListener("DOMContentLoaded", function () {
 	/**
 	 * Generate a list of supported input types (text, date, range, etc.).
