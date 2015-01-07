b0047c60944e6eab88f0af93e3e08d5f9234666a
Chris Marslender <chrismarslender@gmail.com>
2015-01-06 21:34:14 -0700

    Separate out the permalink functions from the html functions for show links/menu items. Also create additional function for getting the live-links endpoint permalink
    Conflicts:
    	plugins/greatermedia-shows/includes/gmi-show-support.php

--- plugins/greatermedia-shows/includes/gmi-show-support.php
+++ plugins/greatermedia-shows/includes/gmi-show-support.php
@@ -18,31 +18,53 @@ function supports_videos( $show_id ) {
 	return (bool) filter_var( get_post_meta( $show_id, 'show_homepage_videos', true ), FILTER_VALIDATE_BOOLEAN );
 }
 
+function get_about_permalink( $show_id ) {
+	return trailingslashit( get_the_permalink( $show_id ) ) . "about/";
+}
+
 function about_link_html( $show_id, $link_text = 'About' ) {
 	$class = 'about' == get_query_var( 'show_section' ) ? 'current-menu-item' : '';
-	?><li class="<?php echo esc_attr( $class ); ?>"><a href="<?php echo get_the_permalink( $show_id ); ?>about/"><?php echo esc_html( $link_text ); ?></a></li><?php
+	?><li class="<?php echo esc_attr( $class ); ?>"><a href="<?php echo esc_url( get_about_permalink( $show_id ) ); ?>"><?php echo esc_html( $link_text ); ?></a></li><?php
+}
+
+function get_galleries_permalink( $show_id ) {
+	return trailingslashit( get_the_permalink( $show_id ) ) . "galleries/";
 }
 
 function galleries_link_html( $show_id, $link_text = 'Galleries' ) {
 	if ( supports_galleries( $show_id ) ) {
 		$class = 'galleries' == get_query_var( 'show_section' ) ? 'current-menu-item' : '';
-		?><li class="<?php echo esc_attr( $class ); ?>"><a href="<?php echo get_the_permalink( $show_id ); ?>galleries/"><?php echo esc_html( $link_text ); ?></a></li><?php
+		?><li class="<?php echo esc_attr( $class ); ?>"><a href="<?php echo esc_url( get_galleries_permalink( $show_id ) ); ?>"><?php echo esc_html( $link_text ); ?></a></li><?php
 	}
+}
+
+function get_podcasts_permalink( $show_id ) {
+	return trailingslashit( get_the_permalink( $show_id ) ) . "podcasts/";
 }
 
 function podcasts_link_html( $show_id, $link_text = 'Podcasts' ) {
 	if ( supports_podcasts( $show_id ) ) {
 		$class = 'podcasts' == get_query_var( 'show_section' ) ? 'current-menu-item' : '';
-		?><li class="<?php echo esc_attr( $class ); ?>"><a href="<?php echo get_the_permalink( $show_id ); ?>podcasts/"><?php echo esc_html( $link_text ); ?></a></li><?php
+		?><li class="<?php echo esc_attr( $class ); ?>"><a href="<?php echo esc_url( get_podcasts_permalink( $show_id ) ); ?>"><?php echo esc_html( $link_text ); ?></a></li><?php
 	}
+}
+
+function get_videos_permalink( $show_id ) {
+	return trailingslashit( get_the_permalink( $show_id ) ) . "videos/";
 }
 
 function videos_link_html( $show_id, $link_text = 'Videos' ) {
 	if ( supports_videos( $show_id ) ) {
 		$class = 'videos' == get_query_var( 'show_section' ) ? 'current-menu-item' : '';
-		?><li class="<?php echo esc_attr( $class ); ?>"><a href="<?php echo get_the_permalink( $show_id ); ?>videos/"><?php echo esc_html( $link_text ); ?></a></li><?php
+		?><li class="<?php echo esc_attr( $class ); ?>"><a href="<?php echo esc_url( get_videos_permalink( $show_id ) ); ?>"><?php echo esc_html( $link_text ); ?></a></li><?php
 	}
 }
+
+function get_live_links_permalink( $show_id ) {
+	return trailingslashit( get_the_permalink( $show_id ) ) . "live-links/";
+}
+
+// No live-links html because this doesn't show up in the show menu ever
 
 /**
  * Gets pagination links for a specific show endpoint query.
