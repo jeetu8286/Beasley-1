8bca8dab8326f432cfbaa55945f728384907d57c
Chancey Mathews <chancey.mathews@10up.com>
2015-01-07 17:11:26 -0500

    Separate the event-specific entry partial from the default entry partial.

--- themes/greatermedia/partials/loop.php
+++ themes/greatermedia/partials/loop.php
@@ -1,3 +1,15 @@
 <?php while ( have_posts() ) : the_post(); ?>
-	<?php get_template_part( 'partials/entry' ); ?>
+
+	<?php
+	global $post; 
+	
+	$partial_name = '';
+	
+	if ( 'tribe_events' == $post->post_type ) {
+		$partial_name = 'tribe_events';
+	}
+	
+	get_template_part( 'partials/entry', $partial_name ); 
+	?>
+	
 <?php endwhile; ?>
