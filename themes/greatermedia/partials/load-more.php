9653fb561496ffae46b8410692604fc6e57c8471
Chancey Mathews <chancey.mathews@10up.com>
2015-01-07 14:49:02 -0500

    Don't show load more button if there are no more pages to show anyway.

--- themes/greatermedia/partials/load-more.php
+++ themes/greatermedia/partials/load-more.php
@@ -1,3 +1,10 @@
+<?php
+// Don't bother showing this if there are no more pages to page through. 
+if ( $GLOBALS['paged'] >= $GLOBALS['wp_query']->max_num_pages ) {
+	return; 
+}
+?>
+
 <button type="button" 
 		class="posts-pagination--load-more"
 		data-url="<?php echo str_replace( PHP_INT_MAX, '{{page}}', get_pagenum_link( PHP_INT_MAX ) ); // do not escape this url because it contains a placeholder ?>"
