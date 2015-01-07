ec4c6221da28c277c3c6a2cb6417d33670039ed9
Taylor Dewey <td@tddewey.com>
2015-01-07 00:09:49 -0800

    Style the keywords autocomplete
    Modify the autocomplete init to append to the header search form and zero out the inline styles it automatically adds.
    
    The style for the autocomplete form are derived in large part from the submenus, and where possible, the jquery ui classes were added to the list of selectors. But there is a new scss stylesheet for everything that still needs some help, layout especially.

--- plugins/greatermedia-keywords/assets/js/greatermedia_keywords_autocomplete.js
+++ plugins/greatermedia-keywords/assets/js/greatermedia_keywords_autocomplete.js
@@ -5,11 +5,26 @@
 	'use strict';
 
 	jQuery( function( $ ) {
-		$('#s').autocomplete({
-			source: GMRKeywords
-		});
+
+		//$('#s').autocomplete({
+		//	source: GMRKeywords
+		//});
+
 		$('#header-search').autocomplete({
-			source: GMRKeywords
+			source: GMRKeywords,
+			appendTo: '.header__search--form',
+			/*
+			 * Nullify all the automaically added inline css things.
+			 * we'll do our own gig, thanks.
+			 */
+			open: function () {
+				$('.header__search--form .ui-menu').css({
+					width  : '',
+					display: '',
+					left   : '',
+					top    : ''
+				});
+			}
 		});
 	});
 
