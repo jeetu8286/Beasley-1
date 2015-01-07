4788ec92f3190e214c8f93a91ae343e517313cb9
AndrewMowe <andrew@team-eight.com>
2015-01-06 16:11:35 -0800

    started with show-highlights partial

---
+++ themes/greatermedia/partials/show-highlights.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Partial for the Front Page Highlights - Community and Events
+ *
+ * @package Greater Media
+ * @since   0.1.0
+ */
+?>
+
+<section class="home__highlights">
+
+		<div class="highlights__col">
+
+			<div class="highlights__community">
+
+				<h2 class="highlights__heading"><?php //bloginfo( 'name' ); ?><?php _e( ' Our Favorites', 'greatermedia' ); ?></h2>
+
+				<?php
+				$hp_comm_query = \GreaterMedia\Shows\get_show_favorites_query();
+				while( $hp_comm_query->have_posts() ) : $hp_comm_query->the_post(); ?>
+					<div class="highlights__community--item">
+						<a href="<?php the_permalink(); ?>">
+
+							<div class="highlights__community--thumb" style='background-image: url(<?php gm_post_thumbnail_url( 'gmr-featured-secondary' ) ?>)'></div>
+	
+							<h3 class="highlights__community--title">
+								<?php the_title(); ?>
+							</h3>
+
+						</a>
+					</div>
+				<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
+			</div>
+
+			<div class="highlights__events">
+
+				<h2 class="highlights__heading"><?php _e( 'Upcoming Events', 'greatermedia' ); ?></h2>
+
+				<?php
+				$events_query = \GreaterMedia\Shows\get_show_events();
+				while( $events_query->have_posts() ) : $events_query->the_post(); ?>
+					<div class="highlights__event--item">
+						<a href="<?php the_permalink(); ?>">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<div class="highlights__event--thumb" style='background-image: url(<?php gm_post_thumbnail_url( 'gmr-featured-secondary' ) ?>)'></div>
+							<?php endif; ?>
+	
+							<div class="highlights__event--meta">
+								<h3 class="highlights__event--title"><?php the_title(); ?></h3>
+								<?php
+								/*
+								 * Moved the class from the span to the time so I could add both the start and end times to the datetime attributes
+								 */
+								$start = tribe_get_start_date( get_the_ID(), false, 'M d' );
+								$start_c = tribe_get_start_date( get_the_ID(), false, 'c' );
+								$end = tribe_get_end_date( get_the_ID(), false, 'M d' );
+								$end_c = tribe_get_end_date( get_the_ID(), false, 'c' );
+								if ( $start != $end ) {
+									printf( '<span class="highlights__event--date"><time datetime="%1$s">%2$s</time> - <time datetime="%3$s">%4$s</time></span>', $start_c, $start, $end_c, $end );
+								} else {
+									printf( '<span class="highlights__event--date"><time datetime="%1$s">%2$s</time></span>', $start_c, $start );
+								}
+								?>
+							</div>
+						</a>
+					</div>
+				<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
+
+			</div>
+
+		</div>
+
+</section>
--- themes/greatermedia/single-show.php
+++ themes/greatermedia/single-show.php
@@ -65,62 +65,12 @@
 					<?php endif; ?>
 
 				
-					<?php
-					global $post;
-					$events = \GreaterMedia\Shows\get_show_events();
-					if ( $events ) { ?>
-						<div class="featured__content">
-							<h2 class="section-header">Upcoming Events</h2>
-							<?php foreach( $events as $post ): setup_postdata( $post ); ?>
-							<div class="featured__content--block">
-								<a href="<?php the_permalink(); ?>">
-								<?php if ( has_post_thumbnail() ) : ?>
-									<div class="featured__content--image">
-										<?php the_post_thumbnail( 'thumbnail' ); ?>
-									</div>
-								<?php endif; ?>
-								<div class="featured__content--meta">
-									<h3 class="featured__content--title"><?php the_title(); ?></h3>
-									<span class="highlights__event--date"><time datetime="2014-12-28T08:00:00+00:00">Dec 28</time></span> <!-- TODO add dynamic time -->
-								</div>
-								</a>
-							</div>
-						<?php endforeach; ?>
-						<?php wp_reset_query(); ?>
-			        	</div>
-		        	<?php } ?>
+					<?php get_template_part( 'partials/show-highlights' ); ?>
 				    
 
 			        <div class="row">
 
 				        <aside class="inner-right-col">
-
-					        <?php
-					        $fav_query = \GreaterMedia\Shows\get_show_favorites_query();
-					        if ( $fav_query->have_posts() ) :
-				        	?>
-
-					        <section class="show__favorites cf">
-					        	<h2 class="section-header">Our Favorites</h2>
-
-							<?php while( $fav_query->have_posts() ): $fav_query->the_post();
-					        ?>
-							<div class="featured__content--block">
-				                <?php if ( has_post_thumbnail() ): ?>
-					                <div class="featured__content--image">
-						                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnail' ); ?></a>
-					                </div>
-								<?php endif; ?>
-				                <div class="featured__content--meta">
-				                    <h3 class="featured__content--title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-				                </div>
-				            </div>
-							<?php endwhile; ?>
-							</section>
-							<?php endif; ?>
-					        <?php wp_reset_query(); ?>
-					        
-
 					        <?php do_action( 'acm_tag', 'mrec-lists' ); ?>
 
 							<?php
