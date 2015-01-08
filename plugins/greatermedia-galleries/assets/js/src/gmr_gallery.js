a5942ce2b7734d46ec1e42a4cecd685054e5dbc6
Eugene Manuilov <eugene.manuilov@10up.com>
2015-01-08 18:47:41 +0200

    Fixed gallery and pjax compatibility issue.

--- plugins/greatermedia-galleries/assets/js/src/gmr_gallery.js
+++ plugins/greatermedia-galleries/assets/js/src/gmr_gallery.js
@@ -1,227 +1,237 @@
-(function ( $, window, undefined ) {
+(function ($, window, undefined) {
 	"use strict";
 
 	var document = window.document,
-		$document = $( document ),
-		$window = $( window ),
-		slideshow = $( '.gallery__slide--images.cycle-slideshow' ),
-		gallery = document.querySelectorAll( '.gallery' ),
-		main = document.querySelector( '.gallery__slide--images' ),
-		$gallery = $( gallery ),
-		$main = $( main ),
-		$main_wrapper = $( '.gallery__slides' ),
-		$caption = $( '.gallery__content' ),
-		$sidebar = $( '.gallery__thumbnails' ),
-		$slide_paging = $( '.gallery__paging' ),
-		$slide_paging_previews = $( '.gallery__previews' ),
-		$single_thumbnail = $( '.gallery__previews div div' );
+		$document = $(document),
+		$window = $(window),
+		__ready;
 
-	/**
-	 * Bind the gallery full screen toggle
-	 */
-	function bind_events() {
-		var hashChange = false;
-
-		slideshow = $( '.gallery__slide--images.cycle-slideshow' );
-		$slide_paging_previews = $( '.gallery__previews' );
+	__ready = function() {
+		var slideshow = $( '.gallery__slide--images.cycle-slideshow' ),
+			gallery = document.querySelectorAll( '.gallery' ),
+			main = document.querySelector( '.gallery__slide--images' ),
+			$gallery = $( gallery ),
+			$main = $( main ),
+			$main_wrapper = $( '.gallery__slides' ),
+			$caption = $( '.gallery__content' ),
+			$sidebar = $( '.gallery__thumbnails' ),
+			$slide_paging = $( '.gallery__paging' ),
+			$slide_paging_previews = $( '.gallery__previews' ),
+			$single_thumbnail = $( '.gallery__previews div div' );
 
 		/**
-		 * Make sure thumbnails are updated before the slideshow cycles.
+		 * Bind the gallery full screen toggle
 		 */
-		slideshow.on( 'cycle-before', function ( event, optionHash ) {
-			update_thumbnails(optionHash.nextSlide); // nextSlide = incoming slide. could be backward
-			//$caption.cycle( 'goto', optionHash.nextSlide );
-		} );
+		function bind_events() {
+			var hashChange = false;
+
+			slideshow = $( '.gallery__slide--images.cycle-slideshow' );
+			$slide_paging_previews = $( '.gallery__previews' );
+
+			/**
+			 * Make sure thumbnails are updated before the slideshow cycles.
+			 */
+			slideshow.on( 'cycle-before', function ( event, optionHash ) {
+				update_thumbnails(optionHash.nextSlide); // nextSlide = incoming slide. could be backward
+				//$caption.cycle( 'goto', optionHash.nextSlide );
+			} );
+
+			/**
+			 * Wire up additional events after the slideshow has fully initialized.
+			 */
+			slideshow.on( 'cycle-update-view', function( event, optionHash ) {
+				update_thumbnails( optionHash.currSlide );
+			} );
+
+			/**
+			 * On mobile, regroup thumbnails on page load
+			 */
+			slideshow.on( 'cycle-initialized', function( e, opts ) {
+				responsive_thumbnails();
+			} );
+
+			/**
+			 * Make sure the preview buttons transition the slideshow.
+			 */
+			$slide_paging_previews.on( 'click', '.cycle-slide div', function () {
+				var $this = $( this ),
+					index = $this.data( 'cycle-index' );
+
+				slideshow.cycle( 'goto', index );
+				$caption.cycle( 'goto', index );
+			} );
+
+			// Make sure we disable other hashchange events that attempt to capture manual hash changes.
+			$document.on( 'cycle-pre-initialize', function( e, opts ) {
+				$( window ).off( 'hashchange', opts._onHashChange);
+			});
+
+			// Rebind Captions - Code taken directly from jquery2.cycle.caption.js (with certain code standard updates)
+			$document.on( 'cycle-update-view', function ( e, opts, slideOpts, currSlide ) {
+				if ( opts.captionModule !== 'caption' ) {
+					return;
+				}
+
+				var el;
+				$.each( ['caption', 'overlay'], function () {
+					var name = this,
+						template = slideOpts[name + 'Template'],
+						el = opts.API.getComponent( name );
+
+					if ( el.length && template ) {
+						el.html( opts.API.tmpl( template, slideOpts, opts, currSlide ) );
+						el.show();
+					}
+					else {
+						el.hide();
+					}
+				} );
+			} );
+
+			$window.resize( responsive_thumbnails );
+		}
 
 		/**
-		 * Wire up additional events after the slideshow has fully initialized.
+		 * Are we on a mobile browser (or anything smaller than 768px)?
+		 * @returns bool
 		 */
-		slideshow.on( 'cycle-update-view', function( event, optionHash ) {
-			update_thumbnails( optionHash.currSlide );
-		} );
+		function isMobile() {
+			return $gallery.hasClass( 'ismobile' );
+		}
+
+		function isTablet() {
+			return $gallery.hasClass( 'istablet' );
+		}
 
 		/**
-		 * On mobile, regroup thumbnails on page load
+		 * Regroup the thumbnails in mobile to fit the screen
 		 */
-		slideshow.on( 'cycle-initialized', function( e, opts ) {
-			responsive_thumbnails();
-		} );
+		function responsive_thumbnails() {
+			// If we're moving from mobile to normal, shift things around
+			if ( $window.width() >= 768 && isMobile() ) {
+				$gallery.removeClass( 'ismobile' );
+				$sidebar.append( $caption );
+				regroup_thumbnails( get_thumbs_per_page() );
+				update_thumbnails( $main.data( "cycle.opts" ).currSlide );
 
-		/**
-		 * Make sure the preview buttons transition the slideshow.
-		 */
-		$slide_paging_previews.on( 'click', '.cycle-slide div', function () {
-			var $this = $( this ),
-				index = $this.data( 'cycle-index' );
-
-			slideshow.cycle( 'goto', index );
-			$caption.cycle( 'goto', index );
-		} );
-
-		// Make sure we disable other hashchange events that attempt to capture manual hash changes.
-		$document.on( 'cycle-pre-initialize', function( e, opts ) {
-			$( window ).off( 'hashchange', opts._onHashChange);
-		});
-
-		// Rebind Captions - Code taken directly from jquery2.cycle.caption.js (with certain code standard updates)
-		$document.on( 'cycle-update-view', function ( e, opts, slideOpts, currSlide ) {
-			if ( opts.captionModule !== 'caption' ) {
+				$main.css( 'height', '' );
+				$main_wrapper.css( 'height', '' );
+				$( '.gallery__previews, .gallery__previews--group' ).css( 'height', '' );
 				return;
 			}
 
-			var el;
-			$.each( ['caption', 'overlay'], function () {
-				var name = this,
-					template = slideOpts[name + 'Template'],
-					el = opts.API.getComponent( name );
+			// If we're on a small screen but ismobile is not set, shift things around
+			if ( $window.width() < 480 && ! isMobile() ) {
+				$gallery.addClass( 'ismobile' );
+				regroup_thumbnails( get_thumbs_per_page() );
+				update_thumbnails( $main.data( "cycle.opts" ).currSlide );
+			}
 
-				if ( el.length && template ) {
-					el.html( opts.API.tmpl( template, slideOpts, opts, currSlide ) );
-					el.show();
-				}
-				else {
-					el.hide();
+			// If the window is being resized, adjust the thumbnail and main image height
+			if ( isMobile() ) {
+				var main_height, thumb_height;
+				$main.css( 'height', ( main_height - 20 ) + 'px' );
+				$main_wrapper.css( 'height', main_height );
+
+				thumb_height = $single_thumbnail.width();
+				$( '.gallery__previews, .gallery__previews--group' ).css( 'height', thumb_height + 'px' );
+			}
+
+			if ( $window.width() >= 480 && $window.width() < 769 && ! isTablet() ) {
+				$gallery.addClass( 'istablet' );
+				regroup_thumbnails( get_thumbs_per_page() );
+				update_thumbnails( $main.data( "cycle.opts" ).currSlide );
+			}
+
+		}
+
+		/**
+		 * Regroup the thumbnails in paged divs
+		 *
+		 * This is used when going between widescreen and other views, because the
+		 * widescreen view can only hold 8 thumbnails per page while the other views
+		 * can hold 15 per page.
+		 *
+		 * @param number_in_group
+		 */
+		function regroup_thumbnails( number_in_group ) {
+			var $thumbnails_group = $( '.gallery__previews--group' );
+
+			$thumbnails_group.children( 'div' ).appendTo( '.gallery__previews' );
+			$thumbnails_group.remove();
+			$( '.gallery__previews div' ).each( function() {
+				var $this = $( this );
+				if ( undefined === $this.attr( 'id' ) ) {
+					$this.remove();
 				}
 			} );
-		} );
-
-		$window.resize( responsive_thumbnails );
-	}
-
-	/**
-	 * Are we on a mobile browser (or anything smaller than 768px)?
-	 * @returns bool
-	 */
-	function isMobile() {
-		return $gallery.hasClass( 'ismobile' );
-	}
-
-	function isTablet() {
-		return $gallery.hasClass( 'istablet' );
-	}
-
-	/**
-	 * Regroup the thumbnails in mobile to fit the screen
-	 */
-	function responsive_thumbnails() {
-		// If we're moving from mobile to normal, shift things around
-		if ( $window.width() >= 768 && isMobile() ) {
-			$gallery.removeClass( 'ismobile' );
-			$sidebar.append( $caption );
-			regroup_thumbnails( get_thumbs_per_page() );
-			update_thumbnails( $main.data( "cycle.opts" ).currSlide );
-
-			$main.css( 'height', '' );
-			$main_wrapper.css( 'height', '' );
-			$( '.gallery__previews, .gallery__previews--group' ).css( 'height', '' );
-			return;
+			$slide_paging_previews
+				.each( function () {
+					var divs = $( 'div', this );
+					for ( var i = 0; i < divs.length; i += number_in_group ) {
+						divs.slice( i, i + number_in_group ).wrapAll( '<div class="gallery__previews--group"></div>' );
+					}
+				} )
+				.cycle( 'reinit' );
 		}
 
-		// If we're on a small screen but ismobile is not set, shift things around
-		if ( $window.width() < 480 && ! isMobile() ) {
-			$gallery.addClass( 'ismobile' );
-			regroup_thumbnails( get_thumbs_per_page() );
-			update_thumbnails( $main.data( "cycle.opts" ).currSlide );
+		/**
+		 * Update the slide thumbnails to we're viewing the correct group.
+		 *
+		 * @param {Number} selected_slide
+		 */
+		function update_thumbnails( selected_slide ) {
+			var slides_per_page = get_thumbs_per_page();
+
+			$( '.gallery__slide--active' ).removeClass( 'gallery__slide--active' );
+			$( '#preview-' + selected_slide ).addClass( 'gallery__slide--active' );
+			var selected_slide_group = Math.floor( selected_slide / slides_per_page );
+			$slide_paging_previews.cycle( 'goto', selected_slide_group );
 		}
 
-		// If the window is being resized, adjust the thumbnail and main image height
-		if ( isMobile() ) {
-			var main_height, thumb_height;
-			$main.css( 'height', ( main_height - 20 ) + 'px' );
-			$main_wrapper.css( 'height', main_height );
-
-			thumb_height = $single_thumbnail.width();
-			$( '.gallery__previews, .gallery__previews--group' ).css( 'height', thumb_height + 'px' );
-		}
-
-		if ( $window.width() >= 480 && $window.width() < 769 && ! isTablet() ) {
-			$gallery.addClass( 'istablet' );
-			regroup_thumbnails( get_thumbs_per_page() );
-			update_thumbnails( $main.data( "cycle.opts" ).currSlide );
-		}
-
-	}
-
-	/**
-	 * Regroup the thumbnails in paged divs
-	 *
-	 * This is used when going between widescreen and other views, because the
-	 * widescreen view can only hold 8 thumbnails per page while the other views
-	 * can hold 15 per page.
-	 *
-	 * @param number_in_group
-	 */
-	function regroup_thumbnails( number_in_group ) {
-		var $thumbnails_group = $( '.gallery__previews--group' );
-
-		$thumbnails_group.children( 'div' ).appendTo( '.gallery__previews' );
-		$thumbnails_group.remove();
-		$( '.gallery__previews div' ).each( function() {
-			var $this = $( this );
-			if ( undefined === $this.attr( 'id' ) ) {
-				$this.remove();
+		function get_thumbs_per_page() {
+			var slides_per_page = 8;
+			if ( isMobile() ) {
+				slides_per_page = 3;
 			}
-		} );
-		$slide_paging_previews
-			.each( function () {
-				var divs = $( 'div', this );
-				for ( var i = 0; i < divs.length; i += number_in_group ) {
-					divs.slice( i, i + number_in_group ).wrapAll( '<div class="gallery__previews--group"></div>' );
-				}
-			} )
-			.cycle( 'reinit' );
-	}
-
-	/**
-	 * Update the slide thumbnails to we're viewing the correct group.
-	 *
-	 * @param {Number} selected_slide
-	 */
-	function update_thumbnails( selected_slide ) {
-		var slides_per_page = get_thumbs_per_page();
-
-		$( '.gallery__slide--active' ).removeClass( 'gallery__slide--active' );
-		$( '#preview-' + selected_slide ).addClass( 'gallery__slide--active' );
-		var selected_slide_group = Math.floor( selected_slide / slides_per_page );
-		$slide_paging_previews.cycle( 'goto', selected_slide_group );
-	}
-
-	function get_thumbs_per_page() {
-		var slides_per_page = 8;
-		if ( isMobile() ) {
-			slides_per_page = 3;
+			if ( isTablet() ) {
+				slides_per_page = 5;
+			}
+			return slides_per_page;
 		}
-		if ( isTablet() ) {
-			slides_per_page = 5;
+
+		/**
+		 * Update sharing links and short URL in sharing overlay
+		 */
+		function update_share_urls() {
+			var share_url, share_title;
+			if ( $( '#share-image' ).is( ':checked' ) ) {
+				share_url   = $( 'input.slide-url' ).val();
+				share_title = $( 'input.slide-title' ).val();
+			} else {
+				share_url   = $( 'input.gallery-url' ).val();
+				share_title = $( 'input.gallery-title' ).val();
+			}
+			var url_twitter  = 'http://twitter.com/home?status=' + share_url + '%20-%20' + share_title;
+			var url_facebook = 'http://www.facebook.com/sharer.php?u=' + share_url + '&amp;t=' + share_title;
+			var url_linkedin = '#';
+
+			$( '.gallery-toolbar .fa-twitter' ).attr( 'href', url_twitter );
+			$( '.gallery-toolbar .fa-facebook' ).attr( 'href', url_facebook );
+			$( '.gallery-toolbar .short-url' ).html( '<a href="' + share_url + '">' + share_url + '</a>' );
 		}
-		return slides_per_page;
-	}
 
-	/**
-	 * Update sharing links and short URL in sharing overlay
-	 */
-	function update_share_urls() {
-		var share_url, share_title;
-		if ( $( '#share-image' ).is( ':checked' ) ) {
-			share_url   = $( 'input.slide-url' ).val();
-			share_title = $( 'input.slide-title' ).val();
-		} else {
-			share_url   = $( 'input.gallery-url' ).val();
-			share_title = $( 'input.gallery-title' ).val();
-		}
-		var url_twitter  = 'http://twitter.com/home?status=' + share_url + '%20-%20' + share_title;
-		var url_facebook = 'http://www.facebook.com/sharer.php?u=' + share_url + '&amp;t=' + share_title;
-		var url_linkedin = '#';
+		bind_events();
 
-		$( '.gallery-toolbar .fa-twitter' ).attr( 'href', url_twitter );
-		$( '.gallery-toolbar .fa-facebook' ).attr( 'href', url_facebook );
-		$( '.gallery-toolbar .short-url' ).html( '<a href="' + share_url + '">' + share_url + '</a>' );
-	}
-
-	bind_events();
-
-	window.GMR_Gallery = {
-		bindEvents  : bind_events
+		window.GMR_Gallery = {
+			bindEvents  : bind_events
+		};
 	};
 
-})( jQuery, window );
+	$document.bind('pjax:end', function() {
+		$document.find('.cycle-slideshow').cycle();
+		__ready();
+	});
+	
+	$document.ready(__ready);
+})(jQuery, window);
--- themes/greatermedia/assets/js/greater_media.js
+++ themes/greatermedia/assets/js/greater_media.js
@@ -26,6 +26,44 @@
 	init();
 
 })(jQuery, window);
+(function($, location) {
+	var $document = $(document),
+		classes = {},
+		last_url = null,
+		current_url = location.href,
+		normalize_url;
+
+	normalize_url = function(url) {
+		return url.replace(/[\?\#].*$/g, '');
+	};
+
+	$document.bind('pjax:popstate', function() {
+		last_url = normalize_url(current_url);
+	});
+
+	$document.bind('pjax:beforeSend', function() {
+		last_url = normalize_url(location.href);
+	});
+
+	$document.bind('pjax:end', function(e, xhr, options) {
+		var $body = $('body'),
+			body_classes = false,
+			pattern = new RegExp('\<body.*?class=\"(.*?)\"', 'im');
+
+		classes[last_url] = $body.attr('class');
+
+		if (xhr) {
+			body_classes = pattern.exec(xhr.responseText);
+			if (body_classes && body_classes.length >= 2) {
+				$body.attr('class', body_classes[1]);
+			}
+		} else {
+			$body.attr('class', classes[normalize_url(options.url)]);
+		}
+
+		current_url = location.href;
+	});
+})(jQuery, location);
 (function() {
 
 	/**
@@ -40,7 +78,7 @@
 		header = document.getElementById( 'header' ),
 		headerHeight = header.offsetHeight,
 		livePlayer = document.getElementById( 'live-player__sidebar' ),
-		livePlayerStream = document.querySelector('.live-player__stream');
+		livePlayerStream = document.querySelector('.live-player__stream'),
 		livePlayerStreamSelect = document.querySelector( '.live-player__stream--current' ),
 		livePlayerStreamSelectHeight = livePlayerStreamSelect.offsetHeight,
 		livePlayerCurrentName = livePlayerStreamSelect.querySelector( '.live-player__stream--current-name' ),
