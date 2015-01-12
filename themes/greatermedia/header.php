f4e697d9e4117d5dff90b1e1c761b898070b3932
Chancey Mathews <chancey.mathews@10up.com>
2015-01-12 14:22:34 -0500

    Breaking News: Add text overflow clipping, fix text sizes, fix hover state, and generally try to make things line up with the rest of the site.

--- themes/greatermedia/header.php
+++ themes/greatermedia/header.php
@@ -43,6 +43,7 @@
 
 	<body <?php body_class(); ?>>
 		<div id="site-wrap" class="site-wrap">
+			<?php do_action( 'show_breaking_news_banner' ); ?>
 			<nav id="mobile-nav" class="mobile-nav">
 				<?php
 				$mobile_nav = array(
@@ -73,7 +74,6 @@
 			</div>
 			<div id="page-wrap" class="page-wrap">
 				<header id="header" class="header" role="banner">
-					<?php do_action( 'show_breaking_news_banner' ); ?>
 					<div class="container">
 						<div class="ad__leaderboard desktop">
 							<?php do_action( 'acm_tag', 'leaderboard-top-of-site' ); ?>
