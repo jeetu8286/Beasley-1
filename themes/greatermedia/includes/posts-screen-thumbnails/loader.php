9a394faf2667cfa7d59a5fd18575fce0e2c98e67
Chancey Mathews <chancey.mathews@10up.com>
2015-01-13 18:50:12 -0500

    Only enable thumbnail column on the galleries post list screen.

--- themes/greatermedia/includes/posts-screen-thumbnails/loader.php
+++ themes/greatermedia/includes/posts-screen-thumbnails/loader.php
@@ -11,5 +11,5 @@ namespace Greater_Media\Posts_Screen_Thumbnails;
 require __DIR__ . '/src/class-thumbnail-column.php'; 
 require __DIR__ . '/src/class-thumbnail-size.php'; 
 
-new Thumbnail_Column(); 
+new Thumbnail_Column( 'gmr_gallery' ); 
 new Thumbnail_Size(); 
