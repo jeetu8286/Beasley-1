dab0ec23d403ecf928d0b18d74f260759bd3e52a
EduardMaghakyan <eduard.maghakyan@10up.com>
2015-01-13 12:06:08 +0400

    moved js changes to parent theme

--- themes/wmgk/assets/js/src/wmgk.js
+++ themes/wmgk/assets/js/src/wmgk.js
@@ -9,13 +9,6 @@
  ( function( window, undefined ) {
 	'use strict';
 
-	 jQuery( function( $ ) {
-		$('.popup').on( 'click', function(event) {
-			event.preventDefault();
-			var x = screen.width/2 - 700/2;
-			var y = screen.height/2 - 450/2;
-			window.open( $(this).attr('href'), $(this).attr('href'), 'height=485,width=700,scrollbars=yes, resizable=yes,left='+x+ ',top='+y);
-		});
-	});
+
 	 
  } )( this );
