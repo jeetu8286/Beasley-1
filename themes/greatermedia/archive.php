8edea9aa9a9a6866abc34166fc8cdf05b51b6c31
Chancey Mathews <chancey.mathews@10up.com>
2015-01-13 17:25:57 -0500

    Modify greatermedia_load_more_button() to accept multiple arguments as an array, because we need more args and using function parameters is already getting out of hand. This little function is starting to get unwieldy. Also fix notices caused by unset/missing variables.

--- themes/greatermedia/archive.php
+++ themes/greatermedia/archive.php
@@ -24,7 +24,7 @@ get_header(); ?>
 				<?php if ( have_posts() ) :  ?>
 
 					<?php get_template_part( 'partials/loop' ); ?>
-					<?php greatermedia_load_more_button( 'partials/loop' ); ?>
+					<?php greatermedia_load_more_button( array( 'partial_slug' => 'partials/loop' ) ); ?>
 					<?php get_template_part( 'partials/pagination' ); ?>
 
 				<?php else : ?>
