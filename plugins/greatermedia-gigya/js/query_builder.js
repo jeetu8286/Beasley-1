c03d5ea876b8aad75d05b8c0b2187922b1a7ce6e
Darshan Sawardekar <darshan@10up.com>
2015-01-14 18:40:17 +0530

    Scrolls the query builder to the last added constraint.

--- plugins/greatermedia-gigya/js/query_builder.js
+++ plugins/greatermedia-gigya/js/query_builder.js
@@ -2259,6 +2259,11 @@ var ActiveConstraintsView = Backbone.CollectionView.extend({
 		}
 	},
 
+	initialize: function(options) {
+		Backbone.CollectionView.prototype.initialize.call(this, options);
+		this.listenTo(this.collection, 'add', this.didAdd);
+	},
+
 	copyConstraint: function(event, constraint) {
 		var newConstraint = constraint.clone();
 		var index         = this.collection.indexOf(constraint);
@@ -2268,7 +2273,25 @@ var ActiveConstraintsView = Backbone.CollectionView.extend({
 
 	removeConstraint: function(event, constraint) {
 		this.collection.remove(constraint);
-	}
+	},
+
+	render: function() {
+		Backbone.CollectionView.prototype.render.call(this);
+	},
+
+	didAdd: function(model) {
+		var view = this.viewManager.findByModel(model);
+		this.scrollTo(view.$el);
+	},
+
+	scrollTo: function($target) {
+		var root   = $('html, body');
+		var params = {
+			scrollTop: $target.offset().top - 60 // admin bar offset
+		};
+
+		root.animate(params, 500);
+	},
 
 });
 
--- plugins/greatermedia-gigya/js/src/views/active_constraints_view.js
+++ plugins/greatermedia-gigya/js/src/views/active_constraints_view.js
@@ -27,6 +27,11 @@ var ActiveConstraintsView = Backbone.CollectionView.extend({
 		}
 	},
 
+	initialize: function(options) {
+		Backbone.CollectionView.prototype.initialize.call(this, options);
+		this.listenTo(this.collection, 'add', this.didAdd);
+	},
+
 	copyConstraint: function(event, constraint) {
 		var newConstraint = constraint.clone();
 		var index         = this.collection.indexOf(constraint);
@@ -36,6 +41,24 @@ var ActiveConstraintsView = Backbone.CollectionView.extend({
 
 	removeConstraint: function(event, constraint) {
 		this.collection.remove(constraint);
-	}
+	},
+
+	render: function() {
+		Backbone.CollectionView.prototype.render.call(this);
+	},
+
+	didAdd: function(model) {
+		var view = this.viewManager.findByModel(model);
+		this.scrollTo(view.$el);
+	},
+
+	scrollTo: function($target) {
+		var root   = $('html, body');
+		var params = {
+			scrollTop: $target.offset().top - 60 // admin bar offset
+		};
+
+		root.animate(params, 500);
+	},
 
 });
