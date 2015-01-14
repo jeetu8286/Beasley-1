793f18ebaeb2e23130d1049af3194401a7c4da20
AndrewMowe <andrew@team-eight.com>
2015-01-14 00:11:40 -0800

    refactor podcast markup to include mobile-specific markup, restyle for responsive, add hologram component

--- plugins/greatermedia-live-player/includes/audio-shortcodes.php
+++ plugins/greatermedia-live-player/includes/audio-shortcodes.php
@@ -88,6 +88,12 @@ class GMR_Audio_Shortcodes {
 		if( $is_podcast && ( $downloadable == 'on' || $downloadable == '' ) ) {
 			$new_html .= '<div class="podcast__download">';
 			$new_html .= '<a href="' . esc_attr( $mp3_src ) . '" download="' . esc_attr( $mp3_src ) . '" class="podcast__download--btn" download>Download</a>';
+			if( $parent_podcast_id && $is_podcast ) {
+				if( $itunes_url != '' ) {
+					$new_html .= '<a class="podcast__subscribe show-mobile" href="' . esc_url( $itunes_url ) . '" target="_blank">Subscribe in iTunes</a>';
+				}
+				$new_html .= '<a class="podcast__rss show-mobile" href="' . esc_url( $feed_url ) . '" target="_blank">Podcast Feed</a>';
+			}
 			$new_html .= '</div>';
 		}
 		// if( $featured_image ) {
@@ -102,7 +108,8 @@ class GMR_Audio_Shortcodes {
 			$new_html .= '<h3 class="podcast__title">' . esc_html( $metadata['title'] ) . '</h3>';			
 		}
 		if( $parent_podcast_id && $is_podcast ) {
-			$new_html .= '<div class="podcast__parent"><div class="podcast__parent--title">'. esc_html( $parent_podcast->post_title ) . '</div>';
+			$new_html .= '<div class="podcast__parent--title podcast__parent--title--mobile show-mobile">'. esc_html( $parent_podcast->post_title ) . '</div>';
+			$new_html .= '<div class="podcast__parent hide-mobile"><div class="podcast__parent--title">'. esc_html( $parent_podcast->post_title ) . '</div>';
 			if( $itunes_url != '' ) {
 				$new_html .= '<a class="podcast__subscribe" href="' . esc_url( $itunes_url ) . '" target="_blank">Subscribe in iTunes</a>';
 			}
