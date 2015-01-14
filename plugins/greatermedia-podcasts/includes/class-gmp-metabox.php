8b13a8eb573410268031e0a0ccbc6beb1bfcddb6
EduardMaghakyan <eduard.maghakyan@10up.com>
2015-01-14 18:24:54 +0400

    added podcast feed settings, updated feed template

--- plugins/greatermedia-podcasts/includes/class-gmp-metabox.php
+++ plugins/greatermedia-podcasts/includes/class-gmp-metabox.php
@@ -29,13 +29,22 @@ class GMP_Meta {
 		if ( in_array( $post_type, $post_types ) ) {
 			add_meta_box(
 				'gmp_episodes_meta_box'
-				, __( 'Podcast Episode Details', 'gmpodcasts' )
+				, __( 'Podcast Details', 'gmpodcasts' )
 				, array( $this, 'render_meta_box_content' )
 				, $post_type
-				, 'side'
+				, 'normal'
 				, 'high'
 			);
 		}
+
+		add_meta_box(
+				'gmp_episodes_meta_box'
+				, __( 'Episode Details', 'gmpodcasts' )
+				, array( $this, 'render_episode_meta_box_content' )
+				, GMP_CPT::EPISODE_POST_TYPE
+				, 'normal'
+				, 'high'
+			);
 
 	}
 
@@ -66,6 +75,28 @@ class GMP_Meta {
 
 		// Sanitize and save the user input.
 		update_post_meta( $post_id, 'gmp_podcast_itunes_url', esc_url_raw( $itunes_url ) );
+
+
+		if ( isset( $_POST['gmp_subtitle'] ) ) {
+			$gmp_subtitle =  sanitize_text_field( $_POST['gmp_subtitle'] );
+			update_post_meta( $post_id, 'gmp_subtitle', esc_html( $gmp_subtitle ) );
+		}
+
+		if ( isset( $_POST['gmp_explicit'] ) ) {
+			$gmp_subtitle =  sanitize_text_field( $_POST['gmp_explicit'] );
+			update_post_meta( $post_id, 'gmp_explicit', esc_html( $gmp_subtitle ) );
+		}
+
+		if ( isset( $_POST['gmp_category'] ) ) {
+			$gmp_category =  sanitize_text_field( $_POST['gmp_category'] );
+			update_post_meta( $post_id, 'gmp_category', esc_html( $gmp_category ) );
+		}
+		
+		if ( isset( $_POST['gmp_sub_category'] ) ) {
+			$gmp_sub_category =  sanitize_text_field( $_POST['gmp_sub_category'] );
+			update_post_meta( $post_id, 'gmp_sub_category', esc_html( $gmp_sub_category ) );
+		}
+		
 	}
 
 	/**
@@ -80,6 +111,10 @@ class GMP_Meta {
 
 		// Use get_post_meta to retrieve an existing value from the database.
 		$itunes_url = esc_url( get_post_meta( $post->ID, 'gmp_podcast_itunes_url', true ) );
+		$gmp_subtitle = sanitize_text_field( get_post_meta( $post->ID, 'gmp_subtitle', true ) );
+		$gmp_explicit = sanitize_text_field( get_post_meta( $post->ID, 'gmp_explicit', true ) );
+		$gmp_category = sanitize_text_field( get_post_meta( $post->ID, 'gmp_category', true ) );
+		$gmp_sub_category = sanitize_text_field( get_post_meta( $post->ID, 'gmp_sub_category', true ) );
 
 		?>
 
@@ -91,9 +126,74 @@ class GMP_Meta {
 				</div>
 			</div>
 		</div>
+		<br>
+		<div class="gmp-meta-row">
+			<div class="gmp-meta-row-content gmp-upload">
+				<label for="gmp_subtitle" class="gmp-meta-row-label"><?php _e( 'Podcast Subtitle:', 'gmpodcasts' ); ?></label>
+				<input type="text" id="gmp_subtitle" name="gmp_subtitle" value="<?php echo esc_attr( $gmp_subtitle ); ?>"/>
+			</div>
+		</div>
+		<br>
+		<div class="gmp-meta-row">
+			<div class="gmp-meta-row-content gmp-upload">
+				<label for="gmp_explicit" class="gmp-meta-row-label"><?php _e( 'Explicit:', 'gmpodcasts' ); ?></label>
+				<input type="checkbox" id="gmp_explicit" name="gmp_explicit" <?php checked( 'on', $gmp_explicit, true ); ?>/>
+			</div>
+		</div>
+		<br>
+		<div class="gmp-meta-row">
+			<div class="gmp-meta-row-content gmp-upload">
+				<label for="gmp_category" class="gmp-meta-row-label">
+					<?php _e( 'iTunes Category:', 'gmpodcasts' ); ?>
+				</label>
+				<input type="text" id="gmp_category" name="gmp_category" value="<?php echo esc_attr( $gmp_category ); ?>" />
+				<br>
+				<?php echo '<span class="description">' . sprintf( __( 'Your podcast\'s category - use one of the first-tier categories from %1$sthis list%2$s.' , 'ss-podcasting' ) , '<a href="' . esc_url( 'http://www.apple.com/itunes/podcasts/specs.html#categories' ) . '" target="' . esc_attr( '_blank' ) . '">' , '</a>' ) . '</span>'; ?>
+			</div>
+		</div>
+		<br>		
+		<div class="gmp-meta-row">
+			<div class="gmp-meta-row-content gmp-upload">
+				<label for="gmp_sub_category" class="gmp-meta-row-label">
+					<?php _e( 'iTunes Sub-Category:', 'gmpodcasts' ); ?>
+				</label>
+				<input type="text" id="gmp_sub_category" name="gmp_sub_category" value="<?php echo esc_attr( $gmp_sub_category ); ?>"/>
+				<br>
+				<?php echo '<span class="description">' . sprintf( __( 'Your podcast\'s sub-category - use one of the second-tier categories from %1$sthis list%2$s (must be a sub-category of your selected primary category).' , 'ss-podcasting' ) , '<a href="' . esc_url( 'http://www.apple.com/itunes/podcasts/specs.html#categories' ) . '" target="' . esc_attr( '_blank' ) . '">' , '</a>' ) . '</span>'; ?>
+			</div>
+		</div>
 
 	<?php
 
+	}
+
+
+	public function render_episode_meta_box_content( $post ) {
+		
+		// Add an nonce field so we can check for it later.
+		wp_nonce_field( 'gmp_episodes_meta_box', 'gmp_episodes_meta_box_nonce' );
+
+		// Use get_post_meta to retrieve an existing value from the database.
+		$gmp_explicit = sanitize_text_field( get_post_meta( $post->ID, 'gmp_episode_explicit', true ) );
+		$gmp_block = sanitize_text_field( get_post_meta( $post->ID, 'gmp_block', true ) );
+
+		?>
+		<div class="gmp-meta-row">
+			<div class="gmp-meta-row-content gmp-upload">
+				<label for="gmp_explicit" class="gmp-meta-row-label"><?php _e( 'Explicit:', 'gmpodcasts' ); ?></label>
+				<input type="checkbox" id="gmp_explicit" name="gmp_explicit" <?php checked( 'on', $gmp_explicit, true ); ?>/>
+			</div>
+		</div>
+		<br>
+
+		<div class="gmp-meta-row">
+			<div class="gmp-meta-row-content gmp-upload">
+				<label for="gmp_block" class="gmp-meta-row-label"><?php _e( 'Block From Feed:', 'gmpodcasts' ); ?></label>
+				<input type="checkbox" id="gmp_block" name="gmp_block" <?php checked( 'on', $gmp_block, true ); ?>/>
+			</div>
+		</div>
+
+	<?php
 	}
 
 	/**
