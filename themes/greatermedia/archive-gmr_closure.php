76de23dc9f3a51dd966c9ee32537edd1a965e6d7
Chancey Mathews <chancey.mathews@10up.com>
2015-01-15 17:19:05 -0500

    Specify partial_name argument for greatermedia_load_more_button() (corresponds to the second parameter of get_template_part()) so that the Ajax-loaded stuff uses the correct template.

--- themes/greatermedia/archive-gmr_closure.php
+++ themes/greatermedia/archive-gmr_closure.php
@@ -38,7 +38,7 @@ get_header(); ?>
 			<?php if ( have_posts() ) : ?>
 
 				<?php get_template_part( 'partials/loop', 'gmr_closure' ); ?>
-				<?php greatermedia_load_more_button( array( 'partial_slug' => 'partials/loop', 'auto_load' => true ) ); ?>
+				<?php greatermedia_load_more_button( array( 'partial_slug' => 'partials/loop', 'partial_name' => 'gmr_closure', 'auto_load' => true ) ); ?>
 				<?php get_template_part( 'partials/pagination' ); ?>
 				
 			<?php else : ?>
