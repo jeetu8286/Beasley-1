0797f084ebac9f66b49ce6e51cd486cc024353a5
Allen Moore <allen@10up.com>
2015-01-16 12:36:25 -0500

    change copy for pre-roll notification

--- plugins/greatermedia-live-player/includes/class-gmlp-player.php
+++ plugins/greatermedia-live-player/includes/class-gmlp-player.php
@@ -40,7 +40,7 @@ class GMLP_Player {
 			<div id="live-stream__container" class="live-stream__container">
 				<div id="td_container" class="live-stream__container--player">
 				</div>
-				<div class="pre-roll__notification">Live stream will be available after this ad in 15 seconds</div>
+				<div class="pre-roll__notification"><?php _e( 'Live stream will be available after this brief ad from our sponsors', ' gmliveplayer' ); ?></div>
 			</div>
 
 		</div>
