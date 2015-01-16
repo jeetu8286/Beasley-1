02ac7e49a610d89ec7ca8efeb1585c57b4bda404
Darshan Sawardekar <darshan@10up.com>
2015-01-16 18:14:15 +0530

    Adds LiveFyre modq management page.

--- plugins/greatermedia-livefyre/includes/GreaterMedia/LiveFyre/Plugin.php
+++ plugins/greatermedia-livefyre/includes/GreaterMedia/LiveFyre/Plugin.php
@@ -26,6 +26,31 @@ class Plugin {
 		$settings_page->register();
 
 		remove_menu_page( 'edit-comments.php' );
+
+		$livefyre_admin_url = $this->get_livefyre_admin_url();
+		if ( $livefyre_admin_url !== '' ) {
+			$this->add_comments_mod_menu( $livefyre_admin_url );
+		}
+	}
+
+	function add_comments_mod_menu( $url ) {
+		add_management_page(
+			'LiveFyre ModQ',
+			'LiveFyre ModQ',
+			'manage_options',
+			'livefyre-admin-comments'
+		);
+
+		global $submenu;
+		$settings_menu = $submenu['tools.php'];
+
+		foreach ( $settings_menu as $key => $menu ) {
+			if ( $menu[2] === 'livefyre-admin-comments' ) {
+				$submenu['tools.php'][ $key ] = array(
+					'LiveFyre ModQ', 'manage_options', $url,
+				);
+			}
+		}
 	}
 
 	function register_ajax_handlers() {
@@ -40,6 +65,25 @@ class Plugin {
 
 	function is_ajax_request() {
 		return defined( 'DOING_AJAX' ) && DOING_AJAX;
+	}
+
+	function get_livefyre_admin_url() {
+		$settings = $this->get_livefyre_settings();
+
+		if ( $settings ) {
+			$network_name  = $settings['network_name'];
+			$network_admin = str_replace( 'fyre.co', 'admin.fyre.co', $network_name );
+			return "https://$network_admin/v3/modq";
+		} else {
+			return '';
+		}
+	}
+
+	function get_livefyre_settings() {
+		$settings = get_option( 'livefyre_settings' );
+		$settings = json_decode( $settings, true );
+
+		return $settings;
 	}
 
 }
