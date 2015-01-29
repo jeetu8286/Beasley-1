edc24bc45820e4d56249a9b3db04b4ca7621d1c3
Eugene Manuilov <eugene.manuilov@10up.com>
2015-01-29 16:26:40 +0200

    Improved blogroll widget scroll UX

--- plugins/greatermedia-live-stream/assets/js/src/blogroll-widget.js
+++ plugins/greatermedia-live-stream/assets/js/src/blogroll-widget.js
@@ -1,18 +1,19 @@
 (function ($, gmr) {
-	var refresh_widget, active_stream;
+	var $document = $(document), refresh_widget, active_stream;
 
 	refresh_widget = function() {
 		$.ajax(gmr.ajaxurl + active_stream + '/').done(function(response) {
 			$('.widget_gmr_blogroll_widget').html(response);
+			$document.trigger('blogroll-widget-updated');
 		});
 	};
 
-	$(document).ready(function () {
+	$document.ready(function () {
 		active_stream = $('.live-player__stream--current-name').text();
 		window.setInterval(refresh_widget, gmr.interval);
 	});
 
-	$(document).bind('live-player-stream-changed', function(e) {
+	$document.bind('live-player-stream-changed', function(e) {
 		active_stream = e.originalEvent.detail;
 		refresh_widget();
 	});
--- themes/greatermedia/assets/js/src/greater_media.js
+++ themes/greatermedia/assets/js/src/greater_media.js
@@ -9,7 +9,7 @@
 	/**
 	 * global variables
 	 *
-	 * @type {HTMLElement}
+	 * @type {jQuery}
 	 */
 	var $ = jQuery;
 
@@ -129,6 +129,7 @@
 			}
 			if (liveLinksWidget != null ) {
 				liveLinksWidget.style.maxHeight = windowHeight(window) - wpAdminHeight - elemHeight(header) - elemHeight(livePlayerStreamSelect) - elemHeight(liveStream) - elemHeight(liveLinksWidgetTitle) - elemHeight(nowPlaying) + 'px';
+				$(document).trigger('live-links-height-changed');
 			}
 		} else {
 			if (livePlayer != null ) {
@@ -140,6 +141,7 @@
 			}
 			if (liveLinksWidget != null ) {
 				liveLinksWidget.style.maxHeight = windowHeight(window) - elemHeight(header) - elemHeight(livePlayerStreamSelect) - elemHeight(liveStream) - elemHeight(liveLinksWidgetTitle) - elemHeight(nowPlaying) + 'px';
+				$(document).trigger('live-links-height-changed');
 			}
 		}
 		if (livePlayer != null ) {
@@ -162,6 +164,7 @@
 			}
 			if (liveLinksWidget != null ) {
 				liveLinksWidget.style.maxHeight = windowHeight(window) - elemTopOffset(header) - elemHeight(livePlayerStreamSelect) - elemHeight(liveStream) - elemHeight(liveLinksWidgetTitle) - elemHeight(nowPlaying) + 'px';
+				$(document).trigger('live-links-height-changed');
 			}
 		} else {
 			if (livePlayer != null ) {
@@ -173,6 +176,7 @@
 			}
 			if (liveLinksWidget != null ) {
 				liveLinksWidget.style.maxHeight = windowHeight(window) - elemHeightOffset(header) - elemHeight(livePlayerStreamSelect) - elemHeight(liveStream) - elemHeight(liveLinksWidgetTitle) - elemHeight(nowPlaying) + 'px';
+				$(document).trigger('live-links-height-changed');
 			}
 		}
 		if (livePlayer != null ) {
@@ -195,6 +199,7 @@
 			}
 			if (liveLinksWidget != null ) {
 				liveLinksWidget.style.maxHeight = windowHeight(window) - wpAdminHeight - elemHeight(livePlayerStreamSelect) - elemHeight(liveStream) - elemHeight(liveLinksWidgetTitle) - elemHeight(nowPlaying) + 'px';
+				$(document).trigger('live-links-height-changed');
 			}
 		} else {
 			if (livePlayer != null ) {
@@ -206,6 +211,7 @@
 			}
 			if (liveLinksWidget != null ) {
 				liveLinksWidget.style.maxHeight = windowHeight(window) - elemHeight(livePlayerStreamSelect) - elemHeight(liveStream) - elemHeight(liveLinksWidgetTitle) - elemHeight(nowPlaying) + 'px';
+				$(document).trigger('live-links-height-changed');
 			}
 		}
 		if (livePlayer != null ) {
@@ -526,6 +532,26 @@
 
 			return false;
 		});
+
+		var _slim_scroll = function() {
+			if ($.fn.slimScroll) {
+				$('.widget_gmr_blogroll_widget ul').slimScroll({
+					height: 'auto',
+					color: false,
+					railColor: false,
+					alwaysVisible: true,
+					railVisible: true,
+					wrapperClass: 'slimscroll--wrapper',
+					barClass: 'slimscroll--bar',
+					railClass: 'slimscroll--rail',
+					borderRadius: 0,
+					railBorderRadius: 0
+				});
+			}
+		};
+
+		$(document).on('live-links-height-changed', _slim_scroll);
+		$(document).on('blogroll-widget-updated', _slim_scroll);
 	})(jQuery);
 
 	function personality_toggle() {
