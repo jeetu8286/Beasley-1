1078e3515b449cbf6b391db2f8cc994d4ebbeb02
Allen Moore <allen@10up.com>
2015-02-12 11:08:56 -0500

    add featured content areas to the front page

---
+++ themes/thefanatic/partials/frontpage-featured.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Partial for the Front Page Featured Content
+ *
+ * @package Greater Media
+ * @since   0.1.0
+ */
+?>
+<section id="featured" class="home__featured">
+		<?php
+		$hp_featured_query = \GreaterMedia\HomepageCuration\get_featured_query();
+
+		/* if ( $hp_featured_query->have_posts() ) : $hp_featured_query->the_post(); */ ?>
+			<div class="featured__article">
+				<a href="#<?php /* the_permalink(); */?>" class="featured__article--link">
+					<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
+						<?php /*
+
+							$image_attr = image_attribution();
+
+							if ( ! empty( $image_attr ) ) {
+								echo $image_attr;
+							} */
+
+						?>
+					</div>
+					<div class="featured__article--content">
+						<div class="featured__article--heading">
+							What Chris Christie might talk about in 2015 State of the State speech today.
+							<?php /* the_title(); */ ?>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="featured__article">
+				<a href="#<?php /* the_permalink(); */?>">
+					<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
+						<?php /*
+
+									$image_attr = image_attribution();
+
+									if ( ! empty( $image_attr ) ) {
+										echo $image_attr;
+									} */
+
+						?>
+					</div>
+					<div class="featured__article--content">
+						<div class="featured__article--heading">
+							What Chris Christie might talk about in 2015 State of the State speech today.
+							<?php /* the_title(); */ ?>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="featured__article">
+				<a href="#<?php /* the_permalink(); */?>">
+					<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
+						<?php /*
+
+									$image_attr = image_attribution();
+
+									if ( ! empty( $image_attr ) ) {
+										echo $image_attr;
+									} */
+
+						?>
+					</div>
+					<div class="featured__article--content">
+						<div class="featured__article--heading">
+							What Chris Christie might talk about in 2015 State of the State speech today.
+							<?php /* the_title(); */ ?>
+						</div>
+					</div>
+				</a>
+			</div>
+		<?php /* endif; */?>
+		<?php // if we still have more posts (we almost always will), render the 3 below the main section ?>
+		<?php if ( $hp_featured_query->have_posts() ) : ?>
+			<div class="featured__content">
+				<?php while ( $hp_featured_query->have_posts() ) : $hp_featured_query->the_post(); ?>
+					<div class="featured__content--block">
+						<a href="<?php the_permalink(); ?>">
+							<div class="featured__content--image" style='background-image: url(<?php gm_post_thumbnail_url( 'gmr-featured-secondary' ) ?>)'></div>
+							<div class="featured__content--meta">
+								<h2 class="featured__content--title"><?php the_title(); ?></h2>
+								<div class="featured__content--link">
+									<span class="featured__content--btn">Read More</span>
+								</div>
+							</div>
+						</a>
+					</div>
+				<?php endwhile; ?>
+			</div>
+		<?php endif; ?>
+		<?php wp_reset_query(); ?>
+</section>
