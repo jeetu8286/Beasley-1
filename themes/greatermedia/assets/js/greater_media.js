660e598763e37e6d50da798364734f0faece7768
Allen Moore <allen@10up.com>
2015-02-17 10:45:52 -0500

    add notes to js functions

--- themes/greatermedia/assets/js/greater_media.js
+++ themes/greatermedia/assets/js/greater_media.js
@@ -1117,6 +1117,9 @@
 	$(document).ready(function() {
 		//showBlocker();
 
+		/**
+		 * Resolves issue that requires a double click on a sub-menu link on iOS.
+		 */
 		$('.sub-menu li a').on('click touchend', function(e) {
 			var el = $(this);
 			var link = el.attr('href');
