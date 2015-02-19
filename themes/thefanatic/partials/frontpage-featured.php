b133b4a391a7cf198b2b7468df976b4023638b7f
Allen Moore <allen@10up.com>
2015-02-19 11:53:35 -0500

    adjust featured areas on front page of news/sports theme to match new design

--- themes/thefanatic/partials/frontpage-featured.php
+++ themes/thefanatic/partials/frontpage-featured.php
@@ -11,68 +11,72 @@
 		$hp_featured_query = \GreaterMedia\HomepageCuration\get_featured_query(); ?>
 
 		<div class="featured__articles">
-			<div class="featured__article">
-				<a href="#<?php /* the_permalink(); */?>" class="featured__article--link">
-					<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
-						<?php /*
+			<div class="featured__article--primary">
+				<div class="featured__article">
+					<a href="#<?php /* the_permalink(); */?>" class="featured__article--link">
+						<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
+							<?php /*
 
-							$image_attr = image_attribution();
+								$image_attr = image_attribution();
 
-							if ( ! empty( $image_attr ) ) {
-								echo $image_attr;
-							} */
+								if ( ! empty( $image_attr ) ) {
+									echo $image_attr;
+								} */
 
-						?>
-					</div>
-					<div class="featured__article--content">
-						<div class="featured__article--heading">
-							What Chris Christie might talk about in 2015 State of the State speech today.
-							<?php /* the_title(); */ ?>
+							?>
 						</div>
-					</div>
-				</a>
+						<div class="featured__article--content">
+							<div class="featured__article--heading">
+								What Chris Christie might talk about in 2015 State of the State speech today.
+								<?php /* the_title(); */ ?>
+							</div>
+						</div>
+					</a>
+				</div>
 			</div>
-			<div class="featured__article">
-				<a href="#<?php /* the_permalink(); */?>">
-					<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
-						<?php /*
+			<div class="featured__article--secondary">
+				<div class="featured__article">
+					<a href="#<?php /* the_permalink(); */?>" class="featured__article--link">
+						<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
+							<?php /*
 
-									$image_attr = image_attribution();
+										$image_attr = image_attribution();
 
-									if ( ! empty( $image_attr ) ) {
-										echo $image_attr;
-									} */
+										if ( ! empty( $image_attr ) ) {
+											echo $image_attr;
+										} */
 
-						?>
-					</div>
-					<div class="featured__article--content">
-						<div class="featured__article--heading">
-							What Chris Christie might talk about in 2015 State of the State speech today.
-							<?php /* the_title(); */ ?>
+							?>
 						</div>
-					</div>
-				</a>
-			</div>
-			<div class="featured__article">
-				<a href="#<?php /* the_permalink(); */?>">
-					<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
-						<?php /*
-
-									$image_attr = image_attribution();
-
-									if ( ! empty( $image_attr ) ) {
-										echo $image_attr;
-									} */
-
-						?>
-					</div>
-					<div class="featured__article--content">
-						<div class="featured__article--heading">
-							What Chris Christie might talk about in 2015 State of the State speech today.
-							<?php /* the_title(); */ ?>
+						<div class="featured__article--content">
+							<div class="featured__article--heading">
+								What Chris Christie might talk about in 2015 State of the State speech today.
+								<?php /* the_title(); */ ?>
+							</div>
 						</div>
-					</div>
-				</a>
+					</a>
+				</div>
+				<div class="featured__article">
+					<a href="#<?php /* the_permalink(); */?>" class="featured__article--link">
+						<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
+							<?php /*
+
+										$image_attr = image_attribution();
+
+										if ( ! empty( $image_attr ) ) {
+											echo $image_attr;
+										} */
+
+							?>
+						</div>
+						<div class="featured__article--content">
+							<div class="featured__article--heading">
+								What Chris Christie might talk about in 2015 State of the State speech today.
+								<?php /* the_title(); */ ?>
+							</div>
+						</div>
+					</a>
+				</div>
 			</div>
 		</div>
 		<?php // if we still have more posts (we almost always will), render the 3 below the main section ?>
