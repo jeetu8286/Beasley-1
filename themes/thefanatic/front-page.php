0edaa9d9ebedadd75be12fce0d1ac3486c21c95e
Allen Moore <allen@10up.com>
2015-02-19 17:47:00 -0500

    adjust blog roll styles to match news/sports theme

---
+++ themes/thefanatic/front-page.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The front page template file
+ *
+ * @package Greater Media
+ * @since   0.1.0
+ */
+
+get_header();
+
+?>
+
+	<main class="main" role="main">
+
+		<div class="container">
+
+			<?php get_template_part( 'partials/frontpage', 'featured' ); ?>
+			<?php get_template_part( 'partials/frontpage', 'highlights' ); ?>
+
+			<section class="entries">				
+				<?php /* get_template_part( 'partials/ad-in-loop' ); */ ?>
+
+				<h2 class="content__heading">News</h2>
+
+				<?php if ( have_posts() ) : ?>
+
+					<?php get_template_part( 'partials/loop', 'front-page' ); ?>
+					<?php greatermedia_load_more_button( array( 'partial_slug' => 'partials/loop', 'auto_load' => true ) ); ?>
+
+				<?php else : ?>
+
+					<article id="post-not-found" class="hentry cf">
+
+						<header class="article-header">
+
+							<h1><?php _e( 'Oops, Post Not Found!', 'greatermedia' ); ?></h1>
+
+						</header>
+
+						<section class="entry-content">
+
+							<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'greatermedia' ); ?></p>
+
+						</section>
+
+					</article>
+
+				<?php endif; ?>
+
+			</section>
+
+		</div>
+
+	</main>
+
+<?php get_footer(); ?>
---
+++ themes/thefanatic/partials/article.php
@@ -0,0 +1,64 @@
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<div class="container">
+
+		<?php if ( has_post_thumbnail() && ! \Greater_Media\Gallery_Post_Thumbnails\post_has_gallery() ): ?>
+			<div class="article__thumbnail" style='background-image: url(<?php gm_post_thumbnail_url( 'full' ); ?>)'>
+				<?php
+
+					$image_attr = image_attribution();
+
+					if ( ! empty( $image_attr ) ) {
+						echo $image_attr;
+					}
+
+				?>
+			</div>
+		<?php endif; ?>
+
+		<section class="content">
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class( 'article cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+				<div class="ad__inline--right desktop">
+					<?php do_action( 'acm_tag_gmr_variant', 'mrec-body', 'desktop', array( 'min_width' => 1024 ) ); ?>
+				</div>
+
+				<header class="article__header">
+
+					<time class="article__date" datetime="<?php echo get_the_time(); ?>"><?php the_date('F j'); ?></time>
+					<h2 class="article__title" itemprop="headline"><?php the_title(); ?></h2>
+					<?php get_template_part( 'partials/social-share' ); ?>
+
+				</header>
+
+				<section class="article__content" itemprop="articleBody">
+
+					<?php the_content(); ?>
+
+				</section>
+
+				<?php get_template_part( 'partials/article-footer' ); ?>
+
+				<div class="ad__inline--right mobile">
+					<?php do_action( 'acm_tag_gmr_variant', 'mrec-body', 'mobile', array( 'max_width' => 1023 ) ); ?>
+				</div>
+
+				<?php if ( post_type_supports( get_post_type(), 'comments' ) ) { // If comments are open or we have at least one comment, load up the comment template. ?>
+					<div class='article__comments'>
+						<?php comments_template(); ?>
+					</div>
+				<?php } ?>
+
+
+				<?php if ( function_exists( 'related_posts' ) ): ?>
+					<?php related_posts( array( 'template' => 'partials/related-posts.php' ) ); ?>
+				<?php endif; ?>
+
+			</article>
+
+		</section>
+
+	</div>
+
+<?php endwhile; ?>
