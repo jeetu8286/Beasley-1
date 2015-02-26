e9a6738bfc3fd7e511f271c3bedf8f85c316e051
Eugene Manuilov <eugene.manuilov@10up.com>
2015-02-26 16:52:04 +0200

    Increased assets version.

--- themes/greatermedia/functions.php
+++ themes/greatermedia/functions.php
@@ -20,7 +20,7 @@
 if ( defined( 'GMR_PARENT_ENV' ) && 'dev' == GMR_PARENT_ENV ) {
 	define( 'GREATERMEDIA_VERSION', time() );
 } else {
-	define( 'GREATERMEDIA_VERSION', '0.1.5' );
+	define( 'GREATERMEDIA_VERSION', '0.1.6' );
 }
 
 add_theme_support( 'homepage-curation' );
