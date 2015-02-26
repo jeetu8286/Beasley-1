267b9a9c77066a6a240e723be76c317f63a56e70
Eugene Manuilov <eugene.manuilov@10up.com>
2015-02-26 14:22:06 +0200

    Fixed podcast "load more" button.

--- themes/greatermedia/partials/loop-gmr_podcast_episodes.php
+++ themes/greatermedia/partials/loop-gmr_podcast_episodes.php
@@ -1,28 +1,35 @@
 <?php
-	global $post;
-	$current_page = get_query_var( 'page' ) ?: 1;
-	$args = array(
-			'post_type' => GMP_CPT::EPISODE_POST_TYPE,
-			'post_parent' => $post->ID,
-			'paged' => $current_page,
-			'posts_per_page' => 50, //TODO: Temporary until loadmore is fixed on podcasts page
-		);
 
-	$query = new WP_Query( $args );
+global $gmr_loadmore_num_pages, $gmr_loadmore_post_count;
 
-	while( $query->have_posts() ) : $query->the_post(); ?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf episode' ); ?> role="article" itemscope itemtype="http://schema.org/OnDemandEvent">
-		<?php GMP_Player::render_podcast_episode(); ?>
-	</article>
-	<?php
+$episodes_query = new WP_Query( array(
+	'post_type'      => GMP_CPT::EPISODE_POST_TYPE,
+	'post_parent'    => get_the_ID(),
+	'paged'          => get_query_var( 'paged' ) ?: 1,
+	'posts_per_page' => get_option( 'posts_per_page', 10 ),
+) );
+
+if ( $episodes_query->have_posts() ) :
+
+	while( $episodes_query->have_posts() ) :
+		$episodes_query->the_post();
+
+		?><article id="post-<?php the_ID(); ?>" <?php post_class( 'cf episode' ); ?> role="article" itemscope itemtype="http://schema.org/OnDemandEvent">
+			<?php GMP_Player::render_podcast_episode(); ?>
+		</article><?php
 	endwhile;
-	greatermedia_load_more_button(
-		array(
-			'page_link_template' => home_url( 'episode/page/%d/' ),
-			'partial_slug' => 'partials/loop-gmr_podcast_episode',
-			'auto_load' => false,
-			'query' => $query
-			)
-		);
+
 	wp_reset_query();
-	?>
+
+	greatermedia_load_more_button( array(
+		'page_link_template' => str_replace( PHP_INT_MAX, '%d', get_pagenum_link( PHP_INT_MAX ) ),
+		'partial_slug'       => 'partials/loop',
+		'partial_name'       => 'gmr_podcast_episodes',
+		'auto_load'          => false,
+		'query'              => $episodes_query,
+	) );
+
+	$gmr_loadmore_num_pages = $episodes_query->max_num_pages;
+	$gmr_loadmore_post_count = $episodes_query->post_count;
+
+endif;
