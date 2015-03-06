ba093182ffa804c2500611618dc2217bd1be2ed1
Allen Moore <allen@10up.com>
2015-03-06 12:11:36 -0500

    adjust featured areas at breakpoints

--- themes/thefanatic/partials/frontpage-highlights.php
+++ themes/thefanatic/partials/frontpage-highlights.php
@@ -15,36 +15,29 @@
 
 			<h2 class="highlights__heading"><?php _e( 'Upcoming Events', 'greatermedia' ); ?></h2>
 
-			<?php
-			$events_query = \GreaterMedia\HomepageCuration\get_events_query();
-			while( $events_query->have_posts() ) : $events_query->the_post(); ?>
-				<div class="highlights__event--item">
-					<a href="<?php the_permalink(); ?>">
-						<?php if ( has_post_thumbnail() ) : ?>
-							<div class="highlights__event--thumb" style='background-image: url(<?php gm_post_thumbnail_url( 'gmr-featured-secondary' ) ?>)'></div>
-						<?php endif; ?>
+			<div class="highlights__event--item">
+				<a href="#">
+					<div class="highlights__event--thumb" style='background-image: url(http://lorempixel.com/200/200/people/1)'></div>
 
-						<div class="highlights__event--meta">
-							<h3 class="highlights__event--title"><?php the_title(); ?></h3>
-							<?php
-							/*
-							 * Moved the class from the span to the time so I could add both the start and end times to the datetime attributes
-							 */
-							$start = tribe_get_start_date( get_the_ID(), false, 'M d' );
-							$start_c = tribe_get_start_date( get_the_ID(), false, 'c' );
-							$end = tribe_get_end_date( get_the_ID(), false, 'M d' );
-							$end_c = tribe_get_end_date( get_the_ID(), false, 'c' );
-							if ( $start != $end ) {
-								printf( '<span class="highlights__event--date"><time datetime="%1$s">%2$s</time> - <time datetime="%3$s">%4$s</time></span>', $start_c, $start, $end_c, $end );
-							} else {
-								printf( '<span class="highlights__event--date"><time datetime="%1$s">%2$s</time></span>', $start_c, $start );
-							}
-							?>
-						</div>
-					</a>
-				</div>
-			<?php endwhile; ?>
-			<?php wp_reset_query(); ?>
+					<div class="highlights__event--meta">
+						<h3 class="highlights__event--title">Hey Kid, It's Santa!</h3>
+						<div class="highlights__event--date"><time datetime="">Wed, Dec 20th</time></div>
+						<div class="highlights__event--time"><time datetime="">8AM - 5PM</time></div>
+					</div>
+				</a>
+			</div>
+
+			<div class="highlights__event--item">
+				<a href="#">
+					<div class="highlights__event--thumb" style='background-image: url(http://lorempixel.com/200/200/people/2)'></div>
+
+					<div class="highlights__event--meta">
+						<h3 class="highlights__event--title">Winter Season Jewelry Sale</h3>
+						<div class="highlights__event--date"><time datetime="">Fri, Dec 22th</time></div>
+						<div class="highlights__event--time"><time datetime="">4PM - 8:30PM</time></div>
+					</div>
+				</a>
+			</div>
 
 		</div>
 
@@ -57,7 +50,8 @@
 
 					<div class="highlights__contest--meta">
 						<h3 class="highlights__contest--title">John Mellencamp Christmas Live</h3>
-						<span class="highlights__event--date"><time datetime="">Sat, Dec 23th</time></span>
+						<div class="highlights__contest--date"><time datetime="">Sat, Dec 23th</time></div>
+						<div class="highlights__contest--time"><time datetime="">4PM - 8:30PM</time></div>
 						<a href="#" class="highlights__contest--btn">Enter To Win</a>
 					</div>
 
@@ -67,10 +61,10 @@
 		<div class="highlights__ad">
 
 			<div class="highlights__ad--desktop">
-				<?php do_action( 'acm_tag_gmr_variant', 'mrec-lists', 'desktop' ); ?>
+				<?php do_action( 'acm_tag_gmr_variant', 'mrec-lists', 'desktop', array( 'min_width' => 1024 ) ); ?>
 			</div>
 			<div class="highlights__ad--mobile">
-				<?php do_action( 'acm_tag_gmr_variant', 'mrec-lists', 'mobile' ); ?>
+				<?php do_action( 'acm_tag_gmr_variant', 'mrec-lists', 'mobile', array( 'max_width' => 1023 ) ); ?>
 			</div>
 
 		</div>
