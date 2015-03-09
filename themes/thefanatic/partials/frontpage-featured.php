a02f11ec02a995b9221b7cfec63252a9c265e8e1
Allen Moore <allen@10up.com>
2015-03-09 11:31:40 -0400

    change featured area examples

--- themes/thefanatic/partials/frontpage-featured.php
+++ themes/thefanatic/partials/frontpage-featured.php
@@ -14,7 +14,7 @@
 			<div class="featured__article--primary">
 				<div class="featured__article">
 					<a href="#<?php /* the_permalink(); */?>" class="featured__article--link">
-						<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
+						<div class="featured__article--image" style='background-image: url(http://lorempixel.com/900/550/sports/1)'>
 							<?php /*
 
 								$image_attr = image_attribution();
@@ -37,7 +37,7 @@
 			<div class="featured__article--secondary">
 				<div class="featured__article">
 					<a href="#<?php /* the_permalink(); */?>" class="featured__article--link">
-						<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
+						<div class="featured__article--image" style='background-image: url(http://lorempixel.com/450/310/people/1)'>
 							<?php /*
 
 										$image_attr = image_attribution();
@@ -50,7 +50,7 @@
 						</div>
 						<div class="featured__article--content">
 							<div class="featured__article--heading">
-								What Chris Christie might talk about in 2015 State of the State speech today.
+								Vigil honors 4 shot dead in Hayes Valley
 								<?php /* the_title(); */ ?>
 							</div>
 						</div>
@@ -58,7 +58,7 @@
 				</div>
 				<div class="featured__article">
 					<a href="#<?php /* the_permalink(); */?>" class="featured__article--link">
-						<div class="featured__article--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'>
+						<div class="featured__article--image" style='background-image: url(http://lorempixel.com/450/310/nature/1)'>
 							<?php /*
 
 										$image_attr = image_attribution();
@@ -71,7 +71,7 @@
 						</div>
 						<div class="featured__article--content">
 							<div class="featured__article--heading">
-								What Chris Christie might talk about in 2015 State of the State speech today.
+								Record spell of Spare the Air days as dirty Bay Area skies linger
 								<?php /* the_title(); */ ?>
 							</div>
 						</div>
@@ -83,25 +83,25 @@
 			<div class="featured__content">
 					<div class="featured__content--block">
 						<a href="#<?php /* the_permalink(); */?>" class="featured__content--link">
-							<div class="featured__content--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'></div>
+							<div class="featured__content--image" style='background-image: url(http://lorempixel.com/450/310/animals/1)'></div>
 							<div class="featured__content--meta">
-								<h2 class="featured__content--title">What Chris Christie might talk about in 2015 State of the State speech today.<?php /* the_title(); */ ?></h2>
+								<h2 class="featured__content--title">Video Shows Tale of Horse's Rescue from Florida Sinkhole<?php /* the_title(); */ ?></h2>
 							</div>
 						</a>
 					</div>
 					<div class="featured__content--block">
 						<a href="#<?php /* the_permalink(); */?>" class="featured__content--link">
-							<div class="featured__content--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'></div>
+							<div class="featured__content--image" style='background-image: url(http://lorempixel.com/450/310/transport/1)'></div>
 							<div class="featured__content--meta">
-								<h2 class="featured__content--title">What Chris Christie might talk about in 2015 State of the State speech today.<?php /* the_title(); */ ?></h2>
+								<h2 class="featured__content--title">United Airlines Passengers Endure Nightmare Layover<?php /* the_title(); */ ?></h2>
 							</div>
 						</a>
 					</div>
 					<div class="featured__content--block">
 						<a href="#<?php /* the_permalink(); */?>" class="featured__content--link">
-							<div class="featured__content--image" style='background-image: url(<?php bloginfo( 'stylesheet_directory' ); ?>/images/chip-kelly-mccoy.jpg)'></div>
+							<div class="featured__content--image" style='background-image: url(http://lorempixel.com/450/310/nightlife/1)'></div>
 							<div class="featured__content--meta">
-								<h2 class="featured__content--title">What Chris Christie might talk about in 2015 State of the State speech today.<?php /* the_title(); */ ?></h2>
+								<h2 class="featured__content--title">WBT Presents Another Holiday On Ice!<?php /* the_title(); */ ?></h2>
 							</div>
 						</a>
 					</div>
