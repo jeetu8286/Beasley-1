4ea0093af23e322e45fa157b6ef0c7dc5772503f
Chris Marslender <chrismarslender@gmail.com>
2015-03-10 21:11:15 -0700

    Bump theme version

--- themes/greatermedia/functions.php
+++ themes/greatermedia/functions.php
@@ -20,7 +20,7 @@
 if ( defined( 'GMR_PARENT_ENV' ) && 'dev' == GMR_PARENT_ENV ) {
 	define( 'GREATERMEDIA_VERSION', time() );
 } else {
-	define( 'GREATERMEDIA_VERSION', '1.0.12' );
+	define( 'GREATERMEDIA_VERSION', '1.0.13' );
 }
 
 add_theme_support( 'homepage-curation' );
