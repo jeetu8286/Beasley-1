457059aca1e85764e5df72c0c4998bb384a69c46
Allen Moore <allen@10up.com>
2015-03-24 16:44:14 -0400

    check if a site is a news/sports site and give the appropriate content

--- themes/greatermedia/partials/frontpage-featured.php
+++ themes/greatermedia/partials/frontpage-featured.php
@@ -7,7 +7,100 @@
  */
 ?>
 <section id="featured" class="home__featured">
+	<?php
+	$news_site = get_option( 'gmr_newssite' );
+
+	if ( $news_site ) {
+
+		$hp_featured_query = \GreaterMedia\HomepageCuration\get_featured_query();
+
+		if ( $hp_featured_query->have_posts() ) : ?>
+		<div class="featured__articles">
+				<div class="featured__article--primary">
+					<?php if ( $hp_featured_query->have_posts() ) : $hp_featured_query->the_post(); ?>
+						<div class="featured__article">
+							<a href="#<?php the_permalink(); ?>" class="featured__article--link">
+								<div class="featured__article--image" style='background-image: url(<?php gm_post_thumbnail_url( 'gmr-featured-primary', null, true ) ?>)'>
+									<?php
+
+									$image_attr = image_attribution();
+
+									if ( ! empty( $image_attr ) ) {
+										echo $image_attr;
+									}
+
+									?>
+								</div>
+								<div class="featured__article--content">
+									<div class="featured__article--heading">
+										<?php the_title(); ?>
+									</div>
+								</div>
+							</a>
+						</div>
+					<?php endif; ?>
+				</div>
+			<?php
+			// if we still have more posts (we almost always will), render the 3 below the main section
+
+			$count = 0;
+			if ( $hp_featured_query->have_posts() ) :
+			?>
+				<div class="featured__article--secondary">
+					<?php
+						while ( $hp_featured_query->have_posts() && $count < 2 ) : $hp_featured_query->the_post();
+						$count++; ?>
+						<div class="featured__article">
+							<a href="#<?php the_permalink(); ?>" class="featured__article--link">
+								<div class="featured__article--image" style='background-image: url(<?php gm_post_thumbnail_url( 'gmr-featured-secondary', null, true ) ?>)'>
+									<?php
+
+									$image_attr = image_attribution();
+
+									if ( ! empty( $image_attr ) ) {
+										echo $image_attr;
+									}
+
+									?>
+								</div>
+								<div class="featured__article--content">
+									<div class="featured__article--heading">
+										<?php the_title(); ?>
+									</div>
+								</div>
+							</a>
+						</div>
+					<?php
+						endwhile;
+
+					endif;
+
+					?>
+				</div>
+				<div class="featured__content">
+					<?php
+					if ( $hp_featured_query->have_posts() ) :
+						while ( $hp_featured_query->have_posts() ) : $hp_featured_query->the_post();
+					?>
+							<div class="featured__content--block">
+								<a href="#<?php the_permalink(); ?>" class="featured__content--link">
+									<div class="featured__content--image" style='background-image: url(<?php gm_post_thumbnail_url( 'gmr-featured-secondary', null, true ) ?>'></div>
+									<div class="featured__content--meta">
+										<h2 class="featured__content--title"><?php the_title(); ?></h2>
+									</div>
+								</a>
+							</div>
+					<?php
+						endwhile;
+					endif;
+					?>
+				</div>
+			<?php wp_reset_query(); ?>
+		</div>
 		<?php
+		else :
+		endif;
+	} else {
 		$hp_featured_query = \GreaterMedia\HomepageCuration\get_featured_query();
 
 		if ( $hp_featured_query->have_posts() ) : $hp_featured_query->the_post(); ?>
@@ -50,5 +143,6 @@
 				<?php endwhile; ?>
 			</div>
 		<?php endif; ?>
-		<?php wp_reset_query(); ?>
+		<?php wp_reset_query();
+	} ?>
 </section>
