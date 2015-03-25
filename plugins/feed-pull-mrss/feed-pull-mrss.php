5f11f173b935c8fd5df6d9927b88f2cd917003fd
Eugene Manuilov <eugene.manuilov@10up.com>
2015-03-25 13:04:26 +0200

    Implemented thumbnails import.

---
+++ plugins/feed-pull-mrss/feed-pull-mrss.php
@@ -0,0 +1,127 @@
+<?php
+/*
+Plugin Name: Feed Pull MRSS
+Description: Extends Feed Pull plugin by adding ability to extract media information from RSS feeds.
+Version: 1.0.0
+Author: 10up
+Author URI: http://10up.com/
+*/
+
+/**
+ * Catches SimpleXML element to use in the next steps.
+ *
+ * @global SimpleXMLElement $fpmrss_feed_item Current SimpleXML element.
+ * @param array $pre_filter_post_value The array of post data.
+ * @param array $field Mapping field array.
+ * @param SimpleXMLElement $xml_element The current xml element.
+ * @return array The inital post data.
+ */
+function fpmrss_catch_feed_item_xml( $pre_filter_post_value, $field, $xml_element ) {
+	global $fpmrss_feed_item;
+	$fpmrss_feed_item = $xml_element;
+
+	return $pre_filter_post_value;
+}
+add_filter( 'fp_pre_post_insert_value', 'fpmrss_catch_feed_item_xml', 10, 3 );
+
+/**
+ * Fetches thumbnail image for a media item.
+ *
+ * @global SimpleXMLElement $fpmrss_feed_item The current SimpleXML element.
+ * @param int $post_id Newly imported post id.
+ */
+function fpmrss_fetch_thumbnail( $post_id ) {
+	global $fpmrss_feed_item;
+	if ( ! $fpmrss_feed_item ) {
+		return;
+	}
+
+	require_once ABSPATH . 'wp-admin/includes/image.php';
+	require_once ABSPATH . 'wp-admin/includes/media.php';
+	require_once ABSPATH . 'wp-admin/includes/file.php';
+
+	$thumbnail = current( (array) $fpmrss_feed_item->xpath( 'media:content/media:thumbnail/@url' ) );
+	if ( $thumbnail ) {
+		$thumbnail = (string) $thumbnail['url'];
+		if ( filter_var( $thumbnail, FILTER_VALIDATE_URL ) ) {
+			$thumbnail_id = fpmrss_download_image( $thumbnail, $post_id );
+			if ( $thumbnail_id && ! is_wp_error( $thumbnail_id ) ) {
+				set_post_thumbnail( $post_id, $thumbnail_id );
+			}
+		}
+	}
+}
+add_action( 'fp_created_post', 'fpmrss_fetch_thumbnail' );
+add_action( 'fp_updated_post', 'fpmrss_fetch_thumbnail' );
+
+/**
+ * Downloads remote image or just returns attachment ID if it has been already imported.
+ *
+ * @param string $image The remote image URL.
+ * @param int $post_id The post id to which the image would be assigned.
+ * @return int The attachment ID on success, otherwise FALSE.
+ */
+function fpmrss_download_image( $image, $post_id ) {
+	$query = new WP_Query( array(
+		'post_type'   => 'attachment',
+		'post_status' => 'inherit',
+		'meta_query'  => array(
+			array(
+				'key'     => 'fp_orig_image',
+				'value'   => $image,
+				'compare' => '='
+			),
+		),
+	) );
+
+	if ( $query->have_posts() ) {
+		return $query->next_post()->ID;
+	}
+
+	// Download file to temp location
+	$tmp = download_url( $image );
+	if ( is_wp_error( $tmp ) ) {
+		return false;
+	}
+
+	// Set variables for storage, fix file filename for query strings
+	$matches = array();
+	preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $image, $matches );
+	$file_array['name'] = fpmrss_generate_image_name( $image );
+	$file_array['tmp_name'] = $tmp;
+	$file_array['error'] = 0;
+
+	$post = get_post( $post_id );
+	$image_id = media_handle_sideload( $file_array, $post_id, null, array( 'post_author' => $post->post_author ) );
+	if ( is_wp_error( $image_id ) ) {
+		@unlink( $file_array['tmp_name'] );
+	} else {
+		add_post_meta( $image_id, 'fp_orig_image', $image );
+	}
+
+	return $image_id;
+}
+
+/**
+ * Generates image name based on its file path.
+ *
+ * @param string $image The image path.
+ * @return string The image name.
+ */
+function fpmrss_generate_image_name( $image ) {
+	$file_name = '';
+	$matches = array();
+
+	preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $image, $matches );
+	if ( is_array( $matches ) && count( $matches ) ) {
+		$file_name = str_replace( '%20', '-', basename( $matches[0] ) );
+	} else {
+		$sizes = getimagesize( $image );
+		if ( is_array( $sizes ) && isset( $sizes['mime'] ) && ! empty( $sizes['mime'] ) ) {
+			$ext = image_type_to_extension( $sizes[2] );
+			$file_name = substr( sanitize_title( pathinfo( $image, PATHINFO_FILENAME ) ), 0, 254 ) . $ext;
+		}
+	}
+
+	return $file_name;
+}
