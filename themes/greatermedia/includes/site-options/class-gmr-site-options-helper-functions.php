2d444e0de97bcb77d45731be02cda78529ca413e
Allen Moore <allen@10up.com>
2015-03-31 16:19:32 -0400

    add notes to new helper function

--- themes/greatermedia/includes/site-options/class-gmr-site-options-helper-functions.php
+++ themes/greatermedia/includes/site-options/class-gmr-site-options-helper-functions.php
@@ -97,6 +97,11 @@ class GreaterMediaSiteOptionsHelperFunctions {
 
 GreaterMediaSiteOptionsHelperFunctions::init();
 
+/**
+ * Helper function for use in conditionals related to content display and the News/Sports theme
+ *
+ * @return bool
+ */
 function is_news_site() {
 	return (bool) filter_var( get_option( 'gmr_newssite' ), FILTER_VALIDATE_BOOLEAN );
 }
