340138e59e6a5b00272da203e2f3413371b465ad
Darshan Sawardekar <darshan@10up.com>
2015-04-06 15:11:45 +0530

    Removes debug logs.

--- plugins/greatermedia-gigya/js/query_builder.js
+++ plugins/greatermedia-gigya/js/query_builder.js
@@ -1026,7 +1026,7 @@ var ListConstraint = Constraint.extend({
 		var parts   = type.split(':');
 		var subType = parts[1];
 
-		console.log('getListTypeName', type, parts, subType);
+		//console.log('getListTypeName', type, parts, subType);
 		return subType.replace('_list', '');
 	},
 
@@ -1041,7 +1041,7 @@ var ListConstraint = Constraint.extend({
 
 	loadList: function() {
 		var type   = this.getListTypeName();
-		console.log('loadList: type', type);
+		//console.log('loadList: type', type);
 		var params = { 'type': type };
 
 		this.trigger('loadListStart');
