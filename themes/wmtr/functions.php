fd73aa6465b93822570998c3b4507eddc714427f
Chris Marslender <chrismarslender@gmail.com>
2015-04-14 15:55:30 -0700

    Update theme version

--- themes/wmtr/functions.php
+++ themes/wmtr/functions.php
@@ -13,7 +13,7 @@
  */
  
  // Useful global constants
-define( 'WMTR_VERSION', '0.1.2' );
+define( 'WMTR_VERSION', '0.1.3' );
  
  /**
   * Set up theme defaults and register supported WordPress features.
