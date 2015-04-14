bde5c14d5bbf776a0ebd163ad7ff6ac8cb5ab189
Allen Moore <allen@10up.com>
2015-04-14 10:23:10 -0400

    add in styles for top 3 category; separate into proper view matching shows

--- themes/greatermedia/partials/loop-category-top3.php
+++ themes/greatermedia/partials/loop-category-top3.php
@@ -7,15 +7,29 @@ $posts_between_ads  = 6;
 
 $current_page       = intval( $wp_query->query_vars['paged'] );
 $current_page       = max( 1, $current_page );
-$current_post_index = ( ( $current_page - 1 ) * $posts_per_page ) + 1;
+$current_post_index = ( ( $current_page - 1 ) * $posts_per_page ) + 1; ?>
+<section class="top-three__features">
+<?php if ( have_posts() ) : the_post();
 
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'partials/entry', get_post_field( 'post_type', null ) );
+	get_template_part( 'partials/top-three', 'featured-primary' );
+
+	$count = 0;
+	if ( have_posts() ) :
+		while ( have_posts() && $count < 2 ) : the_post();
+			$count++;
+			get_template_part( 'partials/top-three', 'featured-secondary' );
+		endwhile; ?>
+	</section>
+	<section class="top-three__blogroll">
+		<?php while ( have_posts() && $count >= 2 ) : the_post();
+			get_template_part( 'partials/entry', get_post_type() );
+		endwhile; ?>
+	</section>
+	<?php endif;
 
 	if ( $current_post_index % $posts_between_ads === 0 ) {
 		get_template_part( 'partials/ad-in-loop' );
 	}
 
 	$current_post_index++;
-endwhile;
+endif; ?>
---
+++ themes/greatermedia/partials/top-three-featured-secondary.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Top 3 Category Partial -- Secondary Featured Item
+ */
+
+?>
+<article id="post-<?php the_ID(); ?>" class="top-three__feature--secondary" role="article" itemscope itemtype="http://schema.org/BlogPosting">
+	<a href="<?php the_permalink(); ?>">
+		<div class="top-three__feature">
+			<div class='top-three__thumbnail'>
+				<?php if ( has_post_thumbnail() ) : ?>
+					<div class='thumbnail' style='background-image: url(<?php gm_post_thumbnail_url( 'gmr-show-featured-primary', null, true ); ?>)'></div>
+				<?php else: ?>
+					<div class='thumbnail thumbnail-placeholder' style=''></div>
+				<?php endif; ?>
+			</div>
+			<div class="top-three__desc">
+				<div class='inner-wrap'>
+					<h3><?php the_title(); ?></h3>
+					<time class="top-three__date" datetime="<?php the_time( 'c' ); ?>"><?php the_time( 'M j, Y' ); ?></time>
+				</div>
+			</div>
+		</div>
+	</a>
+</article>
