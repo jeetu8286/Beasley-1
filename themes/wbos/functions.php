411c4f6d06b483a16b1e8d223c19b45d98989cf1
Denis Prindeville <dprindeville@greatermediaboston.com>
2015-05-26 10:55:55 -0400

    CSS changes to container and live-links sections

--- themes/wbos/functions.php
+++ themes/wbos/functions.php
@@ -13,7 +13,7 @@
  */
  
  // Useful global constants
-define( 'WBOS_VERSION', '0.1.0' );
+define( 'WBOS_VERSION', '0.1.1' );/* Denis Updates as of 5/26/15 */
  
  /**
   * Set up theme defaults and register supported WordPress features.
