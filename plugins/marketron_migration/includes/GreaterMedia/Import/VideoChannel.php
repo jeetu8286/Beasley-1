6b257e981c4a113845c9cb5a821abb441ae51f9d
Darshan Sawardekar <darshan@10up.com>
2015-05-28 19:12:18 +0530

    Adds support for youtube embed tab variant.

--- plugins/marketron_migration/includes/GreaterMedia/Import/VideoChannel.php
+++ plugins/marketron_migration/includes/GreaterMedia/Import/VideoChannel.php
@@ -80,8 +80,14 @@ class VideoChannel extends BaseImporter {
 	}
 
 	function content_from_post( $post ) {
-		$content      = $this->import_string( $post['PostText'] );
-		$embed_code   = $this->import_string( $post['EmbededTag'] );
+		$content    = $this->import_string( $post['PostText'] );
+		$title      = $this->import_string( $post['PostTitle'] );
+		$embed_code = $this->import_string( $post['EmbededTag'] );
+
+		if ( $title === 'WDHA Morning Jolt Road Trip' ) {
+			var_dump( $embed_code );
+		}
+
 		$video_oembed = $this->video_from_embed_code( $embed_code );
 
 		if ( $video_oembed !== false ) {
@@ -105,7 +111,26 @@ class VideoChannel extends BaseImporter {
 			$video_id = $matches[1];
 			return '[embed]https://www.youtube.com/watch?v=' . $video_id . '[/embed]';
 		} else {
-			return false;
+			$pattern = '#youtube\.com/embed/([^"\?]+)#';
+			preg_match( $pattern, $embed_code, $matches );
+
+			if ( ! empty( $matches ) && count( $matches ) >= 1 ) {
+				$video_id = $matches[1];
+				return '[embed]https://www.youtube.com/watch?v=' . $video_id . '[/embed]';
+			} else {
+				return false;
+			}
+			$embed_code = preg_replace(
+				$pattern, '[embed]https://www.youtube.com/watch?v=${1}[/embed]',
+				$embed_code,
+				-1, $videos
+			);
+
+			if ( $videos > 0 ) {
+				return $embed_code;
+			} else {
+				return false;
+			}
 		}
 	}
 
