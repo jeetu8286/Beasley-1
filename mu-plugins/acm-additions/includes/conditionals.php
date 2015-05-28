2f72332129b8a1827aece0837acbe288d48f1997
Chris Marslender <chrismarslender@gmail.com>
2015-05-28 15:13:31 -0700

    Add support for checking gmr_is_show on supported singular types for Ad Code Manager

--- mu-plugins/acm-additions/includes/conditionals.php
+++ mu-plugins/acm-additions/includes/conditionals.php
@@ -20,17 +20,31 @@ function gmr_is_show( $show = null ) {
 	}
 
 	if ( get_post_type() !== ShowsCPT::SHOW_CPT ) {
+		
+		if ( is_singular( ShowsCPT::get_instance()->get_supported_post_types() ) ) {
+			$id = get_the_ID();
+			$id = empty( $id ) ? get_queried_object_id() : $id; // In case we haven't gotten to the loop yet... (header)
+			$show_terms = wp_get_object_terms( $id, ShowsCPT::SHOW_TAXONOMY );
+			$show_term_slugs = wp_list_pluck( $show_terms, 'slug' );
+
+			if ( in_array( $show, $show_term_slugs ) ) {
+				return true;
+			}
+		}
+
 		return false;
+	} else {
+		// Were only checking for shows in general, not a specific show, if $show is null
+		if ( is_null( $show ) ) {
+			return true;
+		}
+
+		if ( get_query_var( 'show' ) == $show ) {
+			return true;
+		}
 	}
 
-	// Were only checking for shows in general, not a specific show
-	if ( is_null( $show ) ) {
-		return true;
-	}
 
-	if ( get_query_var( 'show' ) == $show ) {
-		return true;
-	}
 
 	return false;
 }
