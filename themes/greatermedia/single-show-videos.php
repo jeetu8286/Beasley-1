8f16a0d60c7023404980143cfa25539fa525315f
Allen Moore <allen@10up.com>
2015-05-28 21:59:08 -0400

    add load more option to show video page

--- themes/greatermedia/single-show-videos.php
+++ themes/greatermedia/single-show-videos.php
@@ -11,6 +11,9 @@
 			<div class="videos">
 
 				<?php
+
+				global $gmr_loadmore_num_pages, $gmr_loadmore_post_count, $gmr_loadmore_paged;
+
 				$video_query = \GreaterMedia\Shows\get_show_video_query();
 
 				while ( $video_query->have_posts() ) : $video_query->the_post();
@@ -18,12 +21,23 @@
 					get_template_part( 'partials/entry', get_post_field( 'post_type', null ) );
 
 				endwhile;
+
 				wp_reset_query();
+
+				$gmr_loadmore_paged = get_query_var( 'paged', 1 );
+				if ( $gmr_loadmore_paged < 2 ) :
+					greatermedia_load_more_button( array(
+						'query'        => $video_query,
+						'partial_slug' => 'partials/entry',
+						'partial_name' => get_post_field( 'post_type', null ),
+					) );
+				endif;
+
+				$gmr_loadmore_num_pages = $video_query->max_num_pages;
+				$gmr_loadmore_post_count = $video_query->post_count;
+
 				?>
-
-				<div
-					class="show__paging"><?php echo \GreaterMedia\Shows\get_show_endpoint_pagination_links( $video_query ); ?></div>
-
+				
 			</div>
 
 		</section>
