fd35af6d6d74a950cc9f3e2cac06a388a56b6ff1
Allen Moore <allenmoore@Allens-MacBook-Pro.local>
2015-06-10 10:44:56 -0400

    higher version bump

--- themes/greatermedia/functions.php
+++ themes/greatermedia/functions.php
@@ -20,7 +20,7 @@
 if ( defined( 'GMR_PARENT_ENV' ) && 'dev' == GMR_PARENT_ENV ) {
 	define( 'GREATERMEDIA_VERSION', time() );
 } else {
-	define( 'GREATERMEDIA_VERSION', '1.0.5' );
+	define( 'GREATERMEDIA_VERSION', '1.1.0' );
 }
 
 add_theme_support( 'homepage-curation' );
