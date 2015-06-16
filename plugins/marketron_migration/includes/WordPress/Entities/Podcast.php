cfad0747ec20d425297f3f97c915146913abc4f8
Darshan Sawardekar <darshan@10up.com>
2015-06-16 21:36:36 +0530

    Uses cached podcast reference.

--- plugins/marketron_migration/includes/WordPress/Entities/Podcast.php
+++ plugins/marketron_migration/includes/WordPress/Entities/Podcast.php
@@ -56,13 +56,7 @@ class Podcast extends Post {
 	}
 
 	function get_podcast_by_name( $name ) {
-		$table = $this->get_table( 'posts' );
-
-		if ( $table->has_row_with_field( 'podcast_name', $name ) ) {
-			return $table->get_row_with_field( 'podcast_name', $name );
-		} else {
-			return null;
-		}
+		return $this->cached_podcasts[ $name ];
 	}
 
 	function get_show_id( $name ) {
