3060b564b68a12c779c701d290f08d52d09caed6
Allen Moore <allenmoore@Allens-MacBook-Pro.local>
2015-06-18 08:46:54 -0400

    pull load more out of concat'd file

--- themes/greatermedia/functions.php
+++ themes/greatermedia/functions.php
@@ -20,7 +20,7 @@
 if ( defined( 'GMR_PARENT_ENV' ) && 'dev' == GMR_PARENT_ENV ) {
 	define( 'GREATERMEDIA_VERSION', time() );
 } else {
-	define( 'GREATERMEDIA_VERSION', '1.1.0' );
+	define( 'GREATERMEDIA_VERSION', '1.1.1' );
 }
 
 add_theme_support( 'homepage-curation' );
@@ -169,7 +169,16 @@ function greatermedia_scripts_styles() {
 		array(
 			'jquery',
 			'underscore',
-			'classlist-polyfill',
+			'classlist-polyfill'
+		),
+		GREATERMEDIA_VERSION,
+		true
+	);
+	wp_enqueue_script(
+		'greatermedia-load-more',
+		"{$baseurl}/assets/js/greater_media_load_more{$postfix}.js",
+		array(
+			'jquery',
 			'jquery-waypoints'
 		),
 		GREATERMEDIA_VERSION,
