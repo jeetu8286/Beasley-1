10d7561ba22e0303670a2df4d74b7d9ddd1f2dd5
Chris Marslender <chrismarslender@gmail.com>
2015-06-23 12:34:03 -0700

    Bump

--- themes/wbqt/functions.php
+++ themes/wbqt/functions.php
@@ -13,7 +13,7 @@
  */
  
  // Useful global constants
-define( 'WBQT_VERSION', '0.1.6' );
+define( 'WBQT_VERSION', '0.1.7' );
  
  /**
   * Set up theme defaults and register supported WordPress features.
