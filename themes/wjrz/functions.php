d7a7dc3873364c678bd3c16b8f3e22d047b67df6
Chris Marslender <chrismarslender@gmail.com>
2015-06-24 13:29:19 -0700

    Bump

--- themes/wjrz/functions.php
+++ themes/wjrz/functions.php
@@ -13,7 +13,7 @@
  */
  
  // Useful global constants
-define( 'WJRZ_VERSION', '0.1.1' ); /* Version bump by Allen 6/22/2015 @ 2:45pm EST */
+define( 'WJRZ_VERSION', '0.1.2' );
  
  /**
   * Set up theme defaults and register supported WordPress features.
