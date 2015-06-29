461c3480efea2d979c13ab69f65895ef86d1c640
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2015-06-29 12:18:21 -0400

    Adding LiveFyre to listen live tuner bar for WCTC.  Same as used on Fanatic.

--- themes/wctc/functions.php
+++ themes/wctc/functions.php
@@ -11,10 +11,10 @@
  * @package WCTC
  * @since 0.1.0
  */
- 
+
  // Useful global constants
 define( 'WCTC_VERSION', '0.1.0' );
- 
+
  /**
   * Set up theme defaults and register supported WordPress features.
   *
@@ -33,7 +33,7 @@ define( 'WCTC_VERSION', '0.1.0' );
 	load_theme_textdomain( 'wctc', get_stylesheet_directory_uri() . '/languages' );
  }
  add_action( 'after_setup_theme', 'wctc_setup' );
- 
+
  /**
   * Enqueue scripts and styles for front-end.
   *
@@ -43,17 +43,24 @@ define( 'WCTC_VERSION', '0.1.0' );
 	$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
 	wp_dequeue_style( 'greatermedia' );
-	wp_deregister_style( 'greatermedia' );	
+	wp_deregister_style( 'greatermedia' );
 	wp_enqueue_style( 'wctc', get_stylesheet_directory_uri() . "/assets/css/wctc{$postfix}.css", array(), WCTC_VERSION );
+            wp_enqueue_script(
+                'wctc',
+                get_stylesheet_directory_uri() . "/assets/js/wctc{$postfix}.js",
+                array(),
+                WCTC_VERSION,
+                true
+            );
  }
  add_action( 'wp_enqueue_scripts', 'wctc_scripts_styles', 20 );
- 
+
  /**
   * Add humans.txt to the <head> element.
   */
  function wctc_header_meta() {
 	$humans = '<link type="text/plain" rel="author" href="' . get_stylesheet_directory_uri() . '/humans.txt" />';
-	
+
 	echo apply_filters( 'wctc_humans', $humans );
  }
  add_action( 'wp_head', 'wctc_header_meta' );
