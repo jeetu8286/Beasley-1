73fae86e14ad23394f09981155450317b741354c
Chris Marslender <chrismarslender@gmail.com>
2015-06-30 11:26:52 -0700

    Bump wctc version

--- themes/wctc/functions.php
+++ themes/wctc/functions.php
@@ -13,7 +13,7 @@
  */
 
  // Useful global constants
-define( 'WCTC_VERSION', '0.1.2' );
+define( 'WCTC_VERSION', '0.1.3' );
 
  /**
   * Set up theme defaults and register supported WordPress features.
