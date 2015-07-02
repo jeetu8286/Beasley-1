a7cc9e4937ee7c5470a50e584f4d322ed44c7cf4
Chris Marslender <chrismarslender@gmail.com>
2015-07-02 09:37:24 -0700

    Bump up theme version

--- themes/wklb/functions.php
+++ themes/wklb/functions.php
@@ -13,7 +13,7 @@
  */
  
  // Useful global constants
-define( 'WKLB_VERSION', '0.1.6' );
+define( 'WKLB_VERSION', '0.1.7' );
  
  /**
   * Set up theme defaults and register supported WordPress features.
