471fb2ecc7b22bbc2d939c7c7b7cc291be41f3e8
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2015-08-25 11:32:50 -0400

    Adding Ooyala playlist shortcode and content filter functionality.

---
+++ plugins/greatermedia-ooyala-playlists/assets/js/greater-media-ooyala-playlists.js
@@ -0,0 +1,8 @@
+/*! Greater Media Ooyala Playlists - v0.1.0
+ * http://greatermedia.com
+ * Copyright (c) 2015; * Licensed GPLv2+ */
+( function( window, undefined ) {
+	'use strict';
+
+
+} )( this );
