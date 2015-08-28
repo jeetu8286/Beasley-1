9edcb0a53ac6bb2a4d913b03524150a719257ab8
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2015-08-28 09:17:20 -0400

    Refactoring iframe embed to default to 16x9, but also allow fixed height.

--- themes/greatermedia/includes/iframe-embed.php
+++ themes/greatermedia/includes/iframe-embed.php
@@ -13,7 +13,7 @@ class GMRIframe {
 	{
 		$atts = shortcode_atts( array(
 			'src' => '',
-			'height' => '300',
+			'height' => '',
 			'scrolling' => 'auto'
 		), $atts );
 
@@ -22,8 +22,8 @@ class GMRIframe {
 		ob_start();
 
 		?>
-		<div id="playerwrapper-<?php echo $uniqid ?>" class="iframe-embed">
-			<iframe height="<?php echo esc_attr( $atts['height'] ) ?>px" frameborder="0" src="<?php echo esc_attr( $atts['src'] ) ?>" scrolling="<?php echo esc_attr( $atts['scrolling'] ) ?>" seamless="seamless"></iframe>
+		<div id="iframe-<?php echo $uniqid ?>" class="intrinsic-container <?php if ( empty( $atts['height'] ) ) { ?>intrinsic-container-16x9<?php } else { ?>intrinsic-container-fixed-height<?php } ?> iframe-embed" <?php if ( !empty( $atts['height'] ) ) { ?>style="height: <?php echo esc_attr( $atts['height'] ) ?>px;" <?php } ?>>
+			<iframe <?php if ( !empty( $atts['height'] ) ) { ?>style="height: <?php echo esc_attr( $atts['height'] ) ?>px;" <?php } ?>frameborder="0" src="<?php echo esc_attr( $atts['src'] ) ?>" scrolling="<?php echo esc_attr( $atts['scrolling'] ) ?>" seamless="seamless"></iframe>
 		</div>
 		<?php
 
