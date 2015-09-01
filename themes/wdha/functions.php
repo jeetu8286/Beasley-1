5e5103437a7e2793cfc941118e438677c78affad
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2015-09-01 09:56:54 -0400

    Fixing up a theming on a few sites for the show mini nav.

--- themes/wdha/functions.php
+++ themes/wdha/functions.php
@@ -13,7 +13,7 @@
  */
 
  // Useful global constants
-define( 'WDHA_VERSION', '0.2.7' ); /* Version bump by Steve 8/31/2015 @ 10:30pm EST */
+define( 'WDHA_VERSION', '0.2.8' ); /* Version bump by Steve 9/1/2015 @ 9:30am EST */
 
  /**
   * Set up theme defaults and register supported WordPress features.
