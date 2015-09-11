a4158fff6c220e35acd55ab718e1e1ea049886ac
Jonathan Mauney <jmauney@gmclt.com>
2015-09-11 18:21:17 -0400

    including files that should have been in the last commit

---
+++ themes/wbt/assets/js/src/gmclt-stocks.js
@@ -0,0 +1,56 @@
+jQuery(document).ready(function(){
+	GMCLT.Stocks.stockQuoteSubnav();
+});
+
+GMCLT.Stocks = function() {
+	
+	var stockObject;
+	var currentStock = 0;
+	var index = jQuery("div.secondary-link:contains('Stocks')").parents().eq(1).attr('id');
+	 
+ 	var init = function() {
+		
+	};
+	
+	var stockQuoteSubnav = function() {
+		
+		if (index) {
+		
+			jQuery.getJSON(apiUrl + '/stocks/stocks.cfc?method=getStocksMini&callback=?',
+	
+			function (stockDataObject) {
+				stockObject = stockDataObject;
+				window.setInterval("GMCLT.Stocks.populateStockQuote()", 4000);
+			})
+			.fail(function() {
+			   //do nothing. Not catastrophic
+			});
+			
+		}
+		else {
+			alert('not found');
+		}
+		
+	}
+	
+	var populateStockQuote = function() {
+		var htmlString = '<a href="/category/business-news/"><div class="secondary-link"><img class="gmclt_headerStocksIcon" src="/wp-content/themes/wbt/images/stocks/' + stockObject[currentStock].arrow + '.png"> ' + stockObject[currentStock].shortName + ': ' + stockObject[currentStock].change + '</div></a>';
+		jQuery('#' + index).html(htmlString);
+		
+		if (stockObject.length == currentStock+1) {
+			currentStock = 0;
+		}
+		else {
+			currentStock = currentStock+1;
+		}
+	};
+	
+	var oPublic =
+	    {
+	      init: init,
+	      stockQuoteSubnav: stockQuoteSubnav,
+	      populateStockQuote: populateStockQuote
+	    };
+    return oPublic;
+	 
+}();
