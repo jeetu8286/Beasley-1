e5ab7158cd97d207be4e4f71030f0900b447460a
Denis Prindeville <dprindeville@greatermediaboston.com>
2015-09-14 15:52:00 -0400

    more flxible description for recently plyed page

--- themes/greatermedia/archive-songs.php
+++ themes/greatermedia/archive-songs.php
@@ -13,10 +13,29 @@ get_header(); ?>
 		<section class="content">
 
 			<h2 class="content__heading">
-				Recently played
+				Recently Played 
 				<?php $call_sign = get_query_var( GMR_LIVE_STREAM_CPT ); ?>
 				<?php if ( ! empty( $call_sign ) && ! is_numeric( $call_sign ) ) : ?>
-					on <?php echo esc_html( $call_sign ); ?>
+					<?php
+					$stream_query = new WP_Query( array(
+						'post_type'           => GMR_LIVE_STREAM_CPT,
+						'meta_key'            => 'call_sign',
+						'meta_value'          => $call_sign,
+						'posts_per_page'      => 1,
+						'ignore_sticky_posts' => 1,
+						'no_found_rows'       => true,
+						'fields'              => 'ids',
+					) );
+					if ( $stream_query->have_posts() ) : $stream_query->the_post(); 
+						$description = get_post_meta( get_the_ID(), 'description', true );
+						if ( !empty( $description ) ){
+							echo 'on '.$description;
+						}else{
+							echo 'on '.$call_sign;
+						}
+					endif;
+					wp_reset_postdata();
+					?>
 				<?php endif; ?>
 			</h2><?php
 
