52f5fda2d2363835685d704ee5b02b4b4136d8ca
Jonathan Mauney <jmauney@gmclt.com>
2015-10-17 07:53:43 -0400

    bumped version

--- themes/wbt/functions.php
+++ themes/wbt/functions.php
@@ -13,7 +13,7 @@
  */
  
  // Useful global constants
-define( 'WBT_VERSION', '0.1.3' );
+define( 'WBT_VERSION', '0.1.4' );
  
  /**
   * Set up theme defaults and register supported WordPress features.
