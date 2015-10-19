5da608c66851b16454cab8601f7468b6ba9473dd
Jonathan Mauney <jmauney@gmclt.com>
2015-10-19 18:03:01 -0400

    fixing nav

--- themes/wbt/functions.php
+++ themes/wbt/functions.php
@@ -13,7 +13,7 @@
  */
 
  // Useful global constants
-define( 'WBT_VERSION', '0.1.6' );
+define( 'WBT_VERSION', '0.1.7' );
 
  /**
   * Set up theme defaults and register supported WordPress features.
