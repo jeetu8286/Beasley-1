6535693d52f21259b545e1720049dd47068ef5c5
ninnypants <tyrel@ninnypants.com>
2016-03-08 15:09:04 -0700

    Add basic metabox loading for keep off homepage functionality.

---
+++ mu-plugins/gmr-homepage-curation/includes/homepage-exclude.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Allow posts to be excluded from home page searches.
+ *
+ * @package GreaterMedia\HomepageCuration
+ */
+
+namespace GreaterMedia\HomepageCuration;
+
+function add_meta_boxes() {
+	$screens = apply_filters( 'gmr-homepage-exclude-post-types', [ 'post' ] );
+	add_meta_box( 'keep-off-homepage', 'Keep Off Homepage', __NAMESPACE__ . '\render_meta_box', $screens );
+}
+add_action( 'add_meta_boxes', __NAMESPACE__ . '\add_meta_boxes' );
+
+function render_meta_box() {
+	load_template( 'metabox-keep-off-homepage.php' );
+}
