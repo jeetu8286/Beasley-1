69325cd4dc21d8b4564083036edd521e9e434ac7
Elliott Stocks <elliott.stocks@10up.com>
2016-05-06 03:05:06 +0100

    Tweak footer

--- themes/wlnk/partials/entry.php
+++ themes/wlnk/partials/entry.php
@@ -6,13 +6,11 @@
  * partials for the individual formats and post types. But for now this one 
  * covers them all. 
  */
-// $layout = sprintf( 'gmr_category_%d_layout', get_queried_object_id() );
-// $layout = get_option( $layout );
-// if ( ! empty( $layout ) ) {
-// 	$layout = 'category-' . $layout;
-// }
-
-$layout = 'category-50';
+$layout = sprintf( 'gmr_category_%d_layout', get_queried_object_id() );
+$layout = get_option( $layout );
+if ( ! empty( $layout ) ) {
+	$layout = 'category-' . $layout;
+}
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry2' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 	<?php 
@@ -48,13 +46,13 @@ $layout = 'category-50';
 
 	<footer class="entry2__footer">
 		<?php
-		$category = get_the_category();
+		/*$category = get_the_category();
 
 		if( isset( $category[0] ) ){
 			echo '<a href="' . esc_url( get_category_link($category[0]->term_id ) ) . '" class="entry2__footer--category">' . esc_html( $category[0]->cat_name ) . '</a>';
 		}
-		?>
+		
 
-		<a href="<?php the_permalink(); ?>" class="entry2__footer--read-more"><?php _e( 'read more', 'greatermedia' ); ?></a>
+		<a href="<?php the_permalink(); ?>" class="entry2__footer--read-more"><?php _e( 'read more', 'greatermedia' ); </a> */ ?>
 	</footer>
 </article>
