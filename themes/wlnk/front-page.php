ff04ba3257255ecb11313cc7d5e4e61fc8cb9ecc
Elliott Stocks <elliott.stocks@10up.com>
2016-05-06 02:09:12 +0100

    Add empty front-page for WLNK theme

---
+++ themes/wlnk/front-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The front page template file
+ *
+ * @package Greater Media
+ * @since   0.1.0
+ */
+
+get_header();
+
+?>
+
+	<div class="container">
+
+		
+	</div>
+
+<?php get_footer(); ?>
