5c8c1a31c8ef8999b4c8e5e234f9935eab8a4f29
tstarknj <tstark@greatermedianj.com>
2016-05-12 08:16:43 -0400

    Update code in PHP pages

--- themes/wmgq-christmas/page-weather.php
+++ themes/wmgq-christmas/page-weather.php
@@ -37,7 +37,7 @@ while ( have_posts() ) : the_post(); ?>
 
                 <header class="article__header">
 
-                    <time class="article__date" datetime="<?php echo get_the_time(); ?>"><?php the_date('F j, Y'); ?></time>
+                    <time class="article__date" datetime="<?php echo esc_attr( get_the_time() ); ?>"><?php the_date('F j, Y'); ?></time>
                     <h2 class="article__title" itemprop="headline"><?php the_title(); ?></h2>
                     <?php get_template_part( 'partials/social-share' ); ?>
 
@@ -47,16 +47,14 @@ while ( have_posts() ) : the_post(); ?>
 
                     <?php the_content(); ?>
 
-
                     <a href="http://www.accuweather.com/en/us/new-brunswick-nj/08901/current-weather/329549" class="aw-widget-legal">
-<!--
-By accessing and/or using this code snippet, you agree to AccuWeather’s terms and conditions (in English) which can be found at http://www.accuweather.com/en/free-weather-widgets/terms and AccuWeather’s Privacy Statement (in English) which can be found at http://www.accuweather.com/en/privacy.
--->
-</a><div id="awtd1385567990060" class="aw-widget-36hour"  data-locationkey="329549" data-unit="f" data-language="en-us" data-useip="false" data-uid="awtd1385567990060" data-editlocation="true"></div><script type="text/javascript" src="http://oap.accuweather.com/launch.js"></script>
-<hr />
-<strong>Your Central Jersey Forecast</strong>
-<br /><iframe src="http://www.jerseysgreatest.com/wx/wmgqweather.php" frameborder="0" scrolling="yes" width=“100%" height=“500"></iframe></div>
-
+                    <!--
+                    By accessing and/or using this code snippet, you agree to AccuWeather’s terms and conditions (in English) which can be found at http://www.accuweather.com/en/free-weather-widgets/terms and AccuWeather’s Privacy Statement (in English) which can be found at http://www.accuweather.com/en/privacy.
+                    -->
+                    </a><div id="awtd1385567990060" class="aw-widget-36hour"  data-locationkey="329549" data-unit="f" data-language="en-us" data-useip="false" data-uid="awtd1385567990060" data-editlocation="true"></div><script type="text/javascript" src="http://oap.accuweather.com/launch.js"></script>
+                    <hr />
+                    <strong>Your Central Jersey Forecast</strong>
+                    <br /><iframe src="http://www.jerseysgreatest.com/wx/wmgqweather.php" frameborder="0" scrolling="yes" width=“100%" height=“500"></iframe></div>
 
                 </section>
 
