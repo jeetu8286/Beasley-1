154cf6e274b8982c8a04e29ff79e1b10a5a1a4a5
Elliott Stocks <elliott.stocks@10up.com>
2016-05-12 17:52:26 +0100

    Version bump

--- themes/wmgq/functions.php
+++ themes/wmgq/functions.php
@@ -13,7 +13,7 @@
  */
 
  // Useful global constants
-define( 'WMGQ_VERSION', '0.2.7' ); /* Version bump by Elliott 5/12/2016 @ 10:00 a.m. EST */
+define( 'WMGQ_VERSION', '0.2.8' ); /* Version bump by Elliott 5/12/2016 @ 12:50 a.m. EST */
 
  /**
   * Set up theme defaults and register supported WordPress features.
