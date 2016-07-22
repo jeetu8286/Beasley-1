333a5be7c54f61b2ff51d39fecd03537f998656f
ninnypants <tyrel@ninnypants.com>
2016-07-22 11:47:23 -0600

    Remove emma syncing from registration, and add a check to see if emma has been synced on login.

--- plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Ajax/GigyaLoginAjaxHandler.php
+++ plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Ajax/GigyaLoginAjaxHandler.php
@@ -16,6 +16,11 @@ class GigyaLoginAjaxHandler extends AjaxHandler {
 
 	public function run( $params ) {
 		if ( is_gigya_user_logged_in() ) {
+			if ( ! get_gigya_user_data_field( get_gigya_user_id(), 'EmmaSync' ) ) {
+				$user_id              = get_gigya_user_id();
+				$emma_group_sync_task = new EmmaGroupSyncTask();
+				$emma_group_sync_task->enqueue( array( 'user_id' => $user_id ) );
+			}
 			do_action( 'gigya_login', get_gigya_user_id() );
 			return true;
 		} else {
