4724b29f77c51b6b716e170e53c274ebbdc8570b
Elliott Stocks <elliott.stocks@10up.com>
2016-09-13 11:50:07 +0100

    Fix semi-colon

--- plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Ajax/GigyaLoginAjaxHandler.php
+++ plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Ajax/GigyaLoginAjaxHandler.php
@@ -19,7 +19,7 @@ class GigyaLoginAjaxHandler extends AjaxHandler {
 	public function run( $params ) {
 		if ( is_gigya_user_logged_in() ) {
 			$user_id = get_gigya_user_id();
-			$gigya_account = get_gigya_user_profile_data( $user_id )
+			$gigya_account = get_gigya_user_profile_data( $user_id );
 			if ( empty( $gigya_account['data']['EmmaSync'] ) ) {
 				$emma_group_sync_task = new EmmaGroupSyncTask();
 				$emma_group_sync_task->enqueue( array( 'user_id' => $user_id ) );
