aad9a151b1fb285ca7501feb91f587e0b97952de
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2016-09-30 08:48:17 -0400

    Retrieving built-in capabilities dynamically instead of statically.

--- mu-plugins/capabilities/includes/GreaterMedia/Capabilities/CapabilitiesLoader.php
+++ mu-plugins/capabilities/includes/GreaterMedia/Capabilities/CapabilitiesLoader.php
@@ -14,30 +14,6 @@ class CapabilitiesLoader {
 		'dj'            			=> false,
 	);
 
-	/*
-	 * Capabilities loader should never overwrite the capabilities of built-in
-	 * Wordpress post types.  Identify those here so they won't be affected in
-	 * change_roles.
-	 *
-	 */
-	public $internal_capabilities = array(
-		'read',
-		'edit_post',
-		'read_post',
-		'delete_post',
-		'create_posts',
-		'edit_posts',
-		'edit_others_posts',
-		'publish_posts',
-		'read_private_posts',
-		'delete_posts',
-		'delete_private_posts',
-		'delete_published_posts',
-		'delete_others_posts',
-		'edit_private_posts',
-		'edit_published_posts'
-	);
-
 	function load( $post_type ) {
 		$this->change_roles( $post_type, 'add' );
 	}
@@ -50,12 +26,24 @@ class CapabilitiesLoader {
 		$roles        = $this->get_roles();
 		$capabilities = $this->find( $post_type );
 
+		// Determine built-in capabilities so we can avoid modifying them for
+		// custom post types.
+
+		$_builtin_caps = array();
+
+		$_post_types = get_post_types( array( '_builtin' => true ), 'objects' );
+		foreach ( $_post_types as $_post_type ) {
+			$_builtin_caps = array_merge( $_builtin_caps, array_values( (array) $_post_type->cap ) );
+		}
+
+		$_builtin_caps = array_unique( $_builtin_caps );
+
 		foreach ( $roles as $role_name => $role_info ) {
 			$role         = get_role( $role_name );
 			$role_default = $this->get_role_default( $role_name );
 
 			foreach ( $capabilities as $capability ) {
-				if ( ! in_array( $capability, $this->internal_capabilities ) ){
+				if ( ! in_array( $capability, $_builtin_caps ) ){
 					if ( $action === 'add' ) {
 						//error_log( "add_cap: $post_type - $role_name - " . $capability );
 						$role->add_cap( $capability, $role_default );
