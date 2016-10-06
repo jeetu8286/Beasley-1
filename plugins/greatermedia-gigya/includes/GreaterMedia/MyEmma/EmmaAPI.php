9b91180f7ee64ceba10a3663cf57b1582080f21b
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-10-06 15:23:56 +0300

    Reverted changes into EmmaAPI class which override request method

--- plugins/greatermedia-gigya/includes/GreaterMedia/MyEmma/EmmaAPI.php
+++ plugins/greatermedia-gigya/includes/GreaterMedia/MyEmma/EmmaAPI.php
@@ -28,43 +28,4 @@ class EmmaAPI extends \Emma {
 		return $settings;
 	}
 
-	protected function _request( $url, $verb = null ) {
-		$ch = curl_init( $url );
-		curl_setopt( $ch, CURLOPT_USERPWD, "{$this->_pub_key}:{$this->_priv_key}" );
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
-
-		if ( isset( $verb ) ) {
-			if ( $verb == "post" ) {
-				curl_setopt( $ch, CURLOPT_POST, true );
-			} else {
-				curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, strtoupper( $verb ) );
-			}
-			curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $this->_postData ) );
-		}
-
-		$data = curl_exec( $ch );
-		$info = curl_getinfo( $ch );
-
-		$debug_email = get_option( 'emma_debug_email', false );
-		if ( filter_var( $debug_email, FILTER_VALIDATE_EMAIL ) ) {
-			$message = '';
-
-			$message .= var_export( $url, true ) . PHP_EOL;
-			$message .= var_export( $data, true ) . PHP_EOL;
-			$message .= var_export( $info, true ) . PHP_EOL;
-			$message .= var_export( $this->_postData, true ) . PHP_EOL;
-
-			wp_mail( $debug_email, 'Emma Request Report', $message );
-		}
-
-		curl_close( $ch );
-
-		if ( $this->_validHttpResponseCode( $info['http_code'] ) ) {
-			return $data;
-		} else {
-			throw new Emma_Invalid_Response_Exception( null, 0, $data, $info['http_code'] );
-		}
-	}
-
 }
