2c7eee22cb3a57165b2a97e78c1b34e398cbcb89
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-10-19 14:11:06 +0300

    Reworked profile query to use cursors

--- plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Sync/QueryTask.php
+++ plugins/greatermedia-gigya/includes/GreaterMedia/Gigya/Sync/QueryTask.php
@@ -45,20 +45,22 @@ class QueryTask extends SyncTask {
 	}
 
 	function run_fast_profile_preview() {
-		$paginator  = new QueryPaginator( 'profile', $this->preview_page_size );
+		$cursor     = $this->get_cursor();
 		$query      = $this->get_query();
 		$query      = str_replace( 'select *', 'select profile.email, profile.firstName, profile.lastName, UID', $query );
-		$matches    = $paginator->fetch( $query, 0 );
-		$users      = $this->find_preview_users( $matches['results'] );
 
-		$this->save_preview_users( $users, $matches['total_results'] );
+		$paginator  = new QueryPaginator( 'profile', $this->preview_page_size );
+		$results    = $paginator->fetch_with_cursor( $query, $cursor );
+		$users      = $this->find_preview_users( $results['results'] );
 
-		return $matches;
+		$this->save_preview_users( $users, $results['total_results'] );
+
+		return $results;
 	}
 
 	function run_fast_data_store_preview() {
-		$query     = $this->get_query();
 		$cursor    = $this->get_cursor();
+		$query     = $this->get_query();
 
 		$paginator = new QueryPaginator( 'data_store', $this->preview_page_size );
 		$results   = $paginator->fetch_with_cursor( $query, $cursor );
