00220b835d8b84cf6a54b26667e94a1b81a39ab1
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2016-10-24 12:20:08 -0400

    Inline galleries now using fotorama.  Single galleries not yet functional.

---
+++ plugins/greatermedia-galleries/assets/js/vendor/fotorama-wp.js
@@ -0,0 +1,40 @@
+(function ($) {
+	$(function () {
+		$.fn.fotoramaWPAdapter = function () {
+		    this.each(function () {
+		        var $this = $(this),
+		        	data = $this.data(),
+		        	$fotorama = $('<div></div>');
+
+		        $('dl', this).each(function () {
+		            var $a = $('dt a', this);
+		            $fotorama.append(
+		            	$a.attr('data-caption', $('dd', this).html())
+		            );
+		        });
+
+		        $this.html($fotorama.html());
+		    });
+
+		    return this;
+		};
+
+		$('.fotorama--wp')
+			.fotoramaWPAdapter()
+			.fotorama();
+
+		$('.fotorama').on('fotorama:ready', function(e, fotorama, extra){
+			console.log('fotorama ready!');
+		});
+	});
+})(jQuery);
+
+fotoramaDefaults = {
+	nav: 'thumbs',
+	allowfullscreen: 'native',
+	transition: 'crossfade',
+	loop: true,
+	keyboard: true
+}
+
+$
