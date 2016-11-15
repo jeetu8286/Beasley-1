5e21b143e0e37810291552258db1b3aea8052e53
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-11-15 18:23:00 +0200

    Fixed categories issue

--- themes/greatermedia/partials/article-categories.php
+++ themes/greatermedia/partials/article-categories.php
@@ -7,14 +7,17 @@
  */
 
 
-$the_id = get_the_ID();
 $post_taxonomies = get_post_taxonomies();
 
-?>
+$the_id = get_the_ID();
+$terms = get_the_term_list( $the_id, 'category', '<li class="article__list--item">', ',</li><li class="article__list--item">', '</li>' );
+if ( empty( $terms ) || is_wp_error( $terms ) ) {
+	return;
+}
 
-<div class="article__categories">
+?><div class="article__categories">
 	<div class="article__list--title"><?php _e( 'Category', 'greatermedia' ); ?></div>
 	<ul class="article__list--categories">
-		<?php echo get_the_term_list( $the_id, 'category', '<li class="article__list--item">', ',</li><li class="article__list--item">', '</li>' ); ?>
+		<?php echo $terms; ?>
 	</ul>
 </div>
