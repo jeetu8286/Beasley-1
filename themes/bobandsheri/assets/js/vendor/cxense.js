b4009d593f1f6327913dba2fbba36eb90f5f773d
Jonathan Mauney <jmauney@gmclt.com>
2016-11-21 11:45:36 -0500

    Fixed tracking javascript files

--- themes/bobandsheri/assets/js/vendor/cxense.js
+++ themes/bobandsheri/assets/js/vendor/cxense.js
@@ -1,12 +1,7 @@
-<!-- Cxense script begin -->
-<script type="text/javascript">
-  var cX = cX || {}; cX.callQueue = cX.callQueue || [];
-  cX.callQueue.push(['setSiteId', '1141842309601186431']);
-  cX.callQueue.push(['sendPageViewEvent']);
-</script>
-<script type="text/javascript">
-  (function(d,s,e,t){e=d.createElement(s);e.type='text/java'+s;e.async='async';
-  e.src='http'+('https:'===location.protocol?'s://s':'://')+'cdn.cxense.com/cx.js';
-  t=d.getElementsByTagName(s)[0];t.parentNode.insertBefore(e,t);})(document,'script');
-</script>
-<!-- Cxense script end -->
+var cX = cX || {}; cX.callQueue = cX.callQueue || [];
+cX.callQueue.push(['setSiteId', '1141842309601186431']);
+cX.callQueue.push(['sendPageViewEvent']);
+
+(function(d,s,e,t){e=d.createElement(s);e.type='text/java'+s;e.async='async';
+e.src='http'+('https:'===location.protocol?'s://s':'://')+'cdn.cxense.com/cx.js';
+t=d.getElementsByTagName(s)[0];t.parentNode.insertBefore(e,t);})(document,'script');
--- themes/wlnk/assets/js/vendor/quantcast.js
+++ themes/wlnk/assets/js/vendor/quantcast.js
@@ -1,25 +1,15 @@
-<!-- Quantcast Tag -->
-<script type="text/javascript">
-  var _qevents = _qevents || [];
+var _qevents = _qevents || [];
 
-  (function() {
-    var elem = document.createElement('script');
-    elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
-    elem.async = true;
-    elem.type = "text/javascript";
-    var scpt = document.getElementsByTagName('script')[0];
-    scpt.parentNode.insertBefore(elem, scpt);
-  })();
+(function() {
+  var elem = document.createElement('script');
+  elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+  elem.async = true;
+  elem.type = "text/javascript";
+  var scpt = document.getElementsByTagName('script')[0];
+  scpt.parentNode.insertBefore(elem, scpt);
+})();
 
-  _qevents.push({
-    qacct:"p-e1LfmtdyqxIM6",
-    labels:"Market.Charlotte-NC.WLNK-FM.Website,Format.Music.HotAC"
-  });
-</script>
-
-<noscript>
-  <div style="display:none;">
-    <img src="//pixel.quantserve.com/pixel/p-e1LfmtdyqxIM6.gif?labels=Market.Charlotte-NC.WLNK-FM.Website,Format.Music.HotAC" border="0" height="1" width="1" alt="Quantcast"/>
-  </div>
-</noscript>
-<!-- End Quantcast tag -->
+_qevents.push({
+  qacct:"p-e1LfmtdyqxIM6",
+  labels:"Market.Charlotte-NC.WLNK-FM.Website,Format.Music.HotAC"
+});
