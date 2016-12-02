79898b0079cc9b1b2b0c90ab8e12b0b702998418
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-12-02 19:06:20 +0200

    Improved getty images search

--- plugins/greatermedia-content-cleanup/includes/class-content-cleanup.php
+++ plugins/greatermedia-content-cleanup/includes/class-content-cleanup.php
@@ -8,42 +8,40 @@ class GMR_Content_Cleanup extends WP_CLI_Command {
 	 * @synopsis [--verbose]
 	 *
 	 * @access public
+	 * @global \wpdb $wpdb
 	 * @param array $args The array of arguments.
 	 * @param array $assoc_args The array of associted arguments.
 	 */
 	public function getty( $args, $assoc_args ) {
-		$query = new WP_Query();
+		global $wpdb;
+
+		$prefix = $wpdb->get_blog_prefix();
 		$verbose = ! empty( $assoc_args['verbose'] );
 
 		// remove action hook which would block us
 		remove_action( 'before_delete_post', 'gmr_contests_prevent_hard_delete' );
 
+		$query = <<<EOQ
+SELECT {$prefix}posts.ID
+  FROM {$prefix}posts
+ INNER JOIN {$prefix}postmeta ON {$prefix}posts.ID = {$prefix}postmeta.post_id
+ WHERE {$prefix}posts.post_type = 'attachment'
+   AND ({$prefix}posts.post_excerpt LIKE '%Getty%' OR ({$prefix}postmeta.meta_key = 'gmr_image_attribution' AND {$prefix}postmeta.meta_value LIKE '%Getty%'))
+ GROUP BY {$prefix}posts.ID
+ ORDER BY {$prefix}posts.post_date DESC
+ LIMIT 0, 100
+EOQ;
+
 		do {
 			// fetch posts
-			$query->query( array(
-				'post_type'           => 'attachment',
-				'post_status'         => 'any',
-				'suppress_filters'    => true,
-				'posts_per_page'      => 100,
-				'ignore_sticky_posts' => true,
-				'fields'              => 'ids',
-				'no_found_rows'       => true,
-				'meta_query'          => array(
-					array(
-						'key'     => 'gmr_image_attribution',
-						'value'   => 'Getty',
-						'compare' => 'LIKE',
-					),
-				),
-			) );
+			$images = $wpdb->get_col( $query );
 
 			// delete found posts
-			while ( $query->have_posts() ) {
-				$post_id = $query->next_post();
-				wp_delete_post( $post_id, true );
-				WP_CLI::success( "#{$post_id} deleted" );
+			foreach ( $images as $image ) {
+				wp_delete_post( $image, true );
+				WP_CLI::success( "#{$image} deleted" );
 			}
-		} while( $query->post_count > 0 );
+		} while( count( $images ) > 0 );
 
 		$verbose && WP_CLI::success( 'Clean up completed.' );
 	}
