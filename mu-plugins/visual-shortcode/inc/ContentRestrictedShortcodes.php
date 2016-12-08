1c9e8e7d794c35c1104ba8988723011030319f4e
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-12-08 15:36:09 +0200

    Fixed time-restricted-content issue

--- mu-plugins/visual-shortcode/inc/ContentRestrictedShortcodes.php
+++ mu-plugins/visual-shortcode/inc/ContentRestrictedShortcodes.php
@@ -3,69 +3,40 @@
 class ContentRestrictedShortcodes {
 
 	function register() {
-		add_action(
-			'admin_enqueue_scripts', array( $this, 'add_base_plugin' )
-		);
+		add_action( 'admin_enqueue_scripts', array( $this, 'add_base_plugin' ) );
 
-		add_filter(
-			'mce_external_plugins', array( $this, 'add_plugins' )
-		);
-
-		add_filter(
-			'mce_buttons', array( $this, 'add_buttons' )
-		);
-
-		add_filter(
-			'mce_css', array( $this, 'add_css' )
-		);
+		add_filter( 'mce_external_plugins', array( $this, 'add_plugins' ) );
+		add_filter( 'mce_buttons', array( $this, 'add_buttons' ) );
+		add_filter( 'mce_css', array( $this, 'add_css' ) );
 	}
 
 	function add_base_plugin() {
-		if ( $this->can_add_base_plugin() ) {
-			wp_enqueue_script(
-				'jquery-date-format',
-				$this->get_plugin_url( 'date.format/date-format' ),
-				array( 'jquery' ),
-				'0.1.0',
-				true
-			);
+		if ( $this->can_add_plugin() ) {
+			wp_enqueue_script( 'jquery-date-format', $this->get_plugin_url( 'date.format/date-format' ), array( 'jquery' ), '0.1.0', true );
+			wp_enqueue_script( 'jquery-datetimepicker', $this->get_plugin_url( 'datetimepicker/jquery-datetimepicker' ), array( 'jquery' ), '0.1.0', true );
+			wp_enqueue_script( 'visual-shortcode', $this->get_plugin_url( 'visual-shortcode' ), array( 'jquery' ), '0.1.0', false );
 
-			wp_enqueue_script(
-				'jquery-datetimepicker',
-				$this->get_plugin_url( 'datetimepicker/jquery-datetimepicker' ),
-				array( 'jquery' ),
-				'0.1.0',
-				true
-			);
-
-			wp_enqueue_script(
-				'visual-shortcode',
-				$this->get_plugin_url( 'visual-shortcode' ),
-				array( 'jquery' ),
-				'0.1.0',
-				false
-			);
-
-			wp_enqueue_style(
-				'visual-shortcode-admin',
-				$this->get_css_url( 'visual-shortcode-admin' )
-			);
+			wp_enqueue_style( 'visual-shortcode-admin', $this->get_css_url( 'visual-shortcode-admin' ) );
 		}
 	}
 
-	function can_add_base_plugin() {
+	function can_add_plugin() {
 		global $post;
-		return is_admin() && ! empty( $post ) && post_type_supports( $post->post_type, 'time-restricted-content' );
+		return is_admin() && ! empty( $post ) && post_type_supports( $post->post_type, 'timed-content' );
 	}
 
 	function add_plugins( $plugins ) {
-		$plugins['timeRestricted']  = $this->get_plugin_url( 'time-restricted' );
+		if ( $this->can_add_plugin() ) {
+			$plugins['timeRestricted']  = $this->get_plugin_url( 'time-restricted' );
+		}
 
 		return $plugins;
 	}
 
 	function add_buttons( $buttons ) {
-		$buttons[] = 'timeRestricted';
+		if ( $this->can_add_plugin() ) {
+			$buttons[] = 'timeRestricted';
+		}
 
 		return $buttons;
 	}
