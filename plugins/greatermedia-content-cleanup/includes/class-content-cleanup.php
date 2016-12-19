eb26216d10f55d35ba24f302cb54d5885163a408
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-12-19 11:55:34 +0200

    Implemented ability to delete video thumbnails

--- plugins/greatermedia-content-cleanup/includes/class-content-cleanup.php
+++ plugins/greatermedia-content-cleanup/includes/class-content-cleanup.php
@@ -3,34 +3,18 @@
 class GMR_Content_Cleanup extends WP_CLI_Command {
 
 	/**
-	 * Cleans up getty images.
+	 * Removes posts based on provided query.
 	 *
-	 * @synopsis [--verbose]
-	 *
-	 * @access public
+	 * @access private
 	 * @global \wpdb $wpdb
-	 * @param array $args The array of arguments.
-	 * @param array $assoc_args The array of associted arguments.
+	 * @param string $query
+	 * @param boolean $verbose
 	 */
-	public function getty( $args, $assoc_args ) {
+	private function _cleanup( $query, $verbose ) {
 		global $wpdb;
-
-		$prefix = $wpdb->get_blog_prefix();
-		$verbose = ! empty( $assoc_args['verbose'] );
 
 		// remove action hook which would block us
 		remove_action( 'before_delete_post', 'gmr_contests_prevent_hard_delete' );
-
-		$query = <<<EOQ
-SELECT {$prefix}posts.ID
-  FROM {$prefix}posts
- INNER JOIN {$prefix}postmeta ON {$prefix}posts.ID = {$prefix}postmeta.post_id
- WHERE {$prefix}posts.post_type = 'attachment'
-   AND ({$prefix}posts.post_excerpt LIKE '%Getty%' OR ({$prefix}postmeta.meta_key = 'gmr_image_attribution' AND {$prefix}postmeta.meta_value LIKE '%Getty%'))
- GROUP BY {$prefix}posts.ID
- ORDER BY {$prefix}posts.post_date DESC
- LIMIT 0, 100
-EOQ;
 
 		do {
 			// fetch posts
@@ -44,6 +28,61 @@ EOQ;
 		} while( count( $images ) > 0 );
 
 		$verbose && WP_CLI::success( 'Clean up completed.' );
+	}
+
+	/**
+	 * Cleans up getty images.
+	 *
+	 * @synopsis [--verbose]
+	 *
+	 * @access public
+	 * @global \wpdb $wpdb
+	 * @param array $args The array of arguments.
+	 * @param array $assoc_args The array of associted arguments.
+	 */
+	public function getty( $args, $assoc_args ) {
+		global $wpdb;
+
+		$prefix = $wpdb->get_blog_prefix();
+
+		$query = <<<EOQ
+SELECT {$prefix}posts.ID
+  FROM {$prefix}posts
+ INNER JOIN {$prefix}postmeta ON {$prefix}posts.ID = {$prefix}postmeta.post_id
+ WHERE {$prefix}posts.post_type = 'attachment'
+   AND ({$prefix}posts.post_excerpt LIKE '%Getty%' OR ({$prefix}postmeta.meta_key = 'gmr_image_attribution' AND {$prefix}postmeta.meta_value LIKE '%Getty%'))
+ GROUP BY {$prefix}posts.ID
+ ORDER BY {$prefix}posts.post_date DESC
+ LIMIT 0, 100
+EOQ;
+
+		$this->_cleanup( $query, ! empty( $assoc_args['verbose'] ) );
+	}
+
+	/**
+	 * Cleans up video thumbnail images.
+	 *
+	 * @synopsis [--verbose]
+	 *
+	 * @access public
+	 * @global \wpdb $wpdb
+	 * @param array $args The array of arguments.
+	 * @param array $assoc_args The array of associted arguments.
+	 */
+	public function videothumbs( $args, $assoc_args ) {
+		global $wpdb;
+
+		$prefix = $wpdb->get_blog_prefix();
+
+		$query = <<<EOQ
+SELECT {$prefix}postmeta.post_id
+  FROM {$prefix}postmeta
+ WHERE {$prefix}postmeta.meta_key = '_thumbnail_id'
+   AND {$prefix}postmeta.meta_value = 1
+ LIMIT 0, 100
+EOQ;
+
+		$this->_cleanup( $query, ! empty( $assoc_args['verbose'] ) );
 	}
 
 }
