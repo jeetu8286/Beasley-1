07ea014b4bac349066c9532f9d905b3387857d5a
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-12-20 20:26:30 +0200

    Added anonymous authentication

--- themes/greatermedia/assets/js/frontend.js
+++ themes/greatermedia/assets/js/frontend.js
@@ -1442,4 +1442,17 @@
 })(jQuery, window, document);
 (function(config) {
 	firebase.initializeApp(config);
+
+	firebase.auth().signInAnonymously().catch(function(error) {
+		console.error(error.message);
+	});
+
+	firebase.auth().onAuthStateChanged(function(user) {
+		var isAnonymous, uid;
+
+		if (user) {
+			isAnonymous = user.isAnonymous;
+			uid = user.uid;
+		}
+	});
 })(window.beasley.firebase);
--- themes/greatermedia/assets/js/src/firebase.js
+++ themes/greatermedia/assets/js/src/firebase.js
@@ -1,3 +1,16 @@
 (function(config) {
 	firebase.initializeApp(config);
+
+	firebase.auth().signInAnonymously().catch(function(error) {
+		console.error(error.message);
+	});
+
+	firebase.auth().onAuthStateChanged(function(user) {
+		var isAnonymous, uid;
+
+		if (user) {
+			isAnonymous = user.isAnonymous;
+			uid = user.uid;
+		}
+	});
 })(window.beasley.firebase);
