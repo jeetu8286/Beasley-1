d21e0764a8eecf84a150ad825cda30e35f4408fe
Jonathan Mauney <jmauney@gmclt.com>
2017-01-11 15:24:11 -0500

    Added rss2_item for featured image back into wlnk functions.php

--- themes/wlnk/functions.php
+++ themes/wlnk/functions.php
@@ -80,3 +80,22 @@ define( 'WLNK_VERSION', '0.3.7' ); /* Version bump by Jonathan 1/10/2017 @ 12:26
 	echo apply_filters( 'wlnk_humans', $humans );
  }
  add_action( 'wp_head', 'wlnk_header_meta' );
+
+ function add_featured_image_in_rss() {
+    if ( function_exists( 'get_the_image' ) && ( $featured_image = get_the_image('format=array&echo=0') ) ) {
+        $featured_image[0] = $featured_image['url'];
+    } elseif ( function_exists( 'has_post_thumbnail' ) and has_post_thumbnail() ) {
+        $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'post-thumbnail' );
+    } elseif ( function_exists( 'get_post_thumbnail_src' ) ) {
+        $featured_image = get_post_thumbnail_src();
+        if ( preg_match( '|^<img src="([^"]+)"|', $featured_image[0], $m ) )
+            $featured_image[0] = $m[1];
+    } else {
+        $featured_image = false;
+    }
+    if ( ! empty( $featured_image ) ) {
+        echo "\t" . '<enclosure url="' . $featured_image[0] . '" />' . "\n";
+    }
+
+}
+add_action( 'rss2_item', 'add_featured_image_in_rss' );
