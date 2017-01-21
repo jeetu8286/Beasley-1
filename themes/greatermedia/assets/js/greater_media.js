86502ffa245a0e7bb04c31cc3993c02c7df34580
Dominic Magnifico <dominic.magnifico@10up.com>
2017-01-20 17:17:41 -0700

    Compile JS.

--- themes/greatermedia/assets/js/greater_media.js
+++ themes/greatermedia/assets/js/greater_media.js
@@ -1352,3 +1352,14 @@
 	} );
 	
 })(jQuery, window, document);
+(function ($, window, document, undefined) {
+  if( $('.featured-post-slider').length ) {
+    $('.featured-post-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+  }
+})(jQuery, window, document);
