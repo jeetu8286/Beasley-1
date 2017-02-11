a7a9fb02982a66a698b05f4bce7af97b8b9a8b7f
Ryan Sommers <ryan.sommers@10up.com>
2017-02-11 13:24:19 -0800

    Working on audio readout area, ads, more cleanup

--- themes/greatermedia/partials/live-player-static.php
+++ themes/greatermedia/partials/live-player-static.php
@@ -22,12 +22,12 @@
 						<?php // endforeach; ?>
 					</ul>
 				</li>
-			</ul>
+			</ul><!-- .audio-stream__list -->
 
 			<div class="audio-stream__sponsored">
 				<span class="audio-stream__sponsored--text"><?php esc_html_e( 'Sponsored', 'greatermedia' ); ?></span> <span class="audio-stream__sponsored--name"><?php esc_html_e( 'Company Name', 'greatermedia' ); ?></span>
 			</div>
-		</nav>
+		</nav><!-- .audio-stream -->
 
 		<div id="js-audio-player" class="audio-player">
 
@@ -36,29 +36,25 @@
 				<button class="audio-volume__mute"><span class="audio-volume__text">Mute Volume</span></button>
 				<input type="range" name="player-volume" id="player-volume" min="0" max="100" step="1" value="75" title="Volume"/>
 				<button class="audio-volume__btn"><span class="audio-volume__text">Volume</span></button>
-			</div>
+			</div><!-- .audio-volume -->
 
 			<div class="audio-controls">
 				<div id="playButton" class="audio-controls__play" data-action="play-live"></div>
 				<div id="loadButton" class="audio-controls__loading"><i class="gmr-icon icon-spin icon-loading"></i></div>
 				<div id="pauseButton" class="audio-controls__pause"></div>
-			</div>
+			</div><!-- .audio-controls -->
 
 			<div id="js-audio-readout" class="audio-readout">
-				<div id="td_container" class="live-stream__container--player"></div>
-				<div class="pre-roll__notification"><?php esc_html_e( 'Live stream will be available after this brief ad from our sponsors', ' gmliveplayer' ); ?></div>
 
-				<div id="audio__time" class="audio__time">
-					<div id="audio__progress-bar" class="audio__progress-bar">
-						<span id="audio__progress" class="audio__progress"></span>
-					</div>
-					<div id="audio__time--elapsed" class="audio__time--elapsed"></div>
-					<div id="audio__time--remaining" class="audio__time--remaining"></div>
-				</div>
-				<div id="nowPlaying" class="now-playing">
-					<div id="trackInfo" class="now-playing__info"></div>
-					<div id="npeInfo"></div>
-				</div>
+				<?php // @TODO Available classes to add to audio-readout__notification: -show ?>
+				<div id="js-notification-listen" class="audio-readout__notification audio-readout__notification--listen"><?php esc_html_e( 'Listen Live', ' gmliveplayer' ); ?></div>
+				<div id="js-notification-preroll" class="audio-readout__notification audio-readout__notification--preroll"><?php esc_html_e( 'Live stream will be available after this brief ad from our sponsors', ' gmliveplayer' ); ?></div>
+
+				<div id="js-now-playing" class="audio-playing">
+					<div id="js-track-info" class="audio-playing__track"><?php esc_html_e( 'Track Name', 'greatermedia' ); ?></div>
+					<div id="js-artist-info" class="audio-playing__artist"><?php esc_html_e( 'Artist Name', 'greatermedia' ); ?></div>
+				</div><!-- .audio-playing -->
+
 
 				<div id="live-stream__status" class="live-stream__status">
 					<div id="live-stream__now-playing" class="live-stream__now-playing--btn"><?php esc_html_e( 'Now Playing', 'greatermedia' ); ?></div>
@@ -66,8 +62,23 @@
 				</div>
 
 				<div id="live-player--more" class="live-player--more"><?php esc_html_e( '...', 'greatermedia' ); ?></div>
-			</div>
-		</div>
+
+				<div id="js-audio-time" class="audio-time">
+					<div id="js-audio-time__progressbar" class="audio-time__progressbar">
+						<span id="js-audio-time__progress" class="audio-time__progress"></span>
+					</div>
+					<div id="js-audio-time__elapsed" class="audio-time__elapsed"></div>
+					<div id="js-audio-time__remaining" class="audio-time__remaining"></div>
+				</div><!-- .audio-time -->
+
+		  	<?php // @TODO Available classes to add to audio-ad: -show ?>
+				<div id="js-audio-ad" class="audio-ad -show">
+					<?php // @TODO Ad code ?>
+					Ad code here
+				</div>
+
+			</div><!-- .audio-readout -->
+		</div><!-- .audio-player -->
 
 	</div>
-</div>
+</div><!-- .audio-interface -->
