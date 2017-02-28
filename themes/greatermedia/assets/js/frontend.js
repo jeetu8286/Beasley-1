02febc0e8ea0cd5f50c107e8ca5e1f8ca10dc51a
Ryan Sommers <ryan.sommers@10up.com>
2017-02-28 07:36:08 -0800

    Single station styling, fixed player admin bar fix

--- themes/greatermedia/assets/js/frontend.js
+++ themes/greatermedia/assets/js/frontend.js
@@ -2982,9 +2982,7 @@ $.fn.cycle.transitions.carousel = {
 	var body = document.querySelector( 'body' ),
 		header = document.getElementById( 'header' ),
 		header_main = header.getElementsByClassName( 'header__main' )[0],
-		header_container = header.querySelectorAll( '.header__main > .container' )[0],
-		header_container_height_min,
-		admin_bar;
+		header_container = header.querySelectorAll( '.header__main > .container' )[0];
 
 	if ( window.matchMedia( '(min-width: 768px)' ).matches ) {
 
@@ -2999,20 +2997,9 @@ $.fn.cycle.transitions.carousel = {
 			// Set header height
 			header.style.height = header_height + 'px';
 
-			// Is admin bar present
-			if ( body.classList.contains( 'admin-bar' ) ) {
-				admin_bar = true;
-			}
-
 			// Manage the position of the header during headroom.js events
 			var headroomManagePosition = function() {
-				header_container_height_min = header_container.offsetHeight;
-
-				if ( admin_bar ) {
-					header_main.style.transform = 'translateY(-' + (header_container_height_min + 32) + 'px)';
-				} else {
-					header_main.style.transform = 'translateY(-' + header_container_height_min + 'px)';
-				}
+				header_main.style.transform = 'translateY(-' + header_container.offsetHeight + 'px)';
 			};
 
 			// Headroom.js
