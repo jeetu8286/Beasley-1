47fb5fc47cdb9da79424f39b37553a9864478163
Tom Stark <tom.stark@bbgi.com>
2017-03-02 11:34:56 -0500

    WRAT CSS overrides empty

--- themes/wrat/functions.php
+++ themes/wrat/functions.php
@@ -13,7 +13,7 @@
  */
 
  // Useful global constants
-define( 'WRAT_VERSION', '0.2.5' ); /* Version bump by Steve 2/23/17 */
+define( 'WRAT_VERSION', '0.2.6' ); /* Version bump by Tom 3/2/17 */
 
  /**
   * Set up theme defaults and register supported WordPress features.
