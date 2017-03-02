a7029abe13308b0cd6d7f450f8b80c933cb2492f
Tom Stark <tom.stark@bbgi.com>
2017-03-02 16:52:36 -0500

    WJRZ CSS and variable updates

--- themes/wjrz/functions.php
+++ themes/wjrz/functions.php
@@ -13,7 +13,7 @@
  */
 
  // Useful global constants
-define( 'WJRZ_VERSION', '0.2.7' ); /* Version bump by Steve 2/23/17 */
+define( 'WJRZ_VERSION', '0.2.8' ); /* Version bump by Tom 3/2/17 */
 
  /**
   * Set up theme defaults and register supported WordPress features.
