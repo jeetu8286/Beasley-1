ed7be2379de641ec45c3b6ba8cd3eb65b00ed2b3
Tom Stark <tom.stark@bbgi.com>
2017-03-03 14:00:40 -0500

    WMGQ header update

--- themes/wmgq/functions.php
+++ themes/wmgq/functions.php
@@ -13,7 +13,7 @@
  */
 
  // Useful global constants
-define( 'WMGQ_VERSION', '0.3.6' ); /* Version bump by Tom 3/2/17 */
+define( 'WMGQ_VERSION', '0.3.7' ); /* Version bump by Tom 3/2/17 */
 
  /**
   * Set up theme defaults and register supported WordPress features.
