422eecf67413722372908bbd487703859dae296b
Tom Stark <tom.stark@bbgi.com>
2017-03-07 15:56:22 -0500

    WRAT update search rollover color

--- themes/wrat/functions.php
+++ themes/wrat/functions.php
@@ -13,7 +13,7 @@
  */
 
  // Useful global constants
-define( 'WRAT_VERSION', '0.2.10' ); /* Version bump by Tom 3/7/17 */
+define( 'WRAT_VERSION', '0.2.11' ); /* Version bump by Tom 3/7/17 */
 
  /**
   * Set up theme defaults and register supported WordPress features.
