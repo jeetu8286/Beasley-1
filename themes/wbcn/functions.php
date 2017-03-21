ae00a7a72bdaba3cd39a1309990637554d3dd01a
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2017-03-21 10:51:00 -0400

    Updating wbcn front-page and wsoc background image.

--- themes/wbcn/functions.php
+++ themes/wbcn/functions.php
@@ -13,7 +13,7 @@
  */
 
  // Useful global constants
-define( 'WBCN_VERSION', '0.1.1' ); /* Version bump by Steve 03/17/2017 */
+define( 'WBCN_VERSION', '0.1.2' ); /* Version bump by Steve 03/21/2017 */
 
  /**
   * Set up theme defaults and register supported WordPress features.
--- themes/wsoc/functions.php
+++ themes/wsoc/functions.php
@@ -13,7 +13,7 @@
  */
 
  // Useful global constants
-define( 'WSOC_VERSION', '0.1.2' ); /* Version bump by Steve 03/20/2017 */
+define( 'WSOC_VERSION', '0.1.3' ); /* Version bump by Steve 03/21/2017 */
 
  /**
   * Set up theme defaults and register supported WordPress features.
