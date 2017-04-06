528b1a2e69a8c9d10a8243d7b15e3681765ecd7b
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2017-04-06 13:51:12 -0400

    Styling change for crowdy widget on wwcn front page.

--- themes/wwcn/front-page.php
+++ themes/wwcn/front-page.php
@@ -16,6 +16,19 @@ get_header();
 
 		<section class="content">
 
+		<style>
+			.cnwidget.cnfp .cnheader,
+			.cnwidget.cnfp .cnfilter-menu.cnfilter-dropdown .cnmenu-services .cnfiltermenu-header {
+			background: #ea0017;
+			}
+			.cnwidget.cnfp .cntweet .cnitem-desc a {
+			color: #ea0017;
+			}
+			.cnwidget.cnfp .cnitem-footer {
+			border-bottom: 4px solid #ea0017;
+			}
+		</style>
+
 			<div data-crowdynews-widget="BeasleyBroadcastGroupInc_993-espn-social-spot-full-page"><script src="//widget.crowdynews.com/BeasleyBroadcastGroupInc_993-espn-social-spot-full-page.js" async="true"></script></div>
 
 		</section>
