2b7e222dc31ac312a59be3a10bc24232e30dfd5c
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2017-04-06 00:07:23 -0400

    Updating wjpt, wwcn and wqyk themes for launch.

--- themes/wjpt/functions.php
+++ themes/wjpt/functions.php
@@ -11,10 +11,10 @@
  * @package WJPT
  * @since 0.1.0
  */
- 
+
  // Useful global constants
-define( 'WJPT_VERSION', '0.1.0' );
- 
+define( 'WJPT_VERSION', '0.1.1' );
+
  /**
   * Set up theme defaults and register supported WordPress features.
   *
@@ -33,7 +33,7 @@ define( 'WJPT_VERSION', '0.1.0' );
 	load_theme_textdomain( 'wjpt', get_stylesheet_directory_uri() . '/languages' );
  }
  add_action( 'after_setup_theme', 'wjpt_setup' );
- 
+
  /**
   * Enqueue scripts and styles for front-end.
   *
@@ -43,17 +43,17 @@ define( 'WJPT_VERSION', '0.1.0' );
 	$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
 	wp_dequeue_style( 'greatermedia' );
-	wp_deregister_style( 'greatermedia' );	
+	wp_deregister_style( 'greatermedia' );
 	wp_enqueue_style( 'wjpt', get_stylesheet_directory_uri() . "/assets/css/wjpt{$postfix}.css", array(), WJPT_VERSION );
  }
  add_action( 'wp_enqueue_scripts', 'wjpt_scripts_styles', 20 );
- 
+
  /**
   * Add humans.txt to the <head> element.
   */
  function wjpt_header_meta() {
 	$humans = '<link type="text/plain" rel="author" href="' . get_stylesheet_directory_uri() . '/humans.txt" />';
-	
+
 	echo apply_filters( 'wjpt_humans', $humans );
  }
  add_action( 'wp_head', 'wjpt_header_meta' );
