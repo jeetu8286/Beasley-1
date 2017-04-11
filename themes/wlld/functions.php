6c0715d883151debde62ebb30b017f05d2be25cc
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2017-04-11 09:08:26 -0400

    Updating themes for wlld, wrbq and wrxk-hd2.

--- themes/wlld/functions.php
+++ themes/wlld/functions.php
@@ -11,9 +11,9 @@
  * @package WLLD
  * @since 0.1.0
  */
- 
+
  // Useful global constants
-define( 'WLLD_VERSION', '0.1.0' );
+define( 'WLLD_VERSION', '0.1.1' );
  
  /**
   * Set up theme defaults and register supported WordPress features.
@@ -33,7 +33,7 @@ define( 'WLLD_VERSION', '0.1.0' );
 	load_theme_textdomain( 'wlld', get_stylesheet_directory_uri() . '/languages' );
  }
  add_action( 'after_setup_theme', 'wlld_setup' );
- 
+
  /**
   * Enqueue scripts and styles for front-end.
   *
@@ -43,17 +43,17 @@ define( 'WLLD_VERSION', '0.1.0' );
 	$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
 	wp_dequeue_style( 'greatermedia' );
-	wp_deregister_style( 'greatermedia' );	
+	wp_deregister_style( 'greatermedia' );
 	wp_enqueue_style( 'wlld', get_stylesheet_directory_uri() . "/assets/css/wlld{$postfix}.css", array(), WLLD_VERSION );
  }
  add_action( 'wp_enqueue_scripts', 'wlld_scripts_styles', 20 );
- 
+
  /**
   * Add humans.txt to the <head> element.
   */
  function wlld_header_meta() {
 	$humans = '<link type="text/plain" rel="author" href="' . get_stylesheet_directory_uri() . '/humans.txt" />';
-	
+
 	echo apply_filters( 'wlld_humans', $humans );
  }
  add_action( 'wp_head', 'wlld_header_meta' );
--- themes/wrxk-hd2/functions.php
+++ themes/wrxk-hd2/functions.php
@@ -11,10 +11,10 @@
  * @package WRXK-HD2
  * @since 0.1.0
  */
- 
+
  // Useful global constants
-define( 'WRXKHDTWO_VERSION', '0.1.0' );
- 
+define( 'WRXKHDTWO_VERSION', '0.1.1' );
+
  /**
   * Set up theme defaults and register supported WordPress features.
   *
@@ -33,7 +33,7 @@ define( 'WRXKHDTWO_VERSION', '0.1.0' );
 	load_theme_textdomain( 'wrxkhdtwo', get_stylesheet_directory_uri() . '/languages' );
  }
  add_action( 'after_setup_theme', 'wrxkhdtwo_setup' );
- 
+
  /**
   * Enqueue scripts and styles for front-end.
   *
@@ -43,17 +43,17 @@ define( 'WRXKHDTWO_VERSION', '0.1.0' );
 	$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
 	wp_dequeue_style( 'greatermedia' );
-	wp_deregister_style( 'greatermedia' );	
+	wp_deregister_style( 'greatermedia' );
 	wp_enqueue_style( 'wrxkhdtwo', get_stylesheet_directory_uri() . "/assets/css/wrxk_hd2{$postfix}.css", array(), WRXKHDTWO_VERSION );
  }
  add_action( 'wp_enqueue_scripts', 'wrxkhdtwo_scripts_styles', 20 );
- 
+
  /**
   * Add humans.txt to the <head> element.
   */
  function wrxkhdtwo_header_meta() {
 	$humans = '<link type="text/plain" rel="author" href="' . get_stylesheet_directory_uri() . '/humans.txt" />';
-	
+
 	echo apply_filters( 'wrxkhdtwo_humans', $humans );
  }
  add_action( 'wp_head', 'wrxkhdtwo_header_meta' );
