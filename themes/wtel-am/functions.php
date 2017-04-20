e446f3709d908a2dc7e9e26dac769ab04195d81a
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2017-04-20 12:02:22 -0400

    Updating themes for wrdw, wtel and wuks for today's launches.

--- themes/wtel-am/functions.php
+++ themes/wtel-am/functions.php
@@ -11,10 +11,10 @@
  * @package WTEL-AM
  * @since 0.1.0
  */
- 
+
  // Useful global constants
-define( 'WTELAM_VERSION', '0.1.0' );
- 
+define( 'WTELAM_VERSION', '0.1.1' );
+
  /**
   * Set up theme defaults and register supported WordPress features.
   *
@@ -33,7 +33,7 @@ define( 'WTELAM_VERSION', '0.1.0' );
 	load_theme_textdomain( 'wtelam', get_stylesheet_directory_uri() . '/languages' );
  }
  add_action( 'after_setup_theme', 'wtelam_setup' );
- 
+
  /**
   * Enqueue scripts and styles for front-end.
   *
@@ -43,17 +43,17 @@ define( 'WTELAM_VERSION', '0.1.0' );
 	$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
 	wp_dequeue_style( 'greatermedia' );
-	wp_deregister_style( 'greatermedia' );	
+	wp_deregister_style( 'greatermedia' );
 	wp_enqueue_style( 'wtelam', get_stylesheet_directory_uri() . "/assets/css/wtel_am{$postfix}.css", array(), WTELAM_VERSION );
  }
  add_action( 'wp_enqueue_scripts', 'wtelam_scripts_styles', 20 );
- 
+
  /**
   * Add humans.txt to the <head> element.
   */
  function wtelam_header_meta() {
 	$humans = '<link type="text/plain" rel="author" href="' . get_stylesheet_directory_uri() . '/humans.txt" />';
-	
+
 	echo apply_filters( 'wtelam_humans', $humans );
  }
  add_action( 'wp_head', 'wtelam_header_meta' );
