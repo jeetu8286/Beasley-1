1dc038e663a555b542c7edf9d5f7c2e1c9d64d16
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2017-04-25 06:40:18 -0700

    Theme updates for whhd and wkxc.

--- themes/wkxc/functions.php
+++ themes/wkxc/functions.php
@@ -11,10 +11,10 @@
  * @package WKXC
  * @since 0.1.0
  */
- 
+
  // Useful global constants
-define( 'WKXC_VERSION', '0.1.0' );
- 
+define( 'WKXC_VERSION', '0.1.1' );
+
  /**
   * Set up theme defaults and register supported WordPress features.
   *
@@ -33,7 +33,7 @@ define( 'WKXC_VERSION', '0.1.0' );
 	load_theme_textdomain( 'wkxc', get_stylesheet_directory_uri() . '/languages' );
  }
  add_action( 'after_setup_theme', 'wkxc_setup' );
- 
+
  /**
   * Enqueue scripts and styles for front-end.
   *
@@ -43,17 +43,17 @@ define( 'WKXC_VERSION', '0.1.0' );
 	$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
 	wp_dequeue_style( 'greatermedia' );
-	wp_deregister_style( 'greatermedia' );	
+	wp_deregister_style( 'greatermedia' );
 	wp_enqueue_style( 'wkxc', get_stylesheet_directory_uri() . "/assets/css/wkxc{$postfix}.css", array(), WKXC_VERSION );
  }
  add_action( 'wp_enqueue_scripts', 'wkxc_scripts_styles', 20 );
- 
+
  /**
   * Add humans.txt to the <head> element.
   */
  function wkxc_header_meta() {
 	$humans = '<link type="text/plain" rel="author" href="' . get_stylesheet_directory_uri() . '/humans.txt" />';
-	
+
 	echo apply_filters( 'wkxc_humans', $humans );
  }
  add_action( 'wp_head', 'wkxc_header_meta' );
