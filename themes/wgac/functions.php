1144fe3a85822f2bed34a4f6f1b187543fb338e0
Steve Meyers <smeyers@greatermediaphiladelphia.com>
2017-04-26 07:52:12 -0700

    Updating themes for wgac, wdrr and wgus.

--- themes/wgac/functions.php
+++ themes/wgac/functions.php
@@ -11,10 +11,10 @@
  * @package WGAC
  * @since 0.1.0
  */
- 
+
  // Useful global constants
-define( 'WGAC_VERSION', '0.1.0' );
- 
+define( 'WGAC_VERSION', '0.1.1' );
+
  /**
   * Set up theme defaults and register supported WordPress features.
   *
@@ -33,7 +33,7 @@ define( 'WGAC_VERSION', '0.1.0' );
 	load_theme_textdomain( 'wgac', get_stylesheet_directory_uri() . '/languages' );
  }
  add_action( 'after_setup_theme', 'wgac_setup' );
- 
+
  /**
   * Enqueue scripts and styles for front-end.
   *
@@ -43,17 +43,17 @@ define( 'WGAC_VERSION', '0.1.0' );
 	$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
 	wp_dequeue_style( 'greatermedia' );
-	wp_deregister_style( 'greatermedia' );	
+	wp_deregister_style( 'greatermedia' );
 	wp_enqueue_style( 'wgac', get_stylesheet_directory_uri() . "/assets/css/wgac{$postfix}.css", array(), WGAC_VERSION );
  }
  add_action( 'wp_enqueue_scripts', 'wgac_scripts_styles', 20 );
- 
+
  /**
   * Add humans.txt to the <head> element.
   */
  function wgac_header_meta() {
 	$humans = '<link type="text/plain" rel="author" href="' . get_stylesheet_directory_uri() . '/humans.txt" />';
-	
+
 	echo apply_filters( 'wgac_humans', $humans );
  }
  add_action( 'wp_head', 'wgac_header_meta' );
