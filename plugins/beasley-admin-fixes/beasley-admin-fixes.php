3468e691cfeaab504cc5ceae9c5178d8e9193b9b
Chris Marslender <chrismarslender@gmail.com>
2017-07-21 11:53:39 -0700

    Fix PHP notice when edit flow not active

--- plugins/beasley-admin-fixes/beasley-admin-fixes.php
+++ plugins/beasley-admin-fixes/beasley-admin-fixes.php
@@ -16,7 +16,8 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Sets checkbox of "Needs Photo" to false by default, see bug created here: https://github.com/Automattic/Edit-Flow/issues/397
  */
 function fix_new_post_checkbox_defaults() {
-	wp_enqueue_script( 'new-post-checkbox-js', plugins_url( 'beasley-admin-fixes/js/checkbox.js' ), 'jquery', EDIT_FLOW_VERSION, true );
+	$version = defined( 'EDIT_FLOW_VERSION' ) ? EDIT_FLOW_VERSION : '';
+	wp_enqueue_script( 'new-post-checkbox-js', plugins_url( 'beasley-admin-fixes/js/checkbox.js' ), 'jquery', $version, true );
 }
 
 add_action( 'admin_enqueue_scripts', 'fix_new_post_checkbox_defaults', 100 );
