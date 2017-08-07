4f3085d1f08ae03ef1fc29f7c49f31a66af60a46
Faishal Saiyed <saiyedfaishal@gmail.com>
2017-08-07 18:56:12 +0530

    GMR Gallery: Bump version

--- plugins/greatermedia-galleries/greatermedia-galleries.php
+++ plugins/greatermedia-galleries/greatermedia-galleries.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( "Please don't try to access this file directly." );
 }
 
-define( 'GREATER_MEDIA_GALLERIES_VERSION', '2.2.1' );
+define( 'GREATER_MEDIA_GALLERIES_VERSION', '2.2.2' );
 define( 'GREATER_MEDIA_GALLERIES_URL', plugin_dir_url( __FILE__ ) );
 define( 'GREATER_MEDIA_GALLERIES_PATH', dirname( __FILE__ ) );
 
