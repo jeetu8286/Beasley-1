e844c5366e0f15e645be2790d4cc7238d868545d
Chris Marslender <chrismarslender@gmail.com>
2018-02-12 15:41:01 -0700

    Add test + deploy scripts up through stage env

---
+++ deploy-scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Print commands to the screen
+set -x
+
+pushd plugins/greatermedia-content-syndication || exit 1
+composer install --no-dev -o
+popd || exit 1
+
+pushd themes || exit 1
+npm install
+npm run build
+popd || exit 1
+
+# Stop printing commands to screen
+set +x
