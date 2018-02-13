b81003f8e2f0f6312f9eeb98931e8606d8bfcba7
Chris Marslender <chrismarslender@gmail.com>
2018-02-12 18:25:30 -0700

    Add --delete to plugins

--- deploy-scripts/deploy-stage.sh
+++ deploy-scripts/deploy-stage.sh
@@ -2,4 +2,4 @@
 
 # @todo move all plugins to repo/composer combination, and add --delete flag to plugins
 rsync -vrxc --delete ./themes/ beanstalk@54.87.4.54:/var/www/html/wordpress/wp-content/themes/ --exclude-from=./deploy-scripts/rsync-excludes.txt
-rsync -vrxc ./plugins/ beanstalk@54.87.4.54:/var/www/html/wordpress/wp-content/plugins/ --exclude-from=./deploy-scripts/rsync-excludes.txt
+rsync -vrxc --delete ./plugins/ beanstalk@54.87.4.54:/var/www/html/wordpress/wp-content/plugins/ --exclude-from=./deploy-scripts/rsync-excludes.txt
