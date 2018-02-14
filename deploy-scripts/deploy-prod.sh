09fd57811fed019dd9426c8caca9bcb075bd1afa
Eugene Manuilov <eugene.manuilov@10up.com>
2018-02-14 20:39:13 +0200

    Updated deploy-to-production script to include mu-plugins folder

--- deploy-scripts/deploy-prod.sh
+++ deploy-scripts/deploy-prod.sh
@@ -2,3 +2,4 @@
 
 rsync -vrxc --delete ./themes/ beanstalk@52.0.13.41:/var/www/html/wordpress/wp-content/themes/ --exclude-from=./deploy-scripts/rsync-excludes.txt
 rsync -vrxc --delete ./plugins/ beanstalk@52.0.13.41:/var/www/html/wordpress/wp-content/plugins/ --exclude-from=./deploy-scripts/rsync-excludes.txt
+rsync -vrxc --delete ./mu-plugins/ beanstalk@52.0.13.41:/var/www/html/wordpress/wp-content/mu-plugins/ --exclude-from=./deploy-scripts/rsync-excludes.txt
