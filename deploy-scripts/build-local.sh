5552b011e46df87410a1241699d52641afaebad3
Chris Marslender <chrismarslender@gmail.com>
2018-04-02 13:14:58 -0700

    Add build-local.sh script to build local version of the site (includes dev dependencies, and doesn't change composer cache dir)

---
+++ deploy-scripts/build-local.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Print commands to the screen
+set -x
+
+composer install
+
+pushd plugins/greatermedia-content-syndication || exit 1
+composer install
+popd || exit 1
+
+pushd themes || exit 1
+npm install
+npm run build
+popd || exit 1
+
+# Stop printing commands to screen
+set +x
