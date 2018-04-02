4661cd6237d732c85415af8e8187611dac9488e6
Chris Marslender <chrismarslender@gmail.com>
2018-04-02 14:52:20 -0700

    Update deploy to deploy entire wp-content directory

--- deploy-scripts/deploy-stage.sh
+++ deploy-scripts/deploy-stage.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-rsync -vrxc --delete ./themes/ beanstalk@54.87.4.54:/var/www/html/wordpress/wp-content/themes/ --exclude-from=./deploy-scripts/rsync-excludes.txt
-rsync -vrxc --delete ./plugins/ beanstalk@54.87.4.54:/var/www/html/wordpress/wp-content/plugins/ --exclude-from=./deploy-scripts/rsync-excludes.txt
-rsync -vrxc --delete ./mu-plugins/ beanstalk@54.87.4.54:/var/www/html/wordpress/wp-content/mu-plugins/ --exclude-from=./deploy-scripts/rsync-excludes.txt
+rsync -vrxc --delete ./ beanstalk@54.87.4.54:/var/www/html/wordpress/wp-content/ --exclude-from=./deploy-scripts/rsync-excludes.txt
