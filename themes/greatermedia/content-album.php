ec55d2e2bf2d00eff42cca3eb5cc67bbe45daad1
Karine <karine.do@get10up.com>
2018-04-12 10:59:44 -0400

    Add markup for featured gallery in album page

--- themes/greatermedia/content-album.php
+++ themes/greatermedia/content-album.php
@@ -43,6 +43,28 @@
 				?>
 				<section class="inline__gallery__archive">
 
+					<!-- @TODO Eugene: Album featured gallery or first gallery? -->
+					<article id="post-<?php the_ID(); ?>" <?php post_class( 'gallery__grid--featured' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+						<a href="<?php the_permalink(); ?>">
+							<div class="gallery__grid--thumbnail">
+								<?php if ( 'gmr_album' == get_post_type() ) { ?>
+									<div class="gallery__grid--album"></div>
+								<?php } ?>
+								<div class="thumbnail" style="background-image: url(<?php gm_post_thumbnail_url( 'gmr-gallery-grid-thumb', null, true ); ?>)"></div>
+							</div>
+
+							<div class="gallery__grid--meta">
+								<h3 class="gallery__grid--title">
+									<?php the_title(); ?>
+								</h3>
+							</div>
+						</a>
+
+					</article>
+					<!-- End featured/first gallery -->
+
+
 					<div class="gallery__grid gallery__grid-album">
 
 						<?php
