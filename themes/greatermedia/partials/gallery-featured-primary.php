de8e913a7f6e92b20b5348cf776bd9e075b6f3ee
Karine <karine.do@get10up.com>
2018-04-16 12:27:22 -0400

    Add gallery category

--- themes/greatermedia/partials/gallery-featured-primary.php
+++ themes/greatermedia/partials/gallery-featured-primary.php
@@ -7,12 +7,15 @@
 			<?php } ?>
 			<div class="thumbnail" style="background-image: url(<?php gm_post_thumbnail_url( 'gmr-gallery-grid-thumb', null, true ); ?>)"></div>
 		</div>
+	</a>
 
 		<div class="gallery__grid--meta">
+			<?php echo get_the_category_list( ', ' ); ?>
 			<h3 class="gallery__grid--title">
-				<?php the_title(); ?>
+				<a href="<?php the_permalink(); ?>">
+					<?php the_title(); ?>
+				</a>
 			</h3>
 		</div>
-	</a>
 
 </article>
