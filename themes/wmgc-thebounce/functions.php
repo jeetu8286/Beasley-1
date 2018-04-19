e1f39b387a6b765e0a1044edd061926a80a6d821
Eugene Manuilov <eugene.manuilov@10up.com>
2018-04-19 10:29:48 +0300

    Added GTM to WKML theme

--- themes/wmgc-thebounce/functions.php
+++ themes/wmgc-thebounce/functions.php
@@ -28,18 +28,22 @@ function wmgc_scripts_styles() {
 add_action( 'wp_enqueue_scripts', 'wmgc_scripts_styles', 20 );
 
 /**
- * Add Google tag manager to wp-head for P-T-P
- *
+ * Add Google Tag Manager script to head.
  */
-function wmgc_add_custom_gtm() {
-	?>
-	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KRWQX8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-     '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-KRWQX8');</script>
-	<?php
+function wmgc_add_gtm_script() {
+	?><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KRWQX8');</script><?php
 }
-add_action( 'wp_head', 'wmgc_add_custom_gtm' );
+add_action( 'wp_head', 'wmgc_add_gtm_script' );
+
+/**
+ * Add Google Tag Manager iframe after body.
+ */
+function wmgc_add_gtm_iframe() {
+	?><noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KRWQX8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><?php
+}
+add_action( 'beasley_after_body', 'wmgc_add_gtm_iframe' );
+
