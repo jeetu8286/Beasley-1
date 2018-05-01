36bd7f4f1bcde0e6951f9456b8ac9052fb28e984
Chris Marslender <chrismarslender@gmail.com>
2018-05-01 12:46:11 -0700

    Add a shortcode handler for livestream, and split the render logic to a shared function

--- plugins/beasley-livestream-video/beasley-livestream-video.php
+++ plugins/beasley-livestream-video/beasley-livestream-video.php
@@ -7,12 +7,28 @@
 
 namespace Beasley\LivestreamVideo;
 
-wp_embed_register_handler( 'livestream-video', '#https?://livestream.com/accounts/([^/]+)/events/([^/]+)/videos/([^/]+)/?#i', __NAMESPACE__ . '\embed_handler' );
+add_action( 'init', function() {
+		wp_embed_register_handler( 'livestream-video', '#https?://livestream.com/accounts/([^/]+)/events/([^/]+)/videos/([^/]+)/?#i', __NAMESPACE__ . '\embed_handler' );
+		add_shortcode( 'livestream_video', __NAMESPACE__ . '\shortcode_handler' );
+});
 
 function embed_handler( $matches ) {
 		$account_id = $matches[1];
 		$event_id = $matches[2];
 		$video_id = $matches[3];
+
+		return get_embed_code( $account_id, $event_id, $video_id );
+}
+
+function shortcode_handler( $atts ) {
+		if ( ! isset( $atts['account_id'] ) || ! isset( $atts['event_id'] ) || ! isset( $atts['video_id'] ) ) {
+				return '';
+		}
+
+		return get_embed_code( $atts['account_id'], $atts['event_id'], $atts['video_id'] );
+}
+
+function get_embed_code( $account_id, $event_id, $video_id ) {
 		$embed_id = rand( 1, getrandmax() );
 
 		ob_start();
