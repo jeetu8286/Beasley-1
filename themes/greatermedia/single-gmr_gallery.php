3dbb33fa8c4020fce280de6548d3bcf77bc99c5c
Eugene Manuilov <eugene.manuilov@10up.com>
2018-05-15 13:58:39 +0300

    Fixed twitter image

--- themes/greatermedia/single-gmr_gallery.php
+++ themes/greatermedia/single-gmr_gallery.php
@@ -1,13 +1,8 @@
 <?php
 
-$dimension_width = $dimension_height = false;
-
-add_action( 'wpseo_add_opengraph_images', function( \WPSEO_OpenGraph_Image $opengraph ) use ( $dimension_width, $dimension_height ) {
-	$view = get_query_var( 'view' );
-	if ( empty( $view ) ) {
-		return;
-	}
-
+$view = get_query_var( 'view' );
+if ( ! empty( $view ) ) :
+	$featured_image = $dimension_width = $dimension_height = false;
 	$current_gallery = get_queried_object();
 	$ids = \GreaterMediaGallery::get_attachment_ids_for_post( $current_gallery );
 	if ( ! is_array( $ids ) ) {
@@ -15,54 +10,58 @@ add_action( 'wpseo_add_opengraph_images', function( \WPSEO_OpenGraph_Image $open
 	}
 
 	$images = array_values( array_filter( array_map( 'get_post', array_values( $ids ) ) ) );
-	if ( empty( $images ) ) {
-		return;
-	}
-
-	$view = strtolower( $view );
-	foreach ( $images as $image ) {
-		if ( strtolower( $image->post_name ) == $view ) {
-			$src = wp_get_attachment_image_src( $image->ID, 'full' );
-			if ( ! empty( $src ) && count( $src ) == 3 ) {
-				$opengraph->add_image( $src[0] );
-				$dimension_width = $src[1];
-				$dimension_height = $src[2];
-				add_filter( 'wpseo_opengraph_image', '__return_empty_string' );
-				break;
+	if ( ! empty( $images ) ) {
+		$view = strtolower( $view );
+		foreach ( $images as $image ) {
+			if ( strtolower( $image->post_name ) == $view ) {
+				$src = wp_get_attachment_image_src( $image->ID, 'full' );
+				if ( ! empty( $src ) && count( $src ) >= 3 ) {
+					$featured_image = $src[0];
+					$dimension_width = $src[1];
+					$dimension_height = $src[2];
+					break;
+				}
 			}
 		}
 	}
-} );
 
-add_filter( 'wpseo_og_og:image', function( $content ) use ( $dimension_width, $dimension_height ) {
-	if ( ! empty( $dimension_width ) ) {
-		echo '<meta property="og:image:width" content="', esc_attr( $dimension_width ), '">', "\n";
-	}
+	if ( ! empty( $featured_image ) ) :
+		add_action( 'wpseo_add_opengraph_images', function( \WPSEO_OpenGraph_Image $opengraph ) use ( $featured_image ) {
+			$opengraph->add_image( $featured_image );
+			add_filter( 'wpseo_opengraph_image', '__return_empty_string' );
+		} );
 
-	if ( ! empty( $dimension_height ) ) {
-		echo '<meta property="og:image:height" content="', esc_attr( $dimension_height ), '">', "\n";
-	}
+		add_filter( 'wpseo_og_og_image', function( $content ) use ( $dimension_width, $dimension_height ) {
+			if ( ! empty( $dimension_width ) ) {
+				echo '<meta property="og:image:width" content="', esc_attr( $dimension_width ), '">', "\n";
+			}
 
-	return $content;
-} );
+			if ( ! empty( $dimension_height ) ) {
+				echo '<meta property="og:image:height" content="', esc_attr( $dimension_height ), '">', "\n";
+			}
 
-add_filter( 'wpseo_canonical', function( $canonical ) {
-	$view = get_query_var( 'view' );
-	if ( empty( $view ) ) {
-		return $canonical;
-	}
+			return $content;
+		} );
 
-	$post = get_queried_object();
+		add_filter( 'wpseo_twitter_image', function( $image ) use ( $featured_image ) {
+			return ! empty( $featured_image ) ? $featured_image : $image;
+		} );
+	endif;
 
-	return sprintf(
-		'%s/view/%s/',
-		untrailingslashit(get_permalink( $post->ID ) ),
-		urlencode( $view )
-	);
-} );
+	add_filter( 'wpseo_canonical', function( $canonical ) {
+		$view = get_query_var( 'view' );
+		if ( empty( $view ) ) {
+			return $canonical;
+		}
+
+		return sprintf(
+			'%s/view/%s/',
+			untrailingslashit( get_permalink( get_queried_object_id() ) ),
+			urlencode( $view )
+		);
+	} );
+endif;
 
 get_header();
-
-get_template_part( 'content-gallery', !! get_query_var( 'view' ) ? 'slideshow' : '' );
-
+get_template_part( 'content-gallery', ! empty( $view ) ? 'slideshow' : '' );
 get_footer();
