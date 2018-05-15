dd557845fc9b2eeb4cd57d7720467f14a67e846f
Eugene Manuilov <eugene.manuilov@10up.com>
2018-05-15 13:16:45 +0300

    Added og:image:width and og:image:height attributes

--- themes/greatermedia/single-gmr_gallery.php
+++ themes/greatermedia/single-gmr_gallery.php
@@ -1,6 +1,8 @@
 <?php
 
-add_action( 'wpseo_add_opengraph_images', function( \WPSEO_OpenGraph_Image $opengraph ) {
+$dimension_width = $dimension_height = false;
+
+add_action( 'wpseo_add_opengraph_images', function( \WPSEO_OpenGraph_Image $opengraph ) use ( $dimension_width, $dimension_height ) {
 	$view = get_query_var( 'view' );
 	if ( empty( $view ) ) {
 		return;
@@ -19,11 +21,28 @@ add_action( 'wpseo_add_opengraph_images', function( \WPSEO_OpenGraph_Image $open
 
 	foreach ( $images as $image ) {
 		if ( $image->post_name == $view ) {
-			$opengraph->add_image( wp_get_attachment_image_url( $image->ID, 'full' ) );
-			add_filter( 'wpseo_opengraph_image', '__return_empty_string' );
-			break;
+			$src = wp_get_attachment_image_src( $image->ID, 'full' );
+			if ( ! empty( $src ) && count( $src ) == 3 ) {
+				$opengraph->add_image( $src[0] );
+				$dimension_width = $src[1];
+				$dimension_height = $src[2];
+				add_filter( 'wpseo_opengraph_image', '__return_empty_string' );
+				break;
+			}
 		}
 	}
+} );
+
+add_filter( 'wpseo_og_og:image', function( $content ) use ( $dimension_width, $dimension_height ) {
+	if ( ! empty( $dimension_width ) ) {
+		echo '<meta property="og:image:width" content="', esc_attr( $dimension_width ), '">', "\n";
+	}
+
+	if ( ! empty( $dimension_height ) ) {
+		echo '<meta property="og:image:height" content="', esc_attr( $dimension_height ), '">', "\n";
+	}
+
+	return $content;
 } );
 
 add_filter( 'wpseo_canonical', function( $canonical ) {
