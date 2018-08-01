3a868e27c6b83a3e70c49a949ace334d0ee2374d
Chris Marslender <chrismarslender@gmail.com>
2018-08-01 09:40:06 -0700

    Update deploy scripts to manage all deploy aspects from gitlab

---
+++ deploy-scripts/sync-jobs-to-web.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Run remote commands on the job server to sync web1, web2, and web3
+ssh beanstalk@54.87.4.54 'rsync -vrxc --delete /var/www/html/wordpress/ web1:/var/www/html/wordpress/ --exclude=wp-content/uploads --exclude=wp-content/upgrade'
+ssh beanstalk@54.87.4.54 'rsync -vrxc --delete /var/www/html/wordpress/ web2:/var/www/html/wordpress/ --exclude=wp-content/uploads --exclude=wp-content/upgrade'
+ssh beanstalk@54.87.4.54 'rsync -vrxc --delete /var/www/html/wordpress/ web3:/var/www/html/wordpress/ --exclude=wp-content/uploads --exclude=wp-content/upgrade'
