9a65e4d2b89f891e68fdc2d41784e801c9094c3d
Eugene Manuilov <eugene.manuilov@10up.com>
2018-08-07 21:19:35 +0300

    Updated featured image for an article

--- themes/greatermedia/partials/feature-image-article.php
+++ themes/greatermedia/partials/feature-image-article.php
@@ -2,17 +2,14 @@
 /**
  * This runs a check to determine if the post has a thumbnail, and that it's not a gallery or video post format.
  */
-if ( has_post_thumbnail() && ! \Greater_Media\Fallback_Thumbnails\post_has_gallery() && ! has_post_format( 'video' ) && ! has_post_format( 'audio' )  ): ?>
-  <div class="article__thumbnail">
-    <img src="<?php gm_post_thumbnail_url( 'gm-article-thumbnail' ) ?>" />
-    <?php
+if ( has_post_thumbnail() && ! \Greater_Media\Fallback_Thumbnails\post_has_gallery() && ! has_post_format( 'video' ) && ! has_post_format( 'audio' )  ) :
+	$image_attr = image_attribution();
 
-      $image_attr = image_attribution();
+	?><div class="article__thumbnail">
+		<img src="<?php gm_post_thumbnail_url( 'gmr-gallery' ) ?>" alt=""><?php
 
-      if ( ! empty( $image_attr ) ) {
-        echo $image_attr;
-      }
-
-    ?>
-  </div>
-<?php endif; ?>
+		if ( ! empty( $image_attr ) ) :
+			echo $image_attr;
+		endif;
+	?></div><?php
+endif;
