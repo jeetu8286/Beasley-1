5b55ad150b9a74aea812ddbeebd0682348fb1217
Eugene Manuilov <eugene.manuilov@10up.com>
2018-09-04 17:08:24 +0300

    Improved omny studio clips import

--- plugins/beasley-omny-studio/beasley-omny-studio.php
+++ plugins/beasley-omny-studio/beasley-omny-studio.php
@@ -94,9 +94,7 @@ function omny_start_import_episodes() {
 	}
 }
 
-function omny_run_import_episodes() {
-	global $wpdb;
-
+function omny_run_import_episodes( $args = array(), $assoc_args = array() ) {
 	$token = trim( get_option( 'omny_token' ) );
 	if ( empty( $token ) ) {
 		return;
@@ -107,138 +105,201 @@ function omny_run_import_episodes() {
 	delete_option( 'omny_last_import_finished' );
 	update_option( 'omny_last_import_started', time(), 'no' );
 
-	$podcasts = get_posts( array(
-		'post_type'      => 'podcast',
-		'posts_per_page' => 1000, // should be enough to get all podcasts
+	$page = 1;
+	$per_page = 100;
+	if ( $is_wp_cli ) {
+		if ( ! empty( $assoc_args['page'] ) && $assoc_args['page'] > 0 ) {
+			$page = intval( $assoc_args['page'] );
+		}
+
+		if ( ! empty( $assoc_args['per_page'] ) && $assoc_args['per_page'] > 0 ) {
+			$per_page = intval( $assoc_args['per_page'] );
+		}
+	}
+
+	$clips = omny_get_clips( $page, $per_page, $is_wp_cli );
+	$clips_map = omny_get_clips_map( $clips );
+
+	foreach ( $clips as $clip ) {
+		if ( ! empty( $clips_map[ $clip['Id'] ] ) ) {
+			$is_wp_cli && \WP_CLI::log( sprintf( 'Skipping %s (%s) episode...', $clip['Title'], $clip['Id'] ) );
+			continue;
+		}
+
+		$published_utc = strtotime( $clip['PublishedUtc'] );
+		$date_gmt = date( 'Y-m-d H:i:s', $published_utc );
+		$date = get_date_from_gmt( $date_gmt );
+
+		switch ( $clip['Visibility'] ) {
+			case 'Private':
+				$status = 'private';
+				break;
+			default:
+				$status = 'publish';
+				break;
+		}
+
+		if ( $published_utc > time() ) {
+			$status = 'future';
+		}
+
+		$args = array(
+			'post_title'    => html_entity_decode( $clip['Title'] ),
+			'post_content'  => sprintf( '[embed]%s[/embed]%s%s', $clip['PublishedUrl'], PHP_EOL, html_entity_decode( $clip['Description'] ) ),
+			'post_status'   => $status,
+			'post_type'     => 'episode',
+			'ping_status'   => 'closed',
+			'post_parent'   => $clip['PodcastId'],
+			'post_date'     => $date,
+			'post_date_gmt' => $date_gmt,
+			'guid'          => $clip['Id'],
+			'meta_input'    => array(
+				'omny-clip-id'     => $clip['Id'],
+				'omny-embed-url'   => $clip['EmbedUrl'],
+				'omny-publish-url' => $clip['PublishedUrl'],
+				'omny-audio-url'   => $clip['AudioUrl'],
+				'omny-image-url'   => $clip['ImageUrl'],
+			),
+		);
+
+		$post_id = wp_insert_post( $args, true );
+		if ( ! is_wp_error( $post_id ) ) {
+			omny_import_clip_image( $clip['ImageUrl'], $post_id );
+			$is_wp_cli && \WP_CLI::success( sprintf( 'Imported %s (%s) episode...', $clip['Title'], $clip['Id'] ) );
+		}
+	}
+
+	update_option( 'omny_last_import_finished', time(), 'no' );
+}
+
+function omny_import_clip_image( $image_url, $post_id ) {
+	global $wpdb;
+
+	if ( ! filter_var( $image_url, FILTER_VALIDATE_URL ) ) {
+		return;
+	}
+
+	$url = omny_get_image_url( $image_url );
+	$key = 'omny-image-id-' . $url;
+	$attachment_id = wp_cache_get( $key, 'omny-studio' );
+	if ( $attachment_id === false ) {
+		$query = $wpdb->prepare( "SELECT `ID` FROM {$wpdb->posts} WHERE `post_type` = 'attachment' AND `guid` = %s LIMIT 1", $url );
+		$attachment_id = $wpdb->get_var( $query );
+		if ( $attachment_id > 0 ) {
+			wp_cache_set( $key, $attachment_id, 'omny-studio' );
+		}
+	}
+
+	if ( empty( $attachment_id ) ) {
+		require_once ABSPATH . 'wp-admin/includes/media.php';
+		require_once ABSPATH . 'wp-admin/includes/file.php';
+		require_once ABSPATH . 'wp-admin/includes/image.php';
+
+		$file_array = array();
+		$file_array['name'] = md5( $url ) . '.jpg';
+		$file_array['tmp_name'] = download_url( $url );
+		if ( ! is_wp_error( $file_array['tmp_name'] ) ) {
+			$attachment_id = media_handle_sideload( $file_array, $post_id, null, array( 'guid' => $url ) );
+		}
+	}
+
+	if ( $attachment_id > 0 ) {
+		set_post_thumbnail( $post_id, intval( $attachment_id ) );
+	}
+}
+
+function omny_get_image_url( $image_url ) {
+	$key = 'omny-image-url-' . $image_url;
+	$url = wp_cache_get( $key, 'omny-studio' );
+	if ( $url === false ) {
+		$url = $image_url;
+		$response = wp_remote_head( $url, array( 'redirection' => 0 ) );
+		if ( ! is_wp_error( $response ) ) {
+			$headers = wp_remote_retrieve_headers( $response );
+			if ( ! empty( $headers['location'] ) ) {
+				if ( preg_match( '#^/[^/]#', $headers['location'] ) ) {
+					$parsed = parse_url( $url );
+					$replace = $parsed['path'];
+
+					if ( ! empty( $parsed['query'] ) ) {
+						$replace .= '?' . $parsed['query'];
+					}
+
+					if ( ! empty( $parsed['fragment'] ) ) {
+						$replace .= '#' . $parsed['fragment'];
+					}
+
+					$url = str_replace( $replace, $headers['location'], $url );
+				} else {
+					$url = $headers['location'];
+				}
+			}
+		}
+
+		wp_cache_set( $key, $url, 'omny-studio' );
+	}
+
+	return preg_replace( '#\?.*#', '', $url );
+}
+
+function omny_get_clips( $page, $per_page, $is_wp_cli = false ) {
+	$clips = array();
+
+	$cursor = filter_var( $page, FILTER_VALIDATE_INT, array(
+		'options' => array(
+			'min_range' => 1,
+			'default'   => 1,
+		),
 	) );
 
-	foreach ( $podcasts as $podcast ) {
+	$page_size = filter_var( $per_page, FILTER_VALIDATE_INT, array(
+		'options' => array(
+			'min_range' => 1,
+			'max_range' => 100,
+			'default'   => 10,
+		),
+	) );
+
+	foreach ( get_posts( 'post_type=podcast&posts_per_page=100' ) as $podcast ) {
 		$program_id = get_post_meta( $podcast->ID, 'omny_program_id', true );
 		if ( empty( $program_id ) ) {
 			$is_wp_cli && \WP_CLI::log( sprintf( 'Skipping %s (%s) podcast...', $podcast->post_title, $podcast->ID ) );
 			continue;
 		}
 
-		$is_wp_cli && \WP_CLI::log( sprintf( 'Processing %s (%s) podcast...', $podcast->post_title, $podcast->ID ) );
-
-		$clips = omny_api_request( "programs/{$program_id}/clips?pageSize=100" );
-		if ( is_wp_error( $clips ) ) {
+		$response = omny_api_request( "programs/{$program_id}/clips?cursor={$cursor}&pageSize={$page_size}" );
+		if ( is_wp_error( $response ) ) {
 			continue;
 		}
 
-		$clips = $clips['Clips'];
-		foreach ( $clips as $clip ) {
-			if ( $clip['PublishState'] != 'Published' ) {
-				continue;
-			}
-
-			$query = $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->postmeta} WHERE `meta_key` = 'omny-clip-id' AND `meta_value` = %s", $clip['Id'] );
-			$found = $wpdb->get_var( $query );
-			if ( $found > 0 ) {
-				$is_wp_cli && \WP_CLI::log( sprintf( 'Skipping %s (%s) episode...', $clip['Title'], $clip['Id'] ) );
-				continue;
-			}
-
-			$published_utc = strtotime( $clip['PublishedUtc'] );
-			$date_gmt = date( 'Y-m-d H:i:s', $published_utc );
-			$date = get_date_from_gmt( $date_gmt );
-
-			switch ( $clip['Visibility'] ) {
-				case 'Private':
-					$status = 'private';
-					break;
-				default:
-					$status = 'publish';
-					break;
-			}
-
-			if ( $published_utc > time() ) {
-				$status = 'future';
-			}
-
-			$args = array(
-				'post_title'    => html_entity_decode( $clip['Title'] ),
-				'post_content'  => sprintf( '[embed]%s[/embed]%s%s', $clip['PublishedUrl'], PHP_EOL, html_entity_decode( $clip['Description'] ) ),
-				'post_status'   => $status,
-				'post_type'     => 'episode',
-				'ping_status'   => 'closed',
-				'post_parent'   => $podcast->ID,
-				'post_date'     => $date,
-				'post_date_gmt' => $date_gmt,
-				'guid'          => $clip['Id'],
-				'meta_input'    => array(
-					'omny-clip-id'     => $clip['Id'],
-					'omny-embed-url'   => $clip['EmbedUrl'],
-					'omny-publish-url' => $clip['PublishedUrl'],
-					'omny-audio-url'   => $clip['AudioUrl'],
-					'omny-image-url'   => $clip['ImageUrl'],
-				),
-			);
-
-			$post_id = wp_insert_post( $args, true );
-			if ( is_wp_error( $post_id ) ) {
-				continue;
-			}
-
-			$is_wp_cli && \WP_CLI::success( sprintf( 'Imported %s (%s) episode...', $clip['Title'], $clip['Id'] ) );
-
-			$url = $clip['ImageUrl'];
-			if ( filter_var( $url, FILTER_VALIDATE_URL ) ) {
-				$response = wp_remote_head( $url, array( 'redirection' => 0 ) );
-				if ( ! is_wp_error( $response ) ) {
-					$headers = wp_remote_retrieve_headers( $response );
-					if ( ! empty( $headers['location'] ) ) {
-						if ( preg_match( '#^/[^/]#', $headers['location'] ) ) {
-							$parsed = parse_url( $url );
-							$replace = $parsed['path'];
-
-							if ( ! empty( $parsed['query'] ) ) {
-								$replace .= '?' . $parsed['query'];
-							}
-
-							if ( ! empty( $parsed['fragment'] ) ) {
-								$replace .= '#' . $parsed['fragment'];
-							}
-
-							$url = str_replace( $replace, $headers['location'], $url );
-						} else {
-							$url = $headers['location'];
-						}
-					}
-				}
-
-				$url = preg_replace( '#\?.*#', '', $url );
-				$key = 'omny-image-id-' . $url;
-				$attachment_id = wp_cache_get( $key, 'omny-studio' );
-				if ( $attachment_id === false ) {
-					$query = $wpdb->prepare( "SELECT `ID` FROM {$wpdb->posts} WHERE `post_type` = 'attachment' AND `guid` = %s LIMIT 1", $url );
-					$attachment_id = $wpdb->get_var( $query );
-					if ( $attachment_id > 0 ) {
-						wp_cache_set( $key, $attachment_id, 'omny-studio' );
-					}
-				}
-
-				if ( empty( $attachment_id ) ) {
-					require_once ABSPATH . 'wp-admin/includes/media.php';
-					require_once ABSPATH . 'wp-admin/includes/file.php';
-					require_once ABSPATH . 'wp-admin/includes/image.php';
-
-					$file_array = array();
-					$file_array['name'] = md5( $url ) . '.jpg';
-					$file_array['tmp_name'] = download_url( $url );
-					if ( ! is_wp_error( $file_array['tmp_name'] ) ) {
-						$attachment_id = media_handle_sideload( $file_array, $post_id, null, array( 'guid' => $url ) );
-					}
-				}
-
-				if ( $attachment_id > 0 ) {
-					set_post_thumbnail( $post_id, $attachment_id );
-				}
+		foreach ( $response['Clips'] as $clip ) {
+			if ( ! empty( $clip['PublishState'] ) && strtolower( $clip['PublishState'] ) == 'published' ) {
+				$clip['PodcastId'] = $podcast->ID;
+				$clips[] = $clip;
 			}
 		}
 	}
 
-	update_option( 'omny_last_import_finished', time(), 'no' );
+	return $clips;
+}
+
+function omny_get_clips_map( $clips ) {
+	global $wpdb;
+
+	$clips_map = array();
+	$clip_ids = array_filter( array_map( 'trim', wp_list_pluck( $clips, 'Id' ) ) );
+
+	$query = sprintf(
+		'SELECT `meta_value`, `post_id` FROM %s WHERE `meta_key` = "omny-clip-id" AND `meta_value` IN ("%s")',
+		$wpdb->postmeta,
+		implode( '", "', array_map( 'esc_sql', $clip_ids ) )
+	);
+
+	foreach ( $wpdb->get_results( $query, ARRAY_A ) as $row ) {
+		$clips_map[ $row['meta_value'] ] = $row['post_id'];
+	}
+
+	return $clips_map;
 }
 
 function omny_get_episode_audio_url( $url, $post ) {
