8d02a9cdc1f27b56224b99c9ac86f73ad7e45b60
Eugene Manuilov <eugene.manuilov@10up.com>
2018-09-17 21:34:22 +0300

    Added bbgi-contest shortcode

--- themes/greatermedia/includes/shortcodes.php
+++ themes/greatermedia/includes/shortcodes.php
@@ -3,12 +3,33 @@
 /**
  * The function returns original content of a shortcode and used to replace deprecated shortcodes.
  */
-function greatermedia_empty_shortcode( $atts, $content = null ) {
+function beasley_empty_shortcode( $atts, $content = null ) {
 	return $content;
 }
 
-add_shortcode( 'age-restricted', 'greatermedia_empty_shortcode' );
-add_shortcode( 'login-restricted', 'greatermedia_empty_shortcode' );
-add_shortcode( 'livefyre-wall', 'greatermedia_empty_shortcode' );
-add_shortcode( 'livefyre-poll', 'greatermedia_empty_shortcode' );
-add_shortcode( 'livefyre-app', 'greatermedia_empty_shortcode' );
+function besley_national_contest_shortcode( $atts ) {
+	$atts = shortcode_atts( array(
+		'contest' => 'national-contest',
+		'brand'   => 'WMMR',
+	), $atts, 'bbgi-contest' );
+
+	$contest = urlencode( $atts['contest'] );
+	$brand = urlencode( $atts['brand'] );
+
+	$embed = <<<EOL
+<style>#contestframe {width: 100%;}</style>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.6.1/iframeResizer.min.js"></script>
+<iframe id="contestframe" src="https://contests.bbgi.com/landing?contest={$contest}&brand={$brand}" frameborder="0" scrolling="no"></iframe>
+<script type="text/javascript">iFrameResize({log:true, autoResize: true})</script>
+EOL;
+
+	return $embed;
+}
+
+add_shortcode( 'age-restricted', 'beasley_empty_shortcode' );
+add_shortcode( 'login-restricted', 'beasley_empty_shortcode' );
+add_shortcode( 'livefyre-wall', 'beasley_empty_shortcode' );
+add_shortcode( 'livefyre-poll', 'beasley_empty_shortcode' );
+add_shortcode( 'livefyre-app', 'beasley_empty_shortcode' );
+
+add_shortcode( 'bbgi-contest', 'besley_national_contest_shortcode' );
