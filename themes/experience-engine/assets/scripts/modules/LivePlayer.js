96d16f0266fc5ba5074704dd2ad093db58553fdc
Eugene Manuilov <eugene.manuilov@10up.com>
2018-10-24 16:00:56 +0300

    Merged player into liveplayer

--- themes/experience-engine/assets/scripts/modules/LivePlayer.js
+++ themes/experience-engine/assets/scripts/modules/LivePlayer.js
@@ -1,23 +1,75 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-import Player from '../components/player/Player';
 import Stations from '../components/player/Stations';
+import Controls from '../components/player/Controls';
+import Info from '../components/player/Info';
+import Volume from '../components/player/Volume';
 
-const LivePlayer = () => {
-	const container = document.getElementById( 'live-player' );
-	if ( !container ) {
-		return false;
+import { initTdPlayer } from '../redux/actions/player';
+
+class LivePlayer extends Component {
+
+	componentDidMount() {
+		// @see: https://userguides.tritondigital.com/spc/tdplay2/
+		this.props.initTdPlayer( [
+			{
+				id: 'MediaPlayer',
+				playerId: 'td_container',
+				techPriority: ['Html5'],
+			},
+			{
+				id: 'NowPlayingApi',
+			},
+			{
+				id: 'TargetSpot'
+			},
+			{
+				id: 'SyncBanners',
+				elements: [
+					{
+						id: 'audio-ad-inplayer',
+						width: 320,
+						height: 50
+					}
+				]
+			},
+		] );
 	}
 
-	const children = (
-		<Fragment>
-			<Player />
-			<Stations />
-		</Fragment>
-	);
+	render() {
+		const container = document.getElementById( 'live-player' );
+		if ( !container ) {
+			return false;
+		}
 
-	return ReactDOM.createPortal( children, container );
+		const children = (
+			<Fragment>
+				<div id="td_container" />
+				<div id="audio-ad-inplayer" />
+
+				<div className="controls">
+					<Controls />
+					<Info />
+					<Volume />
+				</div>
+
+				<Stations />
+			</Fragment>
+		);
+
+		return ReactDOM.createPortal( children, container );
+	}
+
+}
+
+LivePlayer.propTypes = {
+	initTdPlayer: PropTypes.func.isRequired,
 };
 
-export default LivePlayer;
+const mapDispatchToProps = ( dispatch ) => bindActionCreators( { initTdPlayer }, dispatch );
+
+export default connect( null, mapDispatchToProps )( LivePlayer );
