240798517bc748ddc3b30c7bec1ca3a3f4ab34d1
Eugene Manuilov <eugene.manuilov@10up.com>
2018-10-29 13:11:09 +0200

    Added episodes information to the podcast tile

--- themes/experience-engine/includes/helpers.php
+++ themes/experience-engine/includes/helpers.php
@@ -1,12 +1,24 @@
 <?php
 
 if ( ! function_exists( 'ee_get_episodes_query' ) ) :
-	function ee_get_episodes_query( $podcast = null ) {
+	function ee_get_episodes_query( $podcast = null, $args = array() ) {
 		$podcast = get_post( $podcast );
 
-		return new \WP_Query( array(
+		return new \WP_Query( array_merge( $args, array(
 			'post_type'   => 'episode',
 			'post_parent' => $podcast->ID,
+		) ) );
+	}
+endif;
+
+if ( ! function_exists( 'ee_get_episodes_count' ) ) :
+	function ee_get_episodes_count( $podcast = null ) {
+		$podcast = get_post( $podcast );
+		$query = ee_get_episodes_query( $podcast, array(
+			'posts_per_page' => 1,
+			'fields'         => 'ids',
 		) );
+
+		return $query->found_posts;
 	}
 endif;
--- themes/experience-engine/partials/tile-podcast.php
+++ themes/experience-engine/partials/tile-podcast.php
@@ -1,8 +1,13 @@
 <div>
-	[tile-podcast]
+	<div ></div>
 	<h3>
 		<a href="<?php the_permalink(); ?>">
 			<?php the_title(); ?>
 		</a>
 	</h3>
+	<div>
+		<span>
+			<?php echo esc_html( ee_get_episodes_count() ); ?> episodes
+		</span>
+	</div>
 </div>
