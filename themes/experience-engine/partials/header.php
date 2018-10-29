c319b9914fdef30c5d68646935d31f8f83373894
Dominic Magnifico <dominicmagnifico@gmail.com>
2018-10-29 09:10:36 -0600

    Add markup for the navigation.

--- themes/experience-engine/partials/header.php
+++ themes/experience-engine/partials/header.php
@@ -4,6 +4,21 @@ if ( ! has_custom_logo() ) :
 	return;
 endif;
 
-?><div>
-	<?php the_custom_logo(); ?>
+?>
+<div class="primary-sidebar">
+	<a href="#primary-menu" id="js-menu-toggle" class="site-menu-toggle">
+		<span class="screen-reader-text">
+			<?php esc_html_e( 'Primary Menu', 'tenup' ); ?>
+		</span>
+		<span aria-hidden="true">☰</span>
+	</a>
+	<div class="logo" itemscope itemtype="http://schema.org/Organization">
+		<a itemprop="url" href="<?php the_permalink(); ?>">
+			<?php the_custom_logo(); ?>
+			<span class="screen-reader-text"><?php wp_title(); ?></span>
+		</a>
+	</div>
+	<nav id="js-primary-nav" class="primary-nav" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" aria-hidden="false">
+		<?php get_template_part( 'partials/primary', 'navigation' ); ?>
+	</nav>
 </div>
