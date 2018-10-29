76d66c190bf3f474cab839f0b9b82407a49f0e9a
Eugene Manuilov <eugene.manuilov@10up.com>
2018-10-29 17:57:52 +0200

    Added podcast title to the archive page

--- themes/experience-engine/archive-podcast.php
+++ themes/experience-engine/archive-podcast.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
 <div class="podcast-archive">
+	<h1>Podcasts</h1>
+
 	<?php while ( have_posts() ) : ?>
 		<?php the_post(); ?>
 		<?php get_template_part( 'partials/tile', get_post_type() ); ?>
