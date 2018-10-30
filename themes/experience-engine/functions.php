6b5997a54517d3edd024e0f08047f7a6a605ba1f
Eugene Manuilov <eugene.manuilov@10up.com>
2018-10-30 16:39:10 +0200

    Rendered show navigation and information blocks

--- themes/experience-engine/functions.php
+++ themes/experience-engine/functions.php
@@ -19,3 +19,4 @@ require_once __DIR__ . '/includes/experience-engine.php';
 require_once __DIR__ . '/includes/assets.php';
 require_once __DIR__ . '/includes/podcasts.php';
 require_once __DIR__ . '/includes/helpers.php';
+require_once __DIR__ . '/includes/shows.php';
--- themes/experience-engine/partials/podcast-information.php
+++ themes/experience-engine/partials/podcast-information.php
@@ -1,1 +1,12 @@
-<div>[podcast information]</div>
+<div style="display: flex">
+	<div style="width: 300px">
+		<?php get_template_part( 'partials/featured-media' ); ?>
+	</div>
+	<div>
+		<div>podcast</div>
+		<h1><?php the_title(); ?></h1>
+		<div>
+			<?php echo esc_html( ee_get_episodes_count() ); ?> episodes
+		</div>
+	</div>
+</div>
