05b408f24365d715d8fd2b9bf4270435eef2c5e3
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-01 10:10:41 +0200

    Updated primary menu styles to make toggle button bigger and easier to use

--- themes/experience-engine/partials/tile-post.php
+++ themes/experience-engine/partials/tile-post.php
@@ -1,4 +1,4 @@
-<div style="margin: 10px 0">
+<div style="margin-bottom: 20px">
 	<div style="max-width: 300px">
 		<a href="<?php the_permalink(); ?>"><?php ee_the_lazy_thumbnail(); ?></a>
 	</div>
