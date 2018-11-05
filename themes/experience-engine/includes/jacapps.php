3e3915a5d3d5e9b0260c28a70cc6e1eb98abf99a
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-05 19:32:10 +0200

    Updated episodes to not use placeholders when in jacapps app

--- themes/experience-engine/includes/jacapps.php
+++ themes/experience-engine/includes/jacapps.php
@@ -1,13 +1,35 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'ee_jacapps_enqueue_scripts', 99 );
-
-add_filter( 'body_class', 'ee_jacapps_body_class' );
+add_action( 'wp_loaded', 'ee_setup_jacapps' );
 
 if ( ! function_exists( 'ee_is_jacapps' ) ) :
 	function ee_is_jacapps() {
-		return false !== stripos( $_SERVER['HTTP_USER_AGENT'], 'jacapps' );
+		static $jacapps_pos = null;
+
+		if ( $jacapps_pos === null ) {
+			$jacapps_pos = stripos( $_SERVER['HTTP_USER_AGENT'], 'jacapps' );
+		}
+
+		return false !== $jacapps_pos;
 	}
+endif;
+
+if ( ! function_exists( 'ee_setup_jacapps' ) ) :
+	function ee_setup_jacapps() {
+		if ( ee_is_jacapps() ) {
+			add_action( 'wp_enqueue_scripts', 'ee_jacapps_enqueue_scripts', 99 );
+
+			add_filter( 'body_class', 'ee_jacapps_body_class' );
+			add_filter( 'omny_embed_key', 'ee_update_jacapps_omny_key' );
+
+			remove_filter( 'omny_embed_html', 'ee_update_omny_embed' );
+		}
+	}
+endif;
+
+if ( function_exists( 'vary_cache_on_function' ) ) :
+	// batcache variant
+	vary_cache_on_function( 'return (bool) preg_match("/jacapps/i", $_SERVER["HTTP_USER_AGENT"]);' );
 endif;
 
 if ( ! function_exists( 'ee_jacapps_body_class' ) ) :
@@ -17,13 +39,14 @@ if ( ! function_exists( 'ee_jacapps_body_class' ) ) :
 	}
 endif;
 
-if ( function_exists( 'vary_cache_on_function' ) ) :
-	// batcache variant
-	vary_cache_on_function( 'return (bool) preg_match("/jacapps/i", $_SERVER["HTTP_USER_AGENT"]);' );
-endif;
-
 if ( ! function_exists( 'ee_jacapps_enqueue_scripts' ) ) :
 	function ee_jacapps_enqueue_scripts() {
 		wp_dequeue_script( 'ee-app' );
+	}
+endif;
+
+if ( ! function_exists( 'ee_update_jacapps_omny_key' ) ) :
+	function ee_update_jacapps_omny_key( $key ) {
+		return $key . ':jacapps';
 	}
 endif;
--- themes/experience-engine/single-episode.php
+++ themes/experience-engine/single-episode.php
@@ -5,9 +5,13 @@
 <div>
 	<?php get_template_part( 'partials/show-block' ); ?>
 
-	<div>
-		<?php ee_the_episode_player(); ?>
-		<h1><?php the_title(); ?></h1>
+	<div><?php
+
+		if ( ! ee_is_jacapps() ) :
+			ee_the_episode_player();
+		endif;
+
+		?><h1><?php the_title(); ?></h1>
 
 		<div>
 			<?php if ( ( $duration = ee_get_episode_meta( null, 'duration' ) ) ) : ?>
@@ -21,9 +25,15 @@
 	</div>
 
 	<div><?php
-		add_filter( 'the_content', 'strip_shortcodes', 1 );
-		the_content();
-		remove_filter( 'the_content', 'strip_shortcodes', 1 );
+
+		if ( ! ee_is_jacapps() ) :
+			add_filter( 'the_content', 'strip_shortcodes', 1 );
+			the_content();
+			remove_filter( 'the_content', 'strip_shortcodes', 1 );
+		else :
+			the_content();
+		endif;
+
 	?></div>
 
 	<?php get_template_part( 'partials/episode/next-episodes' ); ?>
