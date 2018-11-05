7427fa6112a914553a5f2b5646137696b30cff82
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-05 17:52:15 +0200

    Removed app script from jacapps pages

--- themes/experience-engine/includes/jacapps.php
+++ themes/experience-engine/includes/jacapps.php
@@ -1,5 +1,7 @@
 <?php
 
+add_action( 'wp_enqueue_scripts', 'ee_jacapps_enqueue_scripts', 99 );
+
 add_filter( 'body_class', 'ee_jacapps_body_class' );
 
 if ( ! function_exists( 'ee_is_jacapps' ) ) :
@@ -18,4 +20,10 @@ endif;
 if ( function_exists( 'vary_cache_on_function' ) ) :
 	// batcache variant
 	vary_cache_on_function( 'return (bool) preg_match("/jacapps/i", $_SERVER["HTTP_USER_AGENT"]);' );
+endif;
+
+if ( ! function_exists( 'ee_jacapps_enqueue_scripts' ) ) :
+	function ee_jacapps_enqueue_scripts() {
+		wp_dequeue_script( 'ee-app' );
+	}
 endif;
