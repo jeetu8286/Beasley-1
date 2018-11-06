3864bbd32f9e07b8f77281bb7d4e3e7b1a0750c9
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-06 19:36:22 +0200

    Updated footer to have components and elements from mockups

--- themes/experience-engine/partials/footer/about.php
+++ themes/experience-engine/partials/footer/about.php
@@ -1,7 +1,7 @@
-<?php
+<div>
+	<h3>About</h3><?php
 
-if ( ! has_nav_menu( 'about-nav' ) ) :
-	return;
-endif;
-
-wp_nav_menu( array( 'theme_location' => 'about-nav' ) );
+	if ( has_nav_menu( 'about-nav' ) ) :
+		wp_nav_menu( array( 'theme_location' => 'about-nav' ) );
+	endif;
+?></div>
