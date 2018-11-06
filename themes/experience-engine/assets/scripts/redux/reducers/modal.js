de2e0bd2fcf4ad103133741f299120c3d43e7cc1
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-06 13:32:32 +0200

    Updated modal window styles

--- themes/experience-engine/assets/scripts/redux/reducers/modal.js
+++ themes/experience-engine/assets/scripts/redux/reducers/modal.js
@@ -8,11 +8,13 @@ export const DEFAULT_STATE = {
 const reducer = ( state = {}, action = {} ) => {
 	switch ( action.type ) {
 		case ACTION_SHOW_MODAL:
+			document.body.classList.add( 'modal-opened' );
 			return Object.assign( {}, state, {
 				modal: action.modal,
 				payload: action.payload,
 			} );
 		case ACTION_HIDE_MODAL:
+			document.body.classList.remove( 'modal-opened' );
 			return Object.assign( {}, DEFAULT_STATE );
 		default:
 			// do nothing
