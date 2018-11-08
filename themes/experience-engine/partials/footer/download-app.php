37ce11d83143b939920bc3cb420150d754c69225
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-08 09:34:10 +0200

    Updated donwload links to have target blank and aria label attribtues

--- themes/experience-engine/partials/footer/download-app.php
+++ themes/experience-engine/partials/footer/download-app.php
@@ -3,13 +3,13 @@
 	<p>Listen Live to KISS and listen to Dave & Chuck 24/7 with our app!</p>
 
 	<?php if ( ee_has_publisher_information( 'itunes_app' ) ) : ?>
-		<a href="#">
+		<a href="#" aria-label="Download on the App Store" target="_blank" rel="noopener noreferrer">
 			<img src="<?php echo get_template_directory_uri() ?>/assets/images/itunes.svg" alt="Download on the App Store">
 		</a>
 	<?php endif; ?>
 
 	<?php if ( ee_has_publisher_information( 'play_app' ) ) : ?>
-		<a href="#">
+		<a href="#" aria-label="Download on the Google Play" target="_blank" rel="noopener noreferrer">
 			<img src="<?php echo get_template_directory_uri() ?>/assets/images/googleplay.svg" alt="Download on the Google Play">
 		</a>
 	<?php endif; ?>
