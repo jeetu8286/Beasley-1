b0613b589a34e37da5dc7c95e1c7ff3ba37055da
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-12 18:01:43 +0200

    Added "contacts" component

---
+++ themes/experience-engine/assets/scripts/components/player/Contacts.js
@@ -0,0 +1,54 @@
+import React, { PureComponent, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+class Contacts extends PureComponent {
+
+	constructor( props ) {
+		super( props );
+
+		const self = this;
+		self.state = { isOpen: false };
+
+		self.onToggle = self.handleToggleClick.bind( self );
+	}
+
+	handleToggleClick() {
+		const self = this;
+		const { isOpen } = self.state;
+
+		self.setState( { isOpen: !isOpen } );
+	}
+
+	render() {
+		const self = this;
+		const { isOpen } = self.state;
+		const { address, email, phone } = self.props;
+
+		let contacts = false;
+		if ( isOpen ) {
+			contacts = (
+				<div>
+					<a href={`tel:${phone}`}>{phone}</a>
+					<a href={`mailto:${email}`}>{email}</a>
+					<span>{address}</span>
+				</div>
+			);
+		}
+
+		return (
+			<Fragment>
+				{contacts}
+				<button onClick={self.onToggle}>Contacts</button>
+			</Fragment>
+		);
+	}
+
+}
+
+Contacts.propTypes = {
+	address: PropTypes.string.isRequired,
+	email: PropTypes.string.isRequired,
+	phone: PropTypes.string.isRequired,
+};
+
+export default Contacts;
