075a1d23102e155c3321f2c951ef7afa12b4f760
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-14 14:19:19 +0200

    Updated single article template to show article meta information

--- themes/experience-engine/single.php
+++ themes/experience-engine/single.php
@@ -4,11 +4,19 @@
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php get_template_part( 'partials/show/header' ); ?>
-	<?php get_template_part( 'partials/featured-media' ); ?>
+
 	<h1><?php the_title(); ?></h1>
 
 	<div>
 		<div>
+			<div>
+				<span><?php the_author_meta( 'display_name' ); ?></span>
+				<span><?php ee_the_date(); ?></span>
+				<?php get_template_part( 'partials/share' ); ?>
+			</div>
+
+			<?php get_template_part( 'partials/featured-media' ); ?>
+
 			<?php the_content(); ?>
 		</div>
 
