4c91de4652082e557a5eccce991a736a1552234f
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-14 17:01:12 +0200

    Added template for single event template

--- themes/experience-engine/tribe-events/single-event.php
+++ themes/experience-engine/tribe-events/single-event.php
@@ -1,3 +1,54 @@
-<?php get_header(); ?>
+<?php
 
-<?php get_footer(); ?>
+the_post();
+
+$website = tribe_get_event_website_url();
+$cost = tribe_get_cost();
+
+?><div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div>
+		<a href="<?php echo esc_url( tribe_get_events_link() ); ?>">
+			<?php echo esc_html( tribe_get_event_label_plural() ); ?>
+		</a>
+	</div>
+
+	<?php tribe_the_notices(); ?>
+
+	<h1><?php the_title(); ?></h1>
+
+	<div>
+		<div>
+			<?php echo tribe_events_event_schedule_details(); ?>
+		</div>
+
+		<?php if ( ! empty( $website ) || ! empty( $cost ) ) : ?>
+			<div>
+				<?php if ( filter_var( $website, FILTER_VALIDATE_URL ) ) : ?>
+					<div>
+						<span>Website:</span>
+						<a href="<?php echo esc_url( $website ); ?>" target="_blank" rel="noopener noreferrer">
+							View Site
+						</a>
+					</div>
+				<?php endif; ?>
+
+				<?php if ( $cost ) : ?>
+					<div>
+						<span>Cost:</span>
+						<?php echo $cost; ?>
+					</div>
+				<?php endif; ?>
+			</div>
+		<?php endif; ?>
+
+		<div>
+			<?php echo preg_replace( '#\s*\<br\s*/?\>\s*#i', ', ', tribe_get_full_address() ); ?>
+			<?php echo tribe_get_map_link_html(); ?>
+		</div>
+	</div>
+
+	<div>
+		<?php get_template_part( 'partials/featured-media' ); ?>
+		<?php the_content(); ?>
+	</div>
+</div>
