70c3895a77c71839cac12ab199a82ffc932e5c25
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-15 19:14:49 +0200

    Updated video embed to load video scripts on demand

--- themes/experience-engine/assets/scripts/components/content/embeds/Video.js
+++ themes/experience-engine/assets/scripts/components/content/embeds/Video.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
+import { loadAssets } from '../../../library/dom';
+
 class Video extends PureComponent {
 
 	constructor( props ) {
@@ -10,6 +12,33 @@ class Video extends PureComponent {
 
 	componentDidMount() {
 		const self = this;
+
+		const masterScripts = [
+			'//imasdk.googleapis.com/js/sdkloader/ima3.js',
+			'/wp-content/themes/experience-engine/bundle/video.min.js',
+		];
+
+		const slaveScripts = [
+			'/wp-content/themes/experience-engine/bundle/videojs-contrib-ads.min.js',
+			'/wp-content/themes/experience-engine/bundle/videojs.ima.min.js',
+			'/wp-content/themes/experience-engine/bundle/videojs-contrib-quality-levels.min.js',
+			'/wp-content/themes/experience-engine/bundle/videojs-hls-quality-selector.min.js',
+		];
+
+		const styles = [
+			'/wp-content/themes/experience-engine/bundle/video-js.min.css',
+			'/wp-content/themes/experience-engine/bundle/videojs-contrib-ads.css',
+			'/wp-content/themes/experience-engine/bundle/videojs.ima.css',
+		];
+
+		loadAssets( masterScripts )
+			.then( () => loadAssets( slaveScripts, styles ) )
+			.then( self.loadVideoJs.bind( self ) )
+			.catch( error => console.error( error ) ); // eslint-disable-line no-console
+	}
+
+	loadVideoJs() {
+		const self = this;
 		const { current: videoRef } = self.videoRef;
 		if ( !videoRef ) {
 			return;
@@ -17,7 +46,7 @@ class Video extends PureComponent {
 
 		const { id, src, adTagUrl } = self.props;
 
-		const player = videojs( videoRef );
+		const player = window.videojs( videoRef );
 		const videoArgs = {
 			src,
 			type: 'application/x-mpegURL',
@@ -51,7 +80,7 @@ class Video extends PureComponent {
 		const { id, poster } = self.props;
 
 		return (
-			<video id={id} ref={self.videoRef} className="video-js vjs-default-skin" controls preload="auto" poster={poster} />
+			<video id={id} ref={self.videoRef} className="video-js vjs-default-skin" controls poster={poster} />
 		);
 	}
 
