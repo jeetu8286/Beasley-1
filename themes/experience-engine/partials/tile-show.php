e22640ddd150c1b70f27da6e07bef75c3770afc4
dainemawer <daine.mawer@10up.com>
2018-11-21 08:42:37 +0200

    Updates to show archive

--- themes/experience-engine/partials/tile-show.php
+++ themes/experience-engine/partials/tile-show.php
@@ -1,5 +1,5 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 	<?php get_template_part( 'partials/tile/thumbnail' ); ?>
 	<?php get_template_part( 'partials/tile/title' ); ?>
-	<div><?php echo ee_get_show_meta( null, 'show-time' ); ?></div>
+	<p class="show-time"><?php echo ee_get_show_meta( null, 'show-time' ); ?></p>
 </div>
