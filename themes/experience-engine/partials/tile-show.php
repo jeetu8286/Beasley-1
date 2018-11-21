9b26744f494a03b45d2dbe1e273cf9d815b77b3c
dainemawer <daine.mawer@10up.com>
2018-11-21 09:58:57 +0200

    Refactored code to align with previously set out structure

--- themes/experience-engine/partials/tile-show.php
+++ themes/experience-engine/partials/tile-show.php
@@ -1,5 +1,7 @@
-<div id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+<div id="post-<?php the_ID(); ?>" <?php post_class('show-tile'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 	<?php get_template_part( 'partials/tile/thumbnail' ); ?>
-	<?php get_template_part( 'partials/tile/title' ); ?>
-	<p class="show-time"><?php echo ee_get_show_meta( null, 'show-time' ); ?></p>
+	<div class="meta">
+		<?php get_template_part( 'partials/tile/title' ); ?>
+		<p class="timeslot"><?php echo ee_get_show_meta( null, 'show-time' ); ?></p>
+	</div>
 </div>
