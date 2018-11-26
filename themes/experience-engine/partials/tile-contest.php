21b8860e74aed944694ef5f6218c8e2089920123
dainemawer <daine.mawer@10up.com>
2018-11-26 14:37:11 +0200

    Updated contest tiles on archive

--- themes/experience-engine/partials/tile-contest.php
+++ themes/experience-engine/partials/tile-contest.php
@@ -1,4 +1,4 @@
-<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div id="post-<?php the_ID(); ?>" <?php post_class( 'contest-tile' ); ?>>
 	<?php get_template_part( 'partials/tile/thumbnail' ); ?>
 	<?php get_template_part( 'partials/tile/title' ); ?>
 	<?php ee_the_contest_dates(); ?>
