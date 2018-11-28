5c6c036fff03dab0d20fabc47c193f963e76dde2
Eugene Manuilov <eugene.manuilov@10up.com>
2018-11-28 14:46:53 +0200

    Updated gallery page template

--- themes/experience-engine/single-gmr_gallery.php
+++ themes/experience-engine/single-gmr_gallery.php
@@ -4,6 +4,21 @@
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php get_template_part( 'partials/show/header' ); ?>
+	<h1><?php the_title(); ?></h1>
+
+	<div>
+		<div>
+			<?php get_template_part( 'partials/content/meta' ); ?>
+			<?php get_template_part( 'partials/featured-media' ); ?>
+
+			<?php the_content(); ?>
+
+			<?php get_template_part( 'partials/content/categories' ); ?>
+			<?php get_template_part( 'partials/content/tags' ); ?>
+		</div>
+
+		<?php get_template_part( 'partials/ads/sidebar-sticky' ); ?>
+	</div>
 </div>
 
 <?php get_footer(); ?>
--- themes/experience-engine/single.php
+++ themes/experience-engine/single.php
@@ -8,12 +8,7 @@
 
 	<div>
 		<div>
-			<div>
-				<span><?php the_author_meta( 'display_name' ); ?></span>
-				<span><?php ee_the_date(); ?></span>
-				<?php ee_the_share_buttons( get_permalink(), get_the_title() ); ?>
-			</div>
-
+			<?php get_template_part( 'partials/content/meta' ); ?>
 			<?php get_template_part( 'partials/featured-media' ); ?>
 
 			<?php ee_the_content_with_ads(); ?>
