7e8805cfe739ed0e90c87b9d2ffa6e10fe10cc22
Dominic Magnifico <dominicmagnifico@gmail.com>
2018-12-03 10:15:10 -0700

    Ensure all show content types have proper layout markup.

--- themes/experience-engine/single-show-podcasts.php
+++ themes/experience-engine/single-show-podcasts.php
@@ -5,15 +5,13 @@ get_header();
 the_post();
 
 echo '<div class="', join( ' ', get_post_class() ), '">';
+	if ( ee_is_first_page() ) :
+		get_template_part( 'partials/show/header' );
+	endif;
+
 	echo '<div class="content-wrap">';
-
-		if ( ee_is_first_page() ) :
-			get_template_part( 'partials/show/header' );
-			ee_the_subtitle( 'Podcasts' );
-		endif;
-
+		ee_the_subtitle( 'Podcasts' );
 		echo '<div class="archive-tiles -grid -small">';
-
 			get_template_part( 'partials/show/episodes' );
 		echo '</div>';
 	echo '</div>';
--- themes/experience-engine/single-show-videos.php
+++ themes/experience-engine/single-show-videos.php
@@ -7,10 +7,14 @@ the_post();
 echo '<div class="', join( ' ', get_post_class() ), '">';
 	if ( ee_is_first_page() ) :
 		get_template_part( 'partials/show/header' );
-		ee_the_subtitle( 'Videos' );
 	endif;
 
-	get_template_part( 'partials/show/videos' );
+	echo '<div class="content-wrap">';
+		ee_the_subtitle( 'Videos' );
+		echo '<div class="archive-tiles -grid -small">';
+			get_template_part( 'partials/show/videos' );
+		echo '</div>';
+	echo '</div>';
 echo '</div>';
 
 get_footer();
