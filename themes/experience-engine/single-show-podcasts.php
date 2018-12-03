eb89b612379923089bf6b6902a0f8bb3c8284f8b
Dominic Magnifico <dominicmagnifico@gmail.com>
2018-12-03 09:53:40 -0700

    Ensure single show podcast page markup matches existing layout components.

--- themes/experience-engine/single-show-podcasts.php
+++ themes/experience-engine/single-show-podcasts.php
@@ -5,12 +5,18 @@ get_header();
 the_post();
 
 echo '<div class="', join( ' ', get_post_class() ), '">';
-	if ( ee_is_first_page() ) :
-		get_template_part( 'partials/show/header' );
-		ee_the_subtitle( 'Podcasts' );
-	endif;
+	echo '<div class="content-wrap">';
 
-	get_template_part( 'partials/show/episodes' );
+		if ( ee_is_first_page() ) :
+			get_template_part( 'partials/show/header' );
+			ee_the_subtitle( 'Podcasts' );
+		endif;
+
+		echo '<div class="archive-tiles -grid -small">';
+
+			get_template_part( 'partials/show/episodes' );
+		echo '</div>';
+	echo '</div>';
 echo '</div>';
 
 get_footer();
