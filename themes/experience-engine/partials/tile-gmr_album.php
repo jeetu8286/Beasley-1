16b130ed492db93d3d6a197f5660cb843136ca16
Eugene Manuilov <eugene.manuilov@10up.com>
2018-12-05 15:47:50 +0200

    Added tile for album

---
+++ themes/experience-engine/partials/tile-gmr_album.php
@@ -0,0 +1,8 @@
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php get_template_part( 'partials/tile/thumbnail' ); ?>
+	<?php get_template_part( 'partials/tile/title' ); ?>
+
+	<p class="type">
+		photo album
+	</p>
+</div>
