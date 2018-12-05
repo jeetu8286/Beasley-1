b509b8517f505514fc45fa4a4793d45c2a053bf3
Eugene Manuilov <eugene.manuilov@10up.com>
2018-12-05 14:37:29 +0200

    Updated titles to display properly on a page

--- themes/experience-engine/single-gmr_gallery.php
+++ themes/experience-engine/single-gmr_gallery.php
@@ -6,7 +6,9 @@
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php get_template_part( 'partials/show/header' ); ?>
-	<h1><?php the_title(); ?></h1>
+	<div class="content-wrap">
+		<h1><?php the_title(); ?></h1>
+	</div>
 
 	<div class="content-wrap">
 		<div>
