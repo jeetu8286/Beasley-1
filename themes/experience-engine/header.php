646750b08e3e8cc74cf42ecfd2ead7d487f39637
Dominic Magnifico <dominicmagnifico@gmail.com>
2018-12-06 10:05:28 -0700

    Add skip links.

--- themes/experience-engine/header.php
+++ themes/experience-engine/header.php
@@ -5,14 +5,22 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_head(); ?>
 	</head>
-	<body <?php body_class(); ?>><?php
+	<body <?php body_class(); ?>>
+	<div class="skip-links">
+		<a href="#q">Skip to Search</a>
+		<a href="#live-player">Skip to Live Player</a>
+		<a href="#content">Skip to Content</a>
+		<a href="#footer">Skip to Footer</a>
+	</div>
+	<?php
 		do_action( 'beasley_after_body' );
 
 		if ( ! ee_is_jacapps() ) :
 			get_template_part( 'partials/header' );
 		endif;
 
-		?><div class="container">
+		?>
+		<div class="container">
 			<main id="content" class="content">
 				<?php get_template_part( 'partials/ads/leaderboard' ); ?>
 					<div id="inner-content">
