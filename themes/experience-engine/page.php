4d011b60edc4b338c8b49db42b690d6526066eef
Eugene Manuilov <eugene.manuilov@10up.com>
2018-12-06 14:47:23 +0200

    Moved title tag into main content wrap div

--- themes/experience-engine/page.php
+++ themes/experience-engine/page.php
@@ -5,9 +5,6 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="content-wrap">
 		<h1><?php the_title(); ?></h1>
-	</div>
-
-	<div class="content-wrap">
 		<?php get_template_part( 'partials/content/meta' ); ?>
 		<?php the_content(); ?>
 	</div>
--- themes/experience-engine/single-gmr_gallery.php
+++ themes/experience-engine/single-gmr_gallery.php
@@ -8,9 +8,7 @@
 	<?php get_template_part( 'partials/show/header' ); ?>
 	<div class="content-wrap">
 		<h1><?php the_title(); ?></h1>
-	</div>
 
-	<div class="content-wrap">
 		<div>
 			<?php get_template_part( 'partials/content/meta' ); ?>
 			<?php get_template_part( 'partials/featured-media' ); ?>
