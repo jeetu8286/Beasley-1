6af36a54985717f1e39487f99461f85ad2ba56e9
Dominic Magnifico <dominicmagnifico@gmail.com>
2018-12-13 10:21:25 -0700

    Remove inline script.

--- themes/experience-engine/footer.php
+++ themes/experience-engine/footer.php
@@ -10,31 +10,5 @@
 		?></div>
 
 		<?php wp_footer(); ?>
-		<script>
-			//@TODO :: We need to see if we can properly load this in the React App.
-			const carousels = document.querySelectorAll( '.swiper-container' );
-
-			carousels.forEach( carousel => {
-				const count = carousel.classList.contains( '-large' ) ? 2.2 : 4.2;
-				new Swiper( carousel, {
-					slidesPerView: count,
-					spaceBetween: 36,
-					freeMode: true,
-					breakpoints: {
-						900: {
-							slidesPerView: 2.2,
-						},
-						480: {
-							slidesPerView: 1.2,
-							spaceBetween: 27,
-						}
-					},
-					navigation: {
-						nextEl: '.swiper-button-next',
-						prevEl: '.swiper-button-prev',
-					},
-				} );
-			} );
-			</script>
 	</body>
 </html>
