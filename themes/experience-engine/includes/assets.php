6d6d03569e2728d7ca0704418a18e9efdddffec4
Dominic Magnifico <dominicmagnifico@gmail.com>
2018-12-13 11:24:53 -0700

    Revert emoji detection line.

--- themes/experience-engine/includes/assets.php
+++ themes/experience-engine/includes/assets.php
@@ -9,6 +9,7 @@ add_filter( 'script_loader_tag', 'ee_script_loader', 10, 3 );
 add_filter( 'fvideos_show_video', 'ee_fvideos_show_video', 10, 2 );
 add_filter( 'tribe_events_assets_should_enqueue_frontend', '__return_false' );
 
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
