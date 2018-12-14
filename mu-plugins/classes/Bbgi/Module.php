1f2ce6f2212542a59d7c7b2d4da89389e44a2bc0
Eugene Manuilov <eugene.manuilov@10up.com>
2018-12-14 15:35:05 +0200

    Moved modules to the Bbgi\Module class

--- mu-plugins/classes/Bbgi/Module.php
+++ mu-plugins/classes/Bbgi/Module.php
@@ -4,6 +4,8 @@ namespace Bbgi;
 
 abstract class Module {
 
+	private static $_modules = array();
+
 	/**
 	 * Converts method name into callable and returns it to use as a callback for
 	 * an action, filter or another function that needs callable callback.
@@ -23,5 +25,48 @@ abstract class Module {
 	 * @access public
 	 */
 	public abstract function register();
+
+	/**
+	 * Registers modules.
+	 *
+	 * @static
+	 * @access public
+	 */
+	public static function register_modules() {
+		self::$_modules = array(
+			'site'              => new \Bbgi\Site(),
+			'seo'               => new \Bbgi\Seo(),
+			'settings'          => new \Bbgi\Settings(),
+			'shortcodes'        => new \Bbgi\Shortcodes(),
+			'video'             => new \Bbgi\Media\Video(),
+			'image-attributes'  => new \Bbgi\Image\Attributes(),
+			'thumbnail-column'  => new \Bbgi\Image\ThumbnailColumn(),
+			'experience-engine' => new \Bbgi\Integration\ExperienceEngine(),
+			'google'            => new \Bbgi\Integration\Google(),
+			'firebase'          => new \Bbgi\Integration\Firebase(),
+		);
+
+		if ( current_theme_supports( 'secondstreet' ) ) {
+			self::$_modules['secondstreet'] = new \Bbgi\Integration\SecondStreet();
+		}
+
+		foreach ( self::$_modules as $module ) {
+			$module->register();
+		}
+	}
+
+	/**
+	 * Returns a module.
+	 *
+	 * @static
+	 * @access public
+	 * @param string $name
+	 * @return \Bbgi\Module
+	 */
+	public static function get( $name ) {
+		return ! empty( self::$_modules[ $name ] )
+			? self::$_modules[ $name ]
+			: null;
+	}
 
 }
