a745868ea95418ec0e5daf09c738bdcf8e75d7a8
Eugene Manuilov <eugene.manuilov@10up.com>
2018-12-17 16:34:06 +0200

    Added publisher information to the discovery block

--- themes/experience-engine/assets/scripts/components/content/embeds/Discovery.js
+++ themes/experience-engine/assets/scripts/components/content/embeds/Discovery.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
 function Discovery() {
+	const { bbgiconfig } = window;
+	const { publisher } = bbgiconfig;
+	const { picture, title } = publisher;
+
+	const logo = picture && picture.url
+		? <img src={picture.url} alt={title} />
+		: false;
+
 	return (
 		<div className="content-wrap">
 			<div className="meta">
-				{/* @TODO :: How will we pull in the station logo here? */}
-				<img src="https://placehold.it/117x69" alt="station logo" />
-				{/* @TODO:: How are we going to control this content dynamically? Or maybe we're not at all. */}
+				{logo}
+
 				<div className="copy">
 					<h3>Personalize your feed</h3>
 					<p>Subscribe to content tailored just for you</p>
 				</div>
-
 			</div>
+
 			<div className="action">
 				<svg className="waveform">
 					<rect style={{ animationDelay: '.2s' }} width="7" height="42" x="198" y="41" rx="3.5" />
@@ -28,7 +35,7 @@ function Discovery() {
 					<rect style={{ animationDelay: '.4s' }} width="7" height="5" x="342" y="59" rx="2.5" />
 					<rect style={{ animationDelay: '.6s' }} width="7" height="9" x="354" y="57" rx="3.5" />
 					<rect style={{ animationDelay: '.8s' }} width="7" height="5" x="366" y="59" rx="2.5" />
-					<rect style={{ animationDelay: '1"s' }} width="7" height="9" x="378" y="57" rx="3.5" />
+					<rect style={{ animationDelay: '1s' }} width="7" height="9" x="378" y="57" rx="3.5" />
 					<rect style={{ animationDelay: '1.2s' }} width="7" height="5" x="138" y="59" rx="2.5" />
 					<rect style={{ animationDelay: '1.4s' }} width="7" height="9" x="150" y="57" rx="3.5" />
 					<rect style={{ animationDelay: '1.6s' }} width="7" height="5" x="162" y="59" rx="2.5" />
@@ -36,14 +43,15 @@ function Discovery() {
 					<rect style={{ animationDelay: '.2s' }} width="7" height="19" x="186" y="52" rx="3.5" />
 					<rect style={{ animationDelay: '.4s' }} width="7" height="72" x="282" y="26" rx="3.5" />
 					<rect style={{ animationDelay: '.6s' }} width="7" height="123" x="222" rx="3.5" />
-					<rect style={{ animationDelay: '.8s' }} width="7" height="42" rx="3.5" y="43" x="79" />
-					<rect style={{ animationDelay: '1"s' }} width="7" height="42" rx="3.5" y="43" x="55" />
-					<rect style={{ animationDelay: '1.2s' }} width="7" height="19" rx="3.5" y="52" x="43" />
-					<rect style={{ animationDelay: '1.4s' }} width="7" height="9" rx="3.5" y="57" x="31" />
-					<rect style={{ animationDelay: '1.6s' }} width="7" height="9" rx="3.5" y="57" x="19" />
-					<rect style={{ animationDelay: '1.8s' }} width="7" height="5" rx="2.5" y="59.179104" x="7" />
-					<rect style={{ animationDelay: '.2s' }} width="7" height="72" rx="3.5" y="28" x="67" />
+					<rect style={{ animationDelay: '.8s' }} width="7" height="42" x="79" y="43" rx="3.5" />
+					<rect style={{ animationDelay: '1s' }} width="7" height="42" x="55" y="43" rx="3.5" />
+					<rect style={{ animationDelay: '1.2s' }} width="7" height="19" x="43" y="52" rx="3.5" />
+					<rect style={{ animationDelay: '1.4s' }} width="7" height="9" x="31" y="57" rx="3.5" />
+					<rect style={{ animationDelay: '1.6s' }} width="7" height="9" x="19" y="57" rx="3.5" />
+					<rect style={{ animationDelay: '1.8s' }} width="7" height="5" x="7" y="59.179104" rx="2.5" />
+					<rect style={{ animationDelay: '.2s' }} width="7" height="72" x="67" y="28" rx="3.5" />
 				</svg>
+
 				{/* @TODO :: Button needs associated method to add station to users 'subscribed' list */}
 				<button className="btn">
 					Customize Your Feed
