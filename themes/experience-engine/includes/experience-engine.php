e4134cd0b25c6aca697ade7e73c8845610a0418d
Eugene Manuilov <eugene.manuilov@10up.com>
2019-01-02 14:15:37 +0200

    Updated links to iOS and Android apps

--- themes/experience-engine/includes/experience-engine.php
+++ themes/experience-engine/includes/experience-engine.php
@@ -17,11 +17,6 @@ if ( ! function_exists( 'ee_get_publisher_information' ) ) :
 			$publisher_info = \Bbgi\Module::get( 'experience-engine' )->get_publisher();
 		}
 
-		// temporarily return # for itunes_app and play_app
-		if ( $meta == 'itunes_app' || $meta == 'play_app' ) {
-			return '#';
-		}
-
 		if ( empty( $publisher_info ) || empty( $publisher_info[ $meta ] ) ) {
 			return false;
 		}
