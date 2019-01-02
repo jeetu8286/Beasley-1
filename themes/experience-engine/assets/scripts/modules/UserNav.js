003e4116422def662da9b2ebf347c4fdc0e97cd5
Eugene Manuilov <eugene.manuilov@10up.com>
2019-01-02 14:09:38 +0200

    Updated user verification code

--- themes/experience-engine/assets/scripts/modules/UserNav.js
+++ themes/experience-engine/assets/scripts/modules/UserNav.js
@@ -40,12 +40,14 @@ class UserNav extends Component {
 	handleAuthStateChanged( user ) {
 		if ( user ) {
 			user.getIdToken()
-				//.then( token => fetch( `${window.bbgiconfig.eeapi}user?authorization=${encodeURIComponent( token )}` ) )
-				//.then( response => response.text() )
-				.then( data => {
-					console.log( data );
+				.then( token => fetch( `${window.bbgiconfig.eeapi}user?authorization=${encodeURIComponent( token )}` ) )
+				.then( response => response.json() )
+				.then( json => {
+					if ( 'user information has not been set' === json.Error ) {
+						console.log( '@todo: show user complete signup modal.' );
+					}
 				} )
-				.catch( err => console.log( err ) );
+				.catch( data => console.error( data ) ); // eslint-disable-line no-console
 		}
 
 		this.setState( { loading: false, user } );
@@ -110,7 +112,7 @@ class UserNav extends Component {
 						<title id="sign-up-button-title">Sign Up</title>
 						<desc id="sign-up-button-desc">User icon indicating creation of account</desc>
 						<path d="M280.79 314.559c83.266 0 150.803-67.538 150.803-150.803S364.055 13.415 280.79 13.415 129.987 80.953 129.987 163.756s67.537 150.803 150.803 150.803zm0-261.824c61.061 0 111.021 49.959 111.021 111.021s-49.96 111.02-111.021 111.02-111.021-49.959-111.021-111.021 49.959-111.02 111.021-111.02zM19.891 550.015h523.648c11.102 0 19.891-8.789 19.891-19.891 0-104.082-84.653-189.198-189.198-189.198H189.198C85.116 340.926 0 425.579 0 530.124c0 11.102 8.789 19.891 19.891 19.891zm169.307-169.307h185.034c75.864 0 138.313 56.436 148.028 129.524H41.17c9.714-72.625 72.164-129.524 148.028-129.524z"/>
-						
+
 					</svg>
 					Sign Up
 				</button>
