a5d46412088525e50da4bef366dd6f15d8ab5d4a
dainemawer <daine.mawer@10up.com>
2019-01-02 15:22:59 -0600

    Updated tabbing

--- themes/experience-engine/single.php
+++ themes/experience-engine/single.php
@@ -5,42 +5,42 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class( 'single' ); ?>>
 	<?php get_template_part( 'partials/show/header' ); ?>
 
-		<header class="post-info">
+	<header class="post-info">
 
-			<h1>
-				<?php the_title(); ?>
-			</h1>
+		<h1>
+			<?php the_title(); ?>
+		</h1>
 
-			<div class="post-meta">
-				<?php get_template_part( 'partials/content/meta' ); ?>
-			</div>
+		<div class="post-meta">
+			<?php get_template_part( 'partials/content/meta' ); ?>
+		</div>
 
-		</header>
+	</header>
 
-		<div class="episode-content content-wrap">
+	<div class="episode-content content-wrap">
 			
-			<div class="description">
-				<?php get_template_part( 'partials/featured-media' ); ?>
-				<?php ee_the_content_with_ads(); ?>
+		<div class="description">
+			<?php get_template_part( 'partials/featured-media' ); ?>
+			<?php ee_the_content_with_ads(); ?>
 
-				<?php if( is_single() ) : ?>
-					<div class="profile">
-						<?php echo get_the_author_meta( 'description' ); ?>
-					</div>
-				<?php endif; ?>
+			<?php if( is_single() ) : ?>
+				<div class="profile">
+					<?php echo get_the_author_meta( 'description' ); ?>
+				</div>
+			<?php endif; ?>
 				
-				<?php get_template_part( 'partials/content/categories' ); ?>
-				<?php get_template_part( 'partials/content/tags' ); ?>
-			</div>
+			<?php get_template_part( 'partials/content/categories' ); ?>
+			<?php get_template_part( 'partials/content/tags' ); ?>
+		</div>
 
-			<?php get_template_part( 'partials/ads/sidebar-sticky' ); ?>
+		<?php get_template_part( 'partials/ads/sidebar-sticky' ); ?>
 			
-		</div>
-
-		<div class="content-wrap">
-			<?php get_template_part( 'partials/related-articles' ); ?>
-		</div>
-		
 	</div>
+
+	<div class="content-wrap">
+		<?php get_template_part( 'partials/related-articles' ); ?>
+	</div>
+		
+</div>
 
 <?php get_footer(); ?>
