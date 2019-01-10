324cf759310fe609df4882eeef94bf114f55c1ae
Eugene Manuilov <eugene.manuilov@10up.com>
2019-01-10 16:37:48 +0200

    Added "Add Feed" button

--- themes/experience-engine/assets/scripts/components/modals/discovery/Feed.js
+++ themes/experience-engine/assets/scripts/components/modals/discovery/Feed.js
@@ -1,28 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import LazyImage from '../../content/embeds/LazyImage';
 
-function Feed( { id, title, picture, type } ) {
-	const placholder = `${id}-thumbnail`;
-	const image = ( picture.original || picture.large || {} ).url;
-	const lazyImage = image
-		? <LazyImage placeholder={placholder} src={image} width="300" height="300" alt={title} />
-		: false;
+class Feed extends Component {
 
-	return (
-		<div className={`${type} post-tile`}>
-			<div className="post-thumbnail">
-				<div id={placholder} className="placeholder placeholder-lazyimage">
-					{lazyImage}
+	constructor( props ) {
+		super( props );
+
+		const self = this;
+
+		self.state = {
+			loading: false,
+		};
+
+		self.onAdd = self.handleAdd.bind( self );
+	}
+
+	handleAdd() {
+		const self = this;
+		const { id } = self.props;
+
+		self.setState( { loading: true } );
+	}
+
+	render() {
+		const self = this;
+		const { loading } = self.state;
+		const { id, title, picture, type } = self.props;
+
+		const placholder = `${id}-thumbnail`;
+		const image = ( picture.original || picture.large || {} ).url;
+		const lazyImage = image
+			? <LazyImage placeholder={placholder} src={image} width="300" height="300" alt={title} />
+			: false;
+
+		const button = loading
+			? <div className="loading" />
+			: <button onClick={self.onAdd}>Add Feed</button>;
+
+		return (
+			<div className={`${type} post-tile`}>
+				<div className="post-thumbnail">
+					<div id={placholder} className="placeholder placeholder-lazyimage">
+						{lazyImage}
+					</div>
 				</div>
-			</div>
 
-			<div className="post-title">
-				<h3>{title}</h3>
+				<div className="post-title">
+					<h3>{title}</h3>
+				</div>
+
+				{button}
 			</div>
-		</div>
-	);
+		);
+	}
+
 }
 
 Feed.propTypes = {
--- themes/experience-engine/assets/scripts/components/modals/discovery/Filters.js
+++ themes/experience-engine/assets/scripts/components/modals/discovery/Filters.js
@@ -51,47 +51,49 @@ class DiscoveryFilters extends PureComponent {
 		/* eslint-disable jsx-a11y/no-onchange */
 		return (
 			<div className="filters">
-				<div className="select">
-					<select name="brand" value={brand} onChange={self.onFilterChange}>
-						<option value="">All Brands</option>
-						{Object.keys( publishers ).map( ( item, i ) => <option key={`brand-${i}`} value={item}>{publishers[item]}</option> )}
-					</select>
-				</div>
+				<div className="content-wrap">
+					<div className="select">
+						<select name="brand" value={brand} onChange={self.onFilterChange}>
+							<option value="">All Brands</option>
+							{Object.keys( publishers ).map( ( item, i ) => <option key={`brand-${i}`} value={item}>{publishers[item]}</option> )}
+						</select>
+					</div>
 
-				<div className="select">
-					<select name="location" value={location} onChange={self.onFilterChange}>
-						<option value="">All Locations</option>
-						{locations.map( ( item, i ) => <option key={`location-${i}`}>{item}</option> )}
-					</select>
-				</div>
+					<div className="select">
+						<select name="location" value={location} onChange={self.onFilterChange}>
+							<option value="">All Locations</option>
+							{locations.map( ( item, i ) => <option key={`location-${i}`}>{item}</option> )}
+						</select>
+					</div>
 
-				<div className="select">
-					<select name="type" value={type} onChange={self.onFilterChange}>
-						<option>All Types</option>
-						<option value="events">Events</option>
-						<option value="podcast">Podcasts</option>
-						<option value="news">News</option>
-						<option value="video">Video</option>
-						<option value="contests">Contests</option>
-					</select>
-				</div>
+					<div className="select">
+						<select name="type" value={type} onChange={self.onFilterChange}>
+							<option>All Types</option>
+							<option value="events">Events</option>
+							<option value="podcast">Podcasts</option>
+							<option value="news">News</option>
+							<option value="video">Video</option>
+							<option value="contests">Contests</option>
+						</select>
+					</div>
 
-				<div className="select">
-					<select name="genre" value={genre} onChange={self.onFilterChange}>
-						<option value="">All Genres</option>
-						{genres.map( ( item, i ) => <option key={`genre-${i}`}>{item}</option> )}
-					</select>
-				</div>
+					<div className="select">
+						<select name="genre" value={genre} onChange={self.onFilterChange}>
+							<option value="">All Genres</option>
+							{genres.map( ( item, i ) => <option key={`genre-${i}`}>{item}</option> )}
+						</select>
+					</div>
 
-				<form role="search" className="search-form" onSubmit={self.onSubmit}>
-					<label htmlFor="search-q" id="q" className="screen-reader-text">Search for:</label>
-					<input id="search-q" type="search" className="search-field" name="s" value={keyword} placeholder="Search" onChange={self.onKeywordChange} />
-					<button type="submit" className="search-submit" aria-label="Submit search">
-						<svg xmlns="http://www.w3.org/2000/svg" width="14" height="15">
-							<path d="M10.266 9.034h-.65l-.23-.222a5.338 5.338 0 0 0 1.216-4.385C10.216 2.144 8.312.32 6.012.042a5.342 5.342 0 0 0-5.97 5.97c.279 2.3 2.102 4.204 4.385 4.59a5.338 5.338 0 0 0 4.385-1.215l.222.23v.649l3.49 3.49c.337.336.887.336 1.224 0s.336-.887 0-1.224l-3.482-3.498zm-4.928 0c-2.044 0-3.695-1.65-3.695-3.696s1.65-3.695 3.695-3.695 3.696 1.65 3.696 3.695-1.65 3.696-3.696 3.696z" fill="currentcolor" />
-						</svg>
-					</button>
-				</form>
+					<form role="search" className="search-form" onSubmit={self.onSubmit}>
+						<label htmlFor="search-q" id="q" className="screen-reader-text">Search for:</label>
+						<input id="search-q" type="search" className="search-field" name="s" value={keyword} placeholder="Search" onChange={self.onKeywordChange} />
+						<button type="submit" className="search-submit" aria-label="Submit search">
+							<svg xmlns="http://www.w3.org/2000/svg" width="14" height="15">
+								<path d="M10.266 9.034h-.65l-.23-.222a5.338 5.338 0 0 0 1.216-4.385C10.216 2.144 8.312.32 6.012.042a5.342 5.342 0 0 0-5.97 5.97c.279 2.3 2.102 4.204 4.385 4.59a5.338 5.338 0 0 0 4.385-1.215l.222.23v.649l3.49 3.49c.337.336.887.336 1.224 0s.336-.887 0-1.224l-3.482-3.498zm-4.928 0c-2.044 0-3.695-1.65-3.695-3.696s1.65-3.695 3.695-3.695 3.696 1.65 3.696 3.695-1.65 3.696-3.696 3.696z" fill="currentcolor" />
+							</svg>
+						</button>
+					</form>
+				</div>
 			</div>
 		);
 		/* eslint-enable */
