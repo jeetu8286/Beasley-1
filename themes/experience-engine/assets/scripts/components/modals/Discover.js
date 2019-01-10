b72113a6369095f9f3e206b11baed4605820a64a
Eugene Manuilov <eugene.manuilov@10up.com>
2019-01-10 22:06:30 +0200

    Updated discovery popup to refresh the homepage if something has changed

--- themes/experience-engine/assets/scripts/components/modals/Discover.js
+++ themes/experience-engine/assets/scripts/components/modals/Discover.js
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import firebase from 'firebase';
 import trapHOC from '@10up/react-focus-trap-hoc';
 
 import Header from './elements/Header';
@@ -10,6 +13,7 @@ import FeedItem from './discovery/Feed';
 import DiscoveryFilters from './discovery/Filters';
 
 import { discovery, getFeeds, modifyFeeds, deleteFeed } from '../../library/experience-engine';
+import { loadPage } from '../../redux/actions/screen';
 
 class Discover extends Component {
 
@@ -18,6 +22,7 @@ class Discover extends Component {
 
 		const self = this;
 
+		self.needReload = false;
 		self.scrollYPos = 0;
 
 		self.state = {
@@ -30,6 +35,7 @@ class Discover extends Component {
 		self.onFilterChange = self.handleFilterChange.bind( self );
 		self.onAdd = self.handleAdd.bind( self );
 		self.onRemove = self.handleRemove.bind( self );
+		self.onClose = self.handleClose.bind( self );
 	}
 
 	componentDidMount() {
@@ -89,7 +95,8 @@ class Discover extends Component {
 		} );
 
 		modifyFeeds( feedsArray ).then( () => {
-			this.setState( { selectedFeeds } );
+			self.needReload = true;
+			self.setState( { selectedFeeds } );
 		} );
 	}
 
@@ -104,14 +111,35 @@ class Discover extends Component {
 		delete selectedFeeds[id];
 
 		deleteFeed( id ).then( () => {
-			this.setState( { selectedFeeds } );
+			self.needReload = true;
+			self.setState( { selectedFeeds } );
 		} );
+	}
+
+	handleClose() {
+		const self = this;
+
+		if ( self.needReload &&	 document.body.classList.contains( 'home' ) ) {
+			const auth = firebase.auth();
+
+			auth.currentUser.getIdToken().then( ( token ) => {
+				self.props.loadPage( `${window.bbgiconfig.wpapi}feeds-content`, {
+					suppressHistory: true,
+					fetchParams: {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+						body: `format=raw&authorization=${encodeURIComponent( token )}`,
+					},
+				} );
+			} );
+		}
+
+		self.props.close();
 	}
 
 	render() {
 		const self = this;
 		const { error, filteredFeeds, selectedFeeds, loading } = self.state;
-		const { close } = self.props;
 
 		let items = <div className="loading" />;
 		if ( !loading ) {
@@ -127,7 +155,7 @@ class Discover extends Component {
 
 		return (
 			<Fragment>
-				<CloseButton close={close} />
+				<CloseButton close={self.onClose} />
 				<DiscoveryFilters onChange={self.onFilterChange} />
 
 				<div className="content-wrap">
@@ -153,4 +181,8 @@ Discover.propTypes = {
 	close: PropTypes.func.isRequired,
 };
 
-export default trapHOC()( Discover );
+function mapDispatchToProps( dispatch ) {
+	return bindActionCreators( { loadPage }, dispatch );
+}
+
+export default connect( null, mapDispatchToProps )( trapHOC()( Discover ) );
