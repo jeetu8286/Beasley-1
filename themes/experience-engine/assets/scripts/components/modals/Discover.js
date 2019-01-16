2f8329f6cf505dc60755dbe20cc4e57cab677221
Dominic Magnifico <dominicmagnifico@gmail.com>
2019-01-16 10:34:03 -0700

    Remove commented code.

--- themes/experience-engine/assets/scripts/components/modals/Discover.js
+++ themes/experience-engine/assets/scripts/components/modals/Discover.js
@@ -2,12 +2,10 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-// import firebase from 'firebase';
 import trapHOC from '@10up/react-focus-trap-hoc';
 
 import Header from './elements/Header';
 import Alert from './elements/Alert';
-// import CloseButton from './elements/Close';
 
 import FeedItem from './discovery/Feed';
 import DiscoveryFilters from './discovery/Filters';
@@ -114,27 +112,6 @@ class Discover extends Component {
 			self.setState( { selectedFeeds } );
 		} );
 	}
-
-	// handleClose() {
-	// 	const self = this;
-
-	// 	if ( self.needReload &&	 document.body.classList.contains( 'home' ) ) {
-	// 		const auth = firebase.auth();
-
-	// 		auth.currentUser.getIdToken().then( ( token ) => {
-	// 			self.props.loadPage( `${window.bbgiconfig.wpapi}feeds-content`, {
-	// 				suppressHistory: true,
-	// 				fetchParams: {
-	// 					method: 'POST',
-	// 					headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-	// 					body: `format=raw&authorization=${encodeURIComponent( token )}`,
-	// 				},
-	// 			} );
-	// 		} );
-	// 	}
-
-	// 	self.props.close();
-	// }
 
 	render() {
 		const self = this;
