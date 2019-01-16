bbb5bdd08875e5f9b756f4665e10c424f6927777
Dominic Magnifico <dominicmagnifico@gmail.com>
2019-01-15 18:07:02 -0700

    Adjust markup for the discover modal and the feed within.

--- themes/experience-engine/assets/scripts/components/modals/Discover.js
+++ themes/experience-engine/assets/scripts/components/modals/Discover.js
@@ -2,12 +2,12 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import firebase from 'firebase';
+// import firebase from 'firebase';
 import trapHOC from '@10up/react-focus-trap-hoc';
 
 import Header from './elements/Header';
 import Alert from './elements/Alert';
-import CloseButton from './elements/Close';
+// import CloseButton from './elements/Close';
 
 import FeedItem from './discovery/Feed';
 import DiscoveryFilters from './discovery/Filters';
@@ -35,7 +35,6 @@ class Discover extends Component {
 		self.onFilterChange = self.handleFilterChange.bind( self );
 		self.onAdd = self.handleAdd.bind( self );
 		self.onRemove = self.handleRemove.bind( self );
-		self.onClose = self.handleClose.bind( self );
 	}
 
 	componentDidMount() {
@@ -116,26 +115,26 @@ class Discover extends Component {
 		} );
 	}
 
-	handleClose() {
-		const self = this;
+	// handleClose() {
+	// 	const self = this;
 
-		if ( self.needReload &&	 document.body.classList.contains( 'home' ) ) {
-			const auth = firebase.auth();
+	// 	if ( self.needReload &&	 document.body.classList.contains( 'home' ) ) {
+	// 		const auth = firebase.auth();
 
-			auth.currentUser.getIdToken().then( ( token ) => {
-				self.props.loadPage( `${window.bbgiconfig.wpapi}feeds-content`, {
-					suppressHistory: true,
-					fetchParams: {
-						method: 'POST',
-						headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-						body: `format=raw&authorization=${encodeURIComponent( token )}`,
-					},
-				} );
-			} );
-		}
+	// 		auth.currentUser.getIdToken().then( ( token ) => {
+	// 			self.props.loadPage( `${window.bbgiconfig.wpapi}feeds-content`, {
+	// 				suppressHistory: true,
+	// 				fetchParams: {
+	// 					method: 'POST',
+	// 					headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+	// 					body: `format=raw&authorization=${encodeURIComponent( token )}`,
+	// 				},
+	// 			} );
+	// 		} );
+	// 	}
 
-		self.props.close();
-	}
+	// 	self.props.close();
+	// }
 
 	render() {
 		const self = this;
@@ -155,7 +154,7 @@ class Discover extends Component {
 
 		return (
 			<Fragment>
-				<CloseButton close={self.onClose} />
+				{/* <CloseButton close={self.onClose} /> */}
 				<DiscoveryFilters onChange={self.onFilterChange} />
 
 				<div className="content-wrap">
