81a807fb8f0552d951807be6266097726edb5110
Dominic Magnifico <dominicmagnifico@gmail.com>
2019-01-18 13:28:41 -0700

    Utilize a pure component, and add state to the Recent Songs so that it can be opened or closed.

--- themes/experience-engine/assets/scripts/components/player/RecentSongs.js
+++ themes/experience-engine/assets/scripts/components/player/RecentSongs.js
@@ -1,59 +1,75 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const RecentSongs = ( { songs } ) => {
-	/* if ( !Array.isArray( songs ) || !songs.length ) {
-		return false;
-	} */
+class RecentSongs extends PureComponent {
+	constructor( props ) {
+		super( props );
 
-	const items = songs.map( ( song ) => {
-		let time = false;
-		if ( 0 < song.cueTimeStart ) {
-			time = new Date( +song.cueTimeStart );
-			time = time.toLocaleString( 'en-US', {
-				hour: 'numeric',
-				minute: 'numeric',
-				hour12: true,
-			} );
+		const self = this;
+		self.state = { isOpen: false };
 
-			time = <span>{time}</span>;
+		self.onToggle = self.handleToggleClick.bind( self );
+	}
+
+	handleToggleClick() {
+		this.setState( prevState => ( { isOpen: !prevState.isOpen } ) );
+	}
+
+	render() {
+		const self = this;
+		const { songs } = self.props;
+		if ( !Array.isArray( songs ) || !songs.length ) {
+			return false;
 		}
+		const items = songs.map( ( song ) => {
+			let time = false;
+			if ( 0 < song.cueTimeStart ) {
+				time = new Date( +song.cueTimeStart );
+				time = time.toLocaleString( 'en-US', {
+					hour: 'numeric',
+					minute: 'numeric',
+					hour12: true,
+				} );
+
+				time = <span>{time}</span>;
+			}
+
+			return (
+				<li key={song.cueTimeStart}>
+					<span className="cue-point-title">{song.cueTitle}</span>
+					<span className="cue-point-artist">{song.artistName}</span>
+					{time}
+				</li>
+			);
+		} );
 
 		return (
-			<li key={song.cueTimeStart}>
-				<span className="cue-point-title">{song.cueTitle}</span>
-				<span className="cue-point-artist">{song.artistName}</span>
-				{time}
-			</li>
-		);
-	} );
+			<div className="controls-recent">
+				<button onClick={self.onToggle}>
+					<svg width="29" height="6" viewBox="0 0 28 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<rect width="6" height="6" rx="3" fill="#EB108B"/>
+						<rect width="6" height="6" rx="3" fill="#4898D3"/>
+						<rect width="6" height="6" rx="3" fill="#707070"/>
+						<rect x="11" width="6" height="6" rx="3" fill="#EB108B"/>
+						<rect x="11" width="6" height="6" rx="3" fill="#4898D3"/>
+						<rect x="11" width="6" height="6" rx="3" fill="#707070"/>
+						<rect x="22" width="6" height="6" rx="3" fill="#EB108B"/>
+						<rect x="22" width="6" height="6" rx="3" fill="#4898D3"/>
+						<rect x="22" width="6" height="6" rx="3" fill="#707070"/>
+					</svg>
+				</button>
 
-	return (
-		<div className="controls-recent">
-			<button>
-				<svg width="28" height="6" viewBox="0 0 28 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<rect width="6" height="6" rx="3" fill="#EB108B"/>
-					<rect width="6" height="6" rx="3" fill="#4898D3"/>
-					<rect width="6" height="6" rx="3" fill="#707070"/>
-					<rect x="11" width="6" height="6" rx="3" fill="#EB108B"/>
-					<rect x="11" width="6" height="6" rx="3" fill="#4898D3"/>
-					<rect x="11" width="6" height="6" rx="3" fill="#707070"/>
-					<rect x="22" width="6" height="6" rx="3" fill="#EB108B"/>
-					<rect x="22" width="6" height="6" rx="3" fill="#4898D3"/>
-					<rect x="22" width="6" height="6" rx="3" fill="#707070"/>
-				</svg>
-			</button>
-
-			<div className="controls-recent-songs">
-				<div>Recent Songs:</div>
-				<ul>
-					{items}
-				</ul>
+				<div className="controls-recent-songs">
+					<div>Recent Songs:</div>
+					<ul>
+						{items}
+					</ul>
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 RecentSongs.propTypes = {
 	songs: PropTypes.arrayOf( PropTypes.object ).isRequired,
