b49e3bbc83fa2024242e2721a1f82326af5ec066
Eugene Manuilov <eugene.manuilov@10up.com>
2019-01-25 01:00:01 +0200

    Added add-to-favorites button to the archive template

--- themes/experience-engine/partials/archive/title.php
+++ themes/experience-engine/partials/archive/title.php
@@ -3,4 +3,5 @@
 echo '<div class="archive-title content-wrap">';
 	get_template_part( 'partials/archive/icon', get_query_var( 'post_type' ) );
 	the_archive_title( '<h1>', '</h1>' );
+	ee_add_to_favorites( get_queried_object()->slug );
 echo '</div>';
