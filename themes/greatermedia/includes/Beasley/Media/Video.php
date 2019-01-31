7f9dbbe5ae6d6492da845aaa4bfd3c72b315c059
Eugene Manuilov <eugene.manuilov@10up.com>
2019-01-31 19:15:00 +0200

    Updated livestream video to not use proxy m3u8 urls

--- themes/greatermedia/includes/Beasley/Media/Video.php
+++ themes/greatermedia/includes/Beasley/Media/Video.php
@@ -28,9 +28,6 @@ class Video extends \Beasley\Module {
 		add_action( 'init', $this( 'setup_embeds' ) );
 		add_action( 'init', $this( 'setup_shortcodes' ) );
 		add_action( 'beasley-register-settings', $this( 'register_settings' ), 10, 2 );
-
-		add_action( 'wp_ajax_livestream_m3u8_proxy', $this( 'livestream_m3u8_proxy' ) );
-		add_action( 'wp_ajax_nopriv_livestream_m3u8_proxy', $this( 'livestream_m3u8_proxy' ) );
 	}
 
 	/**
@@ -217,13 +214,11 @@ class Video extends \Beasley\Module {
 		}
 
 		$timestamp = round( microtime( true ) * 1000 );
-		$srouce = add_query_arg( array(
+		$source = add_query_arg( array(
 			'timestamp' => $timestamp,
 			'clientId'  => get_option( 'livestream_client_id' ),
 			'token'     => hash_hmac( "md5", "{$key}:playback:{$timestamp}", $key ),
 		), $json['m3u8'] );
-
-		$proxy = admin_url( '/admin-ajax.php?action=livestream_m3u8_proxy&url=' . urlencode( $srouce ) );
 
 		return sprintf(
 			'<div class="livestream livestream-oembed" data-ad-tag="%s">' .
@@ -232,7 +227,7 @@ class Video extends \Beasley\Module {
 			esc_attr( $this->get_ad_tag_url( $event_id, $video_id ) ),
 			esc_attr( $this->_get_embed_id() ),
 			! empty( $json['thumbnailUrl'] ) ? esc_attr( $json['thumbnailUrl'] ) : '',
-			esc_url_raw( $proxy )
+			esc_url_raw( $source )
 		);
 	}
 
@@ -244,41 +239,6 @@ class Video extends \Beasley\Module {
 	 */
 	protected function _get_embed_id() {
 		return 'ls_embed_' . rand( 1, getrandmax() );
-	}
-
-	/**
-	 * Proxies request to Livestream server.
-	 *
-	 * @access public
-	 * @action wp_ajax_livestream_m3u8_proxy
-	 * @action wp_ajax_nopriv_livestream_m3u8_proxy
-	 */
-	public function livestream_m3u8_proxy() {
-		$url = filter_input( INPUT_GET, 'url', FILTER_VALIDATE_URL );
-		if ( ! $url ) {
-			status_header( 404 );
-			exit;
-		}
-
-		$response = wp_remote_get( $url );
-		if ( is_wp_error( $response ) ) {
-			status_header( 400 );
-			exit;
-		}
-
-		$headers = wp_remote_retrieve_headers( $response );
-		foreach ( $headers as $header => $values ) {
-			if ( ! is_array( $values ) ) {
-				$values = array( $values );
-			}
-
-			foreach ( $values as $value ) {
-				header( sprintf( '%s: %s', $header, $value ) );
-			}
-		}
-
-		echo wp_remote_retrieve_body( $response );
-		exit;
 	}
 
 	/**
