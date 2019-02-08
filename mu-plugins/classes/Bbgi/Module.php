4a9542ca17408fb1814d42f2c130127055ce388c
Eugene Manuilov <eugene.manuilov@10up.com>
2019-02-08 12:46:55 +0200

    Updated modules setup process to match what is in the experience-engine branch

--- mu-plugins/classes/Bbgi/Module.php
+++ mu-plugins/classes/Bbgi/Module.php
@@ -4,6 +4,8 @@ namespace Bbgi;
 
 abstract class Module {
 
+	private static $_modules = array();
+
 	/**
 	 * Converts method name into callable and returns it to use as a callback for
 	 * an action, filter or another function that needs callable callback.
@@ -23,5 +25,40 @@ abstract class Module {
 	 * @access public
 	 */
 	public abstract function register();
+
+	/**
+	 * Registers modules.
+	 *
+	 * @static
+	 * @access public
+	 */
+	public static function register_modules() {
+		self::$_modules = array(
+			'seo'               => new \Bbgi\Seo(),
+			'dfp'               => new \Bbgi\Integration\Dfp(),
+		);
+
+		if ( current_theme_supports( 'secondstreet' ) ) {
+			self::$_modules['secondstreet'] = new \Bbgi\Integration\SecondStreet();
+		}
+
+		foreach ( self::$_modules as $module ) {
+			$module->register();
+		}
+	}
+
+	/**
+	 * Returns a module.
+	 *
+	 * @static
+	 * @access public
+	 * @param string $name
+	 * @return \Bbgi\Module
+	 */
+	public static function get( $name ) {
+		return ! empty( self::$_modules[ $name ] )
+			? self::$_modules[ $name ]
+			: null;
+	}
 
 }
