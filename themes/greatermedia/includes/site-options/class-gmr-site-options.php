38991c40f83f59c4696c296ac75f82c255155186
Eugene Manuilov <eugene.manuilov@10up.com>
2019-02-11 17:13:40 +0200

    Added main settings

--- themes/greatermedia/includes/site-options/class-gmr-site-options.php
+++ themes/greatermedia/includes/site-options/class-gmr-site-options.php
@@ -38,10 +38,9 @@ class GreaterMediaSiteOptions {
 
 	public function register_settings( $group, $page ) {
 		// Settings Section
-		add_settings_section( 'beasley_site_settings', 'Station Site', array( $this, 'render_site_settings_section' ), $page );
+		add_settings_section( 'beasley_site_settings', 'Station Type', array( $this, 'render_site_settings_section' ), $page );
 		add_settings_section( 'beasley_social_networks', 'Social Networks', '__return_false', $page );
 
-		add_settings_field( 'gmr_site_logo', 'Site Logo', 'bbgi_image_field', $page, 'beasley_site_settings', 'name=gmr_site_logo' );
 		add_settings_field( 'gmr_livelinks_title', 'Live Links Sidebar Title', 'bbgi_input_field', $page, 'beasley_site_settings', 'name=gmr_livelinks_title' );
 
 		add_settings_field( 'gmr_facebook_url', 'Facebook', 'bbgi_input_field', $page, 'beasley_social_networks', 'name=gmr_facebook_url' );
@@ -54,7 +53,6 @@ class GreaterMediaSiteOptions {
 		register_setting( $group, 'gmr_twitter_name', 'sanitize_text_field' );
 		register_setting( $group, 'gmr_youtube_url', 'esc_url_raw' );
 		register_setting( $group, 'gmr_instagram_name', 'sanitize_text_field' );
-		register_setting( $group, 'gmr_site_logo', 'intval' );
 		register_setting( $group, 'gmr_livelinks_title', 'sanitize_text_field');
 		register_setting( $group, 'gmr_newssite', 'esc_attr' );
 		register_setting( $group, 'gmr_livelinks_more_redirect', 'esc_attr' );
@@ -66,9 +64,7 @@ class GreaterMediaSiteOptions {
 		$livelinks_more = get_option( 'gmr_livelinks_more_redirect', '' );
 		$liveplayer_disabled = get_option( 'gmr_liveplayer_disabled', '' );
 		
-		?><h4>Station Type</h4>
-
-		<div class="gmr__option">
+		?><div class="gmr__option">
 			<input type="checkbox" name="gmr_newssite" id="gmr_newssite" value="1" <?php checked( 1 == esc_attr( $news_site ) ); ?>><label for="gmr_newssite" class="gmr__option--label-inline"><?php _e( 'News/Sports Station', 'greatermedia' ); ?></label>
 			<div class="gmr-option__field--desc"><?php _e( 'Check this box if this site is for a News or Sports Radio Station.', 'greatermedia' ); ?></div>
 		</div>
