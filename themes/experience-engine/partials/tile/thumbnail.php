83ffe91d71827d8ecc3019c747723ca46fc151fc
dainemawer <daine.mawer@10up.com>
2019-02-12 13:15:26 +0200

    Added a11y to svg icon

--- themes/experience-engine/partials/tile/thumbnail.php
+++ themes/experience-engine/partials/tile/thumbnail.php
@@ -3,7 +3,11 @@
 		<?php ee_the_lazy_thumbnail( null, true ); ?>
 		<?php if( has_post_format( 'video', $post->ID ) ) : ?>
 			<div class="post-video-overlay">
-				<svg viewBox="0 0 17 24" xmlns="http://www.w3.org/2000/svg"><path d="M16.1836 12.0055L0.910156 23.124L0.910156 0.887031L16.1836 12.0055Z"></path></svg>
+				<svg viewBox="0 0 17 24" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="video-icon-title video-icon-desc">
+					<title id="vide-icon-title">Video Icon</title>
+					<description id="video-icon-desc">A circular icon, with a red play button that symbolizes that the post is a video</description>
+					<path d="M16.1836 12.0055L0.910156 23.124L0.910156 0.887031L16.1836 12.0055Z"></path>
+				</svg>
 			</div>
 		<?php endif; ?>
 	</a>
