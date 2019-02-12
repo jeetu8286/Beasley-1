d5ec7ad92c428f94712de553b724dbf733a85db5
dainemawer <daine.mawer@10up.com>
2019-02-12 13:04:18 +0200

    Updated video thumbnails with icon

--- themes/experience-engine/partials/tile/thumbnail.php
+++ themes/experience-engine/partials/tile/thumbnail.php
@@ -1,5 +1,10 @@
 <div class="post-thumbnail">
 	<a href="<?php ee_the_permalink(); ?>">
 		<?php ee_the_lazy_thumbnail( null, true ); ?>
+		<?php if( has_post_format( 'video', $post->ID ) ) : ?>
+			<div class="post-video-overlay">
+				<svg viewBox="0 0 17 24" xmlns="http://www.w3.org/2000/svg"><path d="M16.1836 12.0055L0.910156 23.124L0.910156 0.887031L16.1836 12.0055Z"></path></svg>
+			</div>
+		<?php endif; ?>
 	</a>
 </div>
