af26ccde2a5b0d1d0f5f30f44cdcd915347f7833
Eugene Manuilov <eugene.manuilov@10up.com>
2019-02-14 15:30:17 +0200

    Fixed missed items on the tag archive

--- themes/experience-engine/includes/theme.php
+++ themes/experience-engine/includes/theme.php
@@ -10,7 +10,7 @@ remove_action( 'wp_head', 'wp_enqueue_scripts', 1 );
 remove_action( 'wp_head', '_wp_render_title_tag', 1 );
 remove_action( 'do_pings', 'do_all_pings' );
 
-add_filter( 'pre_get_posts','ee_update_search_query' );
+add_filter( 'pre_get_posts','ee_update_main_query' );
 
 if ( ! function_exists( 'ee_setup_theme' ) ) :
 	function ee_setup_theme() {
@@ -37,10 +37,25 @@ if ( ! function_exists( 'ee_register_nav_menus' ) ) :
 	}
 endif;
 
-if ( ! function_exists( 'ee_update_search_query' ) ) :
-	function ee_update_search_query( $query ) {
+if ( ! function_exists( 'ee_update_main_query' ) ) :
+	function ee_update_main_query( $query ) {
+		if ( ! $query->is_main_query() ) {
+			return $query;
+		}
+
 		if ( $query->is_search() ) {
 			$query->set( 'posts_per_page', 12 );
+		} elseif ( $query->is_tag() ) {
+			$post_type = $query->get( 'post_type' );
+			if ( ! is_array( $post_type ) ) {
+				$post_type = array( $post_type );
+			}
+
+			$post_type[] = 'gmr_gallery';
+			$post_type[] = 'gmr_album';
+			$post_type[] = 'contest';
+
+			$query->set( 'post_type', $post_type );
 		}
 
 		return $query;
