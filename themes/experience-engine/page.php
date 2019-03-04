3a606c1501313b32a6dd721d3262fdb9861ce87f
Eugene Manuilov <eugene.manuilov@10up.com>
2019-03-04 19:46:37 +0200

    Updated meta data for a single page template

--- themes/experience-engine/page.php
+++ themes/experience-engine/page.php
@@ -5,7 +5,7 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="content-wrap">
 		<h1><?php the_title(); ?></h1>
-		<?php get_template_part( 'partials/content/meta' ); ?>
+		<?php get_template_part( 'partials/content/meta', 'page' ); ?>
 		<?php the_content(); ?>
 	</div>
 </div>
