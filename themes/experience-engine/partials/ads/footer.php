a4af91f9b71d84b825c7f2c9ca8a9c4288a72984
dainelindleymawer <hello@dainemawer.com>
2019-03-06 08:41:26 +0200

    Removed ad titles

--- themes/experience-engine/partials/ads/footer.php
+++ themes/experience-engine/partials/ads/footer.php
@@ -6,7 +6,6 @@ endif;
 
 ?><div class="ad -footer -centered">
 	<div class="wrapper">
-		<p class="ad-title">Advertisement</p>
 		<?php do_action( 'dfp_tag', 'bottom-leaderboard', false, array( array( 'pos', 2 ) ) ); ?>
 	</div>
 </div>
--- themes/experience-engine/partials/ads/sidebar-sticky.php
+++ themes/experience-engine/partials/ads/sidebar-sticky.php
@@ -1,6 +1,5 @@
 <aside class="ad -sticky">
 	<div class="wrapper">
-		<p class="ad-title">Advertisement</p>
 		<?php do_action( 'dfp_tag', 'right-rail' ); ?>
 	</div>
 </aside>
