9ede9b847bf183899e7f3e6ddac32b4ac59909c0
Darshan Sawardekar <darshan.sawardekar@get10up.com>
2019-04-25 13:18:39 +0530

    Add helpers to send playback events to GA

--- themes/experience-engine/assets/scripts/library/google-analytics.js
+++ themes/experience-engine/assets/scripts/library/google-analytics.js
@@ -5,6 +5,45 @@ export function pageview( title, location ) {
 	}
 }
 
+/**
+ * A ga('send') wrapped behind a check in case GA is blocked or absent.
+ *
+ * @param opts The ga event opts
+ */
+export function sendToGA( opts ) {
+	if ( -1 !== location.search.indexOf( 'gadebug=1' ) ) {
+		window.console.log( 'sendToGA', opts );
+	}
+
+	const { ga } = window;
+
+	if ( ga ) {
+		ga( 'send', opts );
+	}
+}
+
+/**
+ * Sends a Live stream playing event to GA
+ */
+export function sendLiveStreamPlaying() {
+	sendToGA( {
+		hitType       : 'event',
+		eventCategory : 'audio',
+		eventAction   : 'Live stream playing',
+	} );
+}
+
+/**
+ * Sends a Inline audio playing event to GA
+ */
+export function sendInlineAudioPlaying() {
+	sendToGA( {
+		hitType       : 'event',
+		eventCategory : 'audio',
+		eventAction   : 'Inline audio playing',
+	} );
+}
+
 export default {
 	pageview,
 };
