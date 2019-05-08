9cfa371b4b9ff3903367cc92663a36aa7d15ce86
Darshan Sawardekar <darshan.sawardekar@get10up.com>
2019-05-08 15:44:54 +0530

    Adds debug statements #1

--- themes/experience-engine/assets/scripts/modules/UserNav.js
+++ themes/experience-engine/assets/scripts/modules/UserNav.js
@@ -37,9 +37,11 @@ class UserNav extends Component {
 
 	componentDidMount() {
 		const { firebase: config } = window.bbgiconfig;
+		console.log( 'UserNav mounted' );
 
 		if ( config.projectId ) {
 			firebase.initializeApp( config );
+			window.console.log( 'firebase initialized', config );
 
 			const auth = firebase.auth();
 			auth.onAuthStateChanged( this.onAuthStateChanged );
@@ -48,6 +50,7 @@ class UserNav extends Component {
 
 	handleAuthStateChanged( user ) {
 		const self = this;
+		window.console.log( 'handleAuthStateChanged', user );
 
 		if ( user ) {
 			self.props.setUser( user );
