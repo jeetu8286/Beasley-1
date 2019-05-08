3cb5a2d3b3632cba5af907f119157ac1be17cf40
Darshan Sawardekar <darshan.sawardekar@get10up.com>
2019-05-08 13:08:59 +0530

    Revert nav menu changes

--- themes/experience-engine/partials/primary-navigation.php
+++ themes/experience-engine/partials/primary-navigation.php
@@ -1,18 +1,12 @@
 <?php
 
-if ( has_nav_menu( 'primary-nav' ) ) :
-	wp_nav_menu( array(
-		'theme_location' => 'primary-nav',
-		'depth'          => 2,
-		'item_spacing'   => 'discard',
-		'walker'         => new \PrimaryNavWalker(),
-	) );
-elseif ( has_nav_menu( 'ee-primary' ) ):
-	wp_nav_menu( array(
-		'theme_location' => 'ee-primary',
-		'depth'          => 2,
-		'item_spacing'   => 'discard',
-		'walker'         => new \PrimaryNavWalker(),
-	) );
+if ( ! has_nav_menu( 'primary-nav' ) ) :
+	return;
 endif;
 
+wp_nav_menu( array(
+	'theme_location' => 'primary-nav',
+	'depth'          => 2,
+	'item_spacing'   => 'discard',
+	'walker'         => new \PrimaryNavWalker(),
+) );
