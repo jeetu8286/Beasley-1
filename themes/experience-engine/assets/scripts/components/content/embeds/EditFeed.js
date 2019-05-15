74979a40dff330ed28b50a8a790f4e71a62b2f04
Mario Aguiar <me@marioaguiar.net>
2019-05-14 23:13:14 -0500

    Turn function into class

--- themes/experience-engine/assets/scripts/components/content/embeds/EditFeed.js
+++ themes/experience-engine/assets/scripts/components/content/embeds/EditFeed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -6,28 +6,44 @@ import { bindActionCreators } from 'redux';
 import { showEditFeedModal } from '../../../redux/actions/modal';
 
 //function EditFeed( { loggedIn, showModal, feed, title, feeds, className } ) {
-function EditFeed( { loggedIn, className } ) {
-	/** For some reason feeds are not being initializaed */
-	//const hasFeed = feeds.find( item => item.id === feed );
-	//if ( !hasFeed ) {
-	//	return false;
-	//}
+/** For some reason feeds are not being initializaed, leaving for context */
+//const hasFeed = feeds.find( item => item.id === feed );
+//if ( !hasFeed ) {
+//	return false;
+//}
+//}
 
-	return loggedIn
-		? (
-			<button className={className} aria-label='Edit Feed'>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 212.982 212.982" aria-labelledby="close-modal-title close-modal-desc" width="13" height="13">
-					<title id="close-modal-title">Remove</title>
-					<path
-						d="M131.804 106.491l75.936-75.936c6.99-6.99 6.99-18.323 0-25.312-6.99-6.99-18.322-6.99-25.312 0L106.491 81.18 30.554 5.242c-6.99-6.99-18.322-6.99-25.312 0-6.989 6.99-6.989 18.323 0 25.312l75.937 75.936-75.937 75.937c-6.989 6.99-6.989 18.323 0 25.312 6.99 6.99 18.322 6.99 25.312 0l75.937-75.937 75.937 75.937c6.989 6.99 18.322 6.99 25.312 0 6.99-6.99 6.99-18.322 0-25.312l-75.936-75.936z"
-						fillRule="evenodd"
-						clipRule="evenodd"
-					/>
-				</svg>
-			</button>
-		)
-		//? <button className={className} onClick={() => showModal( { feed, title } )} aria-label={`Edit ${title} Feed`}>Edit</button>
-		: false;
+class EditFeed extends PureComponent {
+	constructor() {
+		super();
+		const self = this;
+
+		self.handleRemove = self.handleRemove.bind( self );
+	}
+
+	handleRemove( event ) {
+		console.log( event );
+	}
+
+	render() {
+		const { loggedIn, className } = this.props;
+
+		return loggedIn
+			? (
+				<button className={className} aria-label='Edit Feed' onClick={this.handleRemove}>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 212.982 212.982" aria-labelledby="close-modal-title close-modal-desc" width="13" height="13">
+						<title id="close-modal-title">Remove</title>
+						<path
+							d="M131.804 106.491l75.936-75.936c6.99-6.99 6.99-18.323 0-25.312-6.99-6.99-18.322-6.99-25.312 0L106.491 81.18 30.554 5.242c-6.99-6.99-18.322-6.99-25.312 0-6.989 6.99-6.989 18.323 0 25.312l75.937 75.936-75.937 75.937c-6.989 6.99-6.989 18.323 0 25.312 6.99 6.99 18.322 6.99 25.312 0l75.937-75.937 75.937 75.937c6.989 6.99 18.322 6.99 25.312 0 6.99-6.99 6.99-18.322 0-25.312l-75.936-75.936z"
+							fillRule="evenodd"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</button>
+			)
+			//? <button className={className} onClick={() => showModal( { feed, title } )} aria-label={`Edit ${title} Feed`}>Edit</button>
+			: false;
+	}
 }
 
 EditFeed.propTypes = {
