55b6480a66c7f339703a353f6e77cc451f5b9a8c
Mario Aguiar <me@marioaguiar.net>
2019-05-22 12:40:08 -0500

    Add minus icon

--- themes/experience-engine/assets/scripts/components/content/embeds/AddToFavorites.js
+++ themes/experience-engine/assets/scripts/components/content/embeds/AddToFavorites.js
@@ -100,8 +100,8 @@ class AddToFavorites extends PureComponent {
 		if ( self.hasFeed() ) {
 			if ( showIcon ) {
 				icon = (
-					<svg width="15" height="15" xmlns="http://www.w3.org/2000/svg">
-						<path fillRule="evenodd" clipRule="evenodd" d="M8.5 0h-2v6.5H0v2h6.5V15h2V8.5H15v-2H8.5V0z"/>
+					<svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+						<rect y="6.61502" x="0" fill="none" id="svg_2" height="2" width="15"/>
 					</svg>
 				);
 			}
