8c3696312058c23bba4dca3261e859ab2a9576cf
Darshan Sawardekar <darshan.sawardekar@get10up.com>
2019-05-23 12:08:44 +0530

    Removes global lock from Complete Signup Modal

--- themes/experience-engine/assets/scripts/redux/reducers/modal.js
+++ themes/experience-engine/assets/scripts/redux/reducers/modal.js
@@ -1,4 +1,4 @@
-import { DISCOVER_MODAL, ACTION_SHOW_MODAL, ACTION_HIDE_MODAL } from '../actions/modal';
+import { DISCOVER_MODAL, ACTION_SHOW_MODAL, ACTION_HIDE_MODAL, COMPLETE_SIGNUP_MODAL } from '../actions/modal';
 
 export const DEFAULT_STATE = {
 	modal: 'CLOSED',
@@ -16,7 +16,7 @@ function resizeWindow() {
 function reducer( state = {}, action = {} ) {
 	switch ( action.type ) {
 		case ACTION_SHOW_MODAL:
-			if ( action.modal !== DISCOVER_MODAL ) {
+			if ( action.modal !== DISCOVER_MODAL && action.modal !== COMPLETE_SIGNUP_MODAL ) {
 				document.documentElement.classList.add( 'locked' );
 				document.body.classList.add( 'locked' );
 				document.addEventListener( 'ontouchmove', ( e ) => {
