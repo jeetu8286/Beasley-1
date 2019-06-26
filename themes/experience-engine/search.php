41c09463ea4a84c59dc89cc49776f165b5431167
Darshan Sawardekar <darshan.sawardekar@get10up.com>
2019-06-26 14:23:25 +0530

    Adds Keywords results to top of search

--- themes/experience-engine/search.php
+++ themes/experience-engine/search.php
@@ -7,13 +7,40 @@ if ( ee_is_first_page() ) :
 endif;
 
 if ( have_posts() ) :
+	global $wp_query;
+
+	$search_query   = get_search_query();
+	$search_results = $wp_query->posts;
+	$page_num       = intval( get_query_var( 'paged' ) );
+
+	if ( empty( $page_num ) ) {
+		$page_num = 1;
+	}
+
 	echo '<div class="content-wrap">';
+		if ( $page_num === 1 ) {
+			$keyword = intval( get_post_with_keyword( $search_query ) );
+
+			if ( ! empty( $keyword ) ) {
+				echo '<h4>Keyword: ' . esc_html( $search_query ) . '</h4>';
+				echo '<div class="archive-tiles archive-keyword-tiles -grid -small">';
+
+				setup_postdata( $keyword );
+				get_template_part( 'partials/tile', get_post_type() );
+				wp_reset_postdata();
+
+				echo '</div>';
+			}
+		}
+
+
 		echo '<div class="archive-tiles -grid -small">';
 			while ( have_posts() ) :
 				the_post();
 				get_template_part( 'partials/tile', get_post_type() );
 			endwhile;
-			echo '</div>';
+		echo '</div>';
+
 		ee_load_more();
 	echo '</div>';
 
