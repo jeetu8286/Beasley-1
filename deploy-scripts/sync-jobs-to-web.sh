db6ff34340cc43a2be0a736d51e12aeb30fc8af5
Darshan Sawardekar <darshan.sawardekar@get10up.com>
2019-06-26 08:46:32 +0530

    Removes --delete from the deploy process
    * Temporary fix to prevent debug plugins from being deleted

--- deploy-scripts/sync-jobs-to-web.sh
+++ deploy-scripts/sync-jobs-to-web.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Run remote commands on the job server to sync web1, web2, and web3
-ssh beanstalk@52.0.13.41 'rsync -vrxc --delete --delay-updates --delete-after /var/www/html/wordpress/ web1:/var/www/html/wordpress/ --exclude=wp-content/uploads --exclude=wp-content/upgrade'
-ssh beanstalk@52.0.13.41 'rsync -vrxc --delete --delay-updates --delete-after /var/www/html/wordpress/ web2:/var/www/html/wordpress/ --exclude=wp-content/uploads --exclude=wp-content/upgrade'
-ssh beanstalk@52.0.13.41 'rsync -vrxc --delete --delay-updates --delete-after /var/www/html/wordpress/ web3:/var/www/html/wordpress/ --exclude=wp-content/uploads --exclude=wp-content/upgrade'
+ssh beanstalk@52.0.13.41 'rsync -vrxc --delay-updates --delete-after /var/www/html/wordpress/ web1:/var/www/html/wordpress/ --exclude=wp-content/uploads --exclude=wp-content/upgrade'
+ssh beanstalk@52.0.13.41 'rsync -vrxc --delay-updates --delete-after /var/www/html/wordpress/ web2:/var/www/html/wordpress/ --exclude=wp-content/uploads --exclude=wp-content/upgrade'
+ssh beanstalk@52.0.13.41 'rsync -vrxc --delay-updates --delete-after /var/www/html/wordpress/ web3:/var/www/html/wordpress/ --exclude=wp-content/uploads --exclude=wp-content/upgrade'
