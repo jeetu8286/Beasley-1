031952682ad01e4f6ba2a4b73f184ef6edee926b
Darshan Sawardekar <darshan.sawardekar@get10up.com>
2019-06-28 12:15:17 +0530

    Improves Geo Targetly to only fire on needed pages

--- themes/experience-engine/includes/geotargetly.php
+++ themes/experience-engine/includes/geotargetly.php
@@ -26,21 +26,44 @@ function ee_geotargetly_if() {
 		$embed_code = get_option( 'ee_geotargetly_embed_code' );
 
 		if ( ! empty( $embed_code ) ) {
+			// Assumes that the Geotargetly Embed Code is adjusted to use
+			// the window.geotargetly global
 			echo "\n\n";
 			echo $embed_code; // XSS Ok - Geo Targetly Embed Code is trusted
 			echo "\n\n";
+
+			if ( ee_current_page_needs_geotargetly() ) {
+				echo "\n\n";
+				echo "<script>geotargetly ? geotargetly( document,'script','style','head' ) : 0;</script>";
+				echo "\n\n";
+			}
 		}
+
 	}
 }
 
+/**
+ * Checks if the current page needs Geo Targetly. Currently limited to
+ * the D&C Show & related pages.
+ */
 function ee_current_page_needs_geotargetly() {
 	global $wp;
 	$request = trailingslashit( '/' . $wp->request );
 
 	return ee_is_geotargetly_enabled() &&
 		(
-			in_category( 'dave-chuck-the-freak' ) ||
-			( stripos( $request, '/shows/dave-and-chuck/' ) !== false )
+			(
+				/* Post tagged with D&C category */
+				is_single() && in_category( 'dave-chuck-the-freak' )
+			) ||
+			(
+				/* D&C Show Page */
+				is_single() && get_query_var( 'show' ) === 'dave-and-chuck'
+			) ||
+			(
+				/* Sub Nav page of D&C */
+				stripos( $request, '/shows/dave-and-chuck/' ) !== false
+			)
 		);
 }
 
