7f831843c056ea6b723abaf7c437a72a0df5b99d
Darshan Sawardekar <darshan.sawardekar@get10up.com>
2019-07-24 14:08:20 +0530

    Overrides WP SEO Metadata generation

--- themes/experience-engine/includes/seo.php
+++ themes/experience-engine/includes/seo.php
@@ -1,20 +1,31 @@
 <?php
 
-function ee_update_head_metadata() {
+/**
+ * Overrides the WP SEO Open graph article author.
+ *
+ * @return string|false
+ */
+function ee_update_opengraph_article_author() {
 	if ( is_single() ) {
 		$post_id        = get_the_ID();
 		$article_author = ee_get_opengraph_article_author( $post_id );
 
 		if ( ! empty( $article_author ) ) {
-			?>
-
-				<meta property="article:author" content="<?php echo esc_url( $article_author ); ?>" />;w
-
-			<?php
+			return $article_author;
 		}
 	}
+
+	return false;
 }
 
+/**
+ * Returns the opengraph article author for the specified post id. If
+ * the user does not have a facebook url in meta, uses the author
+ * WP permalink instead.
+ *
+ * @param int $post_id The post id
+ * @return string URL
+ */
 function ee_get_opengraph_article_author( $post_id ) {
 	$post = get_post( $post_id );
 	$author = $post->post_author;
@@ -32,4 +43,12 @@ function ee_get_opengraph_article_author( $post_id ) {
 	}
 }
 
-add_action( 'wp_head', 'ee_update_head_metadata' );
+/**
+ * Overrides WP SEO's article:author opengraph output.
+ */
+add_filter( 'wpseo_opengraph_author_facebook', 'ee_update_opengraph_article_author' );
+
+/**
+ * Enable article:published_time for custom post types
+ */
+add_filter( 'wpseo_opengraph_show_publish_date', '__return_true' );
