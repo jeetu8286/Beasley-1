ead802fe2f3997f37529ac0ced1c19ebf48e2548
Steve Meyers <steve.meyers@bbgi.com>
2019-07-30 09:03:02 -0400

    Removing publish date on page metadata template.

--- themes/experience-engine/partials/content/meta-page.php
+++ themes/experience-engine/partials/content/meta-page.php
@@ -1,9 +1,4 @@
 <div class="meta">
-	<div class="author-meta">
-		<span class="author-meta-date">
-			<?php ee_the_date(); ?>
-		</span>
-	</div>
 	<div class="share-wrap-icons">
 		<span class="label">Share</span>
 		<?php ee_the_share_buttons( get_permalink(), get_the_title() ); ?>
