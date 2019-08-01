c125706b55f04283cf8607ab0c6677a04fcaaef2
Devin Leggett <devin.leggett@paycase.com>
2019-08-01 14:13:21 -0700

    Made header and description partial for page

--- themes/experience-engine/templates/full-bleed.php
+++ themes/experience-engine/templates/full-bleed.php
@@ -9,24 +9,10 @@
 <?php the_post(); ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="post-info">
-	    <?php if ( bbgi_featured_image_layout_is( null, 'top' ) ) : ?>
-            <?php get_template_part( 'partials/featured-media' ); ?>
-        <?php endif; ?>
-
-		<h1><?php the_title(); ?></h1>
-		<div class="post-meta">
-			<?php get_template_part( 'partials/content/meta', 'page' ); ?>
-		</div>
-	</header>
+	<?php get_template_part( 'partials/page/header' ); ?>
 
 	<div class="content-wrap">
-		<div class="description">
-		    <?php if ( bbgi_featured_image_layout_is( null, 'inline' ) ) : ?>
-                <?php get_template_part( 'partials/featured-media' ); ?>
-            <?php endif; ?>
-			<?php the_content(); ?>
-		</div>
+		<?php get_template_part( 'partials/page/description' ); ?>
 	</div>
 </div>
 
