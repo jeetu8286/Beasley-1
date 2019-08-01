c813ba60ebfac5b2cf5e15af50da2a95d6276a0a
Devin Leggett <devin.leggett@paycase.com>
2019-08-01 13:57:36 -0700

    Added full-bleed template

---
+++ themes/experience-engine/templates/full-bleed.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Full Bleed
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php the_post(); ?>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="post-info">
+	    <?php if ( bbgi_featured_image_layout_is( null, 'top' ) ) : ?>
+            <?php get_template_part( 'partials/featured-media' ); ?>
+        <?php endif; ?>
+
+		<h1><?php the_title(); ?></h1>
+		<div class="post-meta">
+			<?php get_template_part( 'partials/content/meta', 'page' ); ?>
+		</div>
+	</header>
+
+	<div class="content-wrap">
+		<div class="description">
+		    <?php if ( bbgi_featured_image_layout_is( null, 'inline' ) ) : ?>
+                <?php get_template_part( 'partials/featured-media' ); ?>
+            <?php endif; ?>
+			<?php the_content(); ?>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
