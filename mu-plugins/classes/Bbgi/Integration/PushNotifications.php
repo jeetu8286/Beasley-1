50341c75a98e5802eec807407ec7384b368a0cf4
Nícholas Oliveira <nicholas.andre@10up.com>
2019-12-10 18:28:29 -0300

    Adding cap only to admins but not granting it yet.

--- mu-plugins/classes/Bbgi/Integration/PushNotifications.php
+++ mu-plugins/classes/Bbgi/Integration/PushNotifications.php
@@ -29,13 +29,13 @@ class PushNotifications extends \Bbgi\Module {
 	 * @return void
 	 */
 	public function register_custom_cap() {
-		$roles = [ 'administrator', 'editor' ];
+		$roles = [ 'administrator' ];
 
 		foreach ( $roles as $role ) {
 			$role_obj = get_role( $role );
 
 			if ( is_a( $role_obj, \WP_Role::class ) ) {
-				$role_obj->add_cap( 'send_notifications', true );
+				$role_obj->add_cap( 'send_notifications', false );
 			}
 		}
 	}
