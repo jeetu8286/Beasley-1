e8944abfabc8922324b6b2247214efbeedb147b5
Nícholas Oliveira <nicholas.andre@10up.com>
2020-03-02 16:06:56 -0300

    Removing temporary return

--- mu-plugins/classes/Bbgi/Redirects.php
+++ mu-plugins/classes/Bbgi/Redirects.php
@@ -24,7 +24,6 @@ class Redirects extends \Bbgi\Module {
 	 * @return void
 	 */
 	public function register() {
-		return;
 		add_filter( 'post_link', [ $this, 'expand_redirect' ], 10, 2 );
 		add_filter( 'page_link', [ $this, 'expand_redirect' ], 10, 2);
 		add_filter( 'post_type_link', [ $this, 'expand_redirect' ], 10, 2 );
