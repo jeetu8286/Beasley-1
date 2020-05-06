a7e40d2e00c11d634297248e2ed85c398d6a0b91
Kevin Gilper <kevin.gilper@bbgi.com>
2020-05-05 21:47:09 -0400

    added ga analytics integration to second street embed https://help.secondstreet.com/google-analytics-integration

--- themes/experience-engine/assets/scripts/components/content/embeds/SecondStreet.js
+++ themes/experience-engine/assets/scripts/components/content/embeds/SecondStreet.js
@@ -10,6 +10,60 @@ class SecondStreet extends PureComponent {
 			return;
 		}
 
+		window.SecondStreetSDK = {
+			version: '1.0.0',
+			ready: function ready(secondstreet) {
+				[
+					{
+						category: 'Second Street',
+						name: 'secondstreet:form:visible',
+						label: 'Visitor saw a Second Street form',
+					},
+					{
+						category: 'Second Street',
+						name: 'secondstreet:form:abandoned',
+						label: 'Visitor abandoned a Second Street form',
+					},
+					{
+						category: 'Second Street',
+						name: 'secondstreet:form:started',
+						label: 'Visitor began filling out a Second Street form',
+					},
+					{
+						category: 'Second Street',
+						name: 'secondstreet:form:submitted',
+						label: 'Visitor successfully submitted a Second Street form',
+					},
+					{
+						category: 'Second Street',
+						name: 'secondstreet:formpage:error',
+						label:
+							'Visitor attempted to submit a page of a Second Street form but there was an error',
+					},
+					{
+						category: 'Second Street',
+						name: 'secondstreet:formpage:submitted',
+						label:
+							'Visitor successfully submitted a page of a Second Street form',
+					},
+				].forEach(function(_ref) {
+					const { category } = _ref;
+					const { name } = _ref;
+					const { label } = _ref;
+					return secondstreet.addEventListener(name, function() {
+						// eslint-disable-next-line no-undef
+						return window.ga('send', 'event', category, name, label);
+					});
+				});
+				secondstreet.addEventListener('secondstreet:route:enter', function(
+					data,
+				) {
+					// eslint-disable-next-line no-undef
+					return window.ga('send', 'pageview', data.detail);
+				});
+			},
+		};
+
 		const element = document.createElement('script');
 
 		element.setAttribute('async', true);
