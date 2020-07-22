9810fbeca3e01314d96b87c8005f88e61a81fee5
mikepersico <mike.persico@bbgi.com>
2020-07-22 15:30:57 -0400

    Removed Tile Icons From Contests, Episodes, Galleries, Albums, Podcast, Posts, and Events...

--- themes/experience-engine/partials/tile-gmr_album.php
+++ themes/experience-engine/partials/tile-gmr_album.php
@@ -1,8 +1,4 @@
 <div data-post-id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php get_template_part( 'partials/tile/thumbnail' ); ?>
 	<?php get_template_part( 'partials/tile/title' ); ?>
-
-	<p class="type">
-		photo album
-	</p>
 </div>
