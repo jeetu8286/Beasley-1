ff46c7e381eb61fa207c2cce9a02a068b1865165
mikepersico <mike.persico@bbgi.com>
2020-09-18 17:09:10 -0400

    Update Targeting Should Refresh.

--- themes/experience-engine/assets/scripts/redux/utilities/screen/updateTargeting.js
+++ themes/experience-engine/assets/scripts/redux/utilities/screen/updateTargeting.js
@@ -1,12 +1,14 @@
 export default function updateTargeting() {
 	const { googletag } = window;
-
-	const { dfp } = window.bbgiconfig;
-
-	if (dfp && Array.isArray(dfp.global)) {
-		for (let i = 0, pairs = dfp.global; i < pairs.length; i++) {
-			googletag.pubads().setTargeting(pairs[i][0], pairs[i][1]);
-		}
-		// googletag.pubads().refresh();
+	if (googletag) {
+		googletag.cmd.push(() => {
+			const { dfp } = window.bbgiconfig;
+			if (dfp && Array.isArray(dfp.global)) {
+				for (let i = 0, pairs = dfp.global; i < pairs.length; i++) {
+					googletag.pubads().setTargeting(pairs[i][0], pairs[i][1]);
+				}
+				googletag.pubads().refresh(); // Refresh ALL Slots
+			}
+		});
 	}
 }
