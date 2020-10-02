daff924b932c5d82f1067062d9e41fbea8170674
Oscar Sanchez <oscar@oscarssanchez.com>
2020-10-01 19:45:18 -0500

    Force exact matching with EP

--- mu-plugins/elasticpress-customizations.php
+++ mu-plugins/elasticpress-customizations.php
@@ -16,3 +16,18 @@ add_action( 'pre_get_posts', function ( $query ) {
 		$query->set( 'order', 'desc' );
 	}
 }, 10, 1 );
+
+/**
+ * Forces exact matching
+ */
+add_filter( 'ep_formatted_args', function ( $formatted_args, $args  ) {
+	if ( ! is_admin() && is_search() ) {
+		if ( ! empty( $formatted_args['query']['bool']['should'] ) ) {
+			$formatted_args['query']['bool']['must'] = $formatted_args['query']['bool']['should'];
+			$formatted_args['query']['bool']['must'][0]['multi_match']['operator'] = 'AND';
+			unset( $formatted_args['query']['bool']['should'] );
+			unset( $formatted_args["query"]["bool"]["must"][0]["multi_match"]["type"] );
+		}
+	}
+	return $formatted_args;
+}, 10, 2 );
