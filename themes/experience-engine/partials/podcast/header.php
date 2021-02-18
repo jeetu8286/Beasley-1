ab79f9478c5bbc3a2d921d4a50ac78ca7a54953f
mikepersico <mike.persico@bbgi.com>
2021-02-18 10:57:40 -0500

    Missed semicolon.

--- themes/experience-engine/partials/podcast/header.php
+++ themes/experience-engine/partials/podcast/header.php
@@ -10,7 +10,7 @@
 		</p>
 		<h1><?php the_title(); ?></h1>
 		<div>
-			<?php the_content() ?>
+			<?php the_content(); ?>
 		</div>
 		<p class="count">
 			<?php echo esc_html( ee_get_episodes_count() ); ?> episodes
