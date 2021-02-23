6fbd25cd9a75814173f166b29b53e535cf7757c6
mikepersico <mike.persico@bbgi.com>
2021-02-23 12:49:43 -0500

    Show Description only when /category/ is in path

--- themes/experience-engine/partials/archive/title.php
+++ themes/experience-engine/partials/archive/title.php
@@ -4,3 +4,7 @@ echo '<div class="archive-title content-wrap">';
 	get_template_part( 'partials/archive/icon', get_query_var( 'post_type' ) );
 	the_archive_title( '<h1>', '</h1>' );
 echo '</div>';
+
+if (strpos($_SERVER['REQUEST_URI'], '/category/') !== false) {
+	the_archive_description('<div class="content-wrap" style="padding-bottom: 0">', '</div>');
+}
