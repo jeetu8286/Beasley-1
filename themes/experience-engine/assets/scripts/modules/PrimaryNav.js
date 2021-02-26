23754a2272b30798b0e6c9367bae43eed98697f4
mikepersico <mike.persico@bbgi.com>
2021-02-26 14:43:33 -0500

    Temporarily add console debugs.

--- themes/experience-engine/assets/scripts/modules/PrimaryNav.js
+++ themes/experience-engine/assets/scripts/modules/PrimaryNav.js
@@ -65,7 +65,9 @@ class PrimaryNav extends PureComponent {
 			sidebarContainer.classList.add('is-safari');
 		}
 
+		console.log('componentDidMount()');
 		document.body.classList.remove('-lock');
+		console.log('removed lock');
 	}
 
 	componentWillUnmount() {
@@ -286,6 +288,10 @@ class PrimaryNav extends PureComponent {
 			parent.classList.contains('sub-menu') &&
 			parent.contains(linkToActivate)
 		) {
+			console.log(
+				'Remove lock because parent has submenu, but dont close menus',
+			);
+			document.body.classList.remove('-lock');
 			return;
 		}
 
@@ -293,6 +299,7 @@ class PrimaryNav extends PureComponent {
 	}
 
 	closeMenus() {
+		console.log('Closing Menus');
 		document.body.classList.remove('-lock');
 		const container = this.primaryNavRef.current;
 		const actives = container.querySelectorAll(
