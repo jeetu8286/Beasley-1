f084cb98afa7adb8e1ca04121ed14c54c1c2e06b
mikepersico <mike.persico@bbgi.com>
2021-04-21 15:58:06 -0400

    Do not show interstitial unless lazy loading and empty.

--- themes/experience-engine/assets/scripts/redux/utilities/screen/updateInterstitialAdDiv.js
+++ themes/experience-engine/assets/scripts/redux/utilities/screen/updateInterstitialAdDiv.js
@@ -7,9 +7,15 @@ export default function updateInterstitialAdDiv() {
 		);
 
 		if (interstitialAdDiv) {
-			// Open Div To Full Screen So That Lazy Ad Can Show
-			interstitialAdDiv.style.cssText =
-				'bottom: 0; height: 100%; left: 0; position: fixed; right: 0; top: 0; width: 100%; z-index: 9000003;';
+			const nonScriptInterstitialChildren = interstitialAdDiv.querySelectorAll(
+				':not(script)',
+			);
+
+			// If Interstitial Div Is Empty, Open Interstial Div Full Screen So That Lazy Load Can Occur
+			if (nonScriptInterstitialChildren.length === 0) {
+				interstitialAdDiv.style.cssText =
+					'bottom: 0; height: 100%; left: 0; position: fixed; right: 0; top: 0; width: 100%; z-index: 9000003;';
+			}
 		}
 	}
 }
