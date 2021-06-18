f6e282acdf940d7cfc00a5aa017dac40230ad9f0
Rupesh Jorkar <rupesh.jorkar@bbgi.com>
2021-06-18 17:18:24 +0530

    Syndicate feature, favorite and custom logo in Shows

--- plugins/greatermedia-content-syndication/includes/syndication-cpt.php
+++ plugins/greatermedia-content-syndication/includes/syndication-cpt.php
@@ -14,7 +14,7 @@ class SyndicationCPT {
 	 */
 	private static $filter_nonce = 'filter-syndication-nonce';
 
-	public static $supported_subscriptions = array( 'post', 'content-kit', 'contest', 'survey', 'gmr_gallery', 'affiliate_marketing' );
+	public static $supported_subscriptions = array( 'post', 'content-kit', 'contest', 'survey', 'gmr_gallery', 'affiliate_marketing', 'show' );
 
 	/**
 	 * The post statuses that are supported during syndication
