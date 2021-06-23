1cc046ebfe0533d20844ef336b9deca3c2b99954
Rupesh Jorkar <rupesh.jorkar@bbgi.com>
2021-06-23 20:23:38 +0530

    exclude some post type for category and tag page

--- themes/experience-engine/includes/theme.php
+++ themes/experience-engine/includes/theme.php
@@ -67,10 +67,14 @@ if ( ! function_exists( 'ee_update_main_query' ) ) :
 			}
 
 			$post_types_array = get_post_types( '', 'names' );
+			$exclude_post_type = array( 'gmr_homepage', 'gmr_mobile_homepage', 'page', 'nav_menu_item', 'user_request', 'acf-field-group', 'acf-field', 'cmm-redirect', 'attachment', 'revision', 'custom_css', 'customize_changeset', 'oembed_cache', 'live-stream', 'songs', 'redirect_rule' );
 			foreach ( $post_types_array as $posttype ) {
-				$post_type[] = $posttype;
-			}
-
+				if( !in_array( $posttype, $exclude_post_type ) )
+				{
+					$post_type[] = $posttype;
+				}
+			} 
+			
 			$query->set( 'post_type', $post_type );
 		}
 
