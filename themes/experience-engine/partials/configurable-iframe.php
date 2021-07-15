6ace3cb46692f8e4ccc88014f7cae56e843dea04
mikepersico <mike.persico@bbgi.com>
2021-07-15 14:33:09 -0400

    Mock up HTML and CSS.

---
+++ themes/experience-engine/partials/configurable-iframe.php
@@ -0,0 +1,26 @@
+
+<style>
+	#footer {
+		margin-bottom: 300px;
+	}
+
+	div.configurable-iframe-holder {
+		position: fixed;
+		bottom: 90px;
+		left: 0;
+		right: 0;
+		height: 300px;
+		z-index: 9;
+		overflow: hidden;
+	}
+
+	@media only screen and (min-width: 900px) {
+		div.configurable-iframe-holder {
+			left: 188px;
+		}
+	}
+</style>
+<div class="configurable-iframe-holder">
+	<iframe width="100%" height="100%" frameborder="0" scrolling="no" style="overflow: hidden" src="https://luvurnxtgig.wpengine.com/" ></iframe>
+</div>
+
