e6e59516b724260445e6cabda557093ff6279fcf
Surjit Vala <surjit@Admins-Mac-mini.local>
2021-08-02 17:49:20 +0530

    Fix Links to cross-origin destinations are unsafe

--- themes/experience-engine/includes/assets.php
+++ themes/experience-engine/includes/assets.php
@@ -159,7 +159,7 @@ endif;
 
 if ( ! function_exists( 'ee_the_beasley_logo' ) ) :
 	function ee_the_beasley_logo() {
-		echo '<a href="https://bbgi.com" target="_blank">
+		echo '<a href="https://bbgi.com" target="_blank" rel="noopener">
 			<img src="', get_template_directory_uri(), '/assets/images/large-BMG60YearsLogo.png" style="max-height: 150px; max-width: 150px; padding-bottom: 10px;" alt="Beasley Media Group">
 		</a>';
 	}
@@ -190,7 +190,7 @@ endif;
 
 if ( ! function_exists( 'ee_the_beasley_logo' ) ) :
 	function ee_the_beasley_logo() {
-		echo '<a href="https://bbgi.com" target="_blank">
+		echo '<a href="https://bbgi.com" target="_blank" rel="noopener">
 			<img src="', get_template_directory_uri(), '/assets/images/large-BMG60YearsLogo.png" style="max-height: 150px; max-width: 150px;" alt="Beasley Media Group">
 		</a>';
 	}
