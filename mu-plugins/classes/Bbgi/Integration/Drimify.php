d1b1440708bc11681618122ab5750fdfeec4ac3e
Surjit Vala <surjit@Admins-Mac-mini.local>
2021-09-09 18:53:42 +0530

    Fix Drimify issue in showing url entered with preview attached

--- mu-plugins/classes/Bbgi/Integration/Drimify.php
+++ mu-plugins/classes/Bbgi/Integration/Drimify.php
@@ -33,6 +33,10 @@ class Drimify extends \Bbgi\Module {
 			'app_style' => ''
 		), $atts, 'drimify' );
 
+		if( empty( $attributes['app_url'] ) ) {
+			$attributes['app_url'] = $this->extract_app_link( $atts );
+		}
+
 		if($attributes['app_url'] != '' && false === stripos( $attributes['app_url'], 'https://go.drimify.com' )) {
 			return;
 		}
@@ -49,4 +53,42 @@ class Drimify extends \Bbgi\Module {
 		return apply_filters( 'drimify_html', $embed, $attributes );
 	}
 
+	/**
+	 * Extract APP URL if not sent as text.
+	 *
+	 * @access public
+	 * @param array $attributes Array of shortcode arguments.
+	 * @return string App URL string.
+	 */
+	public function extract_app_link( $atts ) {
+		$atts = (array) $atts;
+
+		if(!empty( $atts['href'] )) {
+			return $atts['href'];
+		}
+
+		$string_with_app_url = "";
+		if(false !== stripos( $atts[0], 'href' )) {
+			$string_with_app_url = $atts[0];
+		}
+		else if(false !== stripos( $atts[1], 'href' )) {
+			$string_with_app_url = $atts[1];
+		}
+
+		if( !empty($string_with_app_url) ) {
+			$link_available = "";
+			$parts = explode( '"', $string_with_app_url );
+	
+			foreach($parts as $part) {
+				if (stripos($part, 'https://go.drimify.com') !== false && stripos($part, 'https://go.drimify.com') == 0) {
+					$link_available = trim($part);
+					break;
+				}
+			}
+
+			if($link_available) {
+				return $link_available;
+			}
+		}
+	}
 }
