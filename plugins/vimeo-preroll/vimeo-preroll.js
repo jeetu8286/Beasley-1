4a40c80f3e57f1d170145f771700eda4d1c58c16
mikepersico <mike.persico@bbgi.com>
2021-10-25 14:02:01 -0400

    Add vimeo player control to pause Play and mimick preroll.

---
+++ plugins/vimeo-preroll/vimeo-preroll.js
@@ -0,0 +1,48 @@
+	console.log('Vimeo-preroll loading.')
+
+	const loadVimeoPlayers = () => {
+		const iframeList = Array.from(document.querySelectorAll('iframe'));
+		iframeList.filter(iframeElement => iframeElement.src && iframeElement.src.toLowerCase().indexOf('vimeo') > -1)
+				  .map(filteredEl => {
+				  	return loadVimeoPlayer(filteredEl)
+				  });
+
+	}
+
+	const loadVimeoPlayer = (iframe) => {
+		const vimeoplayer = new Vimeo.Player(iframe);
+		let isPlayingPreroll = false;
+
+		vimeoplayer.on('play', async function () {
+			console.log('Attempted to Play the video');
+
+			if (!isPlayingPreroll) {
+				isPlayingPreroll = true;
+				console.log('Played And Instantly Pausing');
+				await vimeoplayer.pause();
+				console.log('Paused and now Playing Preroll');
+				setTimeout(async () => {
+					await vimeoplayer.play();
+					isPlayingPreroll = false;
+					console.log('Vimeo Playing');
+				}, 5000);
+			}
+		});
+
+		vimeoplayer.on('pause', async function () {
+			console.log('Paused the video');
+		});
+
+		vimeoplayer.getVideoTitle().then(function (title) {
+			console.log('title:', title);
+		});
+	}
+
+	const oldOnload=window.onload;
+	window.onload=function(){
+		oldOnload && oldOnload();
+		loadVimeoPlayers();
+		console.log('Vimeo-preroll loaded.')
+	}
+
+
---
+++ plugins/vimeo-preroll/vimeo-preroll.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: VimeoPreroll
+Plugin URI: https://bbgi.com/
+Description: This plugin adds support adding GAM prerolls to any Vimeo iframe found on page.
+Version: 1.0
+Author: M Persico
+Author URI: https://bbgi.com/
+License: GPL2
+*/
+
+function enqueue_vimeopreroll_scripts() {
+	$vimeoplayerjsurl = plugins_url( '/vimeo-player-v2-16-1.js', __FILE__ );
+	echo "<script type='text/javascript' async src='". $vimeoplayerjsurl ."'></script>";
+
+	$vimeoprerolljsurl = plugins_url( '/vimeo-preroll.js', __FILE__ );
+	echo "<script type='text/javascript' src='". $vimeoprerolljsurl ."'></script>";
+}
+
+
+
+
+
+
