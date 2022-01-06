6f184928d306a3231d26abced6ff33fc5ac63f8e
Surjit Vala <surjit.vala@bbgi.com>
2022-01-06 19:05:30 +0530

    Allow all network user in dropdown box and add capability for co author metabox

--- themes/experience-engine/partials/content/meta.php
+++ themes/experience-engine/partials/content/meta.php
@@ -5,41 +5,18 @@
 
 			// Co author Checking
 			$is_co_author_cpt = get_field( 'is_co_author_cpt', $post );
-			$reported_attribution_cpt = get_field( 'reported_attribution_cpt', $post );
-
+			$additional_author_name = get_field( 'reported_attribution_cpt', $post );
 			$is_coauthor = (!empty($is_co_author_cpt) && $is_co_author_cpt[0] == 'true') ? true : false;
-			$additional_author_name = '';
-			$additional_author_avatar = '';
-			if(!empty($reported_attribution_cpt)) {
-				$additional_author_name = $reported_attribution_cpt['display_name'];
-				$additional_author_avatar = get_avatar( $reported_attribution_cpt['ID'], 40 );
-			}
 		?>
 		<?php if ( ! $contest_is_singular ) : ?>
 			<span class="author-avatar hide-avatar">
 				<?php if ( is_singular() ) : ?>
 					<?php
 						$avatar = get_avatar( get_the_author_meta( 'ID' ), 40 );
-						if($additional_author_name) {
-							if($is_coauthor) {
-								if ( $avatar ) {
-									echo $avatar;	
-								} else {
-									echo '<img class="avatar avatar-40 photo" '.( $additional_author_name ? 'style="margin-right: 0.3rem;"' : '' ).' src="https://2.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=96&d=mm&r=g" height="40" width="40" alt="Placeholder Shilloutte User Image">';
-								}
-							}
-
-							if ( $additional_author_avatar ) {
-								echo $additional_author_avatar;	
-							} else {
-								echo '<img class="avatar avatar-40 photo" src="https://2.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=96&d=mm&r=g" height="40" width="40" alt="Placeholder Shilloutte User Image">';
-							}
+						if ( $avatar ) {
+							echo $avatar;	
 						} else {
-							if ( $avatar ) {
-								echo $avatar;	
-							} else {
-								echo '<img class="avatar avatar-40 photo" src="https://2.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=96&d=mm&r=g" height="40" width="40" alt="Placeholder Shilloutte User Image">';
-							}
+							echo '<img class="avatar avatar-40 photo" src="https://2.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=96&d=mm&r=g" height="40" width="40" alt="Placeholder Shilloutte User Image">';
 						}
 					?>
 				<?php endif; ?>
