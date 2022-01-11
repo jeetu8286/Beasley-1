cff9786e8b3a94f7c0b618779067ebfe0635c871
kevingilper <dpc12pkd3blindmice!>
2022-01-11 10:42:11 -0500

    Revert to prior footer logo.

--- themes/experience-engine/partials/footer.php
+++ themes/experience-engine/partials/footer.php
@@ -1,6 +1,6 @@
 <footer id="footer" class="footer">
 	<div itemscope itemtype="http://schema.org/Organization">
-		<?php ee_the_beasley_logo(); ?>
+		<?php ee_the_custom_logo(); ?>
 	</div>
 	<div class="footer-meta">
 		<?php get_template_part( 'partials/footer/download-app' ); ?>
