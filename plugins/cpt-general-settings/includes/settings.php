953c9ffffdd2144aba5ae8b5d51c9607ba39f844
Rupesh Jorkar <rupesh.jorkar@c-metric.com>
2022-01-12 17:45:21 +0530

    Alt attribution required field from Add media button

--- plugins/cpt-general-settings/includes/settings.php
+++ plugins/cpt-general-settings/includes/settings.php
@@ -9,9 +9,42 @@ class CommonSettings {
 	public static function init() {
 		add_action( 'init', array( __CLASS__, 'settings_cpt_init' ), 0 );
 		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
+		add_action( 'admin_head', array( __CLASS__, 'required_alt_text' ) );	// Script for validate Alt text from Add media button
 	}
 
-	function settings_cpt_init() {
+	public function required_alt_text() {
+		global $typenow, $pagenow;
+
+		if ( in_array( $pagenow, array( 'post.php', 'post-new.php' ) ) ) { ?>
+			<script type="text/javascript">
+				jQuery(document).ready(function($) {
+					jQuery('button#insert-media-button').click(function(e){
+						setTimeout(function(e){
+							var button_insert = jQuery('.media-button-insert');
+							clickListener = jQuery._data(button_insert[0], 'events').click[0];
+							button_insert.off('click');
+							jQuery('.media-button-insert').click(function(e){
+								var alt_text = jQuery('input#attachment-details-alt-text').val();
+								// console.log(alt_text);
+								if ( alt_text )
+								{
+									$('.media-button-insert').unbind("click");
+									button_insert.click(clickListener.handler);
+									button_insert.triggerHandler('click');
+								} else {
+									alert( 'ERROR: Please fill the Alt text.' );
+									jQuery('input#attachment-details-alt-text').focus();
+									return false;
+								}
+							});
+						},500);
+					});
+				});
+			</script>
+		<?php }
+	}
+
+	public static function settings_cpt_init() {
 		// Register custom capability for Draft Kings On/Off Setting.
 		$roles = [ 'administrator' ];
 
@@ -40,7 +73,7 @@ class CommonSettings {
 	 */
 	public static function enqueue_scripts() {
 		global $typenow, $pagenow;
-		
+
 		if ( in_array( $typenow, CommonSettings::allow_fontawesome_posttype_list() ) && in_array( $pagenow, array( 'post.php', 'post-new.php' ) ) ) {
 			$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 			wp_register_style('general-font-awesome',GENERAL_SETTINGS_CPT_URL . "assets/css/general-font-awesome". $postfix .".css", array(), GENERAL_SETTINGS_CPT_VERSION, 'all');
