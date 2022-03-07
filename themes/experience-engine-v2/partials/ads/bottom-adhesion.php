43168350f2442e3b6c16afd3b297368bbb5040bc
mikepersico <mike.persico@bbgi.com>
2022-03-07 14:26:25 -0500

    Remove Tuner And Ad Bottom Adhesion Ad.

---
+++ themes/experience-engine-v2/partials/ads/bottom-adhesion.php
@@ -0,0 +1,6 @@
+<?php
+
+
+?><div id="bottom-adhesion-container" class="bottom-adhesion-container -ad -centered">
+	<div id="div-bottom-adhesion-slot" class="placeholder placeholder-dfp"></div>
+</div>
