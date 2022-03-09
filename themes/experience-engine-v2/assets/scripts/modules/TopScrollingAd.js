4ceb7ad7b6475135adfda99a4d46d67527db315f
mikepersico <mike.persico@bbgi.com>
2022-03-09 11:46:08 -0500

    Fix Console Log Typo.

--- themes/experience-engine-v2/assets/scripts/modules/TopScrollingAd.js
+++ themes/experience-engine-v2/assets/scripts/modules/TopScrollingAd.js
@@ -4,7 +4,7 @@ import Dfp from '../components/content/embeds/Dfp';
 import ErrorBoundary from '../components/ErrorBoundary';
 
 const TopScrollingAd = () => {
-	console.log('FIRED Topscrolling');
+	console.log('FIRED TopScrollingAd');
 
 	const [pageURL] = document.location.href;
 	// this id is also compared in /assets/scripts/components/content/embeds/Dfp.js
