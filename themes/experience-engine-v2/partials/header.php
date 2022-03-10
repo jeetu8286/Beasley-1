b176beda8db1373dca40a38ab66ccc6ab8508758
Rupesh Jorkar <rupesh.jorkar@c-metric.com>
2022-03-10 13:03:08 +0530

    CSS and code changes as per the feedback list on 9th March by Chris

--- themes/experience-engine-v2/partials/header.php
+++ themes/experience-engine-v2/partials/header.php
@@ -6,10 +6,12 @@
 		<div class="top-header">
 			<div class="brand-logo">
 				<div class="logo" itemscope itemtype="http://schema.org/Organization">
-						<?php ee_the_custom_logo( 154, 88 ); ?>
-						<?php // ee_the_subheader_logo( 'desktop', 154, 88 ); ?>
-						<?php // ee_the_subheader_logo( 'mobile', 462, 88 ); ?>
-						<span class="screen-reader-text"><?php wp_title(); ?></span>
+					<?php ee_the_custom_logo( 154, 88 ); ?>
+					<span class="screen-reader-text"><?php wp_title(); ?></span>
+				</div>
+				<div class="additional-logos">
+					<?php ee_the_subheader_logo( 'desktop'); ?>
+					<?php ee_the_subheader_logo( 'mobile'); ?>
 				</div>
 			</div>
 
