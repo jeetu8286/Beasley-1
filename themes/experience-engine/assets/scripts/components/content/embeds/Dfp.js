da85758886baaa4afcea726d810fa470f3fd3cc1
mikepersico <mike.persico@bbgi.com>
2022-04-19 12:34:38 -0400

    Remove most of GAM console logs.

--- themes/experience-engine/assets/scripts/components/content/embeds/Dfp.js
+++ themes/experience-engine/assets/scripts/components/content/embeds/Dfp.js
@@ -83,16 +83,16 @@ const slotRenderEndedHandler = event => {
 
 	const placeholder = slot.getSlotElementId();
 
-	console.log(
-		`slotRenderEndedHandler for ${slot.getAdUnitPath()}(${placeholder}) with line item: ${lineItemId} of size: ${size}`,
-	);
+	// console.log(
+	// 	`slotRenderEndedHandler for ${slot.getAdUnitPath()}(${placeholder}) with line item: ${lineItemId} of size: ${size}`,
+	// );
 
 	// FOR DEBUG - LOG TARGETING
-	const pbTargetKeys = slot.getTargetingKeys();
-	console.log(`Slot Keys Of Rendered Ad`);
-	pbTargetKeys.forEach(pbtk => {
-		console.log(`${pbtk}: ${slot.getTargeting(pbtk)}`);
-	});
+	// const pbTargetKeys = slot.getTargetingKeys();
+	// console.log(`Slot Keys Of Rendered Ad`);
+	// pbTargetKeys.forEach(pbtk => {
+	//	console.log(`${pbtk}: ${slot.getTargeting(pbtk)}`);
+	// });
 
 	if (placeholder && isNotPlayerOrInterstitial(placeholder)) {
 		const slotElement = document.getElementById(placeholder);
@@ -109,12 +109,12 @@ const slotRenderEndedHandler = event => {
 			let adSize;
 			if (size && size.length === 2 && (size[0] !== 1 || size[1] !== 1)) {
 				adSize = size;
-				console.log(`Prebid Ad Not Shown - Using Size: ${adSize}`);
+				// console.log(`Prebid Ad Not Shown - Using Size: ${adSize}`);
 			} else if (slot.getTargeting('hb_size')) {
 				// We ASSUME when an incomplete size is sent through event, we are dealing with Prebid.
 				// Compute Size From hb_size.
 				const hbSizeString = slot.getTargeting('hb_size').toString();
-				console.log(`Prebid Sizestring: ${hbSizeString}`);
+				// console.log(`Prebid Sizestring: ${hbSizeString}`);
 				const idxOfX = hbSizeString.toLowerCase().indexOf('x');
 				if (idxOfX > -1) {
 					const widthString = hbSizeString.substr(0, idxOfX);
@@ -133,11 +133,11 @@ const slotRenderEndedHandler = event => {
 						.toString()
 						.trim()
 				) {
-					console.log(
-						`PREBID AD SHOWN - ${slot.getTargeting(
-							'hb_bidder',
-						)} - ${slot.getAdUnitPath()} - ${slot.getTargeting('hb_pb')}`,
-					);
+					// console.log(
+					//	`PREBID AD SHOWN - ${slot.getTargeting(
+					//		'hb_bidder',
+					//	)} - ${slot.getAdUnitPath()} - ${slot.getTargeting('hb_pb')}`,
+					// );
 
 					try {
 						window.ga('send', {
@@ -232,7 +232,7 @@ class Dfp extends PureComponent {
 		const isAffiliateMarketingPage = this.isAffiliateMarketingPage(pageURL);
 
 		const adjustedUnitId = this.getAdjustedUnitId(unitId, unitName, pageURL);
-		console.log(`Adjusted Ad Unit: ${adjustedUnitId}`);
+		// console.log(`Adjusted Ad Unit: ${adjustedUnitId}`);
 
 		// Initialize State. NOTE: Ensure that Minimum Poll Intervavl Is Much Longer Than
 		// 	Round Trip to Ad Server. Initially we enforce 5 second minimum.
