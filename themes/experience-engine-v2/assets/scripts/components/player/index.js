92c3e53cbd243f98582020534f12381609841374
Surjit Vala <surjit.vala@bbgi.com>
2022-05-19 12:54:23 +0530

    Add volume control feature in Listen Live drop-down

--- themes/experience-engine-v2/assets/scripts/components/player/index.js
+++ themes/experience-engine-v2/assets/scripts/components/player/index.js
@@ -3,3 +3,4 @@ export { default as ControlsV2 } from './ControlsV2';
 export { default as Offline } from './Offline';
 export { default as Stations } from './Stations';
 export { default as GamPreroll } from './GamPreroll';
+export { default as Volume } from './Volume';
