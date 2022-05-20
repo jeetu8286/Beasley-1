7937c1b553cade5a369a91fb18ce4f42defacbcb
mikepersico <mike.persico@bbgi.com>
2022-05-20 14:16:33 -0400

    Hide Dropdown Ad on non-delivery.

--- themes/experience-engine-v2/assets/scripts/components/content/embeds/Dfp.js
+++ themes/experience-engine-v2/assets/scripts/components/content/embeds/Dfp.js
@@ -7,6 +7,7 @@ const playerSponsorDivID = 'div-gpt-ad-1487117572008-0';
 const interstitialDivID = 'div-gpt-ad-1484200509775-3';
 const topScrollingDivID = 'div-top-scrolling-slot';
 const bottomAdhesionDivID = 'div-bottom-adhesion-slot';
+const dropDownDivID = 'div-drop-down-slot';
 
 const isNotSponsorOrInterstitial = placeholder => {
 	return (
@@ -138,6 +139,13 @@ const slotRenderEndedHandler = event => {
 		const slotElement = document.getElementById(placeholder);
 		if (isEmpty) {
 			console.log('Empty Ad Returned');
+			// DropDown Ads Should Not Retain Their Realestate
+			if (placeholder === dropDownDivID) {
+				if (slotElement) {
+					slotElement.style.height = 0;
+				}
+			}
+
 			// If Slot Is Visible
 			if (slotElement.offsetParent !== null) {
 				// Trick Slot to pull new Ad on next poll.
