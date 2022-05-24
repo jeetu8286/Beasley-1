5d467f88ece120de34995aa2a726e8ccc7de2970
riddhi.panchal <riddhi.panchal@bbgi.com>
2022-05-24 17:49:19 +0530

    Add Custom header cache tag for wordpress page API. Change header Array

--- mu-plugins/classes/Bbgi/Endpoints/Page.php
+++ mu-plugins/classes/Bbgi/Endpoints/Page.php
@@ -109,8 +109,7 @@ class Page extends Module {
 			$response['redirect']['url']      = $is_absolute ? $matched_redirect : home_url( $matched_redirect );
 			$response['redirect']['internal'] = ! $is_absolute;
 			$response['status']               = 301;
-			$response['headers'] = ['Cache-Tag' => 'testing',
-				'X-Cache-BBGI-Tag' => 'testing for API'];
+			$response['headers']['X-Cache-BBGI-Tag'] = 'testing for API';
 		}
 
 		// only fetch page if there's no redirect
