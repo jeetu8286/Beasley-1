44b3105af5411786e7545abb59eb3300b90b8669
riddhi.panchal <riddhi.panchal@bbgi.com>
2022-05-31 17:43:54 +0530

    Remove header from the  redirect page.

--- mu-plugins/classes/Bbgi/Endpoints/Page.php
+++ mu-plugins/classes/Bbgi/Endpoints/Page.php
@@ -103,7 +103,7 @@ class Page extends Module {
 
 		if ( $matched_redirect ) {
 			$requestHeaders = apache_request_headers($url);
- print_r($requestHeaders);
+ print_r($requestHeaders.'test');
 			$is_absolute = $this->is_absolute_url( $matched_redirect );
 			$response['redirect']['url']      = $is_absolute ? $matched_redirect : home_url( $matched_redirect );
 			$response['redirect']['internal'] = ! $is_absolute;
