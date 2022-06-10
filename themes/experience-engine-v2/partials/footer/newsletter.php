a60db2fb23faa273dabeb0676efee6e21e647cef
Surjit Vala <surjit.vala@bbgi.com>
2022-06-10 15:32:26 +0530

    Remove newsletter note for MustHavesandFunFinds.com

--- themes/experience-engine-v2/partials/footer/newsletter.php
+++ themes/experience-engine-v2/partials/footer/newsletter.php
@@ -1,7 +1,9 @@
 <?php if ( ee_has_newsletter_page() ) : ?>
 	<div class="newsletter">
 		<h6>Newsletter sign up</h6>
-		<p>Don't miss on pre-sales, member-only contests and member only events.</p>
+		<?php if ( stripos(get_site_url(),"musthavesandfunfinds") == false ) : ?>
+			<p>Don't miss on pre-sales, member-only contests and member only events.</p>
+		<?php endif; ?>
 		<a class="btn -square -empty -secondary" href="<?php ee_the_newsletter_page_permalink(); ?>">
 			<?php bloginfo( 'name' ) ?> Newsletter
 		</a>
