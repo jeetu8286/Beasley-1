1aea95215b9b3dbc96a9b070c6519ba27a890863
Rupesh Jorkar <rupesh.jorkar@c-metric.com>
2022-06-10 18:55:00 +0530

    Update code for post type

--- plugins/cpt-general-settings/includes/dashboard-activity.php
+++ plugins/cpt-general-settings/includes/dashboard-activity.php
@@ -10,15 +10,15 @@ class DashboardActivity {
 		add_action( 'admin_init', array( $this, 'wp_admin_init' ), 1 );
 	}
 	public function wp_admin_init() {
-		add_action( 'save_post_post', array( $this, 'remove_dashboard_activity_cache_result'), 10, 3 );
-		add_action( 'save_post_gmr_gallery', array( $this, 'remove_dashboard_activity_cache_result'), 10, 3 );
-		add_action( 'save_post_listicle_cpt', array( $this, 'remove_dashboard_activity_cache_result'), 10, 3 );
+		// add_action( 'save_post_post', array( $this, 'remove_dashboard_activity_cache_result'), 10, 3 );
+		// add_action( 'save_post_gmr_gallery', array( $this, 'remove_dashboard_activity_cache_result'), 10, 3 );
+		// add_action( 'save_post_listicle_cpt', array( $this, 'remove_dashboard_activity_cache_result'), 10, 3 );
 		add_action( 'save_post_affiliate_marketing', array( $this, 'remove_dashboard_activity_cache_result'), 10, 3 );
 	}
 	public function remove_dashboard_activity_cache_result( $post_id, $post, $update ) {
 		$found_dashboard	= false;
 		$key				= md5('bbgi_recent_published_posts');
-		wp_cache_get( $key, 'bbgi', false, $found_dashboard );
+		$cachedata			= wp_cache_get( $key, 'bbgi', false, $found_dashboard );
 		if ( $found_dashboard ) {
 			wp_cache_delete($key, 'bbgi');
 		}
