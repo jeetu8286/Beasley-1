f0f124c9a905a8a968615a5f98bcea1d96384908
shahzaib.mushtaq <shahzaibmushtaq@10up.com>
2022-06-13 23:08:30 +0500

    add slack alert in deploy-prod.sh

--- deploy-scripts/deploy-prod.sh
+++ deploy-scripts/deploy-prod.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+SLACK_WEBHOOK="https://hooks.slack.com/services/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"
+
 # Print commands to the screen
 set -x
+
+# Send Slack Notification about Pipeline deployment on production
+curl -X POST -H 'Content-type: application/json' --data "{\"username\":\"Gitlab Pipeline\", \"text\":\"Gitlab pipeline is running the Production Deployment.\"}" $SLACK_WEBHOOK
 
 # Catch Errors
 set -euo pipefail
