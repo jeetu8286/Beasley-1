bc41b62386328ede237ac4b63e363c2c4c457cb1
riddhi.panchal <riddhi.panchal@bbgi.com>
2022-06-24 15:43:25 +0530

    Print response content in purge API

--- mu-plugins/classes/Bbgi/Integration/ExperienceEngine.php
+++ mu-plugins/classes/Bbgi/Integration/ExperienceEngine.php
@@ -339,8 +339,10 @@ class ExperienceEngine extends \Bbgi\Module {
 		//clear ee content feed values
 		$publisher = $this->_get_publisher_key();
 		$url = "experience/channels/{$publisher}/feeds/content/";
-		wp_cache_delete($url, 'experience_engine_api-ee_data');
-
+		// wp_cache_delete($url, 'experience_engine_api-ee_data');
+		$content = $request->get_body();
+	print_R($content);exit;
+		wp_cache_set($url, $content, 'experience_engine_api-ee_data', 120);
 		// Clear specific page caches
 		if ( function_exists( 'batcache_clear_url' ) && class_exists( 'batcache' ) ) {
 			$home = trailingslashit( get_option( 'home' ) );
