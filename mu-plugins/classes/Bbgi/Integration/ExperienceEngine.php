d7ed3d07fa0ea162de557963cbdbbd5d8e35cfe0
kevingilper <dpc12pkd3blindmice!>
2022-06-24 10:42:43 -0400

    debugging message for staging

--- mu-plugins/classes/Bbgi/Integration/ExperienceEngine.php
+++ mu-plugins/classes/Bbgi/Integration/ExperienceEngine.php
@@ -336,6 +336,8 @@ class ExperienceEngine extends \Bbgi\Module {
 	}
 
 	public function rest_purge_cache( \WP_REST_Request $request ) {
+		error_log( 'purpleturtle' );
+
 		//clear ee content feed values
 		$publisher = $this->_get_publisher_key();
 		$url = "experience/channels/{$publisher}/feeds/content/";
@@ -343,8 +345,6 @@ class ExperienceEngine extends \Bbgi\Module {
 		$content = $request->get_json_params();
 
 		wp_cache_set($url, $content, 'experience_engine_api-ee_data', 120);
-
-		error_log( 'purplemoose' );
 
 		// Clear specific page caches
 		if ( function_exists( 'batcache_clear_url' ) && class_exists( 'batcache' ) ) {
