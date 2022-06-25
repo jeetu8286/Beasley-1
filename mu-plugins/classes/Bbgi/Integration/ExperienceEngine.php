04de988b59b0bb345545b40547ce17e5f7bffdec
kevingilper <dpc12pkd3blindmice!>
2022-06-25 00:13:19 -0400

    Additional Debugging

--- mu-plugins/classes/Bbgi/Integration/ExperienceEngine.php
+++ mu-plugins/classes/Bbgi/Integration/ExperienceEngine.php
@@ -347,7 +347,7 @@ class ExperienceEngine extends \Bbgi\Module {
 		if ($request->is_json_content_type()) {
 			error_log( $this->log_prefix() . 'received json' );
 			$content = $request->get_body();
-			return rest_ensure_response( wp_json_encode($content) );
+			return wp_send_json($content);
 		}
 	}
 
