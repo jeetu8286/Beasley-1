c060a4e3f55998d7637c23e0f0ebfbf1a180ffae
Rupesh Jorkar <rupesh.jorkar@c-metric.com>
2022-07-06 16:18:21 +0530

    update code for confliction

--- plugins/cpt-general-settings/includes/rendering.php
+++ plugins/cpt-general-settings/includes/rendering.php
@@ -9,6 +9,7 @@ class GeneralSettingsFrontRendering {
 		// Register scripts
 		// add_action( 'wp_enqueue_scripts', array( __CLASS__, 'register_scripts' ), 1 );
 		add_action('pre_get_posts', array( __CLASS__, 'author_pre_get_posts') );
+
 		add_action( 'template_redirect', array( __CLASS__,'show_404_for_disabled_feeds' ) );
 	}
 	function show_404_for_disabled_feeds() {
