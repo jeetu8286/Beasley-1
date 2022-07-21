b759ace2e16123045b76a4b9398dd29b6edf74f1
mikepersico <mike.persico@bbgi.com>
2022-07-21 08:51:29 -0400

    Mark ads as shown after refresh.

--- themes/experience-engine-v2/assets/scripts/redux/utilities/screen/refreshAllAds.js
+++ themes/experience-engine-v2/assets/scripts/redux/utilities/screen/refreshAllAds.js
@@ -55,9 +55,8 @@ export default function refreshAllAds() {
 					googletag.pubads().refresh([...slotList.values()]);
 				}
 				// Mark Slots as shown
-				statsObjKeysToRefresh.map(statsObj => {
-					statsObj.shouldRefresh = false;
-					return statsObj;
+				statsObjKeysToRefresh.forEach(statsKey => {
+					statsCollectionObject[statsKey].shouldRefresh = false;
 				});
 			}
 		}
