c7bee42474f7f1ddc604359388b23b0ecdfd2ee4
mikepersico <mike.persico@bbgi.com>
2022-07-28 09:29:19 -0400

    Add explicit hieght and width for footer images. Set content width to 100%.

--- themes/experience-engine-v2/partials/footer/download-app.php
+++ themes/experience-engine-v2/partials/footer/download-app.php
@@ -5,13 +5,13 @@
 
 		<?php if ( ee_has_publisher_information( 'ios' ) ) : ?>
 			<a href="<?php echo esc_url( ee_get_publisher_information( 'ios' ) ); ?>" aria-label="Download on the App Store" target="_blank" rel="noopener">
-				<img src="<?php echo get_template_directory_uri() ?>/assets/images/itunes.svg" alt="Download on the App Store">
+				<img src="<?php echo get_template_directory_uri() ?>/assets/images/itunes.svg" height="57px" width="170px" alt="Download on the App Store">
 			</a>
 		<?php endif; ?>
 
 		<?php if ( ee_has_publisher_information( 'android' ) ) : ?>
 			<a href="<?php echo esc_url( ee_get_publisher_information( 'android' ) ); ?>" aria-label="Download on the Google Play" target="_blank" rel="noopener">
-				<img src="<?php echo get_template_directory_uri() ?>/assets/images/googleplay.svg" alt="Download on the Google Play">
+				<img src="<?php echo get_template_directory_uri() ?>/assets/images/googleplay.svg" height="57px" width="170px" alt="Download on the Google Play">
 			</a>
 		<?php endif; ?>
 	</div>
