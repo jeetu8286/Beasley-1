57c9f570aac8c16d282614bb59f0e1a9cefd4eb4
riddhi.panchal <riddhi.panchal@bbgi.com>
2022-08-04 13:44:37 +0530

    Working on error for array_walk function

--- themes/experience-engine-v2/header.php
+++ themes/experience-engine-v2/header.php
@@ -12,8 +12,9 @@ use Bbgi\Integration\Google;
 		$current_url = home_url( add_query_arg( array(), $wp->request ) );
 		$categories = get_the_category( $post );
 		$categoriesSlug = wp_list_pluck($categories, 'slug' );
-		error_log('IN the archive part part of header');
+
 		array_walk($categoriesSlug, function ($value, $key) use ($current_url, &$headerCacheTag){
+			error_log('IN the archive part part of header'.implode(',',$value));
 //			if(str_contains($current_url, $value)) {
 //				error_log('IN the archive part part of header-'.$value);
 //				$headerCacheTag[] =   "archive" . "-" . $value;
