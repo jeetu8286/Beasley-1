b4f2f1438a75df945b0c2b7d647574fdfb117634
Rupesh Jorkar <rupesh.jorkar@c-metric.com>
2022-08-05 15:11:33 +0530

    Add code for Whiz-View Contest rules

---
+++ plugins/cpt-general-settings/includes/whiz-changes.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Class WhizChanges
+ */
+class WhizChanges {
+    /**
+	 * Hook into the appropriate actions when the class is constructed.
+	 */
+	function __construct()
+	{
+		$this->init();
+	}
+	public function init() {
+		if( $this->ee_is_common_mobile_thumbnail() ) {
+			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
+        }
+	}
+
+    /**
+	 * Enqueues scripts and styles.
+	 */
+	public static function enqueue_scripts() {
+		$min = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
+        wp_enqueue_script( 'thumbnail-video-jacapps', GENERAL_SETTINGS_CPT_URL . "assets/js/whiz_script{$min}.js", array('jquery'), GENERAL_SETTINGS_CPT_VERSION, true);
+	}
+
+    public function ee_is_common_mobile_thumbnail() {
+		static $jacapps_pos = null,
+				$whiz_pos = null;
+
+		if ( $jacapps_pos === null ) {
+			$jacapps_pos = stripos( $_SERVER['HTTP_USER_AGENT'], 'jacapps' );
+
+			// Allow way to toggle jacapps through URL querystring
+			if ( isset( $_GET['jacapps'] ) ) {
+				$jacapps_pos = 1;
+			}
+		}
+		if($whiz_pos === null ) {
+			$whiz_pos = stripos( $_SERVER['HTTP_USER_AGENT'], 'whiz' );
+
+			// Allow way to toggle whiz through URL querystring
+			if ( isset( $_GET['whiz'] ) ) {
+				$whiz_pos = 1;
+			}
+		}
+		return false !== $jacapps_pos || false !== $whiz_pos;
+	}
+}
+
+// WhizChanges::init();
+new WhizChanges();
