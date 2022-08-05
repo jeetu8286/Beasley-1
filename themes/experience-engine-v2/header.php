8d36786804285abc8568f6a0ec4d941ebbf93d77
riddhi.panchal <riddhi.panchal@bbgi.com>
2022-08-05 12:09:47 +0530

    Add multiple categories tags on header

--- themes/experience-engine-v2/header.php
+++ themes/experience-engine-v2/header.php
@@ -8,20 +8,32 @@ use Bbgi\Integration\Google;
 		$headerCacheTag[] = $_SERVER['HTTP_HOST'].'-'.'home';
 	} else if (is_archive()) {
 		global $wp;
+		$urlCatArray = [];
+
 		$current_url = home_url( add_query_arg( array(), $wp->request ) );
+		$urlArray = wp_parse_url($current_url);
+
 		$categories = get_categories();
 		$categoriesSlug = wp_list_pluck($categories, 'slug' );
 
-		array_walk($categoriesSlug, function ($value, $key) use ($current_url, &$headerCacheTag){
-			if(strpos($current_url, $value)) {
+		if($urlArray['path']){
+			$pathArray = explode('/',$urlArray['path']);
+			foreach($pathArray as $key=> $val){
+				if(empty($val) || $val == 'category' || sizeof($urlCatArray) > 0){
+					unset($pathArray[$key]);
+					continue;
+				}
+				$urlCatArray =  explode(',',$val);
+			}
+		}
+
+		array_walk($categoriesSlug, function ($value, $key) use ($urlCatArray, &$headerCacheTag){
+			if(in_array($value,$urlCatArray)) {
+				error_log('IN the archive part part of header-'.$value);
 				$headerCacheTag[] =   "archive" . "-" . $value;
 			}
 		});
 
-		if (isset($wp_query->query['post_type'])) {
-			$headerCacheTag[] = "archive-" . $wp_query->query['post_type'];
-			$headerCacheTag[] = $wp_query->query['post_type'];
-		}
 	}  else {
 		$currentPostType	= "";
 		$currentPostSlug	= "";
