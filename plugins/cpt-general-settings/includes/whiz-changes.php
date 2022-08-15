8305822dfa1a929724a6822b188fc00c259af15b
kevingilper <dpc12pkd3blindmice!>
2022-08-15 06:32:28 -0400

    Wrong file path

--- plugins/cpt-general-settings/includes/whiz-changes.php
+++ plugins/cpt-general-settings/includes/whiz-changes.php
@@ -22,7 +22,7 @@ class WhizChanges {
 	public static function enqueue_scripts() {
 		$min = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
         wp_enqueue_script( 'general-whiz-script', GENERAL_SETTINGS_CPT_URL . "assets/js/whiz_script{$min}.js", array('jquery'), GENERAL_SETTINGS_CPT_VERSION, true);
-        wp_enqueue_script( 'show-on-device', GENERAL_SETTINGS_CPT_URL . "assets/js/show_on_device{$min}.js",[],GENERAL_SETTGENERAL_SETTINGS_CPT_VERSION, true);
+        wp_enqueue_script( 'show-on-device', GENERAL_SETTINGS_CPT_URL . "assets/js/show_on_device_client{$min}.js",[],GENERAL_SETTGENERAL_SETTINGS_CPT_VERSION, true);
 	}
 
     public function ee_is_common_mobile_thumbnail() {
