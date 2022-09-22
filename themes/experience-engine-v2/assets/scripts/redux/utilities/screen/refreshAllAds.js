efad68f46e8f000440c7278c51ac60668ae2f6ba
mikepersico <mike.persico@bbgi.com>
2022-09-22 12:24:20 -0400

    Reset Top Ad.

--- themes/experience-engine-v2/assets/scripts/redux/utilities/screen/refreshAllAds.js
+++ themes/experience-engine-v2/assets/scripts/redux/utilities/screen/refreshAllAds.js
@@ -1,10 +1,13 @@
 import {
 	doPubadsRefreshForAllRegisteredAds,
-	// hidePlaceholder,
-	// topScrollingDivID,
+	hidePlaceholder,
+	topScrollingDivID,
 } from '../../../library/ad-utils';
 
 export default function refreshAllAds() {
+	window.topAdsShown = 0; // Reset Header Ad Counter
+	hidePlaceholder(topScrollingDivID);
+
 	const { googletag } = window;
 	googletag.cmd.push(() => {
 		// googletag.pubads().refresh();
