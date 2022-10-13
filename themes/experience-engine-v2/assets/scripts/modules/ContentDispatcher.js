2f1c1c9dba4f73f8e0fcd36863aa574e5c8df125
mikepersico <mike.persico@bbgi.com>
2022-10-13 12:22:54 -0400

    Update ContentDispatcher to match Staging.

--- themes/experience-engine-v2/assets/scripts/modules/ContentDispatcher.js
+++ themes/experience-engine-v2/assets/scripts/modules/ContentDispatcher.js
@@ -25,7 +25,7 @@ class ContentDispatcher extends Component {
 		this.onClick = this.handleClick.bind(this);
 		this.handleSliders = this.handleSliders.bind(this);
 		this.handleSliderLoad = this.handleSliderLoad.bind(this);
-		this.onPageHistoryPop = this.onPageHistoryPop.bind(this);
+		this.handlePageHistoryPop = this.handlePageHistoryPop.bind(this);
 	}
 
 	/**
@@ -35,11 +35,7 @@ class ContentDispatcher extends Component {
 		const { initPage } = this.props;
 
 		window.addEventListener('click', this.onClick);
-		// a zero timeout ensures that the callback runs when the new history state is in place.
-		// https://developer.mozilla.org/en-US/docs/Web/API/Window/popstate_event
-		window.addEventListener('popstate', () =>
-			setTimeout(this.onPageHistoryPop, 0),
-		);
+		window.addEventListener('popstate', this.handlePageHistoryPop);
 
 		// load current page into the state
 		initPage();
@@ -63,7 +59,7 @@ class ContentDispatcher extends Component {
 
 	componentWillUnmount() {
 		window.removeEventListener('click', this.onClick);
-		window.removeEventListener('popstate', this.onPageHistoryPop);
+		window.removeEventListener('popstate', this.handlePageHistoryPop);
 	}
 
 	/**
@@ -196,32 +192,14 @@ class ContentDispatcher extends Component {
 		this.loadPage(link);
 	}
 
-	onPageHistoryPop() {
-		/* 2022-08-17 - THIS CODE BLOCK WAS BUILT ON FALSE PREMISE THAT SECOND STREET ONLY MODIFIED HISTORY LIKE:
-		 *   window.history.pushState(null,'','#//');
-		 * Unfortunately This Code Did Not Work Well With Actual Second Street behavior.
-
-		const lastCanonicalUrl = getBeasleyCanonicalUrl();
-		console.log(
-			`BACK - Canonical: ${lastCanonicalUrl} Current: ${window.location.href}`,
-		);
-
-		if (window.location.href.replace('#//', '') === lastCanonicalUrl) {
-			console.log(`Current Matched Canonical - doubling back`);
-			window.history.back();
-		} else if (window.location.href.indexOf('#') > -1) {
-			console.log('Found # - doubling back');
-			window.history.back();
-		} else {
-			console.log(`Back caused load of ${window.location.href}`);
-			this.loadPage(window.location.href, { suppressHistory: true });
-		}
-		*/
-
-		if (window.location.href.indexOf('#') === -1) {
-			console.log(`History Pop - loading - ${window.location.href}`);
-			this.loadPage(window.location.href, { suppressHistory: true });
-		}
+	handlePageHistoryPop() {
+		// a zero timeout ensures that the callback runs when the new history state is in place.
+		// https://developer.mozilla.org/en-US/docs/Web/API/Window/popstate_event
+		setTimeout(() => {
+			if (window.location.href.indexOf('#') === -1) {
+				this.loadPage(window.location.href, { suppressHistory: true });
+			}
+		}, 0);
 	}
 
 	/**
