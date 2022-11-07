f6a4c97de358e7f2e5628e74d07c19c6dbac64d1
shahzaib cb <shahzaib.mushtaq@get10up.com>
2022-11-07 23:04:15 +0500

    update preprod instance ip to EIP

--- deploy-scripts/deploy-preprod.sh
+++ deploy-scripts/deploy-preprod.sh
@@ -6,6 +6,6 @@ set -x
 # Catch Errors
 set -euo pipefail
 
-rsync -vrxc --delay-updates --delete-after ./ beanstalk@52.87.184.224:/var/www/html/wordpress/wp-content/ --exclude-from=./deploy-scripts/rsync-excludes.txt
+rsync -vrxc --delay-updates --delete-after ./ beanstalk@34.230.103.178:/var/www/html/wordpress/wp-content/ --exclude-from=./deploy-scripts/rsync-excludes.txt
 
 set +x
