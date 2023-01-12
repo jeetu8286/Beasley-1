8e995f52925d5e6abc2de15c75c1fb74f9310147
Douglas Barahona <douglas.barahona@get10up.com>
2023-01-12 10:49:51 -0600

    Reverted stage IP in CICD

--- deploy-scripts/deploy-stage.sh
+++ deploy-scripts/deploy-stage.sh
@@ -6,6 +6,6 @@ set -x
 # Catch Errors
 set -euo pipefail
 
-rsync -vrxc --delay-updates --delete-after ./payload/ beanstalk@34.195.141.227:/var/www/html/wordpress/wp-content/ --exclude-from=./deploy-scripts/rsync-excludes.txt
+rsync -vrxc --delay-updates --delete-after ./payload/ beanstalk@54.87.4.54:/var/www/html/wordpress/wp-content/ --exclude-from=./deploy-scripts/rsync-excludes.txt
 
 set +x
