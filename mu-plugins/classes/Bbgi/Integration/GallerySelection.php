6ada725ba83f7d9b9894cecb069aaf8f2ca66851
Surjit Vala <surjit.vala@bbgi.com>
2023-02-08 16:59:44 +0530

    Fix indexing issue

--- mu-plugins/classes/Bbgi/Integration/GallerySelection.php
+++ mu-plugins/classes/Bbgi/Integration/GallerySelection.php
@@ -85,9 +85,7 @@ class GallerySelection extends \Bbgi\Module {
 		$ids = $this->get_attachment_ids_for_post( $gallery_id, $attributes['syndication_name'] );
 
 		$gallery_object = get_post( $gallery_id );
-		remove_filter( 'the_content', 'ee_add_ads_to_content', 100 );
 		$content = apply_filters( 'bbgi_gallery_cotnent', false, $post, $ids );
-		add_filter( 'the_content', 'ee_add_ads_to_content', 100 );
 		if ( ! empty( $content ) ) {
 			$content_updated = "<h2 class=\"section-head\"><span>".$gallery_object->post_title."</span></h2>";
 			if( !empty( $attributes['description'] ) &&  ($attributes['description'] == 'yes') ) {
