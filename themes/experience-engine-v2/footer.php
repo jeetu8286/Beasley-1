030d2df560173bf2230c5731ef582aa5249c65e3
mikepersico <mike.persico@bbgi.com>
2023-03-09 16:13:37 -0500

    Add a stub for link click mparticle event.

--- themes/experience-engine-v2/footer.php
+++ themes/experience-engine-v2/footer.php
@@ -1,6 +1,26 @@
 					</div>
 				<?php
 					get_template_part( 'partials/ads/footer' );
+
+				$mparticle_implementation = sprintf(
+			'<script class="mparticle_implementation">
+						document.body.addEventListener(\'click\', (e) => {
+							const ev = window.event||e;
+                        	if (ev.srcElement.tagName === \'A\') {
+                        		window.beasleyanalytics.setAnalyticsForMParticle(\'container_id\', \'%s\');
+                        		window.beasleyanalytics.setAnalyticsForMParticle(\'link_name\', ev.srcElement.ariaLabel);
+								window.beasleyanalytics.setAnalyticsForMParticle(\'link_text\', ev.srcElement.innerText);
+								window.beasleyanalytics.setAnalyticsForMParticle(\'link_url\', ev.srcElement.href);
+
+								window.beasleyanalytics.sendMParticleEvent(
+									BeasleyAnalyticsMParticleProvider.mparticleEventNames.linkClicked,
+								);
+							}
+						});
+					</script>',
+						'container_id?',
+				);
+				echo $mparticle_implementation;
 				?>
 			</main><?php
 
