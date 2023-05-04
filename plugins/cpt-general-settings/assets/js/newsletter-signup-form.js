b931ed872d769ea0927e8f3d448e959d67c01142
riddhi.panchal <riddhi.panchal@bbgi.com>
2023-05-04 14:17:09 +0530

    Add dummy endpoint

--- plugins/cpt-general-settings/assets/js/newsletter-signup-form.js
+++ plugins/cpt-general-settings/assets/js/newsletter-signup-form.js
@@ -2,12 +2,18 @@
 	var $nsf_document = $(document);
 	$nsf_document.ready(function () {
 
-        $(".nsf-container #nsf-form").submit(function(event) {
+        $nsf_document.on('click', '.nsf-container #nsf-form .nsf-form-submit', function(event) {
 
             event.preventDefault();
-        
-            var name = $(".nsf-first-name").val();
-            var email = $(".nsf-email").val();
+
+            var name 						= $(".nsf-first-name").val();
+            var email 						= $(".nsf-email").val();
+            var nsf_last_name 				= $("#nsf-last-name").val();
+            var nsf_subscription_attributes = $("#nsf_subscription_attributes").val();
+            var nsf_subscription_ID 		= $("#nsf_subscription_ID").val();
+            var nsf_mailing_list_name 		= $("#nsf_mailing_list_name").val();
+            var nsf_mailing_list_description = $("#nsf_mailing_list_description").val();
+            var nsf_template_token 			= $("#nsf_template_token").val();
 
             if (name == "") {
                 $(".nsf-first-name").focus();
@@ -27,10 +33,40 @@
                 $(".nsf-email").focus();
                 return false;
             }
-           
-            this.submit();
+
+            $(this).prop('disabled', true).css('opacity', 0.5).css('cursor', 'not-allowed');
+
+            $.ajax({
+				type : 'POST',
+                dataType: 'json',
+				url : nsf_ajax_object.url,
+				data : {
+					action: 'newsletter_signup_form_data_submit',
+                    nonce: nsf_ajax_object.nonce,
+                    name : name,
+                    email : email,
+                    nsf_last_name : nsf_last_name,
+                    nsf_subscription_attributes : nsf_subscription_attributes,
+                    nsf_subscription_ID : nsf_subscription_ID,
+                    nsf_mailing_list_name : nsf_mailing_list_name,
+                    nsf_mailing_list_description : nsf_mailing_list_description,
+                    nsf_template_token : nsf_template_token,
+				},
+                beforeSend: function() {
+                    $('.nsf-container #nsf-form .nsf-form-submit').prop('disabled', true).css('opacity', 0.5).css('cursor', 'not-allowed');
+                },
+				success : function( response ) {
+					console.log(response.body);
+				},
+				error : function( error ) {
+					console.log(error);
+				},
+                complete: function() {
+                    $('.nsf-container #nsf-form .nsf-form-submit').prop('disabled', false).css('opacity', 1).css('cursor', 'pointer');
+                }
+			});
 
         });
-    
+
     });
 })(jQuery);
--- plugins/cpt-general-settings/includes/newsletter-signup-form.php
+++ plugins/cpt-general-settings/includes/newsletter-signup-form.php
@@ -12,15 +12,27 @@ class NewsletterSignupForm {
 		add_action( 'wp_enqueue_scripts', array( $this, 'nsf_register_scripts' ), 1 );
 		add_shortcode( 'nsf-show', array( $this, 'nsf_function' ) );
 		add_action( 'bbgi_register_settings', array( $this, 'nsf_register_settings' ) , 10, 2 );
+		add_action( 'wp_ajax_newsletter_signup_form_data_submit', array( $this, 'newsletter_signup_form_data_submit_action' ) );
+		add_action( 'wp_ajax_nopriv_newsletter_signup_form_data_submit', array( $this, 'newsletter_signup_form_data_submit_action' ) );
 	}
 	public function nsf_register_scripts() {
 		//Script for front end
+		$nsf_ajax_nonce = wp_create_nonce( 'nsf-ajax-nonce' );
 		$postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
 
 		wp_register_style('nsf-style',GENERAL_SETTINGS_CPT_URL . "assets/css/newsletter-signup-form". $postfix .".css", array(), GENERAL_SETTINGS_CPT_VERSION, 'all');
 		wp_enqueue_style('nsf-style');
 
-		wp_register_script('nsf-script', GENERAL_SETTINGS_CPT_URL . 'assets/js/newsletter-signup-form'. $postfix .'.js', array('jquery'), '1.0');
+		// wp_register_script('nsf-script', GENERAL_SETTINGS_CPT_URL . 'assets/js/newsletter-signup-form'. $postfix .'.js', array('jquery'), '1.0');
+		wp_register_script('nsf-script', GENERAL_SETTINGS_CPT_URL . 'assets/js/newsletter-signup-form.js', array('jquery'), '1.0');
+		wp_localize_script( 
+			'nsf-script',
+			'nsf_ajax_object',
+			array( 
+				'url' => admin_url( 'admin-ajax.php' ),
+				'nonce'    => $nsf_ajax_nonce,
+				) 
+		);
 
         wp_enqueue_script('nsf-script');
 	}
@@ -53,15 +65,15 @@ class NewsletterSignupForm {
 			$nsf_mailing_list_description	= get_option('nsf_mailing_list_description');
 			$nsf_template_token				= get_option('nsf_template_token');
 
-			$hidden_fields .= $attr['nsf_subscription_attributes'] != '' ? '<input type="hidden" name="nsf_subscription_attributes" value="'.esc_attr($attr['nsf_subscription_attributes']).'">' : ($nsf_subscription_attributes != '' ? '<input type="hidden" name="nsf_subscription_attributes" value="'.$nsf_subscription_attributes.'">' : '');
+			$hidden_fields .= $attr['nsf_subscription_attributes'] != '' ? '<input type="hidden" name="nsf_subscription_attributes" id="nsf_subscription_attributes" value="'.esc_attr($attr['nsf_subscription_attributes']).'">' : ($nsf_subscription_attributes != '' ? '<input type="hidden" name="nsf_subscription_attributes" id="nsf_subscription_attributes" value="'.$nsf_subscription_attributes.'">' : '');
 
-			$hidden_fields .= $attr['nsf_subscription_id'] != '' ? '<input type="hidden" name="nsf_subscription_ID" value="'.esc_attr($attr['nsf_subscription_id']).'">' : ($nsf_subscription_ID != '' ? '<input type="hidden" name="nsf_subscription_ID" value="'.$nsf_subscription_ID.'">' : '');
+			$hidden_fields .= $attr['nsf_subscription_id'] != '' ? '<input type="hidden" name="nsf_subscription_ID" id="nsf_subscription_ID" value="'.esc_attr($attr['nsf_subscription_id']).'">' : ($nsf_subscription_ID != '' ? '<input type="hidden" name="nsf_subscription_ID" id="nsf_subscription_ID" value="'.$nsf_subscription_ID.'">' : '');
 
-			$hidden_fields .= $attr['nsf_mailing_list_name'] != '' ? '<input type="hidden" name="nsf_mailing_list_name" value="'.esc_attr($attr['nsf_mailing_list_name']).'">' : ($nsf_mailing_list_name != '' ? '<input type="hidden" name="nsf_mailing_list_name" value="'.$nsf_mailing_list_name.'">' : '');
+			$hidden_fields .= $attr['nsf_mailing_list_name'] != '' ? '<input type="hidden" name="nsf_mailing_list_name" id="nsf_mailing_list_name" value="'.esc_attr($attr['nsf_mailing_list_name']).'">' : ($nsf_mailing_list_name != '' ? '<input type="hidden" name="nsf_mailing_list_name" id="nsf_mailing_list_name" value="'.$nsf_mailing_list_name.'">' : '');
 
-			$hidden_fields .= $attr['nsf_mailing_list_description'] != '' ? '<input type="hidden" name="nsf_mailing_list_description" value="'.esc_attr($attr['nsf_mailing_list_description']).'">' : ($nsf_mailing_list_description != '' ? '<input type="hidden" name="nsf_mailing_list_description" value="'.$nsf_mailing_list_description.'">' : '');
+			$hidden_fields .= $attr['nsf_mailing_list_description'] != '' ? '<input type="hidden" name="nsf_mailing_list_description" id="nsf_mailing_list_description" value="'.esc_attr($attr['nsf_mailing_list_description']).'">' : ($nsf_mailing_list_description != '' ? '<input type="hidden" name="nsf_mailing_list_description" id="nsf_mailing_list_description" value="'.$nsf_mailing_list_description.'">' : '');
 
-			$hidden_fields .= $attr['nsf_template_token'] != '' ? '<input type="hidden" name="nsf_template_token" value="'.esc_attr($attr['nsf_template_token']).'">' : ($nsf_template_token != '' ? '<input type="hidden" name="nsf_template_token" value="'.$nsf_template_token.'">' : '');
+			$hidden_fields .= $attr['nsf_template_token'] != '' ? '<input type="hidden" name="nsf_template_token" id="nsf_template_token" value="'.esc_attr($attr['nsf_template_token']).'">' : ($nsf_template_token != '' ? '<input type="hidden" name="nsf_template_token" id="nsf_template_token" value="'.$nsf_template_token.'">' : '');
 
 			$html .= '<div class="nsf-container" id="root">';
 				$html .= '<div class="nsf-image-container" >';
@@ -70,18 +82,19 @@ class NewsletterSignupForm {
 				$html .= '<div class="nsf-form-container">';
 					$html .= '<h1 class="nsf-header">'.$nsf_label.'</h1>';
 					$html .= '<h2 class="nsf-subheader">'.$nsf_description.'</h2>';
-					$html .= '<form id="nsf-form" class="nsf-form" name="nsf_form" action="" method="POST">';
+					$html .= '<p style="color:red; font-size:14px;">We used dummy API CURL for now</p>';
+					$html .= '<form id="nsf-form" class="nsf-form" name="nsf_form" action="#" method="POST">';
 						$html .= $hidden_fields;
 						$html .= '<div class="nsf-input-container">';
-							$html .= '<div class="input-label"><label>First Name</label><span class="nsf-name-error">required</span></div>';
+							$html .= '<div class="input-label"><label>First Name</label><span> *</span></div>';
 							$html .= '<div class="input-field"><input type="text" name="nsf_first_name" class="nsf-first-name" /></div>';
 						$html .= '</div>';
 						$html .= '<div class="nsf-input-container">';
 							$html .= '<div class="input-label"><label>Last Name</label></div>';
-							$html .= '<div class="input-field"><input type="text" name="nsf_last_name" class="nsf-last-name" /></div>';
+							$html .= '<div class="input-field"><input type="text" name="nsf_last_name" id="nsf-last-name" class="nsf-last-name" /></div>';
 						$html .= '</div>';
 						$html .= '<div class="nsf-input-container">';
-							$html .= '<div class="input-label"><label>Email</label><span class="nsf-email-error">required</span></div>';
+							$html .= '<div class="input-label"><label>Email</label><span> *</span></div>';
 							$html .= '<div class="input-field"><input type="text" name="nsf_email" class="nsf-email" /><span class="nsf-email-error-msg"></span></div>';
 						$html .= '</div>';
 						$html .= '<div class="nsf-action-container">';
@@ -133,6 +146,42 @@ class NewsletterSignupForm {
 		register_setting( $group, 'nsf_mailing_list_name', 'sanitize_text_field' );
 		register_setting( $group, 'nsf_mailing_list_description', 'sanitize_text_field' );
 		register_setting( $group, 'nsf_template_token', 'sanitize_text_field' );
+	}
+
+	public function newsletter_signup_form_data_submit_action () {
+
+		if ( ! wp_verify_nonce( sanitize_text_field($_POST['nonce']), 'nsf-ajax-nonce' ) ) {
+			wp_send_json_error( 'Invalid Nonce.' );
+		}
+		
+		$name 							= sanitize_text_field($_POST['name']);
+		$email 							= sanitize_email($_POST['email']);
+		$nsf_last_name 					= sanitize_text_field($_POST['nsf_last_name']);
+		$nsf_subscription_attributes 	= sanitize_text_field($_POST['nsf_subscription_attributes']);
+		$nsf_subscription_ID 			= sanitize_text_field($_POST['nsf_subscription_ID']);
+		$nsf_mailing_list_name	 		= sanitize_text_field($_POST['nsf_mailing_list_name']);
+		$nsf_mailing_list_description 	= sanitize_text_field($_POST['nsf_mailing_list_description']);
+		$nsf_template_token 			= sanitize_text_field($_POST['nsf_template_token']);
+
+		// Make an HTTP POST request using wp_remote_post()
+		$response = wp_remote_post( 'https://rest.iad-05.braze.com/preference_center/v1/', array(
+			'body' => array(
+				'name' => $name,
+				'email' => $email,
+				'nsf_last_name' => $nsf_last_name,
+				'nsf_subscription_attributes' => $nsf_subscription_attributes,
+				'nsf_subscription_ID' => $nsf_subscription_ID,
+				'nsf_mailing_list_name' => $nsf_mailing_list_name,
+				'nsf_mailing_list_description' => $nsf_mailing_list_description,
+				'nsf_template_token' => $nsf_template_token,
+			),
+		) );
+		
+		if ( ! is_wp_error( $response ) ) {
+			wp_send_json($response);
+		} else {
+			wp_send_json_error( $response->get_error_message() );
+		}
 	}
 
 }
