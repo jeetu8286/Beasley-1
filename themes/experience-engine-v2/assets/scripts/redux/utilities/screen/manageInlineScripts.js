efeff1dfb2672dd840d354bb5cd46f11bb57e0dc
mikepersico <mike.persico@bbgi.com>
2023-05-08 12:28:58 -0400

    Strengthen escape routine when copying JS.

--- themes/experience-engine-v2/assets/scripts/redux/utilities/screen/manageInlineScripts.js
+++ themes/experience-engine-v2/assets/scripts/redux/utilities/screen/manageInlineScripts.js
@@ -1,11 +1,29 @@
+/* eslint-disable */
+function stringEscape(s) {
+	return s
+		? s
+				.replace(/\\/g, '\\\\')
+				.replace(/\n/g, '\\n')
+				.replace(/\t/g, '\\t')
+				.replace(/\v/g, '\\v')
+				.replace(/'/g, "\\'")
+				.replace(/"/g, '\\"')
+				.replace(/[\x00-\x1F\x80-\x9F]/g, hex)
+		: s;
+	function hex(c) { const v = '0'+c.charCodeAt(0).toString(16); return '\\x'+v.substring(v.length-2); }
+}
+/* eslint-enable */
+
 export default function manageInlineScripts(scriptArray) {
 	if (scriptArray && scriptArray.length > 0) {
 		scriptArray.forEach(inlineScript => {
 			try {
 				// eslint-disable-next-line no-eval
-				eval(inlineScript.replace(`\\`, `\\\\`));
+				eval(stringEscape(inlineScript));
 			} catch {
-				console.error(`ERROR RUNNING INLINE SCRIPT -> '${inlineScript}'`);
+				console.error(
+					`ERROR RUNNING INLINE SCRIPT. LIKELY PROBLEM WITH ESCAPE CHARS -> '${inlineScript}'`,
+				);
 			}
 		});
 	}
