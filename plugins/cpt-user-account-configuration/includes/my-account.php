410b41a053e29e1325b8abad615343e16d9db2a0
riddhi.panchal <riddhi.panchal@bbgi.com>
2023-06-13 12:37:32 +0530

    Remove commented code

--- plugins/cpt-user-account-configuration/includes/my-account.php
+++ plugins/cpt-user-account-configuration/includes/my-account.php
@@ -2,15 +2,14 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	die( "Please don't try to access this file directly." );
 }
-class 	MyAccount {
-	function __construct()
-	{
+class MyAccount {
+	function __construct(){
+
 		add_action( 'init', array( $this, 'configuration_init' ), 1 );
 		add_shortcode('cancel_account', [$this,'render_cancel_account_button']);
-
-		// Register scripts
-		// add_action( 'wp_enqueue_scripts', array( __CLASS__, 'register_scripts' ), 1 );
+	
 	}
+
 	public function configuration_init() {
 		$myaccount	= get_page_by_path( 'my-account', OBJECT );
 		if ( !isset( $myaccount ) ) {
@@ -34,17 +33,9 @@ class 	MyAccount {
 			}
 		}
 	}
-	public static function register_scripts() {
-		/* $min = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
-		wp_enqueue_script( 'user-account-configuration-script', USER_ACCOUNT_CONFIGURATION_URL . "assets/js/user-account-configuration{$min}.js", array( 'jquery' ), USER_ACCOUNT_CONFIGURATION_VERSION, true ); */
-	}
 
 	function render_cancel_account_button() {
-
-		// Things that you want to do.
 		$html = '<div class="accountCancellation"></div>';
-
-		// Output needs to be return
 		return $html;
 	}
 
