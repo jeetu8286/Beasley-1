2e29ef02c377acbf7fb7355257a8339640deb2a3
riddhi.panchal <riddhi.panchal@bbgi.com>
2023-07-11 16:51:45 +0530

    improve code to disable newsletter signup button

--- plugins/cpt-general-settings/assets/js/newsletter-signup-form.js
+++ plugins/cpt-general-settings/assets/js/newsletter-signup-form.js
@@ -2,19 +2,53 @@
 	var $nsf_document = $(document);
 	$nsf_document.ready(function () {
 
-        if ($("#nsf-checkbox-content").is(":checked")) {
-            $("#nsf-form .nsf-form-submit").prop("disabled", false).css('opacity', 1).css('cursor', 'pointer');          
-        } else {
-            $("#nsf-form .nsf-form-submit").prop("disabled", true).css('opacity', 0.5).css('cursor', 'not-allowed');
-            $("#nsf-form .nsf-form-submit").off("click");
-        }
- 
-        $nsf_document.on('change', '#nsf-checkbox-content', function(event) {
-            if ($(this).is(":checked")) {
-                $("#nsf-form .nsf-form-submit").prop("disabled", false).css('opacity', 1).css('cursor', 'pointer');
-            } else {
+        function nsf_disable_button(){
+            var email = $(".nsf-email").val();
+            var pattern = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+
+            if ($('.nsf-first-name').val() == "" || email == "" || !$("#nsf-checkbox-content").is(":checked") ) {
                 $("#nsf-form .nsf-form-submit").prop("disabled", true).css('opacity', 0.5).css('cursor', 'not-allowed');
+                $("#nsf-form .nsf-form-submit").off("click");
+            }else{
+                if (!pattern.test(email)) {
+                    $("#nsf-form .nsf-form-submit").prop("disabled", true).css('opacity', 0.5).css('cursor', 'not-allowed');
+                    $("#nsf-form .nsf-form-submit").off("click");
+                } else {
+                    $("#nsf-form .nsf-form-submit").prop("disabled", false).css('opacity', 1).css('cursor', 'pointer');
+                }
             }
+        }
+
+        nsf_disable_button();
+
+        $nsf_document.on('change', '.nsf-first-name', function(event) {
+            nsf_disable_button();
+            if ($(this).val() == "") {
+                $('.nsf-fname-error-msg').text('First name required.');
+                $(this).focus();
+            } else {
+                $('.nsf-fname-error-msg').html('');
+            }
+        });
+
+        $nsf_document.on('change', '.nsf-email', function(event) {
+            var pattern = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+            nsf_disable_button();
+            if ($(this).val() == "") {
+                $('.nsf-email-error-msg').text('Email required.');
+                $(this).focus();
+            } else {
+                if (!pattern.test($(this).val())) {
+                    $('.nsf-email-error-msg').text('invalid email');
+                    $(this).focus();
+                } else {
+                    $('.nsf-email-error-msg').html('');
+                }
+            }
+        });
+
+        $nsf_document.on('change', '#nsf-checkbox-content', function(event) {
+            nsf_disable_button();
         });
 
         $nsf_document.on('click', '.nsf-container #nsf-form .nsf-form-submit', function(event) {
