227ae80175a8d65f1c662c0f4b21e591dc0befc5
mikepersico <mike.persico@bbgi.com>
2023-07-12 14:15:29 -0400

    Increment JS Version.

--- mu-plugins/classes/Bbgi/Integration/Google.php
+++ mu-plugins/classes/Bbgi/Integration/Google.php
@@ -53,21 +53,21 @@ class Google extends \Bbgi\Module {
 			'mparticle_enqueue_scripts',
 			plugins_url( 'assets/js/mparticle-schema.js', __FILE__ ),
 			array(),
-			'2.0.48'
+			'2.0.49'
 		);
 
 		wp_enqueue_script(
 			'window_utils_enqueue_scripts',
 			plugins_url( 'assets/js/window-utils.js', __FILE__ ),
 			array(),
-			'2.0.48'
+			'2.0.49'
 		);
 
 		wp_enqueue_script(
 			'ga_enqueue_scripts',
 			plugins_url( 'assets/js/beasley-analytics.js', __FILE__ ),
 			array( 'window_utils_enqueue_scripts', 'mparticle_enqueue_scripts' ),
-			'2.0.48'
+			'2.0.49'
 		);
 	}
 
