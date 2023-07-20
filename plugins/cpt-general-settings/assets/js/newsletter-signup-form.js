de038ecab3d41cd75619474ed1593b6b52ad9ff6
mikepersico <mike.persico@bbgi.com>
2023-07-20 17:08:59 -0400

    Fix bug with creating user with masked email.

--- plugins/cpt-general-settings/assets/js/newsletter-signup-form.js
+++ plugins/cpt-general-settings/assets/js/newsletter-signup-form.js
@@ -125,7 +125,8 @@
 							{'attributeName': '$firstname' , 'attributeValue': name},
 							{'attributeName': '$lastname' , 'attributeValue': nsf_last_name},
 						];
-						const emailIsMaskedMParticleVal =  $(".nsf-email").disabled;
+						elementsWithEmailClass = document.getElementsByClassName('nsf-email');
+						const emailIsMaskedMParticleVal =  elementsWithEmailClass && elementsWithEmailClass.length > 0 ? !!elementsWithEmailClass[0].disabled : false;
 						window.beasleyanalytics?.setMParticleUserAttributeArray(email, emailIsMaskedMParticleVal, userAttributeArray);
 					}else{
                         $('.response-error-container').html('Error Code '+res.code+' : '+res.message).css('color','red');
