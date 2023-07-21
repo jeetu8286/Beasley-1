e3e48bc7957543e4afbd2e1eebba3f60a8fdf9da
riddhi.panchal <riddhi.panchal@bbgi.com>
2023-07-21 20:08:31 +0530

    Fix load more issue

--- themes/experience-engine-v2/archive-tribe_events.php
+++ themes/experience-engine-v2/archive-tribe_events.php
@@ -8,22 +8,29 @@ if ( ee_is_first_page() ):
 endif;
 
 if ( have_posts() ) :
+
 	echo '<div class="archive-tiles content-wrap ', ! is_post_type_archive( 'contest' ) && ! is_post_type_archive( 'tribe_events' ) ? '-grid -large' : '-list', '">';
 		while ( have_posts() ) :
 			the_post();
 			get_template_part( 'partials/tile', get_post_type() );
 		endwhile;
 	echo '</div>';
-
 	echo '<div class="content-wrap">';
-		ee_load_more();
-        // $page_number = (get_query_var('paged')) ? max(get_query_var('paged'), 1) : 1;
-        // $dynamic_url = get_site_url() . '/events/page/' . $page_number + 1 . '/';
-        //echo $dynamic_url;
+		// ee_load_more();
+
+		$count_posts = wp_count_posts( 'tribe_events' );
+		$posts_per_page = tribe_get_option( 'postsPerPage');
+		
+		$total_posts = $count_posts->publish;
+		$max_pages = ceil( $total_posts / $posts_per_page );
         
-        ?>
-            <!-- <a href="<?php //echo $dynamic_url;?>" class="load-more">Load More</a> -->
-        <?php
+		$page_number = (get_query_var('paged')) ? max(get_query_var('paged'), 1) : 1;
+		$max_page_number = $page_number+1;
+		$dynamic_url = get_site_url() . '/events/page/' . $max_page_number . '/';
+        
+		if( $max_pages >= $max_page_number ){
+        	?><a href="<?php echo $dynamic_url;?>" class="load-more">Load More</a><?php
+		}
 
 	echo '</div>';
 else :
--- themes/experience-engine-v2/includes/events.php
+++ themes/experience-engine-v2/includes/events.php
@@ -1,6 +1,7 @@
 <?php 
 
 add_filter( 'tribe_events_event_schedule_details_inner', 'ee_update_event_schedule_details' );
+add_action('pre_get_posts', 'tribe_events_archive_posts_per_page');
 
 if ( ! function_exists( 'ee_update_event_schedule_details' ) ) :
 	function ee_update_event_schedule_details( $details ) {
@@ -9,5 +10,13 @@ if ( ! function_exists( 'ee_update_event_schedule_details' ) ) :
 		$details = preg_replace( '/\s*(,|\@)\s*/', $replace, $details );
 
 		return $details;
+	}
+endif;
+
+if ( ! function_exists( 'tribe_events_archive_posts_per_page' ) ) :
+	function tribe_events_archive_posts_per_page($query) {
+		if (is_post_type_archive('tribe_events')) { // Replace 'triue-event' with your custom post type slug.
+			$query->set('posts_per_page', tribe_get_option( 'postsPerPage')); // Set the number of posts you want to display per page.
+		}
 	}
 endif;
