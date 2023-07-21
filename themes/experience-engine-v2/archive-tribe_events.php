699217dcf0cc4388386926c7fcc6df8aa5605eca
riddhi.panchal <riddhi.panchal@bbgi.com>
2023-07-21 16:09:46 +0530

    Fix design and css related issue

---
+++ themes/experience-engine-v2/archive-tribe_events.php
@@ -0,0 +1,35 @@
+<?php
+
+get_header();
+
+if ( ee_is_first_page() ):
+	get_template_part( 'partials/archive/title' );
+	get_template_part( 'partials/archive/meta' );
+endif;
+
+if ( have_posts() ) :
+	echo '<div class="archive-tiles content-wrap ', ! is_post_type_archive( 'contest' ) && ! is_post_type_archive( 'tribe_events' ) ? '-grid -large' : '-list', '">';
+		while ( have_posts() ) :
+			the_post();
+			get_template_part( 'partials/tile', get_post_type() );
+		endwhile;
+	echo '</div>';
+
+	echo '<div class="content-wrap">';
+		// ee_load_more();
+        $page_number = (get_query_var('paged')) ? max(get_query_var('paged'), 1) : 1;
+        $dynamic_url = get_site_url() . '/events/page/' . $page_number + 1 . '/';
+        //echo $dynamic_url;
+        
+        ?>
+            <a href="<?php echo $dynamic_url;?>" class="load-more">Load More</a>
+        <?php
+
+	echo '</div>';
+else :
+	echo '<div class="content-wrap">';
+		ee_the_have_no_posts();
+	echo '</div>';
+endif;
+
+get_footer();
